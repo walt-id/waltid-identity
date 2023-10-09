b208b775cb611be326807f7895822f0ac155fd87
mikeplotean <101570226+mikeplotean@users.noreply.github.com>
2023-10-09 17:31:38 +0300

    fix: gradle multiproject setup (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,10 @@ allprojects {
         mavenCentral()
     }
 }
+
+plugins {
+    val kotlinVersion = "1.9.10"
+    kotlin("multiplatform") version kotlinVersion apply false
+    kotlin("plugin.serialization") version kotlinVersion apply false
+    id("com.github.ben-manes.versions") version "0.48.0" apply false
+}
