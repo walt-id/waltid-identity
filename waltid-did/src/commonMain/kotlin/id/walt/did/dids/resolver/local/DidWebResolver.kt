7cdd03f816f9d45a571719e974e0df9cd987ea05
mikeplotean <101570226+mikeplotean@users.noreply.github.com>
2023-10-10 14:53:43 +0300

    fix: test server tls, local-did-web-resolver inject http client (#4)

--- waltid-did/src/commonMain/kotlin/id/walt/did/dids/resolver/local/DidWebResolver.kt
+++ waltid-did/src/commonMain/kotlin/id/walt/did/dids/resolver/local/DidWebResolver.kt
@@ -2,33 +2,26 @@ package id.walt.did.dids.resolver.local
 
 import id.walt.core.crypto.keys.Key
 import id.walt.core.crypto.keys.LocalKey
+import id.walt.did.dids.DidUtils
 import id.walt.did.dids.document.DidDocument
 import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 
-class DidWebResolver : LocalResolverMethod("web") {
+class DidWebResolver(private val client: HttpClient) : LocalResolverMethod("web") {
 
     companion object {
-        var URL_PROTOCOL = "https"//TODO: fix (exposed for test purpose)
+        const val URL_PROTOCOL = "https"
         val json = Json { ignoreUnknownKeys = true }
     }
 
-    private val http = HttpClient {
-        install(ContentNegotiation) {
-            json(json)
-        }
-    }
-
-    fun resolveDidToUrl(did: String): String {
-        val didParts = did.removePrefix("did:web:").split(":")
+    private fun resolveDidToUrl(did: String): String = DidUtils.identifierFromDid(did)?.let {
+        val didParts = it.split(":")
 
         val domain = didParts[0].replace("%3A", ":")
         val selectedPath = didParts.drop(1)
@@ -38,15 +31,15 @@ class DidWebResolver : LocalResolverMethod("web") {
             else -> "/${selectedPath.joinToString("/")}/did.json"
         }
 
-        return "$URL_PROTOCOL://$domain$path"
-    }
+        "$URL_PROTOCOL://$domain$path"
+    } ?: throw IllegalArgumentException("Unexpected did format (missing identifier): $did")
 
     override suspend fun resolve(did: String): Result<DidDocument> {
         val url = resolveDidToUrl(did)
 
         val response = runCatching {
             DidDocument(
-                jsonObject = http.get(url).body<JsonObject>()
+                jsonObject = client.get(url).body<JsonObject>()
             )
         }
 
