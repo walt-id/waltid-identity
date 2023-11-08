65d97d31923fee1770fb3026616b1582800c18f7
waltkb <68587968+waltkb@users.noreply.github.com>
2023-11-08 03:47:31 +0100

    Add issuanceKey & issuerDid to BaseIssuanceRequest and update JwtIssuanceRequest, SdJwtIssuanceRequest

--- waltid-issuer/src/main/kotlin/id/walt/issuer/IssuanceRequests.kt
+++ waltid-issuer/src/main/kotlin/id/walt/issuer/IssuanceRequests.kt
@@ -4,38 +4,28 @@ import id.walt.credentials.vc.vcs.W3CVC
 import id.walt.sdjwt.SDMap
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonObject
 
 sealed class BaseIssuanceRequest {
+    abstract val issuanceKey: JsonObject
+    abstract val issuerDid: String
+
     abstract val vc: W3CVC
     abstract val mapping: JsonObject?
 }
 
-
 @Serializable
-class JwtIssuanceRequest(
+data class JwtIssuanceRequest(
+    override val issuanceKey: JsonObject,
+    override val issuerDid: String,
+
     override val vc: W3CVC,
     override val mapping: JsonObject? = null
-) : BaseIssuanceRequest() {
-    companion object {
-        /**
-         * Return JwtIssuanceRequest of W3CVC in `vc` and mapping in `mapping` if it has `vc`. Otherwise,
-         * return complete JSON as W3CVC and no mapping.
-         */
-        fun fromJsonObject(jsonObj: JsonObject): JwtIssuanceRequest {
-            val maybeHasVc = jsonObj["vc"]?.jsonObject
-            return when {
-                maybeHasVc != null -> JwtIssuanceRequest(W3CVC(maybeHasVc), jsonObj["mapping"]?.jsonObject)
-                else -> JwtIssuanceRequest(W3CVC(jsonObj), null)
-            }
-        }
-    }
-}
+) : BaseIssuanceRequest()
 
 @Serializable
 data class SdJwtIssuanceRequest(
-    val issuanceKey: JsonObject,
-    val issuerDid: String,
+    override val issuanceKey: JsonObject,
+    override val issuerDid: String,
 
     override val vc: W3CVC,
     override val mapping: JsonObject? = null,
