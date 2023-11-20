d5850a64b108b805e985508c16bdf2f4cc98bba8
waltkb <68587968+waltkb@users.noreply.github.com>
2023-11-20 03:45:31 +0100

    Add todo in openid4vc

--- waltid-openid4vc/build.gradle.kts
+++ waltid-openid4vc/build.gradle.kts
@@ -73,6 +73,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
                 implementation("io.ktor:ktor-http:$ktor_version")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
+                // TODO: set to project version: implementation(project(":waltid-sdjwt"))
                 implementation("id.walt:waltid-sd-jwt:1.2310101347.0")
             }
         }
@@ -97,6 +98,7 @@ kotlin {
                 implementation("io.kotest:kotest-assertions-json:5.7.2")
 
                 implementation("id.walt.servicematrix:WaltID-ServiceMatrix:1.1.3")
+                // TODO: current version implementation("id.walt:waltid-ssikit:1.2311131043.0")
                 implementation("id.walt:waltid-ssikit:1.JWTTYP")
                 implementation(project(":waltid-crypto"))
 
