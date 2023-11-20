14df68e7c32fc9bd62b1c495c011c101bef14d19
mikeplotean <mike@walt.id>
2023-11-20 19:24:27 +0200

    test: crypto - jvm-local-key

--- waltid-crypto/src/jvmTest/kotlin/TestUtils.kt
+++ waltid-crypto/src/jvmTest/kotlin/TestUtils.kt
@@ -5,4 +5,6 @@ object TestUtils {
     fun loadPem(filename: String): String = loadResource("pem/$filename")
     fun loadResource(relativePath: String): String =
         this::class.java.classLoader.getResource(relativePath)!!.path.let { File(it).readText() }
+    fun loadResourceBytes(relativePath: String): ByteArray =
+        this::class.java.classLoader.getResource(relativePath)!!.path.let { File(it).readBytes() }
 }
