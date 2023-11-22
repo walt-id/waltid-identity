1fb3ef7ec32f3ac5c8c64a6faa4ab461f0a9fb5b
mikeplotean <101570226+mikeplotean@users.noreply.github.com>
2023-11-22 17:16:14 +0200

    test: crypto (#21)
    * test: crypto - import key
    * test: crypto - key serialization, minor refactor
    * test: crypto - export key
    * test: crypto - key sign
    * test: crypto - jvm-local-key-creator
    * test: crypto - jvm-local-key
    * test: crypto - signatures
    * test: added tse deserialization test, restructured test resources folder
    * test: refactor tse-key

--- waltid-crypto/src/jvmTest/kotlin/TestJvm.kt
+++ waltid-crypto/src/jvmTest/kotlin/TestJvm.kt
@@ -1,24 +1,102 @@
-import id.walt.crypto.keys.KeyType
-import id.walt.crypto.keys.LocalKey
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import id.walt.crypto.keys.*
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.util.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
+import org.junit.jupiter.api.condition.EnabledIf
+import kotlin.reflect.KClass
 import kotlin.system.measureTimeMillis
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import kotlin.test.*
 import kotlin.time.Duration.Companion.minutes
 
 class TestJvm {
 
-    suspend fun test(keyType: KeyType) {
+    @Test
+    fun apiTestAll() = runTest {
+        KeyType.entries.forEach { exampleKeyCompleteFlow(it) }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    @Test
+    fun signatureSpeedTestAll() = runTest(timeout = 5.minutes) {
+        KeyType.entries.forEach { keyType ->
+            val key = LocalKey.generate(keyType)
+            key.signJws("abc".encodeToByteArray())
+
+            val jobs = ArrayList<Job>()
+
+            val n = 10_000
+            val dispatchMs = measureTimeMillis {
+                repeat(n) {
+                    jobs.add(GlobalScope.launch {
+                        key.signJws(byteArrayOf(it.toByte()))
+                    })
+                }
+            }
+
+            val signMs = measureTimeMillis {
+                jobs.forEach { it.join() }
+            }
+
+            println("$keyType: Dispatch $dispatchMs ms, Signing: $signMs ms (for $n signatures)")
+        }
+    }
+
+    @Test
+    @EnabledIf("hostCondition")
+    fun testKeySerialization() = runTest {
+        val localKey = LocalKey.generate(KeyType.Ed25519)
+        val localKeySerialized = KeySerialization.serializeKey(localKey)
+
+        val jsons = listOf(
+            localKeySerialized to LocalKey::class,
+            """{"type":"tse","server":"http://127.0.0.1:8200/v1/transit","accessKey":"dev-only-token","id":"k-307668075","_publicKey":[-41,-105,-126,77,-74,88,-28,123,93,-81,105,-13,-93,-111,27,81,-90,-1,86,59,68,105,-108,118,-68,121,18,-114,71,-69,-106,-109],"_keyType":"Ed25519"}""" to TSEKey::class,
+            """{"type":"tse","server":"http://127.0.0.1:8200/v1/transit","accessKey":"dev-only-token","id":"k-307668075"}""" to TSEKey::class
+        )
+
+        jsons.forEach {
+            check(it)
+        }
+    }
+
+    private val testObj = JsonObject(mapOf("value1" to JsonPrimitive("123456789")))
+
+    @Test
+    fun testDeserializedVerify() = runTest {
+        val testObjJson = Json.encodeToString(testObj)
+
+        val key = LocalKey.generate(KeyType.Ed25519)
+        val key2 = KeySerialization.deserializeKey(KeySerialization.serializeKey(key)).getOrThrow()
+
+        val jws = key.signJws(testObjJson.toByteArray())
+
+        val res = key2.verifyJws(jws)
+        println(res)
+        assertEquals(testObj, res.getOrThrow())
+    }
+
+
+    @Test
+    fun testDeserializedSign() = runTest {
+        val testObjJson = Json.encodeToString(testObj)
+
+        val keyToUseForVerifying = LocalKey.generate(KeyType.Ed25519)
+        val keyToUseForSigning = KeySerialization.deserializeKey(KeySerialization.serializeKey(keyToUseForVerifying)).getOrThrow()
+
+        val jws = keyToUseForSigning.signJws(testObjJson.toByteArray())
+
+        val res = keyToUseForVerifying.verifyJws(jws)
+        println(res)
+        assertEquals(testObj, res.getOrThrow())
+    }
+
+    private suspend fun exampleKeyCompleteFlow(keyType: KeyType) {
         val plaintext = JsonObject(
             mapOf("id" to JsonPrimitive("abc123-${keyType.name}-JVM"))
         )
@@ -57,35 +135,62 @@ class TestJvm {
         println("  Public key: ${check2.getOrNull()}")
     }
 
-    @Test
-    fun apiTestAll() = runTest {
-        KeyType.entries.forEach { test(it) }
+    private suspend fun check(value: Pair<String, KClass<out Key>>) {
+        println("Parsing: ${value.first}")
+        val key = KeySerialization.deserializeKey(value.first).getOrThrow()
+
+        println("Got key: $key")
+        println("Of type: " + key::class.simpleName)
+
+        println("Key ID: ${key.getKeyId()}")
+        println("Key type: ${key.keyType}")
+        println("Public key: ${key.getPublicKey().exportJWK()}")
+
+        assertEquals(value.second.simpleName, key::class.simpleName)
+        assertNotEquals("Key", key::class.simpleName)
+
+        println()
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    @Test
-    fun signatureSpeedTestAll() = runTest(timeout = 5.minutes) {
-        KeyType.entries.forEach { keyType ->
-            val key = LocalKey.generate(keyType)
-            key.signJws("abc".encodeToByteArray())
+    fun exampleSignJwsLocalKey() = runTest {
+        val localKey by lazy { runBlocking { LocalKey.generate(KeyType.Ed25519) } }
 
-            val jobs = ArrayList<Job>()
+        val payload = JsonObject(
+            mapOf(
+                "sub" to JsonPrimitive("16bb17e0-e733-4622-9384-122bc2fc6290"),
+                "iss" to JsonPrimitive("http://localhost:3000"),
+                "aud" to JsonPrimitive("TOKEN"),
+            )
+        )
 
-            val n = 10_000
-            val dispatchMs = measureTimeMillis {
-                repeat(n) {
-                    jobs.add(GlobalScope.launch {
-                        key.signJws(byteArrayOf(it.toByte()))
-                    })
-                }
-            }
+        println("Signing JWS: $payload")
+        val signed = localKey.signJws(payload.toString().toByteArray())
+        println("Signed: $signed")
 
-            val signMs = measureTimeMillis {
-                jobs.forEach { it.join() }
-            }
+        println("Verifying signed: $signed")
+        localKey.verifyJws(signed).also { println("Verified: $it") }
+    }
 
-            println("$keyType: Dispatch $dispatchMs ms, Signing: $signMs ms (for $n signatures)")
+    private suspend fun exampleSignRawTSEKey(key: TSEKey) {
+        try {
+            println("TSEKey: $key")
+            val plaintext = "This is a plaintext for ${key.keyType.name}... 123".encodeToByteArray()
+            println("Plaintext: ${plaintext.decodeToString()}")
+
+            val signed = key.signRaw(plaintext) as String
+            println("Signed: $signed")
+
+            val verified = key.verifyRaw(signed.decodeBase64Bytes(), plaintext)
+            println("Verified signature success: ${verified.isSuccess}")
+            println("Verified plaintext: ${verified.getOrNull()!!.decodeToString()}")
+        } finally {
+            println("Deleting $key...")
+            key.delete()
         }
     }
+
+    private fun hostCondition() = runCatching {
+        runBlocking { HttpClient().get("http://127.0.0.1:8200") }.status == HttpStatusCode.OK
+    }.fold(onSuccess = { true }, onFailure = { false })
 
 }
