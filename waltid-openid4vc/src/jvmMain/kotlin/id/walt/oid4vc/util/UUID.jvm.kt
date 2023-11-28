05822940f038540c52a3baee66402dc5b86b35aa
Severin Stampler <severin@walt.id>
2023-11-28 17:28:46 +0100

    resolve request parameters from request object or request uri reference

--- waltid-openid4vc/src/jvmMain/kotlin/id/walt/oid4vc/util/UUID.jvm.kt
+++ waltid-openid4vc/src/jvmMain/kotlin/id/walt/oid4vc/util/UUID.jvm.kt
@@ -1,5 +1,10 @@
 package id.walt.oid4vc.util
 
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.java.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import java.security.MessageDigest
 import java.util.*
 
@@ -8,3 +13,9 @@ actual fun randomUUID(): String {
 }
 
 actual fun sha256(data: ByteArray): ByteArray = MessageDigest.getInstance("SHA-256").digest(data)
+
+private val ktorClient = HttpClient(Java) {
+}
+actual suspend fun httpGet(url: String): String {
+    return ktorClient.get(url).bodyAsText()
+}
