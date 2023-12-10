1bf2f31c13d5f17d9f7cf06d234ced5e61633ce5
waltkb <68587968+waltkb@users.noreply.github.com>
2023-12-10 01:59:51 +0100

    Added optional wallet selection for initiatePresentation and initiateIssuance when more than 1 wallet exists

--- waltid-web-wallet/web/src/pages/api/siop/initiatePresentation.vue
+++ waltid-web-wallet/web/src/pages/api/siop/initiatePresentation.vue
@@ -1,25 +1,30 @@
 <template>
     <CenterMain>
-        <LoadingIndicator>Loading presentation parameters...</LoadingIndicator>
+        <WalletListing v-if="wallets && wallets.length > 1" :wallets="wallets" :use-url="walletUrlFunction"/>
+        <LoadingIndicator v-else>Loading wallets...</LoadingIndicator>
     </CenterMain>
 </template>
 
 <script lang="ts" setup>
 import CenterMain from "~/components/CenterMain.vue";
 import LoadingIndicator from "~/components/loading/LoadingIndicator.vue";
-import { encodeRequest, fixRequest } from "~/composables/siop-requests";
+import WalletListing from "~/components/wallets/WalletListing.vue";
+import {encodeRequest, fixRequest} from "~/composables/siop-requests";
+const queryRequest = new URL("http://example.invalid" + useRoute().fullPath).search // new URL(window.location.href).search
+console.log("queryRequest: ", queryRequest)
 
-const currentWallet = useCurrentWallet()
 
-if (process.client) {
-    let request = encodeURI(decodeURI(fixRequest("openid://" + window.location.search)));
-    console.log("Fixed request: ", request);
+let fixedRequest = encodeURI(decodeURI(fixRequest("openid://" + window.location.search)));
+console.log("Fixed request: ", fixedRequest);
 
-    const encoded = encodeRequest(request);
-    console.log("Encoded request: ", encoded);
+const encodedWalletRequestUrl = encodeRequest(fixedRequest);
+console.log("Encoded request: ", encodedWalletRequestUrl);
 
-    navigateTo({ path: `/wallet/${currentWallet.value}/exchange/presentation`, query: { request: encoded } });
+const wallets = (await listWallets())?.value?.wallets;
+
+const walletUrlFunction = (wallet: WalletListing) => `/wallet/${wallet.id}/exchange/presentation?request=${encodedWalletRequestUrl}`
+
+if (wallets && wallets.length == 1) {
+    navigateTo(walletUrlFunction(wallets[0]))
 }
 </script>
-
-<style scoped></style>
