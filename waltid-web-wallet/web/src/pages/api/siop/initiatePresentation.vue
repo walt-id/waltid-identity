8c71485d74a6f9fd11848dccc93fcee0cf4eb6fe
waltkb <68587968+waltkb@users.noreply.github.com>
2023-12-11 16:01:40 +0100

    Set auto-selected wallet

--- waltid-web-wallet/web/src/pages/api/siop/initiatePresentation.vue
+++ waltid-web-wallet/web/src/pages/api/siop/initiatePresentation.vue
@@ -25,6 +25,8 @@ const wallets = (await listWallets())?.value?.wallets;
 const walletUrlFunction = (wallet: WalletListing) => `/wallet/${wallet.id}/exchange/presentation?request=${encodedWalletRequestUrl}`
 
 if (wallets && wallets.length == 1) {
+    const wallet = wallets[0]
+    setWallet(wallet.id, undefined)
     navigateTo(walletUrlFunction(wallets[0]))
 }
 </script>
