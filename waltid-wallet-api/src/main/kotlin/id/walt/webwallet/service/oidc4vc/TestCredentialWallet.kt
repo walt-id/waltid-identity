cd0510fc162c520a189ffd3bf8b508ea7320798e
Severin Stampler <severin@walt.id>
2024-01-10 17:29:14 +0100

    update test credential wallet in waltid-wallet-api

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/oidc4vc/TestCredentialWallet.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/oidc4vc/TestCredentialWallet.kt
@@ -12,9 +12,7 @@ import id.walt.oid4vc.data.dif.PresentationSubmission
 import id.walt.oid4vc.data.dif.VCFormat
 import id.walt.oid4vc.interfaces.PresentationResult
 import id.walt.oid4vc.interfaces.SimpleHttpResponse
-import id.walt.oid4vc.providers.CredentialWalletConfig
-import id.walt.oid4vc.providers.OpenIDCredentialWallet
-import id.walt.oid4vc.providers.TokenTarget
+import id.walt.oid4vc.providers.*
 import id.walt.oid4vc.requests.AuthorizationRequest
 import id.walt.oid4vc.requests.TokenRequest
 import id.walt.webwallet.service.SSIKit2WalletService
@@ -148,19 +146,20 @@ class TestCredentialWallet(
 
         println("Credentials presented: $credentialsPresented")
 
-
-        val vp = Json.encodeToString( // TODO: Use PresentationBuilder
+        val presentationId = (session.presentationDefinition?.id ?: "urn:uuid:${UUID.generateUUID().toString().lowercase()}")
+        val vp = Json.encodeToString(
             mapOf(
                 "sub" to this.did,
                 "nbf" to Clock.System.now().minus(1.minutes).epochSeconds,
                 "iat" to Clock.System.now().epochSeconds,
-                "jti" to "urn:uuid:" + UUID.generateUUID().toString(),
+                "jti" to presentationId,
                 "iss" to this.did,
                 "nonce" to (session.nonce ?: ""),
+                "aud" to session.authorizationRequest.clientId,
                 "vp" to mapOf(
                     "@context" to listOf("https://www.w3.org/2018/credentials/v1"),
                     "type" to listOf("VerifiablePresentation"),
-                    "id" to "urn:uuid:${UUID.generateUUID().toString().lowercase()}",
+                    "id" to presentationId,
                     "holder" to this.did,
                     "verifiableCredential" to credentialsPresented
                 )
@@ -183,8 +182,8 @@ class TestCredentialWallet(
 
         return PresentationResult(
             listOf(JsonPrimitive(signed)), PresentationSubmission(
-                id = "submission 1",
-                definitionId = session.presentationDefinition?.id ?: "",
+                id = presentationId,
+                definitionId = presentationId,
                 descriptorMap = matchedCredentials.map { it.document }.mapIndexed { index, vcJwsStr ->
                     val vcJws = vcJwsStr.base64UrlToBase64().decodeJws()
                     val type =
@@ -192,12 +191,13 @@ class TestCredentialWallet(
                             ?: "VerifiableCredential"
 
                     DescriptorMapping(
-                        id = type,
-                        format = VCFormat.jwt_vp_json,  // jwt_vp_json
-                        path = "$[$index]",
+                        id = session.presentationDefinition?.inputDescriptors?.get(index)?.id,
+                        format = VCFormat.jwt_vp,  // jwt_vp_json
+                        path = "$",
                         pathNested = DescriptorMapping(
+                            id = session.presentationDefinition?.inputDescriptors?.get(index)?.id,
                             format = VCFormat.jwt_vc_json,
-                            path = "$.vp.verifiableCredential[0]",
+                            path = "$.verifiableCredential[$index]",
                         )
                     )
                 }
@@ -266,6 +266,10 @@ class TestCredentialWallet(
         authorizationRequest: AuthorizationRequest?,
         expirationTimestamp: Instant
     ) = VPresentationSession(id, authorizationRequest, expirationTimestamp, setOf())
+
+    override fun getDidFor(session: VPresentationSession): String {
+        return did
+    }
 
     override fun getSession(id: String) = sessionCache[id]
     override fun putSession(id: String, session: VPresentationSession) = sessionCache.put(id, session)
