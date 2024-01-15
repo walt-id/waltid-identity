ec8270aaed8e006afb41045a2ed486748c8113f4
waltkb <68587968+waltkb@users.noreply.github.com>
2024-01-15 16:47:42 +0100

    Fixed DID import bug

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/dids/DidService.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/dids/DidService.kt
@@ -11,7 +11,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object DidsService {
     fun get(wallet: UUID, did: String): WalletDid? = transaction {
-        WalletDids.select { (WalletDids.wallet eq wallet) and (WalletDids.did eq did) }
+        WalletDids.select { (WalletDids.wallet eq wallet) and (WalletDids.did eq did.replace("%3A", ":")) }
             .singleOrNull()?.let { WalletDid(it) }
     }
 
@@ -22,7 +22,7 @@ object DidsService {
 
         WalletDids.insert {
             it[WalletDids.wallet] = wallet
-            it[WalletDids.did] = did
+            it[WalletDids.did] = did.replace("%3A", ":")
             it[WalletDids.document] = document
             it[WalletDids.keyId] = keyId
             it[WalletDids.alias] = alias ?: "Unnamed from $now"
@@ -31,7 +31,7 @@ object DidsService {
     }.insertedCount
 
     fun delete(wallet: UUID, did: String): Boolean =
-        transaction { WalletDids.deleteWhere { (WalletDids.wallet eq wallet) and (WalletDids.did eq did) } } > 0
+        transaction { WalletDids.deleteWhere { (WalletDids.wallet eq wallet) and (WalletDids.did eq did.replace("%3A", ":")) } } > 0
 
 
     fun makeDidDefault(wallet: UUID, newDefaultDid: String) {
@@ -40,14 +40,14 @@ object DidsService {
                 it[default] = false
             }
 
-            WalletDids.update({ (WalletDids.wallet eq wallet) and (WalletDids.did eq newDefaultDid) }) {
+            WalletDids.update({ (WalletDids.wallet eq wallet) and (WalletDids.did eq newDefaultDid.replace("%3A", ":")) }) {
                 it[default] = true
             }
         }
     }
 
     fun renameDid(wallet: UUID, did: String, newName: String) =
-        WalletDids.update({ (WalletDids.wallet eq wallet) and (WalletDids.did eq did) }) {
+        WalletDids.update({ (WalletDids.wallet eq wallet) and (WalletDids.did eq did.replace("%3A", ":")) }) {
             it[alias] = newName
         } > 0
 }
