ead2299195498ca12d527cfc01c0f5aa81f97584
mikeplotean <mike@walt.id>
2024-01-18 17:13:52 +0200

    chore: reset default-manifest-extrator

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/manifest/extractor/DefaultManifestExtractor.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/manifest/extractor/DefaultManifestExtractor.kt
@@ -1,23 +1,10 @@
 package id.walt.webwallet.manifest.extractor
 
-import id.walt.crypto.utils.JsonUtils.toJsonElement
-import id.walt.webwallet.manifest.provider.ManifestProvider
 import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
 
-class DefaultManifestExtractor() : ManifestExtractor {
-    override suspend fun extract(offerRequestUrl: String): JsonObject =
-        JsonObject(createManifestObject(offerRequestUrl).plus(mapOf("type" to "oidc".toJsonElement())))
+class DefaultManifestExtractor : ManifestExtractor {
+    override suspend fun extract(offerRequestUrl: String): JsonObject = JsonObject(emptyMap())
 
 
-    private fun createManifestObject(offerRequestUrl: String): JsonObject =
-        ManifestProvider.json.decodeFromString<JsonObject>(offerRequestUrl).let {
-            JsonObject(
-                mapOf(
-                    "issuer" to it.jsonObject["credential_issuer"]!!,
-                    "credentials" to it.jsonObject["credentials"]!!.jsonArray
-                )
-            )
-        }
+    private fun createManifestObject(offerRequestUrl: String): JsonObject = JsonObject(emptyMap())
 }
