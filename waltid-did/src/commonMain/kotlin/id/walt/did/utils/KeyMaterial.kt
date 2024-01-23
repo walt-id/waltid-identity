38706c0f1d5ec32ab3a5982491d8b7c6981cd952
mikeplotean <101570226+mikeplotean@users.noreply.github.com>
2024-01-23 18:50:57 +0200

    chore: uni-resolver publicKeyBase58, publicKeyMultibase, publicKeyHex (#74)
    * chore: uni-resolver publicKeyBase58
    * chore: uni-resolver publicKeyMultibase, publicKeyHex
    * chore: did-jwk key reference
    * test: uni-resolver enabled condition

--- waltid-did/src/commonMain/kotlin/id/walt/did/utils/KeyMaterial.kt
+++ waltid-did/src/commonMain/kotlin/id/walt/did/utils/KeyMaterial.kt
@@ -1,30 +1,50 @@
 package id.walt.did.utils
 
 import id.walt.crypto.keys.Key
+import id.walt.crypto.keys.KeyType
 import id.walt.crypto.keys.LocalKey
-import id.walt.did.dids.DidUtils
-import kotlinx.serialization.json.*
+import id.walt.crypto.keys.LocalKeyMetadata
+import id.walt.did.utils.KeyUtils.fromPublicKeyMultiBase
+import id.walt.did.utils.KeyUtils.getKeyTypeForVerificationMaterialType
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 object KeyMaterial {
     suspend fun get(element: JsonElement): Result<Key> = when (element) {
-        is JsonObject -> parseKey(element)
-        is JsonPrimitive -> parseBase58(element.jsonPrimitive.content)
+        is JsonObject -> importKey(element)
+//        is JsonPrimitive -> TODO: did
         else -> throw Exception("Failed to find public key element: $element")
     }
 
-    private suspend fun parseKey(element: JsonObject): Result<Key> {
+    private suspend fun importKey(element: JsonObject): Result<Key> {
+        val type = element.jsonObject["type"]!!.jsonPrimitive.content
         element.jsonObject["publicKeyJwk"]?.jsonObject?.run {
-            return parseJwk(this)
+            return importJwk(this)
         }
         element.jsonObject["publicKeyBase58"]?.jsonPrimitive?.content?.run {
-            return parseBase58(this)
+            return importBase58(this, getKeyTypeForVerificationMaterialType(type))
         }
-        throw IllegalArgumentException("Could not parse public key format: $element.")
+        element.jsonObject["publicKeyMultibase"]?.jsonPrimitive?.content?.run {
+            return importMultibase(this)
+        }
+        element.jsonObject["publicKeyHex"]?.jsonPrimitive?.content?.run {
+            return importHex(this, getKeyTypeForVerificationMaterialType(type))
+        }
+        //TODO: blockchainAccountId
+        throw IllegalArgumentException("Public key format not supported: $element.")
     }
 
-    private suspend fun parseJwk(element: JsonObject): Result<Key> = LocalKey.importJWK(element.toString())
+    private suspend fun importJwk(element: JsonObject): Result<Key> = LocalKey.importJWK(element.toString())
 
-    private suspend fun parseBase58(content: String): Result<Key> = DidUtils.identifierFromDid(content)?.let {
-        KeyUtils.fromPublicKeyMultiBase58(it)
-    } ?: Result.failure(Exception("Failed to compute publicKeyBase58: $content."))
+    private suspend fun importBase58(content: String, type: KeyType): Result<Key> = runCatching {
+        LocalKey.importRawPublicKey(type, EncodingUtils.base58Decode(content), LocalKeyMetadata())
+    }
+
+    private suspend fun importMultibase(content: String): Result<Key> = fromPublicKeyMultiBase(content)
+
+    private suspend fun importHex(content: String, type: KeyType): Result<Key> = runCatching {
+        LocalKey.importRawPublicKey(type, EncodingUtils.fromHexString(content), LocalKeyMetadata())
+    }
 }
