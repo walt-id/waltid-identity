363bbcb56c51d34a293f93316e4d14c668af6475
mikeplotean <mike@walt.id>
2024-01-29 13:10:18 +0200

    chore: credential category map model update

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/db/models/WalletCategory.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/db/models/WalletCategory.kt
@@ -1,16 +1,14 @@
 package id.walt.webwallet.db.models
 
 import kotlinx.serialization.Serializable
-import kotlinx.uuid.exposed.KotlinxUUIDTable
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.Table
 
-object WalletCategory : KotlinxUUIDTable("category") {
+object WalletCategory : Table("category") {
     val wallet = reference("wallet", Wallets)
     val name = varchar("name", 128)
 
-    init {
-        uniqueIndex(wallet, name)
-    }
+    override val primaryKey = PrimaryKey(wallet, name)
 }
 
 @Serializable
--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/db/models/WalletCredentialCategoryMap.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/db/models/WalletCredentialCategoryMap.kt
@@ -1,16 +1,16 @@
 package id.walt.webwallet.db.models
 
 import kotlinx.uuid.UUID
-import kotlinx.uuid.exposed.KotlinxUUIDTable
+import org.jetbrains.exposed.sql.Table
 
-object WalletCredentialCategoryMap : KotlinxUUIDTable("credential_category") {
-    val credential = reference("credential", WalletCredentials.id)//thankfully it's very unlikely credential-id gets updated
-    val category = reference("category", WalletCategory)
-    val wallet = reference("wallet", Wallets)
+object WalletCredentialCategoryMap : Table("credential_category") {
     //TODO: how are we sure wallet is consistent for both category and credential?
-    init {
-        uniqueIndex(wallet, credential, category)
-    }
+    val wallet = reference("wallet", Wallets)
+    //thankfully it's very unlikely credential-id gets updated
+    val credential = reference("credential", WalletCredentials.id)
+    val category = reference("category", WalletCategory.name)
+
+    override val primaryKey = PrimaryKey(wallet, credential, category)
 }
 
 data class WalletCredentialCategory(
