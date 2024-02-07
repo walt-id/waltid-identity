ee6d7cf645960cdf4ae5065ec0a8fca3461791b3
Dinkar <jaindinkar@ymail.com>
2024-02-07 14:06:54 +0530

    added presentation verification helper functions

--- waltid-verifiable-credentials/src/jsMain/kotlin/id/walt/credentials/utils/CredentialBuilderUtils.kt
+++ waltid-verifiable-credentials/src/jsMain/kotlin/id/walt/credentials/utils/CredentialBuilderUtils.kt
@@ -1,13 +1,11 @@
 package id.walt.credentials.utils
 
 import id.walt.crypto.utils.JsonUtils.toJsonObject
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.*
 
 @ExperimentalJsExport
 @JsExport
-object credentialBuilderUtils {
+object CredentialBuilderUtils {
     fun generateCredentialSubject(credentialSubject: String): JsonObject {
         val credentialSubjectJson = Json.parseToJsonElement(credentialSubject)
         return credentialSubjectJson.jsonObject.toJsonObject()
