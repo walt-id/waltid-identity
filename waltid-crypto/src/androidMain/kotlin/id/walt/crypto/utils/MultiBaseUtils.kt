860727dc295bade368f3c3316b8eb90e7a1d2be1
sunrisekcdeveloper <nadinebalanco@gmail.com>
2024-02-13 20:42:26 +0000

    add: implementation of MultiBaseUtils

--- waltid-crypto/src/androidMain/kotlin/id/walt/crypto/utils/MultiBaseUtils.kt
+++ waltid-crypto/src/androidMain/kotlin/id/walt/crypto/utils/MultiBaseUtils.kt
@@ -1,16 +1,25 @@
 package id.walt.crypto.utils
 
+import io.ipfs.multibase.Multibase
+
 actual object MultiBaseUtils {
 
     actual fun convertRawKeyToMultiBase58Btc(key: ByteArray, code: UInt): String {
-        TODO("Not yet implemented")
+        val codeVarInt = MultiCodecUtils.UVarInt(code)
+        val multicodecAndRawKey = ByteArray(key.size + codeVarInt.length)
+        codeVarInt.bytes.copyInto(multicodecAndRawKey)
+        key.copyInto(multicodecAndRawKey, codeVarInt.length)
+        return encodeMultiBase58Btc(multicodecAndRawKey)
     }
 
     actual fun convertMultiBase58BtcToRawKey(mb: String): ByteArray {
-        TODO("Not yet implemented")
+        val bytes = decodeMultiBase58Btc(mb)
+        val code = MultiCodecUtils.UVarInt.fromBytes(bytes)
+        return bytes.drop(code.length).toByteArray()
     }
 
-    actual fun decodeMultiBase58Btc(mb: String): ByteArray {
-        TODO("Not yet implemented")
-    }
+    actual fun decodeMultiBase58Btc(mb: String): ByteArray = Multibase.decode(mb)
+
+    private fun encodeMultiBase58Btc(byteArray: ByteArray): String =
+        Multibase.encode(Multibase.Base.Base58BTC, byteArray)
 }
