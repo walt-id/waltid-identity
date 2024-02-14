74b156de1deb1011df060610694edd0beeec117a
mikeplotean <mike@walt.id>
2024-02-14 20:10:25 +0200

    refactor: trust use case and service

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/trustusecase/TrustValidationUseCaseImpl.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/trustusecase/TrustValidationUseCaseImpl.kt
@@ -5,20 +5,19 @@ import id.walt.webwallet.seeker.Seeker
 import id.walt.webwallet.service.trust.TrustValidationService
 
 class TrustValidationUseCaseImpl(
-    private val issuerTrustValidationService: TrustValidationService,
-    private val verifierTrustValidationService: TrustValidationService,
+    private val trustValidationService: TrustValidationService,
     private val didSeeker: Seeker<String>,
     private val credentialTypeSeeker: Seeker<String>,
 ) : TrustValidationUseCase {
 
-    override suspend fun status(credential: WalletCredential, checkIssuer: Boolean): TrustStatus = let {
-        checkIssuer.takeIf { it }?.let {
-            issuerTrustValidationService
-        } ?: verifierTrustValidationService
-    }.let {
-        when (it.validate(didSeeker.get(credential), credentialTypeSeeker.get(credential))) {
+    override suspend fun status(credential: WalletCredential): TrustStatus = runCatching {
+        trustValidationService.validate(didSeeker.get(credential), credentialTypeSeeker.get(credential))
+    }.fold(onSuccess = {
+        when (it) {
             true -> TrustStatus.Trusted
             false -> TrustStatus.Untrusted
         }
-    }
+    }, onFailure = {
+        TrustStatus.Untrusted
+    })
 }
