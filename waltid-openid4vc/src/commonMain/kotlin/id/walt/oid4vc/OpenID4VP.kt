6ae763c8b9991ed21c9af3e2e72c81ccd86451c8
Severin Stampler <severin@walt.id>
2024-02-15 15:03:44 +0100

    start test for new oid4vp utility functions more documentation for function parameters

--- waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/OpenID4VP.kt
+++ waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/OpenID4VP.kt
@@ -5,14 +5,25 @@ import id.walt.oid4vc.requests.AuthorizationRequest
 
 object OpenID4VP {
 
+  /**
+   * Create a OpenID4VP presentation request object
+   * @param presentationDefinition Presentation definition either by value, by reference or scope
+   * @param responseMode Response mode, defaults to direct_post
+   * @param responseTypes Expected response types, defaults to "vp_token", use "code" for authorization code flow, or "vp_token id_token" for SIOPv2 flow with "openid" scope
+   * @param redirectOrResponseUri Sets either the "redirect_uri" or "response_uri" parameter on the request, depending on the response mode used.
+   * @param nonce Used to securely bind the Verifiable Presentation provided by the wallet to the particular transaction
+   * @param state State property that can be used to identify the presentation session when receiving the response from the wallet
+   * @param scopes Can be used to set additional scopes on the request, e.g. to request presentations based on a pre-defined scope (see also presentationDefinition param), default: empty
+   *
+   */
   fun createPresentationRequest(
     presentationDefinition: PresentationDefinitionParameter,
-    responseMode: ResponseMode,
-    responseTypes: Set<ResponseType>,
+    responseMode: ResponseMode = ResponseMode.DirectPost,
+    responseTypes: Set<ResponseType> = setOf(ResponseType.VpToken),
     redirectOrResponseUri: String?,
     nonce: String?,
     state: String?,
-    accessTokenScopes: Set<String>,
+    scopes: Set<String> = setOf(),
     clientId: String,
     clientIdScheme: ClientIdScheme?,
     clientMetadataParameter: ClientMetadataParameter?,
@@ -31,7 +42,7 @@ object OpenID4VP {
       },
       presentationDefinition = presentationDefinition.presentationDefinition,
       presentationDefinitionUri = presentationDefinition.presentationDefinitionUri,
-      scope = presentationDefinition.presentationDefinitionScope?.let { setOf(it).plus(accessTokenScopes) } ?: accessTokenScopes,
+      scope = presentationDefinition.presentationDefinitionScope?.let { setOf(it).plus(scopes) } ?: scopes,
       state = state,
       nonce = nonce,
       clientIdScheme = clientIdScheme,
@@ -39,4 +50,13 @@ object OpenID4VP {
       clientMetadataUri = clientMetadataParameter?.clientMetadataUri
     )
   }
+
+  /**
+   * Generates the authorization URL for the given presentation request, which can be rendered as a QR code (cross-device flow),
+   * or called on the authorization endpoint of the wallet (same-device flow).
+   * @param presentationRequest Presentation request to be rendered
+   * @param authorizationEndpoint Authorization endpoint of the wallet (same-device flow), default: "openid4vp://authorize" (cross-device flow)
+   */
+  fun getAuthorizationUrl(presentationRequest: AuthorizationRequest, authorizationEndpoint: String = "openid4vp://authorize") = "$authorizationEndpoint?${presentationRequest.toHttpQueryString()}"
+
 }
