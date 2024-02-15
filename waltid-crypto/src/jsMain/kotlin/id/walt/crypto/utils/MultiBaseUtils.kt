73e042171dfd27f705d9c03e07d6a27a4a87860c
Dinkar <jaindinkar@ymail.com>
2024-02-15 15:48:55 +0530

    adding js support for MultiBaseUtils

--- waltid-crypto/src/jsMain/kotlin/id/walt/crypto/utils/MultiBaseUtils.kt
+++ waltid-crypto/src/jsMain/kotlin/id/walt/crypto/utils/MultiBaseUtils.kt
@@ -1,11 +1,22 @@
 package id.walt.crypto.utils
 
+import io.ktor.utils.io.core.*
+import multibase
+
 @ExperimentalJsExport
 @JsExport
 actual object MultiBaseUtils {
-    actual fun convertRawKeyToMultiBase58Btc(key: ByteArray, code: UInt): String = TODO("Not yet implemented")
+    actual fun convertRawKeyToMultiBase58Btc(key: ByteArray, code: UInt): String {
+        return multibase.encode("base58btc", key).toString()
+    }
 
-    actual fun convertMultiBase58BtcToRawKey(mb: String): ByteArray = TODO("Not yet implemented")
+    actual fun convertMultiBase58BtcToRawKey(mb: String): ByteArray {
+        val bytes = decodeMultiBase58Btc(mb)
+        val code = MultiCodecUtils.UVarInt.fromBytes(bytes)
+        return bytes.drop(code.length).toByteArray()
+    }
 
-    actual fun decodeMultiBase58Btc(mb: String): ByteArray = TODO("Not yet implemented")
+    actual fun decodeMultiBase58Btc(mb: String): ByteArray {
+        return multibase.decode(mb.toByteArray())
+    }
 }
