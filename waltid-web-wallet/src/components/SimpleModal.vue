9d8aea9202334eec47f3486700c997a27c7cfd87
waltkb <68587968+waltkb@users.noreply.github.com>
2024-02-21 02:32:53 +0100

    New login page for the dynamic server-configured supported login methods configuration

---
+++ waltid-web-wallet/src/components/SimpleModal.vue
@@ -0,0 +1,56 @@
+<template>
+    <TransitionRoot :show="open" as="template">
+        <Dialog as="div" class="relative z-10" @close="close">
+            <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <TransitionChild
+                        as="template"
+                        enter="ease-out duration-300"
+                        enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enter-to="opacity-100 translate-y-0 sm:scale-100"
+                        leave="ease-in duration-200"
+                        leave-from="opacity-100 translate-y-0 sm:scale-100"
+                        leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    >
+                        <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                            <div class="sm:flex sm:items-start">
+                                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                    <DialogTitle as="h3" class="text-lg font-semibold leading-6 text-gray-900">{{ title }}</DialogTitle>
+                                    <div class="mt-3">
+                                        <slot/>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </DialogPanel>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
+
+<script lang="ts" setup>
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from "@headlessui/vue";
+
+const props = defineProps({
+    open: {
+        type: Boolean,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    }
+})
+
+const emit = defineEmits(["close"])
+
+function close() {
+    emit("close")
+}
+</script>
