aa257e1ce331d41103cfd5846c4976a9c6204bd7
Alexandre Gomes <alegomes@gmail.com>
2024-02-21 17:58:50 -0300

    Basic structure for the "did" command.

---
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/DidCreateCmd.kt
@@ -0,0 +1,13 @@
+package id.walt.cli.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+class DidCreateCmd : CliktCommand(
+    name = "create",
+    help = "Create a brand new Decentralized Identity",
+    printHelpOnEmptyArgs = true
+) {
+    override fun run() {
+
+    }
+}
