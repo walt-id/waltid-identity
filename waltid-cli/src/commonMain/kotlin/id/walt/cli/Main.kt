ce8258bd264e5517cddfa69e326691097bf5f666
waltkb <68587968+waltkb@users.noreply.github.com>
2024-02-22 11:50:44 +0100

    Various cleanup & refactor

--- waltid-cli/src/commonMain/kotlin/id/walt/cli/Main.kt
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/Main.kt
@@ -1,24 +1,3 @@
 package id.walt.cli
 
 fun main(args: Array<String>) = WaltIdCmd().main(args)
-
-//
-// fun main(args: Array<String>) {
-//     println("-- walt.id SSI Kit v2 CLI --")
-//
-//     MainCommand()
-//         .subcommands(
-//             Did().subcommands(
-//                 Did.Create()
-//             )
-//
-//         .main(args)
-//
-//
-//     /*WaltidServices.init()
-//     WaltidServices.minimalInit()
-//
-//     val dr = DidService.register(DidWebCreateOptions("localhost:3000"))
-//     println(dr.did)
-//     println(dr.didDocument)*/
-// }
--- waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/KeyConvertCmd.kt
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/KeyConvertCmd.kt
@@ -1,9 +1,9 @@
 package id.walt.cli.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.core.FileNotFound
 import com.github.ajalt.clikt.core.InvalidFileFormat
 import com.github.ajalt.clikt.core.terminal
+import com.github.ajalt.clikt.parameters.options.defaultLazy
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
@@ -11,7 +11,6 @@ import com.github.ajalt.clikt.parameters.types.file
 import id.walt.crypto.keys.Key
 import id.walt.crypto.keys.LocalKey
 import kotlinx.coroutines.runBlocking
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.openssl.PEMEncryptedKeyPair
 import org.bouncycastle.openssl.PEMParser
@@ -24,113 +23,93 @@ import java.io.File
 import java.io.FileReader
 import java.security.PrivateKey
 import java.security.Security
-import java.util.*
+import java.util.Base64
+import kotlin.io.path.useLines
+import kotlin.js.ExperimentalJsExport
 
 
 enum class KeyFileFormat {
-    JWK, PEM, ENCRYPTED_PEM
+    JWK, PEM, ENCRYPTED_PEM;
+
+    companion object {
+        fun getNames() = KeyFileFormat.entries.joinToString { it.name }
+    }
 }
 
+@OptIn(ExperimentalJsExport::class)
 class KeyConvertCmd : CliktCommand(
-    name = "convert",
-    help = "Convert key files between PEM and JWK formats.",
-    printHelpOnEmptyArgs = true
+    name = "convert", help = "Convert key files between PEM and JWK formats.", printHelpOnEmptyArgs = true
 ) {
 
-    val input by option("-i", "--input")
-        .help("The input file path. Accepted formats are: JWK and PEM")
-        .file()
-        .required()
+    private val input by option("-i", "--input").help("The input file path. Accepted formats are: JWK and PEM")
+        .file(mustExist = true, canBeFile = true, canBeDir = false, mustBeReadable = true).required()
 
-    val output by option("-o", "--output")
-        .help("The output file path. Accepted formats are: JWK and PEM. If not provided the input filename will be used with a different extension.")
-        .file()
-    // .required()
 
-    val passphrase by option("-p", "--passphrase")
-        .help("Passphrase to open an encrypted PEM")
+    private val sourceKeyType by lazy { getKeyType(input) }
+    private val targetKeyType by lazy { getOutputKeyType(sourceKeyType) }
+
+    private val output by option(
+        "-o",
+        "--output"
+    ).help("The output file path. Accepted formats are: JWK and PEM. If not provided the input filename will be used with a different extension.")
+        .file().defaultLazy {
+            File(input.parent, "${input.nameWithoutExtension}.${targetKeyType.toString().lowercase()}")
+        }
+
+    private val passphrase by option("-p", "--passphrase").help("Passphrase to open an encrypted PEM")
     // .prompt(text = "Please, inform the PEM passphrase", hideInput = true)
 
     override fun run() {
-
-        checkIfInputFileExists(input)
-
-        // Identify input key type
-        val inputKeyType = getKeyType(input)
-        // Decide which is the output file format
-        val outputKeyType = getOutputKeyType(inputKeyType)
-
         // Read the source key from the input file
-        val key = runBlocking { getKey(input) }
+        val inputKey = runBlocking { getKey(input) }
 
-        // Set output file
-        // e.g. if --input=myKey.jwt and --output is not provided, the output file will be myKey.pem
-        val outputFile =
-            output ?: File(input.parent, "${input.nameWithoutExtension}.${outputKeyType.toString().lowercase()}")
+        val outputContent = convertKey(inputKey, targetKeyType)
 
-        val outputContent = getOutputContent(key, outputKeyType)
+        output.writeText(outputContent)
 
-        outputFile.writeText(outputContent)
-
-        echo("Done. $input file converted to $outputFile")
+        echo("Converted \"${input.path}\" to \"${output.path}\".")
     }
 
-    private fun checkIfInputFileExists(input: File): Unit { // Result<Boolean> {
-        if (!input.exists()) {
-            throw FileNotFound(input.path)
-        }
-    }
-
-    private fun getKeyType(input: File): KeyFileFormat {
-
-        operator fun Regex.contains(text: CharSequence): Boolean = this.matches(text)
-
-        val firstLine = input.readLines()[0]
-        return when (firstLine) {
-            // For more PEM's content type, see:
-            // https://github.com/openssl/openssl/blob/master/include/openssl/pem.h
+    private operator fun Regex.contains(text: CharSequence?): Boolean = this.matches(text ?: "")
+    private fun getKeyType(input: File): KeyFileFormat = input.toPath().useLines { lines ->
+        when (lines.firstOrNull()) {
+            null -> throw InvalidFileFormat(input.path, "No lines in file.")
+            // other PEM content types: https://github.com/openssl/openssl/blob/master/include/openssl/pem.h
             in Regex("""^\{.*""") -> KeyFileFormat.JWK
-            in Regex("""^-+BEGIN PUBLIC KEY-+""") -> KeyFileFormat.PEM
-            in Regex("""^-+BEGIN PRIVATE KEY-+""") -> KeyFileFormat.PEM
-            in Regex("""^-+BEGIN ENCRYPTED PRIVATE KEY-+""") -> KeyFileFormat.ENCRYPTED_PEM
-            else -> throw InvalidFileFormat(input.path, "Unknown file format")
+            in Regex("""^-+BEGIN .*PUBLIC KEY-+""") -> KeyFileFormat.PEM
+            in Regex("""^-+BEGIN .*PRIVATE KEY-+""") -> KeyFileFormat.PEM
+            in Regex("""^-+BEGIN .*ENCRYPTED PRIVATE KEY-+""") -> KeyFileFormat.ENCRYPTED_PEM
+            else -> throw InvalidFileFormat(input.path, "Unknown file format (expected ${KeyFileFormat.getNames()}).")
         }
     }
 
-    private suspend fun getKey(input: File): LocalKey {
-
-        val errorMsg = "It's neither JWK nor a PEM."
-
+    private suspend fun getKey(input: File, keyType: KeyFileFormat = getKeyType(input)): LocalKey {
         val inputContent = input.readText()
-        val keyType = getKeyType(input)
-        try {
-            return when (keyType) {
+
+        return runCatching {
+            when (keyType) {
                 KeyFileFormat.JWK -> LocalKey.importJWK(inputContent).getOrThrow()
                 KeyFileFormat.PEM -> LocalKey.importPEM(inputContent).getOrThrow()
                 KeyFileFormat.ENCRYPTED_PEM -> LocalKey.importPEM(decrypt(input).getOrThrow()).getOrThrow()
-                else -> throw InvalidFileFormat(input.path, errorMsg)
             }
-        } catch (e: Exception) {
-            throw InvalidFileFormat(input.path, errorMsg)
-        }
+        }.getOrElse { throw IllegalArgumentException("Could not process key file at ${input.absolutePath}.", it) }
     }
 
-    private fun getOutputKeyType(inputKeyType: KeyFileFormat): KeyFileFormat {
-        // If the provided file is of type JWK, convert it to PEM.
-        // If PEM, convert it to JWK.
-        return when (inputKeyType) {
-            KeyFileFormat.JWK -> KeyFileFormat.PEM
-            KeyFileFormat.PEM -> KeyFileFormat.JWK
-            KeyFileFormat.ENCRYPTED_PEM -> KeyFileFormat.JWK
-        }
-    }
+    /**
+     * If the provided file is of type JWK, convert it to PEM.
+     * If PEM, convert it to JWK.
+     */
+    private fun getOutputKeyType(inputKeyType: KeyFileFormat): KeyFileFormat =
+        if (inputKeyType == KeyFileFormat.JWK) KeyFileFormat.PEM else KeyFileFormat.JWK
 
-    private fun getOutputContent(inputKey: Key, outputKeyType: KeyFileFormat): String {
-        return when (outputKeyType) {
-            KeyFileFormat.JWK -> runBlocking { inputKey.exportJWK() }
-            KeyFileFormat.PEM -> runBlocking { inputKey.exportPEM() }
-            KeyFileFormat.ENCRYPTED_PEM -> runBlocking { inputKey.exportPEM() }
-        }
+
+    /**
+    Convert provided source key to specified target key type
+     */
+    private fun convertKey(inputKey: Key, targetKeyType: KeyFileFormat): String = when (targetKeyType) {
+        KeyFileFormat.JWK -> runBlocking { inputKey.exportJWK() }
+        KeyFileFormat.PEM -> runBlocking { inputKey.exportPEM() }
+        KeyFileFormat.ENCRYPTED_PEM -> runBlocking { inputKey.exportPEM() }
     }
 
     private fun decrypt(input: File): Result<String> {
@@ -158,36 +137,39 @@ class KeyConvertCmd : CliktCommand(
 
     // TODO: Extract
     fun decryptKey(passphrase: String): String {
-        Security.addProvider(BouncyCastleProvider())
+        Security.addProvider(BouncyCastleProvider()) // TODO: do not add at every function call
 
         try {
             val pemParser = PEMParser(FileReader(input))
-            val o = pemParser.readObject()
-            val pki: PrivateKeyInfo
+            val pemObject = runCatching {
+                pemParser.readObject() ?: error("No object in PEM")
+            }.getOrElse { throw IllegalArgumentException("Could not parse object from PEM", it) }
+            val pki = when (pemObject) {
+                is PKCS8EncryptedPrivateKeyInfo -> {
+                    val decryptorProviderBuilder = JcePKCSPBEInputDecryptorProviderBuilder().setProvider("BC")
+                    val inputDecryptorProvider = decryptorProviderBuilder.build(passphrase.toCharArray())
 
-            if (o is PKCS8EncryptedPrivateKeyInfo) {
-                val builder = JcePKCSPBEInputDecryptorProviderBuilder().setProvider("BC")
-                val idp = builder.build(passphrase.toCharArray())
+                    pemObject.decryptPrivateKeyInfo(inputDecryptorProvider)
+                }
 
-                pki = o.decryptPrivateKeyInfo(idp)
-            } else if (o is PEMEncryptedKeyPair) {
-                val pkp = o.decryptKeyPair(BcPEMDecryptorProvider(passphrase.toCharArray()))
+                is PEMEncryptedKeyPair -> {
+                    val pkp = pemObject.decryptKeyPair(BcPEMDecryptorProvider(passphrase.toCharArray()))
 
-                pki = pkp.privateKeyInfo
-            } else {
-                throw PKCSException("Invalid encrypted private key class: " + o.javaClass.name)
+                    pkp.privateKeyInfo
+                }
+
+                else -> throw PKCSException("Invalid encrypted private key class: " + pemObject::class.qualifiedName)
             }
 
             val converter = JcaPEMKeyConverter().setProvider("BC")
 
             val encodedKey = Base64.getEncoder().encodeToString(converter.getPrivateKey(pki).encoded)
-            val pem = """
+
+            return """
                 -----BEGIN PRIVATE KEY-----
                 $encodedKey
                 -----END PRIVATE KEY-----
             """.trimIndent()
-
-            return pem
         } catch (e: Exception) {
             throw Exception("Failed to load key from $input: $e")
         }
