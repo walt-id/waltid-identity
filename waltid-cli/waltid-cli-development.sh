8268ac371aee3257b428efa992837185610148a6
waltkb <68587968+waltkb@users.noreply.github.com>
2024-02-22 13:47:07 +0100

    Added comments to CLI run scripts

--- waltid-cli/waltid-cli-development.sh
+++ waltid-cli/waltid-cli-development.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+# Launcher script which rebuilds the waltid-cli from source and executes the command supplied
+
 gradle --quiet installDist && build/install/waltid-cli/bin/waltid-cli "$@"
