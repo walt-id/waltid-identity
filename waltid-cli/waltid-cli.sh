64ea33de379a66e973d6c870e3fa1f78435f7482
waltkb <68587968+waltkb@users.noreply.github.com>
2024-02-22 13:47:07 +0100

    Added comments to CLI run scripts

--- waltid-cli/waltid-cli.sh
+++ waltid-cli/waltid-cli.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# Launcher script which builds the waltid-cli from source **once** and executes the command supplied
+# If changes are made to the source then waltid-cli has to manually be rebuild (e.g. through waltid-cli-development.sh)
+
 if [ -f 'build/install/waltid-cli/bin/waltid-cli' ]; then
   build/install/waltid-cli/bin/waltid-cli "$@"
 else
