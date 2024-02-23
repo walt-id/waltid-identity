cee1f5b6b47ad8f469c625a5fd99734703dbb599
SuperBatata <khaled.battiche@esprit.tn>
2024-02-23 09:52:26 +0100

    feat : Support for OCI KMS

---
+++ waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/OCIKeyConfig.kt
@@ -0,0 +1,12 @@
+package id.walt.crypto.keys
+
+data class OCIKeyConfig (
+    val tenancyOcid : String,
+    val userOcid : String,
+    val fingerprint : String,
+    val managementEndpoint : String,
+    val keyId : String ,
+    val OCIDKeyID : String,
+    val cryptoEndpoint : String,
+
+    )
