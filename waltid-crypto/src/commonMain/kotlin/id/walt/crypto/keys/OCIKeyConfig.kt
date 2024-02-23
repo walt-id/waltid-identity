29f4dccbef1acb96943cc18f5d6c2580d64fd194
Philipp Potisk <phil@walt.id>
2024-02-23 19:11:45 +0100

    code cleanup

--- waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/OCIKeyConfig.kt
+++ waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/OCIKeyConfig.kt
@@ -1,12 +1,11 @@
 package id.walt.crypto.keys
 
-data class OCIKeyConfig (
-    val tenancyOcid : String,
-    val userOcid : String,
-    val fingerprint : String,
-    val managementEndpoint : String,
-    val keyId : String ,
-    val OCIDKeyID : String,
-    val cryptoEndpoint : String,
-
-    )
+data class OCIKeyConfig(
+    val tenancyOcid: String,
+    val userOcid: String,
+    val fingerprint: String,
+    val managementEndpoint: String,
+    val keyId: String,
+    val OCIDKeyID: String,
+    val cryptoEndpoint: String
+)
