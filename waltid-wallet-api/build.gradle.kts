53e3b3435fbbc939af5802d4e5631b8d3be84252
waltkb <68587968+waltkb@users.noreply.github.com>
2024-02-26 11:53:18 +0100

    Fix DID lib dependency reference

--- waltid-wallet-api/build.gradle.kts
+++ waltid-wallet-api/build.gradle.kts
@@ -1,10 +1,10 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.9.22"
-    kotlin("jvm") version kotlinVersion
-    id("io.ktor.plugin") version "2.3.7"
-    id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
+    kotlin("jvm")
+    id("io.ktor.plugin") version "2.3.8"
+    kotlin("plugin.serialization")
 
     id("com.github.ben-manes.versions") version "0.49.0"
 }
@@ -49,7 +49,7 @@ dependencies {
 
     /* -- KTOR -- */
 
-    val ktorVersion = "2.3.7"
+    val ktorVersion = "2.3.8"
     // Ktor server
     implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
@@ -65,6 +65,7 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-call-id-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("io.ktor:ktor-server-cio-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-method-override:$ktorVersion")
 
@@ -84,13 +85,13 @@ dependencies {
     /* -- Kotlin -- */
 
     // Kotlinx.serialization
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
     // Date
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 
     // UUID
     implementation("app.softwork:kotlinx-uuid-core:0.0.22")
@@ -98,34 +99,34 @@ dependencies {
 
     /* -- Security -- */
     // Bouncy Castle
-    implementation("org.bouncycastle:bcprov-jdk18on:1.76")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.77")
 
     // Argon2
     implementation("de.mkammerer:argon2-jvm:2.11")
 
 
     // waltid-did
-    implementation("id.walt.did:waltid-did:1.1.1")//id.walt.crypto provided by id.walt.did:waltid-did
+    implementation(project(":waltid-did"))//id.walt.crypto provided by id.walt.did:waltid-did
 
     // OIDC
     implementation(project(":waltid-openid4vc"))
     implementation(project(":waltid-sdjwt"))
-    implementation("com.nimbusds:nimbus-jose-jwt:9.37.1")
+    implementation("com.nimbusds:nimbus-jose-jwt:9.37.3")
     implementation("io.ktor:ktor-client-java:$ktorVersion")
 
     /* -- Misc --*/
 
     // Cache
-    implementation("io.github.reactivecircus.cache4k:cache4k:0.12.0")
+    implementation("io.github.reactivecircus.cache4k:cache4k:0.13.0")
 
     // DB
-    implementation("org.jetbrains.exposed:exposed-core:0.45.0")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.45.0")
-    implementation("org.jetbrains.exposed:exposed-dao:0.45.0")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.45.0")
-    implementation("org.jetbrains.exposed:exposed-json:0.45.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.47.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.47.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.47.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.47.0")
+    implementation("org.jetbrains.exposed:exposed-json:0.47.0")
     implementation("org.xerial:sqlite-jdbc:3.44.1.0")
-    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("org.postgresql:postgresql:42.7.2")
     // migration
     //implementation("org.flywaydb:flyway-core:9.22.2")
 
@@ -137,12 +138,12 @@ dependencies {
     implementation("com.sksamuel.hoplite:hoplite-hocon:2.8.0.RC3")
     implementation("com.sksamuel.hoplite:hoplite-yaml:2.8.0.RC3")
     implementation("com.sksamuel.hoplite:hoplite-hikaricp:2.8.0.RC3")
-    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("com.zaxxer:HikariCP:5.1.0")
 
     // Logging
-    implementation("io.github.oshai:kotlin-logging-jvm:5.1.1")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
-    implementation("org.slf4j:jul-to-slf4j:2.0.9")
+    implementation("io.github.oshai:kotlin-logging-jvm:6.0.3")
+    implementation("org.slf4j:slf4j-simple:2.0.12")
+    implementation("org.slf4j:jul-to-slf4j:2.0.12")
 
     // Test
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.22")
@@ -151,4 +152,12 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:5.5.5")
     testImplementation("io.kotest.extensions:kotest-assertions-ktor:2.0.0")*/
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+}
+
+
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        listOf("-beta", "-alpha", "-rc").any { it in candidate.version.lowercase() } || candidate.version.takeLast(4).contains("RC")
+    }
 }
