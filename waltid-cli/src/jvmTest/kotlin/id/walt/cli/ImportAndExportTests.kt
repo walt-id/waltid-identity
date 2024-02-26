f688ceaea30cc4e674227f8f0fc5409d8c7bd0b1
Alexandre Gomes <alegomes@gmail.com>
2024-02-26 09:46:25 -0300

    [work in progress] Investigating key conversion issues

---
+++ waltid-cli/src/jvmTest/kotlin/id/walt/cli/ImportAndExportTests.kt
@@ -0,0 +1,54 @@
+package id.walt.cli
+
+import id.walt.crypto.keys.KeyType
+import id.walt.crypto.keys.LocalKey
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.assertDoesNotThrow
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ImportAndExportTests {
+
+    @Test
+    fun testRSA() {
+        testBidirectionalConversion(KeyType.RSA)
+    }
+
+    @Test
+    @Ignore
+    fun testEd25519() {
+        // kotlin.NotImplementedError: Ed25519 keys cannot be exported as PEM yet.
+        testBidirectionalConversion(KeyType.Ed25519)
+    }
+
+    @Test
+    fun testSecp256k1() {
+        testBidirectionalConversion(KeyType.secp256k1)
+    }
+
+    @Test
+    fun testSecp256r1() {
+        testBidirectionalConversion(KeyType.secp256r1)
+    }
+
+    private fun testBidirectionalConversion(keyType: KeyType) {
+
+        val generatedKey = runBlocking { LocalKey.generate(keyType) }
+        val generatedJwk = runBlocking { generatedKey.exportJWK() }
+
+        val importedKeyFromJwk = assertDoesNotThrow {
+            runBlocking { LocalKey.importJWK(generatedJwk).getOrThrow() }
+        }
+
+        val exportedPem = runBlocking { importedKeyFromJwk.exportPEM() }
+
+        val importedKeyFromPem = assertDoesNotThrow {
+            runBlocking { LocalKey.importPEM(exportedPem).getOrThrow() }
+        }
+
+        val exportedJwk = runBlocking { importedKeyFromPem.exportJWK() }
+
+        assertEquals(generatedJwk, exportedJwk)
+    }
+}
