4b3c817c48967bc4c3f895b157f83c703a3e08c7
Alexandre Gomes <alegomes@gmail.com>
2024-02-27 09:25:35 -0300

    Handling syntax errors in the command call.

--- waltid-cli/src/commonMain/kotlin/id/walt/cli/Main.kt
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/Main.kt
@@ -1,6 +1,7 @@
 package id.walt.cli
 
 import com.github.ajalt.clikt.core.*
+import com.github.ajalt.clikt.output.ParameterFormatter
 import com.github.ajalt.mordant.rendering.TextColors
 import com.github.ajalt.mordant.rendering.Whitespace
 import com.github.ajalt.mordant.widgets.Panel
@@ -18,6 +19,19 @@ fun main(args: Array<String>) {
         printError(cmd, e)
         printUsage(cmd, e)
         exitProcess(e.statusCode)
+    } catch (e: MultiUsageError) {
+        var msgs = "Invalid command. Please, review the usage instructions bellow and try again."
+        // for (error in e.errors) {
+        //     if (msgs.length == 0) {
+        //         // msgs = error.formatMessage(error.context!!.localization, parameterFormatter(error.context!!))
+        //         msgs = "${error.localizedMessage} - ${error.message} "
+        //     } else {
+        //         msgs = """${msgs} ${error.toString() ?: ""}"""
+        //     }
+        // }
+        printError(cmd, e, msgs)
+        printUsage(cmd, e)
+
     } catch (e: CliktError) {
         printError(cmd, e)
         printUsage(cmd, e)
@@ -40,4 +54,24 @@ fun printError(cmd: CliktCommand, e: CliktError? = null, msg: String? = null) {
 fun printUsage(cmd: CliktCommand, e: CliktError) {
     val ctx = (e as ContextCliktError).context
     cmd.echoFormattedHelp(PrintHelpMessage(ctx))
+}
+
+fun parameterFormatter(context: Context): ParameterFormatter {
+    return object : ParameterFormatter {
+        override fun formatOption(name: String): String {
+            // return styleOptionName(name)
+            return context.theme.style("info")(name)
+        }
+
+        override fun formatArgument(name: String): String {
+            // return styleArgumentName(normalizeParameter(name))
+            return context.theme.style("info")("<${name.lowercase()}>")
+        }
+
+        override fun formatSubcommand(name: String): String {
+            // return styleSubcommandName(name)
+            return context.theme.style("info")(name)
+        }
+
+    }
 }
