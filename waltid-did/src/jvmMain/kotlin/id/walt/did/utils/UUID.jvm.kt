3c05629be93bc96453c9186a164760e1c5b45b22
waltkb <68587968+waltkb@users.noreply.github.com>
2024-02-27 17:33:50 +0100

    Remove no longer needed specific-platform code

--- waltid-did/src/jvmMain/kotlin/id/walt/did/utils/UUID.jvm.kt
+++
@@ -1,5 +0,0 @@
-package id.walt.did.utils
-
-import java.util.*
-
-actual fun randomUUID(): String = UUID.randomUUID().toString()
--- waltid-verifiable-credentials/src/jvmMain/kotlin/id/walt/credentials/utils/UUID.jvm.kt
+++
@@ -1,5 +0,0 @@
-package id.walt.credentials.utils
-
-import java.util.*
-
-actual fun randomUUID(): String = UUID.randomUUID().toString()
