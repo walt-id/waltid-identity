482bea35c02b02cffbb7791ed3d3b9d6a39d2232
SuperBatata <khaled.battiche@esprit.tn>
2024-02-28 16:35:45 +0100

    feat : add OCIkey subclass to KeySerialization

--- waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/KeySerialization.kt
+++ waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/KeySerialization.kt
@@ -1,5 +1,7 @@
 package id.walt.crypto.keys
 
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
@@ -10,35 +12,36 @@ import kotlinx.serialization.modules.subclass
 import love.forte.plugin.suspendtrans.annotation.JsPromise
 import love.forte.plugin.suspendtrans.annotation.JvmAsync
 import love.forte.plugin.suspendtrans.annotation.JvmBlocking
-import kotlin.js.ExperimentalJsExport
-import kotlin.js.JsExport
-
 
 @OptIn(ExperimentalJsExport::class)
 @JsExport
 object KeySerialization {
 
-    private val keySerializationModule = SerializersModule {
-        polymorphic(Key::class) {
-            subclass(LocalKey::class)
-            subclass(TSEKey::class)
-        }
+  private val keySerializationModule = SerializersModule {
+    polymorphic(Key::class) {
+      subclass(LocalKey::class)
+      subclass(TSEKey::class)
+      subclass(OCIKey::class)
     }
+  }
 
-    private val keySerializationJson = Json {
-        serializersModule =
-            keySerializationModule
-    }
+  private val keySerializationJson = Json { serializersModule = keySerializationModule }
 
-    fun serializeKey(key: Key): String = keySerializationJson.encodeToString(key)
-    @JvmBlocking
-    @JvmAsync
-    @JsPromise
-    @JsExport.Ignore
-    suspend fun deserializeKey(json: String): Result<Key> = runCatching { keySerializationJson.decodeFromString<Key>(json).apply { init() } }
-    @JvmBlocking
-    @JvmAsync
-    @JsPromise
-    @JsExport.Ignore
-    suspend fun deserializeKey(json: JsonObject): Result<Key> = runCatching { keySerializationJson.decodeFromJsonElement<Key>(json).apply { init() } }
+  fun serializeKey(key: Key): String = keySerializationJson.encodeToString(key)
+
+  @JvmBlocking
+  @JvmAsync
+  @JsPromise
+  @JsExport.Ignore
+  suspend fun deserializeKey(json: String): Result<Key> = runCatching {
+    keySerializationJson.decodeFromString<Key>(json).apply { init() }
+  }
+
+  @JvmBlocking
+  @JvmAsync
+  @JsPromise
+  @JsExport.Ignore
+  suspend fun deserializeKey(json: JsonObject): Result<Key> = runCatching {
+    keySerializationJson.decodeFromJsonElement<Key>(json).apply { init() }
+  }
 }
