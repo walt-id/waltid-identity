484f3bf375f611fd0ccb7e1d17260642724bde9d
Alexandre Gomes <alegomes@gmail.com>
2024-03-02 07:17:28 -0300

    Passing tests:
    should print help message when called with --help argument
    should create a key of type did when called with no argument
    should create a valid did-key when called with no argument
    should have --method option

---
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/util/DidUtil.kt
@@ -0,0 +1,24 @@
+package id.walt.cli.util
+
+class DidUtil {
+
+    fun createDid() {
+        // val keyId = args["keyId"]?.content?.takeIf { it.isNotEmpty() } ?: generateKey(KeyType.Ed25519.name)
+        // val key = getKey(keyId)
+        // val options = getDidOptions(method, args)
+        // val result = DidService.registerByKey(method, key, options)
+        // DidsService.add(
+        //     wallet = walletId,
+        //     did = result.did,
+        //     document = Json.encodeToString(result.didDocument),
+        //     alias = args["alias"]?.content,
+        //     keyId = keyId
+        // )
+        // logEvent(
+        //     EventType.Did.Create, "wallet", DidEventData(
+        //         did = result.did, document = result.didDocument.toString()
+        //     )
+        // )
+        // return result.did
+    }
+}
