d55963dfcb86d912aa6b981e876a8350a2e0a3a8
Severin Stampler <severin@walt.id>
2024-03-04 18:47:46 +0100

    continue oid4vci lib implementation, ci sequence diagram first draft (tbc)

--- waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/OpenID4VCI.kt
+++ waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/OpenID4VCI.kt
@@ -1,17 +1,26 @@
 package id.walt.oid4vc
 
 import id.walt.oid4vc.data.CredentialOffer
+import id.walt.oid4vc.data.GrantType
 import id.walt.oid4vc.data.OfferedCredential
 import id.walt.oid4vc.data.OpenIDProviderMetadata
 import id.walt.oid4vc.definitions.CROSS_DEVICE_CREDENTIAL_OFFER_URL
+import id.walt.oid4vc.definitions.JWTClaims
+import id.walt.oid4vc.errors.TokenError
+import id.walt.oid4vc.interfaces.ITokenProvider
+import id.walt.oid4vc.providers.IssuanceSession
+import id.walt.oid4vc.providers.TokenTarget
 import id.walt.oid4vc.requests.CredentialOfferRequest
+import id.walt.oid4vc.requests.TokenRequest
+import id.walt.oid4vc.responses.TokenErrorCode
+import id.walt.oid4vc.responses.TokenResponse
 import id.walt.oid4vc.util.http
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.util.*
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.datetime.Clock
+import kotlinx.serialization.json.*
 
 object OpenID4VCI {
     fun getCredentialOfferRequestUrl(
@@ -93,5 +102,64 @@ object OpenID4VCI {
         }
     }
 
+//    fun processTokenRequest(tokenRequest: TokenRequest): TokenResponse {
+//        val code = when (tokenRequest.grantType) {
+//            GrantType.authorization_code -> tokenRequest.code ?: throw TokenError(
+//                tokenRequest = tokenRequest,
+//                errorCode = TokenErrorCode.invalid_grant,
+//                message = "No code parameter found on token request"
+//            )
+//
+//            GrantType.pre_authorized_code -> tokenRequest.preAuthorizedCode ?: throw TokenError(
+//                tokenRequest = tokenRequest,
+//                errorCode = TokenErrorCode.invalid_grant,
+//                message = "No pre-authorized_code parameter found on token request"
+//            )
+//
+//            else -> throw TokenError(tokenRequest, TokenErrorCode.unsupported_grant_type, "Grant type not supported")
+//        }
+//        val payload = validateAuthorizationCode(code) ?: throw TokenError(
+//            tokenRequest = tokenRequest,
+//            errorCode = TokenErrorCode.invalid_grant,
+//            message = "Authorization code could not be verified"
+//        )
+//
+//        val sessionId = payload["sub"]!!.jsonPrimitive.content
+//        val session = getVerifiedSession(sessionId) ?: throw TokenError(
+//            tokenRequest = tokenRequest,
+//            errorCode = TokenErrorCode.invalid_request,
+//            message = "No authorization session found for given authorization code, or session expired."
+//        )
+//
+//        return generateTokenResponse(session, tokenRequest)
+//    }
+//
+//    private fun generateTokenResponse(session: IssuanceSession, tokenRequest: TokenRequest, tokenProvider: ITokenProvider): TokenResponse {
+//        if (tokenRequest.grantType == GrantType.pre_authorized_code && !session.preAuthUserPin.isNullOrEmpty() &&
+//            session.preAuthUserPin != tokenRequest.userPin
+//        ) {
+//            throw TokenError(
+//                tokenRequest,
+//                TokenErrorCode.invalid_grant,
+//                message = "User PIN required for this issuance session has not been provided or PIN is wrong."
+//            )
+//        }
+//        return TokenResponse.success(
+//            generateToken(tokenProvider, session.id, TokenTarget.ACCESS, session.issuer),
+//            "bearer", state = session.authorizationRequest?.state,
+//            cNonce = session.cNonce,
+//            cNonceExpiresIn = session.expirationTimestamp - Clock.System.now()
+//            // TODO: authorization_pending, interval
+//        )
+//    }
+//
+//    private fun generateToken(tokenProvider: ITokenProvider, sub: String, audience: TokenTarget, issuer: String, tokenId: String? = null): String {
+//        return tokenProvider.signToken(audience, buildJsonObject {
+//            put(JWTClaims.Payload.subject, sub)
+//            put(JWTClaims.Payload.issuer, issuer)
+//            put(JWTClaims.Payload.audience, audience.name)
+//            tokenId?.let { put(JWTClaims.Payload.jwtID, it) }
+//        })
+//    }
 
 }
