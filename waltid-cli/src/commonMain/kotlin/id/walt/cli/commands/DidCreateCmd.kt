0f5f9d9841598d93e8a0039bb681ed56901f1304
Alexandre Gomes <alegomes@gmail.com>
2024-03-05 06:41:37 -0300

    Pretty output format

--- waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/DidCreateCmd.kt
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/DidCreateCmd.kt
@@ -1,11 +1,15 @@
 package id.walt.cli.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.terminal
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.enum
 import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.mordant.markdown.Markdown
+import com.github.ajalt.mordant.rendering.TextColors
+import com.github.ajalt.mordant.rendering.TextStyles
 import id.walt.cli.util.DidMethod
 import id.walt.crypto.keys.KeyType
 import id.walt.crypto.keys.LocalKey
@@ -28,19 +32,49 @@ class DidCreateCmd : CliktCommand(
     // .enum<DidMethod>(ignoreCase = true)
 
     private val key by lazy {
-        keyFile?.readText() ?: generateDefaultKey()
+        runBlocking { getKey() }
     }
 
     override fun run() {
-        echo("Subject's key: ${key}")
-        echo("DID created: did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK")
+        runBlocking {
+            echo(TextStyles.dim("DID subject ${key.keyType} key thumbprint: ${key.getThumbprint()}"))
+            // terminal.println(
+            //     Markdown(
+            //         """
+            //     |```json
+            //     |${key.getThumbprint()}
+            //     |```
+            // """.trimMargin()
+            //     )
+            // )
+            echo("DID created: did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK")
+        }
     }
 
-    private fun generateDefaultKey(): LocalKey {
-        echo("Key not provided. Let's generate a new one...")
+    private suspend fun getKey(): LocalKey {
+        if (keyFile != null) {
+            return LocalKey.importJWK(keyFile!!.readText()).getOrThrow()
+        } else {
+            return generateDefaultKey()
+        }
+    }
+
+    private suspend fun generateDefaultKey(): LocalKey {
+        echo(TextStyles.dim("Key not provided. Let's generate a new one..."))
         val key = runBlocking { LocalKey.generate(KeyType.Ed25519) }
+        echo(TextStyles.dim("Key thumbprint is: ${key.getThumbprint()}"))
+
         val jwk = runBlocking { key.exportJWKPretty() }
-        echo("Key generated: ${jwk}")
+        echo(TextColors.green("Generated Key (JWK):"))
+        terminal.println(
+            Markdown(
+                """
+                |```json
+                |$jwk
+                |```
+            """.trimMargin()
+            )
+        )
 
         return key
         // return KeyUtils.generateKey(KeyType.Ed25519)
