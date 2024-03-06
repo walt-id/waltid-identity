5c457afd8f46331d20c2ea029cd6db3a0c663453
SuperBatata <khaled.battiche@esprit.tn>
2024-03-06 10:25:36 +0100

    feat : add keycloak account request

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/model/AccountRequest.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/model/AccountRequest.kt
@@ -8,31 +8,49 @@ import kotlinx.serialization.modules.polymorphic
 
 @Serializable
 sealed class AccountRequest {
-    abstract val name: String?
+  abstract val name: String?
 }
 
 @Serializable
 @SerialName("email")
-data class EmailAccountRequest(override val name: String? = null, val email: String, val password: String) : AccountRequest()
+data class EmailAccountRequest(
+    override val name: String? = null,
+    val email: String,
+    val password: String
+) : AccountRequest()
 
 @Serializable
 @SerialName("address")
-data class AddressAccountRequest(override val name: String? = null, val address: String, val ecosystem: String) : AccountRequest()
+data class AddressAccountRequest(
+    override val name: String? = null,
+    val address: String,
+    val ecosystem: String
+) : AccountRequest()
 
 @Serializable
 @SerialName("oidc")
-data class OidcAccountRequest(override val name: String? = null, val token: String) : AccountRequest()
+data class OidcAccountRequest(override val name: String? = null, val token: String) :
+    AccountRequest()
 
+@Serializable
+@SerialName("keycloak")
+data class KeycloakAccountRequest(
+    override val name: String? = null,
+    val email: String,
+    val username: String,
+    val password: String
+) : AccountRequest()
 
 val module = SerializersModule {
-    polymorphic(AccountRequest::class) {
-        EmailAccountRequest::class
-        AddressAccountRequest::class
-        OidcAccountRequest::class
-    }
+  polymorphic(AccountRequest::class) {
+    EmailAccountRequest::class
+    AddressAccountRequest::class
+    OidcAccountRequest::class
+    KeycloakAccountRequest::class
+  }
 }
 
 val LoginRequestJson = Json {
-    serializersModule = module
-    ignoreUnknownKeys = true
+  serializersModule = module
+  ignoreUnknownKeys = true
 }
