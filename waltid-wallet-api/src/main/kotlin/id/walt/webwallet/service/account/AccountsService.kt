7de340c4dcf2d0166a20e440d4789bd9be48e31f
SuperBatata <khaled.battiche@esprit.tn>
2024-03-06 10:25:51 +0100

    feat : add keycloak account request

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/account/AccountsService.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/account/AccountsService.kt
@@ -12,10 +12,7 @@ import id.walt.webwallet.service.issuers.IssuersService
 import id.walt.webwallet.service.settings.SettingsService
 import id.walt.webwallet.service.settings.WalletSetting
 import id.walt.webwallet.web.controllers.generateToken
-import id.walt.webwallet.web.model.AccountRequest
-import id.walt.webwallet.web.model.AddressAccountRequest
-import id.walt.webwallet.web.model.EmailAccountRequest
-import id.walt.webwallet.web.model.OidcAccountRequest
+import id.walt.webwallet.web.model.*
 import kotlinx.datetime.toKotlinInstant
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonPrimitive
@@ -26,125 +23,134 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object AccountsService {
 
-    fun registerAuthenticationMethods() {
-        val loginMethods = ConfigManager.getConfig<LoginMethodsConfig>().enabledLoginMethods
+  fun registerAuthenticationMethods() {
+    val loginMethods = ConfigManager.getConfig<LoginMethodsConfig>().enabledLoginMethods
+  }
 
-    }
+  suspend fun register(tenant: String = "", request: AccountRequest): Result<RegistrationResult> =
+      when (request) {
+            is EmailAccountRequest -> EmailAccountStrategy.register(tenant, request)
+            is AddressAccountRequest -> Web3WalletAccountStrategy.register(tenant, request)
+            is OidcAccountRequest -> OidcAccountStrategy.register(tenant, request)
+            is KeycloakAccountRequest -> KeycloakAccountStrategy.register(tenant, request)
+          }
+          .onSuccess { registrationResult ->
+            val registeredUserId = registrationResult.id
 
-    suspend fun register(tenant: String = "", request: AccountRequest): Result<RegistrationResult> = when (request) {
-        is EmailAccountRequest -> EmailAccountStrategy.register(tenant, request)
-        is AddressAccountRequest -> Web3WalletAccountStrategy.register(tenant, request)
-        is OidcAccountRequest -> OidcAccountStrategy.register(tenant, request)
-    }.onSuccess { registrationResult ->
-        val registeredUserId = registrationResult.id
+            val createdInitialWalletId =
+                transaction { WalletServiceManager.createWallet(tenant, registeredUserId) }
+                    .also { walletId ->
+                      IssuersService.addToWallet(walletId, "walt.id")
+                      SettingsService.set(walletId, WalletSetting.default)
+                    }
 
-        val createdInitialWalletId = transaction {
-            WalletServiceManager.createWallet(tenant, registeredUserId)
-        }.also { walletId ->
-            IssuersService.addToWallet(walletId, "walt.id")
-            SettingsService.set(walletId, WalletSetting.default)
+            val walletService =
+                WalletServiceManager.getWalletService(
+                    tenant, registeredUserId, createdInitialWalletId)
+            EventService.add(
+                Event(
+                    action = EventType.Account.Create,
+                    tenant = tenant,
+                    originator = "wallet",
+                    account = registeredUserId,
+                    wallet = createdInitialWalletId,
+                    data = AccountEventData(accountId = request.name)))
+
+            // Add default data:
+            val createdDid =
+                walletService.createDid("key", mapOf("alias" to JsonPrimitive("Onboarding")))
+            walletService.setDefault(createdDid)
+          }
+          .onFailure { throw IllegalStateException("Could not register user: ${it.message}", it) }
+
+  suspend fun authenticate(tenant: String, request: AccountRequest): Result<AuthenticationResult> =
+      runCatching {
+            when (request) {
+              is EmailAccountRequest -> EmailAccountStrategy.authenticate(tenant, request)
+              is AddressAccountRequest -> Web3WalletAccountStrategy.authenticate(tenant, request)
+              is OidcAccountRequest -> OidcAccountStrategy.authenticate(tenant, request)
+              is KeycloakAccountRequest -> KeycloakAccountStrategy.authenticate(tenant, request)
+            }
+          }
+          .fold(
+              onSuccess = {
+                EventService.add(
+                    Event(
+                        action = EventType.Account.Login,
+                        tenant = tenant,
+                        originator = "wallet",
+                        account = it.id,
+                        wallet = null,
+                        data = AccountEventData(accountId = it.username)))
+                Result.success(
+                    AuthenticationResult(
+                        id = it.id, username = it.username, token = generateToken()))
+              },
+              onFailure = { Result.failure(it) })
+
+  fun getAccountWalletMappings(tenant: String, account: UUID) =
+      AccountWalletListing(
+          account,
+          wallets =
+              transaction {
+                AccountWalletMappings.innerJoin(Wallets)
+                    .selectAll()
+                    .where {
+                      (AccountWalletMappings.tenant eq tenant) and
+                          (AccountWalletMappings.accountId eq account)
+                    }
+                    .map {
+                      AccountWalletListing.WalletListing(
+                          id = it[AccountWalletMappings.wallet].value,
+                          name = it[Wallets.name],
+                          createdOn = it[Wallets.createdOn].toKotlinInstant(),
+                          addedOn = it[AccountWalletMappings.addedOn].toKotlinInstant(),
+                          permission = it[AccountWalletMappings.permissions])
+                    }
+              })
+
+  fun hasAccountEmail(tenant: String, email: String) = transaction {
+    Accounts.selectAll()
+        .where { (Accounts.tenant eq tenant) and (Accounts.email eq email) }
+        .count() > 0
+  }
+
+  fun hasAccountWeb3WalletAddress(address: String) = transaction {
+    Accounts.innerJoin(Web3Wallets).selectAll().where { Web3Wallets.address eq address }.count() > 0
+  }
+
+  fun hasAccountOidcId(oidcId: String): Boolean = transaction {
+    Accounts.crossJoin(OidcLogins) // TODO crossJoin
+        .selectAll()
+        .where {
+          (Accounts.tenant eq OidcLogins.tenant) and
+              (Accounts.id eq OidcLogins.accountId) and
+              (OidcLogins.oidcId eq oidcId)
         }
+        .count() > 0
+  }
 
-        val walletService = WalletServiceManager.getWalletService(tenant, registeredUserId, createdInitialWalletId)
-        EventService.add(
-            Event(
-                action = EventType.Account.Create,
-                tenant = tenant,
-                originator = "wallet",
-                account = registeredUserId,
-                wallet = createdInitialWalletId,
-                data = AccountEventData(accountId = request.name)
-            )
-        )
+  fun getAccountByWeb3WalletAddress(address: String) = transaction {
+    Accounts.innerJoin(Web3Wallets)
+        .selectAll()
+        .where { Web3Wallets.address eq address }
+        .map { Account(it) }
+  }
 
-        // Add default data:
-        val createdDid = walletService.createDid("key", mapOf("alias" to JsonPrimitive("Onboarding")))
-        walletService.setDefault(createdDid)
-    }.onFailure {
-        throw IllegalStateException("Could not register user: ${it.message}", it)
-    }
-
-    suspend fun authenticate(tenant: String, request: AccountRequest): Result<AuthenticationResult> = runCatching {
-        when (request) {
-            is EmailAccountRequest -> EmailAccountStrategy.authenticate(tenant, request)
-            is AddressAccountRequest -> Web3WalletAccountStrategy.authenticate(tenant, request)
-            is OidcAccountRequest -> OidcAccountStrategy.authenticate(tenant, request)
+  fun getAccountByOidcId(oidcId: String) = transaction {
+    Accounts.crossJoin(OidcLogins) // TODO crossJoin
+        .selectAll()
+        .where {
+          (Accounts.tenant eq OidcLogins.tenant) and
+              (Accounts.id eq OidcLogins.accountId) and
+              (OidcLogins.oidcId eq oidcId)
         }
-    }.fold(onSuccess = {
-        EventService.add(
-            Event(
-                action = EventType.Account.Login,
-                tenant = tenant,
-                originator = "wallet",
-                account = it.id,
-                wallet = null,
-                data = AccountEventData(accountId = it.username)
-            )
-        )
-        Result.success(
-            AuthenticationResult(
-                id = it.id,
-                username = it.username,
-                token = generateToken()
-            )
-        )
-    },
-        onFailure = { Result.failure(it) })
+        .map { Account(it) }
+        .firstOrNull()
+  }
 
-    fun getAccountWalletMappings(tenant: String, account: UUID) =
-        AccountWalletListing(account, wallets =
-        transaction {
-            AccountWalletMappings.innerJoin(Wallets)
-                .selectAll().where { (AccountWalletMappings.tenant eq tenant) and (AccountWalletMappings.accountId eq account) }
-                .map {
-                    AccountWalletListing.WalletListing(
-                        id = it[AccountWalletMappings.wallet].value,
-                        name = it[Wallets.name],
-                        createdOn = it[Wallets.createdOn].toKotlinInstant(),
-                        addedOn = it[AccountWalletMappings.addedOn].toKotlinInstant(),
-                        permission = it[AccountWalletMappings.permissions]
-                    )
-                }
-        }
-        )
-
-
-    fun hasAccountEmail(tenant: String, email: String) =
-        transaction { Accounts.selectAll().where { (Accounts.tenant eq tenant) and (Accounts.email eq email) }.count() > 0 }
-
-    fun hasAccountWeb3WalletAddress(address: String) = transaction {
-        Accounts.innerJoin(Web3Wallets)
-            .selectAll().where { Web3Wallets.address eq address }
-            .count() > 0
-    }
-
-
-    fun hasAccountOidcId(oidcId: String): Boolean = transaction {
-        Accounts.crossJoin(OidcLogins) // TODO crossJoin
-            .selectAll()
-            .where { (Accounts.tenant eq OidcLogins.tenant) and (Accounts.id eq OidcLogins.accountId) and (OidcLogins.oidcId eq oidcId) }
-            .count() > 0
-    }
-
-
-    fun getAccountByWeb3WalletAddress(address: String) =
-        transaction {
-            Accounts.innerJoin(Web3Wallets)
-                .selectAll().where { Web3Wallets.address eq address }
-                .map { Account(it) }
-        }
-
-    fun getAccountByOidcId(oidcId: String) =
-        transaction {
-            Accounts.crossJoin(OidcLogins) // TODO crossJoin
-                .selectAll()
-                .where { (Accounts.tenant eq OidcLogins.tenant) and (Accounts.id eq OidcLogins.accountId) and (OidcLogins.oidcId eq oidcId) }
-                .map { Account(it) }
-                .firstOrNull()
-        }
-
-    fun getNameFor(account: UUID) = Accounts.selectAll().where { Accounts.id eq account }.single()[Accounts.email]
-
+  fun getNameFor(account: UUID) =
+      Accounts.selectAll().where { Accounts.id eq account }.single()[Accounts.email]
 }
 
 @Serializable
@@ -159,12 +165,10 @@ data class RegistrationResult(
     val id: UUID,
 )
 
-data class AuthenticatedUser(
-    val id: UUID,
-    val username: String
-)
+data class AuthenticatedUser(val id: UUID, val username: String)
 
 abstract class AccountStrategy<in T : AccountRequest>(id: String) {
-    abstract suspend fun register(tenant: String, request: T): Result<RegistrationResult>
-    abstract suspend fun authenticate(tenant: String, request: T): AuthenticatedUser
+  abstract suspend fun register(tenant: String, request: T): Result<RegistrationResult>
+
+  abstract suspend fun authenticate(tenant: String, request: T): AuthenticatedUser
 }
