757fab6c01f0b8768143d30a450edb8da7581eff
mikeplotean <mike@walt.id>
2024-03-06 12:03:50 +0200

    code review: reformat, cleanup

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/WalletKitWalletService.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/WalletKitWalletService.kt
@@ -12,7 +12,6 @@ import id.walt.webwallet.service.dto.LinkedWalletDataTransferObject
 import id.walt.webwallet.service.dto.WalletDataTransferObject
 import id.walt.webwallet.service.events.EventLogFilter
 import id.walt.webwallet.service.events.EventLogFilterResult
-import id.walt.webwallet.service.issuers.IssuerDataTransferObject
 import id.walt.webwallet.service.keys.SingleKeyResponse
 import id.walt.webwallet.service.report.ReportRequestParameter
 import id.walt.webwallet.service.settings.WalletSetting
@@ -44,7 +43,8 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import java.net.URLDecoder
 import java.nio.charset.Charset
 
-class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : WalletService(tenant, accountId, walletId) {
+class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) :
+    WalletService(tenant, accountId, walletId) {
 
     private var token: Lazy<String> = lazy { runBlocking { auth() } }
 
@@ -70,8 +70,7 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
 
     private val userEmail: String by lazy {
         transaction {
-            Accounts.selectAll().where { Accounts.id eq this@WalletKitWalletService.walletId }
-                .single()[Accounts.email]
+            Accounts.selectAll().where { Accounts.id eq this@WalletKitWalletService.walletId }.single()[Accounts.email]
         } ?: throw IllegalArgumentException("No such account: ${this.walletId}")
     }
 
@@ -96,25 +95,22 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
         }.checkValid { runBlocking { authenticatedJsonDelete(path, block) } }
 
     private suspend inline fun <reified T> authenticatedJsonPost(
-        path: String,
-        body: T,
-        crossinline block: HttpRequestBuilder.() -> Unit = {}
-    ): HttpResponse =
-        http.post(path) {
-            bearerAuth(token.value)
-            contentType(ContentType.Application.Json)
-            setBody(body)
-            block.invoke(this)
-        }.checkValid {
-            runBlocking {
-                http.post(path) {
-                    bearerAuth(token.value)
-                    contentType(ContentType.Application.Json)
-                    setBody(body)
-                    block.invoke(this)
-                }
+        path: String, body: T, crossinline block: HttpRequestBuilder.() -> Unit = {}
+    ): HttpResponse = http.post(path) {
+        bearerAuth(token.value)
+        contentType(ContentType.Application.Json)
+        setBody(body)
+        block.invoke(this)
+    }.checkValid {
+        runBlocking {
+            http.post(path) {
+                bearerAuth(token.value)
+                contentType(ContentType.Application.Json)
+                setBody(body)
+                block.invoke(this)
             }
         }
+    }
 
     @Serializable
     data class AuthRequest(val id: String, val password: String)
@@ -124,12 +120,10 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
         token = lazy { runBlocking { auth() } }
     }
 
-    private suspend fun auth(): String =
-        (http.post("/api/auth/login") {
-            setBody(AuthRequest(userEmail, userEmail))
-            contentType(ContentType.Application.Json)
-        }.body<JsonObject>()["token"] ?: throw IllegalStateException("Could not login: $userEmail")
-                ).jsonPrimitive.content
+    private suspend fun auth(): String = (http.post("/api/auth/login") {
+        setBody(AuthRequest(userEmail, userEmail))
+        contentType(ContentType.Application.Json)
+    }.body<JsonObject>()["token"] ?: throw IllegalStateException("Could not login: $userEmail")).jsonPrimitive.content
 
     /* WalletCredentials */
 
@@ -158,31 +152,36 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
     override suspend fun restoreCredential(id: String): WalletCredential = throw NotImplementedError("")
 
     override suspend fun getCredential(credentialId: String) =
-        listCredentials(CredentialFilterObject.default).first { it.parsedDocument?.get("id")?.jsonPrimitive?.content == credentialId }.let {
-            WalletCredential(
-                wallet = walletId,
-                id = credentialId,
-                document = it.toString(),
-                disclosures = null,
-                addedOn = Instant.DISTANT_PAST,
-                manifest = it.manifest,
+        listCredentials(CredentialFilterObject.default).first { it.parsedDocument?.get("id")?.jsonPrimitive?.content == credentialId }
+            .let {
+                WalletCredential(
+                    wallet = walletId,
+                    id = credentialId,
+                    document = it.toString(),
+                    disclosures = null,
+                    addedOn = Instant.DISTANT_PAST,
+                    manifest = it.manifest,
 //                delete = false,
-                deletedOn = null,
-            )
-        }
+                    deletedOn = null,
+                )
+            }
 
-    override suspend fun attachCategory(credentialId: String, categories: List<String>): Boolean = throw NotImplementedError("")
+    override suspend fun attachCategory(credentialId: String, categories: List<String>): Boolean =
+        throw NotImplementedError("")
 
-    override suspend fun detachCategory(credentialId: String, categories: List<String>): Boolean = throw NotImplementedError("")
+    override suspend fun detachCategory(credentialId: String, categories: List<String>): Boolean =
+        throw NotImplementedError("")
+
     override suspend fun renameCategory(oldName: String, newName: String): Boolean = throw NotImplementedError("")
 
-    override suspend fun acceptCredential(parameter: CredentialRequestParameter): Boolean = throw NotImplementedError("")
+    override suspend fun acceptCredential(parameter: CredentialRequestParameter): Boolean =
+        throw NotImplementedError("")
 
-    override suspend fun rejectCredential(parameter: CredentialRequestParameter): Boolean = throw NotImplementedError("")
+    override suspend fun rejectCredential(parameter: CredentialRequestParameter): Boolean =
+        throw NotImplementedError("")
 
     override fun matchCredentialsByPresentationDefinition(presentationDefinition: PresentationDefinition): List<WalletCredential> =
-        throw NotImplementedError("")
-    /*prettyJson.encodeToString(*/
+        throw NotImplementedError("")/*prettyJson.encodeToString(*/
     //)
     /* override suspend fun getCredential(credentialId: String): String =
          authenticatedJsonGet("/api/wallet/credentials/$credentialId")
@@ -193,8 +192,7 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
         val params: MutableMap<String, MutableList<String>> = HashMap()
         val urlParts = url.split("\\?".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
 
-        if (urlParts.size <= 1)
-            return params
+        if (urlParts.size <= 1) return params
 
         val query = urlParts[1]
         for (param in query.split("&".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()) {
@@ -228,10 +226,7 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
 
     @Serializable
     data class SIOPv2Response(
-        val vp_token: String,
-        val presentation_submission: String,
-        val id_token: String?,
-        val state: String?
+        val vp_token: String, val presentation_submission: String, val id_token: String?, val state: String?
     )
 
     override suspend fun usePresentationRequest(parameter: PresentationRequestParameter): Result<String?> {
@@ -241,8 +236,7 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
             ?: throw IllegalArgumentException("Could not get redirect_uri from request!")
 
         val sessionId = authenticatedJsonPost(
-            "/api/wallet/presentation/startPresentation",
-            mapOf("oidcUri" to parameter.request)
+            "/api/wallet/presentation/startPresentation", mapOf("oidcUri" to parameter.request)
         ).bodyAsText()
 
         val presentableCredentials = authenticatedJsonGet("/api/wallet/presentation/continue") {
@@ -266,14 +260,12 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
         println("Presentation response: $presentationResponse")
 
         println("Posting to redirect uri: $redirectUri")
-        val redirectUriResp = http.submitForm(redirectUri,
-            formParameters = parameters {
-                append("vp_token", presentationResponse.vp_token)
-                append("presentation_submission", presentationResponse.presentation_submission)
-                append("id_token", presentationResponse.id_token.toString())
-                append("state", presentationResponse.state.toString())
-            }
-        )
+        val redirectUriResp = http.submitForm(redirectUri, formParameters = parameters {
+            append("vp_token", presentationResponse.vp_token)
+            append("presentation_submission", presentationResponse.presentation_submission)
+            append("id_token", presentationResponse.id_token.toString())
+            append("state", presentationResponse.state.toString())
+        })
 
         val redirect = redirectUriResp.headers[HttpHeaders.Location]
 
@@ -327,11 +319,9 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
         return authenticatedJsonPost("/api/wallet/did/create", createParams).bodyAsText()
     }
 
-    override suspend fun listDids() = authenticatedJsonGet("/api/wallet/did/list")
-        .body<List<WalletDid>>()
+    override suspend fun listDids() = authenticatedJsonGet("/api/wallet/did/list").body<List<WalletDid>>()
 
-    override suspend fun loadDid(did: String) = authenticatedJsonGet("/api/wallet/did/$did")
-        .body<JsonObject>()
+    override suspend fun loadDid(did: String) = authenticatedJsonGet("/api/wallet/did/$did").body<JsonObject>()
 
     override suspend fun deleteDid(did: String) =
         authenticatedJsonDelete("/api/wallet/did/delete/$did").status.isSuccess()
@@ -344,26 +334,25 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
     override suspend fun loadKey(alias: String) = authenticatedJsonGet("/api/wallet/keys/$alias").body<JsonObject>()
 
 
-    override suspend fun exportKey(alias: String, format: String, private: Boolean): String =
-        authenticatedJsonPost(
-            "/api/wallet/keys/export", mapOf(
-                "keyAlias" to JsonPrimitive(alias),
-                "format" to JsonPrimitive(format),
-                "exportPrivate" to JsonPrimitive(private)
-            ).toMutableMap()
-        )
-            .body<String>()
+    override suspend fun exportKey(alias: String, format: String, private: Boolean): String = authenticatedJsonPost(
+        "/api/wallet/keys/export", mapOf(
+            "keyAlias" to JsonPrimitive(alias),
+            "format" to JsonPrimitive(format),
+            "exportPrivate" to JsonPrimitive(private)
+        ).toMutableMap()
+    ).body<String>()
 
-    override suspend fun listKeys() = authenticatedJsonGet("/api/wallet/keys/list")
-        .body<JsonObject>()["list"]!!.jsonArray.map { Json.decodeFromJsonElement<SingleKeyResponse>(it) }
+    override suspend fun listKeys() =
+        authenticatedJsonGet("/api/wallet/keys/list").body<JsonObject>()["list"]!!.jsonArray.map {
+            Json.decodeFromJsonElement<SingleKeyResponse>(it)
+        }
 
     override suspend fun generateKey(type: String) =
         authenticatedJsonPost("/api/wallet/keys/generate", type).body<String>()
 
 
     override suspend fun importKey(jwkOrPem: String) =
-        authenticatedJsonPost("/api/wallet/keys/import", body = jwkOrPem)
-            .body<String>()
+        authenticatedJsonPost("/api/wallet/keys/import", body = jwkOrPem).body<String>()
 
     override suspend fun deleteKey(alias: String) =
         authenticatedJsonDelete("/api/wallet/keys/delete/$alias").status.isSuccess()
@@ -378,11 +367,9 @@ class WalletKitWalletService(tenant: String, accountId: UUID, walletId: UUID) : 
 //fun infoAboutOfferRequest
 
     override fun getHistory(limit: Int, offset: Long): List<WalletOperationHistory> = transaction {
-        WalletOperationHistories
-            .selectAll().where { (WalletOperationHistories.tenant eq tenant) and (WalletOperationHistories.accountId eq walletId) }
-            .orderBy(WalletOperationHistories.timestamp)
-            .limit(10)
-            .map { WalletOperationHistory(it) }
+        WalletOperationHistories.selectAll()
+            .where { (WalletOperationHistories.tenant eq tenant) and (WalletOperationHistories.accountId eq walletId) }
+            .orderBy(WalletOperationHistories.timestamp).limit(10).map { WalletOperationHistory(it) }
     }
 
     override suspend fun addOperationHistory(operationHistory: WalletOperationHistory) {
