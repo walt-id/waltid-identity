3d55bdbd241cbacf61ea82c551fd4f91e5770f95
mikeplotean <mike@walt.id>
2024-03-06 14:18:25 +0200

    updated credential-category db model

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/db/models/WalletCredentialCategoryMap.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/db/models/WalletCredentialCategoryMap.kt
@@ -6,8 +6,7 @@ import org.jetbrains.exposed.sql.Table
 object WalletCredentialCategoryMap : Table("credential_category") {
     //TODO: validate wallet is consistent for both category and credential
     val wallet = kotlinxUUID("wallet")
-    //TODO: what if the [id] column in credentials table gets updated?
-    val credential = varchar("credential", 256)
+    val credential = reference("credential", WalletCredentials.id)
     val category = reference("category", WalletCategory)
 
     override val primaryKey = PrimaryKey(wallet, credential, category)
