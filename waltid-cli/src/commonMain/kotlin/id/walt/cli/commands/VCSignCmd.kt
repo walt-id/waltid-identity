fd0e8ecf942ad566b57618ab6afba3425e287f5e
Alexandre Gomes <alegomes@gmail.com>
2024-03-13 18:20:25 -0300

    Start of VC command.

---
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/VCSignCmd.kt
@@ -0,0 +1,13 @@
+package id.walt.cli.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+class VCSignCmd : CliktCommand(
+    name = "sign",
+    help = "Signs a Verifiable Credential.",
+    printHelpOnEmptyArgs = true
+) {
+    override fun run() {
+
+    }
+}
