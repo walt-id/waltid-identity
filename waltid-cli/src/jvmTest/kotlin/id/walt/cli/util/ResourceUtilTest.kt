adf7d7eec685107b0ba6480a7b2ce31330885a5c
Alexandre Gomes <alegomes@gmail.com>
2024-03-13 15:57:21 -0300

    A unit test to reproduce an issue with path mounting in Windows environments

---
+++ waltid-cli/src/jvmTest/kotlin/id/walt/cli/util/ResourceUtilTest.kt
@@ -0,0 +1,22 @@
+package id.walt.cli.util
+
+import org.junit.jupiter.api.assertDoesNotThrow
+import java.nio.file.FileSystems
+import kotlin.io.path.exists
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class ResourceUtilTest {
+
+    @Test
+    fun `should return the right path if there is whitespaces in the folder name hirearchy`() {
+
+        val filename = "ed25519_key_sample1.json"
+        val filepath = getResourcePath(this, filename)
+
+        assertDoesNotThrow {
+            val path = FileSystems.getDefault().getPath(filepath)
+            assertTrue(path.exists())
+        }
+    }
+}
