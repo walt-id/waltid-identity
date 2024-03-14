2a1aa525d548633a76483cb478e8d90894dbba96
ahmed smiai <ahmedsmiai3@gmail.com>
2024-03-15 00:02:56 +0100

    windows path

--- waltid-cli/src/commonMain/kotlin/id/walt/cli/util/ResourceUtil.kt
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/util/ResourceUtil.kt
@@ -1,5 +1,6 @@
 package id.walt.cli.util
 
+import java.io.File
 import java.io.FileNotFoundException
 import java.net.URI
 import java.nio.file.Paths
@@ -9,18 +10,19 @@ fun getResourcePath(obj: Any, filename: String): String {
     // The returned URL has white spaces replaced by %20.
     // So, we need to decode it first to get rid of %20 from the file path
     val path = obj.javaClass.getClassLoader().getResource(filename)?.let {
-        URI(it.toString()).path
+        File(URI(it.toString())).path
     }
-
-    // If Windows path, escape backslash
-    val winPathRegex = """"^[a-zA-Z]:\\\\.*"""".toRegex()
-    if (winPathRegex.matches(path!!)) {
-        return path.replace("\\", "\\\\")
-    }
-
-    if (Paths.get(path).exists()) {
-        return path
-    } else {
-        throw FileNotFoundException(path)
-    }
+   return path!!.replace("\\", "/")
 }
+//    // If Windows path, escape backslash
+//    val winPathRegex = """"^[a-zA-Z]:\\\\.*"""".toRegex()
+//    if (winPathRegex.matches(path!!)) {
+//        return path.replace("\\", "\\\\")
+//    }
+//
+//    if (Paths.get(path).exists()) {
+//        return path
+//    } else {
+//        throw FileNotFoundException(path)
+//    }
+
