da568903b3a9b62b146965559b8cf6b4ff183a5a
mikeplotean <mike@walt.id>
2024-03-14 13:59:29 +0200

    refactor account strategy, store password for keycloak strategy

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/account/AccountsService.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/account/AccountsService.kt
@@ -173,9 +173,3 @@ data class RegistrationResult(
 )
 
 data class AuthenticatedUser(val id: UUID, val username: String)
-
-abstract class AccountStrategy<in T : AccountRequest>(id: String) {
-  abstract suspend fun register(tenant: String, request: T): Result<RegistrationResult>
-
-  abstract suspend fun authenticate(tenant: String, request: T): AuthenticatedUser
-}
