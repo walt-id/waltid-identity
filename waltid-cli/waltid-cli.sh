afef8126dd1428759e76b8f87f3c86ac815721ff
Alexandre Gomes <alegomes@gmail.com>
2024-03-14 17:15:56 -0300

    Fixing script name.

--- waltid-cli/waltid-cli.sh
+++ waltid-cli/waltid-cli.sh
@@ -3,8 +3,8 @@
 # Launcher script which builds the waltid-cli from source **once** and executes the command supplied
 # If changes are made to the source then waltid-cli has to manually be rebuild (e.g. through waltid-cli-development.sh)
 
-if [ -f 'build/install/waltid-cli/bin/waltid-cli' ]; then
-  build/install/waltid-cli/bin/waltid-cli "$@"
+if [ -f 'build/install/waltid/bin/waltid' ]; then
+  build/install/waltid/bin/waltid "$@"
 else
   echo "waltid-cli not yet build."
 
@@ -12,5 +12,5 @@ else
   ../gradlew installDist
 
   echo "Trying to run CLI command after build..."
-  build/install/waltid-cli/bin/waltid-cli "$@"
+  build/install/waltid/bin/waltid "$@"
 fi
