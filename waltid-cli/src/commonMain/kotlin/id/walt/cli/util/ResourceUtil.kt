251afbe00789cdca8402b5f960817a666fedc614
Alexandre Gomes <alegomes@gmail.com>
2024-03-14 18:08:14 -0300

    Trying to fix the path issue in Windows environments.

--- waltid-cli/src/commonMain/kotlin/id/walt/cli/util/ResourceUtil.kt
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/util/ResourceUtil.kt
@@ -2,13 +2,25 @@ package id.walt.cli.util
 
 import java.io.FileNotFoundException
 import java.net.URI
+import java.nio.file.Paths
+import kotlin.io.path.exists
 
 fun getResourcePath(obj: Any, filename: String): String {
     // The returned URL has white spaces replaced by %20.
     // So, we need to decode it first to get rid of %20 from the file path
-    obj.javaClass.getClassLoader().getResource(filename)?.let {
-        return URI(it.toString()).path
+    val path = obj.javaClass.getClassLoader().getResource(filename)?.let {
+        URI(it.toString()).path
     }
 
-    throw FileNotFoundException(filename)
+    // If Windows path, escape backslash
+    val winPathRegex = """"^[a-zA-Z]:\\\\.*"""".toRegex()
+    if (winPathRegex.matches(path!!)) {
+        return path.replace("\\", "\\\\")
+    }
+
+    if (Paths.get(path).exists()) {
+        return path
+    } else {
+        throw FileNotFoundException(path)
+    }
 }
