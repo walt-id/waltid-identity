8b9e19a6bcdd9bd5a943b4561c46eaa88250461d
waltkb <68587968+waltkb@users.noreply.github.com>
2024-03-14 21:38:26 +0100

    Remove unneeded dependency

--- waltid-web3login-microservice/build.gradle.kts
+++ waltid-web3login-microservice/build.gradle.kts
@@ -1,6 +1,5 @@
 val ktor_version: String by project
 val kotlin_version: String by project
-val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
@@ -31,7 +30,6 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-cio-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
