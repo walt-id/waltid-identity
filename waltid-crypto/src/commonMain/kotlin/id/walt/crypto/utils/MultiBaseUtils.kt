37298f312ae3dcd47a5df9455c216169ca2d3f39
waltkb <68587968+waltkb@users.noreply.github.com>
2024-03-18 11:18:43 +0100

    Switch to multiplatform multibase

--- waltid-crypto/src/commonMain/kotlin/id/walt/crypto/utils/MultiBaseUtils.kt
+++ waltid-crypto/src/commonMain/kotlin/id/walt/crypto/utils/MultiBaseUtils.kt
@@ -1,29 +1,52 @@
 package id.walt.crypto.utils
 
-expect object MultiBaseUtils {
 
-    fun convertRawKeyToMultiBase58Btc(key: ByteArray, code: UInt): String
-    /*fun convertRawKeyToMultiBase58Btc(key: ByteArray, code: UInt): String {
+
+object MultiBaseUtils {
+
+    fun convertRawKeyToMultiBase58Btc(key: ByteArray, code: UInt): String {
         val codeVarInt = MultiCodecUtils.UVarInt(code)
         val multicodecAndRawKey = ByteArray(key.size + codeVarInt.length)
         codeVarInt.bytes.copyInto(multicodecAndRawKey)
         key.copyInto(multicodecAndRawKey, codeVarInt.length)
         return encodeMultiBase58Btc(multicodecAndRawKey)
-    }*/
+    }
 
-    fun convertMultiBase58BtcToRawKey(mb: String): ByteArray
-
-    /*fun convertMultiBase58BtcToRawKey(mb: String): ByteArray {
+    fun convertMultiBase58BtcToRawKey(mb: String): ByteArray {
         val bytes = decodeMultiBase58Btc(mb)
         val code = MultiCodecUtils.UVarInt.fromBytes(bytes)
         return bytes.drop(code.length).toByteArray()
-    }*/
+    }
+
+    fun decodeMultiBase58Btc(mb: String): ByteArray = mb.substring(1).decodeBase58()
+
+    fun encodeMultiBase58Btc(byteArray: ByteArray): String = 'z' + byteArray.encodeToBase58String()
+}
+
+/*expect object MultiBaseUtils {
+
+    fun convertRawKeyToMultiBase58Btc(key: ByteArray, code: UInt): String
+    *//*fun convertRawKeyToMultiBase58Btc(key: ByteArray, code: UInt): String {
+        val codeVarInt = MultiCodecUtils.UVarInt(code)
+        val multicodecAndRawKey = ByteArray(key.size + codeVarInt.length)
+        codeVarInt.bytes.copyInto(multicodecAndRawKey)
+        key.copyInto(multicodecAndRawKey, codeVarInt.length)
+        return encodeMultiBase58Btc(multicodecAndRawKey)
+    }*//*
+
+    fun convertMultiBase58BtcToRawKey(mb: String): ByteArray
+
+    *//*fun convertMultiBase58BtcToRawKey(mb: String): ByteArray {
+        val bytes = decodeMultiBase58Btc(mb)
+        val code = MultiCodecUtils.UVarInt.fromBytes(bytes)
+        return bytes.drop(code.length).toByteArray()
+    }*//*
     fun decodeMultiBase58Btc(mb: String): ByteArray
 
     //    fun decodeMultiBase58Btc(mb: String): ByteArray = mb.substring(1).decodeBase58()
     fun encodeMultiBase58Btc(byteArray: ByteArray): String
 //    fun encodeMultiBase58Btc(byteArray: ByteArray): String = 'z' + byteArray.encodeToBase58String()
-}
+}*/
 
 /*
 For use with: implementation("com.github.multiformats:java-multibase:v1.1.1")
