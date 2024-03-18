f7de75d238808d766b4df4e8487659ea350c99fa
SuperBatata <khaled.battiche@esprit.tn>
2024-03-18 23:52:10 +0100

    refactor : remove keyid from config file

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/OciKeyConfig.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/OciKeyConfig.kt
@@ -8,7 +8,6 @@ data class OciKeyConfig(
     val userOcid: String,
     val fingerprint: String,
     val managementEndpoint: String,
-    val keyId: String,
     val cryptoEndpoint: String,
     val signingKeyPem: String? = null
 ) : WalletConfig
