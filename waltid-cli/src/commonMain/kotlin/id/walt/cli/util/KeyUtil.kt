ecb42dc7b33a00be10512b6b4d90879b372fa7fc
Alexandre Gomes <alegomes@gmail.com>
2024-03-19 18:04:37 -0300

    Adjustment to print on the Clikt terminal instead of the stdout.

--- waltid-cli/src/commonMain/kotlin/id/walt/cli/util/KeyUtil.kt
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/util/KeyUtil.kt
@@ -1,5 +1,6 @@
 package id.walt.cli.util
 
+import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.InvalidFileFormat
 import com.github.ajalt.mordant.rendering.TextStyles
 import id.walt.crypto.keys.KeyType
@@ -8,8 +9,9 @@ import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.text.ParseException
 
-class KeyUtil {
-    suspend fun getKey(keyFile: File): LocalKey {
+class KeyUtil(private val cmd: CliktCommand? = null) {
+
+    suspend fun getKey(keyFile: File?): LocalKey {
         if (keyFile != null) {
             try {
                 return LocalKey.importJWK(keyFile.readText()).getOrThrow()
@@ -22,10 +24,17 @@ class KeyUtil {
     }
 
     private suspend fun generateDefaultKey(): LocalKey {
-        println(TextStyles.dim("Key not provided. Let's generate a new one..."))
+        val msg1 = TextStyles.dim("Key not provided. Let's generate a new one...")
+        printit(msg1)
         val key = runBlocking { LocalKey.generate(KeyType.Ed25519) }
-        println(TextStyles.dim("Key generated with thumbprint ${key.getThumbprint()}"))
+        val msg2 = TextStyles.dim("Key generated with thumbprint ${key.getThumbprint()}")
+        printit(msg2)
 
         return key
+    }
+
+    private fun printit(msg: String) {
+        if (cmd != null) cmd.echo(msg)
+        else println(msg)
     }
 }
