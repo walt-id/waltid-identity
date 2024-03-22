81c6e02c76105c8f33c5895156aeb6f228024e23
Alexandre Gomes <alegomes@gmail.com>
2024-03-22 17:02:42 -0300

    Replacing LocalKey by JWKKey

--- waltid-cli/src/commonMain/kotlin/id/walt/cli/util/KeyUtil.kt
+++ waltid-cli/src/commonMain/kotlin/id/walt/cli/util/KeyUtil.kt
@@ -4,17 +4,17 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.InvalidFileFormat
 import com.github.ajalt.mordant.rendering.TextStyles
 import id.walt.crypto.keys.KeyType
-import id.walt.crypto.keys.LocalKey
+import id.walt.crypto.keys.jwk.JWKKey
 import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.text.ParseException
 
 class KeyUtil(private val cmd: CliktCommand? = null) {
 
-    suspend fun getKey(keyFile: File?): LocalKey {
+    suspend fun getKey(keyFile: File?): JWKKey {
         if (keyFile != null) {
             try {
-                return LocalKey.importJWK(keyFile.readText()).getOrThrow()
+                return JWKKey.importJWK(keyFile.readText()).getOrThrow()
             } catch (e: ParseException) {
                 throw InvalidFileFormat(keyFile.name, e.let { e.message!! })
             }
@@ -23,10 +23,10 @@ class KeyUtil(private val cmd: CliktCommand? = null) {
         }
     }
 
-    private suspend fun generateDefaultKey(): LocalKey {
+    private suspend fun generateDefaultKey(): JWKKey {
         val msg1 = TextStyles.dim("Key not provided. Let's generate a new one...")
         printit(msg1)
-        val key = runBlocking { LocalKey.generate(KeyType.Ed25519) }
+        val key = runBlocking { JWKKey.generate(KeyType.Ed25519) }
         val msg2 = TextStyles.dim("Key generated with thumbprint ${key.getThumbprint()}")
         printit(msg2)
 
