2fe5217fad909bea1507a756a2a6f813a3c80740
waltkb <68587968+waltkb@users.noreply.github.com>
2024-03-27 19:51:46 +0100

    Create parent directory for data file when sqlite is configured

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/DatabaseConfiguration.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/DatabaseConfiguration.kt
@@ -1,7 +1,14 @@
 package id.walt.webwallet.config
 
 import com.zaxxer.hikari.HikariDataSource
+import id.walt.webwallet.db.Db
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlin.io.path.Path
+import kotlin.io.path.createParentDirectories
+import kotlin.io.path.notExists
 
 data class DatabaseConfiguration(
     val database: String
@@ -15,4 +22,21 @@ data class DatasourceConfiguration(
 data class DatasourceJsonConfiguration(
     val hikariDataSource: JsonObject,
     val recreateDatabaseOnStart: Boolean = false
-) : WalletConfig
+) : WalletConfig {
+
+    companion object {
+        private val log = KotlinLogging.logger {  }
+    }
+
+    val jdbcUrl by lazy { hikariDataSource.jsonObject["jdbcUrl"]?.jsonPrimitive?.content }
+
+    init {
+        if (jdbcUrl?.startsWith(Db.SQLITE_PREFIX) == true) {
+            val path = Path(jdbcUrl!!.removePrefix(Db.SQLITE_PREFIX))
+            if (path.notExists()) {
+                log.info { "Creating directory for sqlite database: $path" }
+                path.createParentDirectories()
+            }
+        }
+    }
+}
