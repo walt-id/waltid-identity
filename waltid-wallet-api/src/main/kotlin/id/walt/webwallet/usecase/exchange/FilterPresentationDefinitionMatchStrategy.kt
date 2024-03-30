09886f8304ef1b876ec81a77c74944e9ea4c72ba
mikeplotean <mike@walt.id>
2024-03-30 20:22:26 +0200

    refactor: presentation definition filter parser

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/usecase/exchange/FilterPresentationDefinitionMatchStrategy.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/usecase/exchange/FilterPresentationDefinitionMatchStrategy.kt
@@ -7,10 +7,12 @@ import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
 
-class FilterPresentationDefinitionMatchStrategy : PresentationDefinitionMatchStrategy {
+class FilterPresentationDefinitionMatchStrategy(
+    private val filterParser: PresentationDefinitionFilterParser,
+) : PresentationDefinitionMatchStrategy {
     override fun match(
         credentials: List<WalletCredential>, presentationDefinition: PresentationDefinition
-    ): List<WalletCredential> = match(credentials, getFilters(presentationDefinition))
+    ): List<WalletCredential> = match(credentials, filterParser.parse(presentationDefinition))
 
     private fun match(
         credentialList: List<WalletCredential>, filters: List<List<TypeFilter>>
@@ -28,23 +30,4 @@ class FilterPresentationDefinitionMatchStrategy : PresentationDefinitionMatchStr
             }
         }
     }
-
-    private fun getFilters(presentationDefinition: PresentationDefinition): List<List<TypeFilter>> {
-        val filters = presentationDefinition.inputDescriptors.mapNotNull { inputDescriptor ->
-            inputDescriptor.constraints?.fields?.filter { field -> field.path.any { path -> path.contains("type") } }
-                ?.map {
-                    val path = it.path.first().removePrefix("$.")
-                    val filterType = it.filter?.get("type")?.jsonPrimitive?.content
-                    val filterPattern =
-                        it.filter?.get("pattern")?.jsonPrimitive?.content ?: throw IllegalArgumentException(
-                            "No filter pattern in presentation definition constraint"
-                        )
-
-                    TypeFilter(path, filterType, filterPattern)
-                }?.plus(inputDescriptor.schema?.map { schema -> TypeFilter("type", "string", schema.uri) } ?: listOf())
-        }
-        return filters
-    }
-
-    data class TypeFilter(val path: String, val type: String? = null, val pattern: String)
 }
