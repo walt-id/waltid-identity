8e655e8a3ffbfa4f5e8bd654cd6103c65c42741e
mikeplotean <mike@walt.id>
2024-04-02 23:03:20 +0300

    add credential status service factory discriminator

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/credentials/CredentialStatusServiceFactory.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/credentials/CredentialStatusServiceFactory.kt
@@ -1,9 +1,13 @@
 package id.walt.webwallet.service.credentials
 
+import id.walt.webwallet.service.credentials.status.CredentialStatusTypes
 import id.walt.webwallet.service.credentials.status.StatusListCredentialStatusService
 
 class CredentialStatusServiceFactory(
     private val statusListService: StatusListCredentialStatusService,
 ) {
-    fun new() = statusListService
+    fun new(type: String) = when (type) {
+        in CredentialStatusTypes.StatusList.type -> statusListService
+        else -> error("Not supported credential status type: $type")
+    }
 }
