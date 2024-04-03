a8b168ee628a3e92709e0ac68d409cbfb4e2a7e1
mikeplotean <mike@walt.id>
2024-04-03 18:46:56 +0300

    refactor: match presentation definition credentials use-case
    # Conflicts:
    #	waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/WalletServiceManager.kt
    
    # Conflicts:
    #	waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/WalletServiceManager.kt

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/usecase/exchange/MatchPresentationDefinitionCredentialsUseCase.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/usecase/exchange/MatchPresentationDefinitionCredentialsUseCase.kt
@@ -1,19 +1,16 @@
 package id.walt.webwallet.usecase.exchange
 
-import id.walt.oid4vc.data.dif.InputDescriptor
 import id.walt.oid4vc.data.dif.PresentationDefinition
 import id.walt.webwallet.db.models.WalletCredential
 import id.walt.webwallet.service.credentials.CredentialFilterObject
 import id.walt.webwallet.service.credentials.CredentialsService
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonPrimitive
 import kotlinx.uuid.UUID
 
 class MatchPresentationDefinitionCredentialsUseCase(
     private val credentialService: CredentialsService,
+    private vararg val matchStrategies: PresentationDefinitionMatchStrategy,
 ) {
     private val logger = KotlinLogging.logger { }
 
@@ -23,68 +20,23 @@ class MatchPresentationDefinitionCredentialsUseCase(
         val credentialList = credentialService.list(wallet, CredentialFilterObject.default)
         logger.debug { "WalletCredential list is: ${credentialList.map { it.parsedDocument?.get("type")!!.jsonArray }}" }
 
-        val filters = getFilters(presentationDefinition)
-        logger.debug { "Using filters: $filters" }
+//        val filters = getFilters(presentationDefinition)
+//        logger.debug { "Using filters: $filters" }
 
-        val matchedCredentials = when {
-            filters.isNotEmpty() -> matchFilter(credentialList, filters)
-            else -> matchInputDescriptor(credentialList, presentationDefinition.inputDescriptors)
+//        val matchedCredentials = when {
+//            filters.isNotEmpty() -> match(credentialList, filters)
+//            else -> match(credentialList, presentationDefinition.inputDescriptors)
+//        }
+        var matchedCredentials = emptyList<WalletCredential>()
+        run loop@{
+            matchStrategies.forEach {
+                matchedCredentials = it.match(credentialList, presentationDefinition)
+                if (matchedCredentials.isNotEmpty()) return@loop
+            }
         }
+
         logger.debug { "Matched credentials: $matchedCredentials" }
 
         return matchedCredentials//.ifEmpty { credentialList }
     }
-
-    private fun matchInputDescriptor(
-        credentialList: List<WalletCredential>, inputDescriptors: List<InputDescriptor>
-    ) = credentialList.filter { cred ->
-        inputDescriptors.any { desc ->
-            desc.name == cred.parsedDocument?.get("type")?.jsonArray?.last()?.jsonPrimitive?.content
-        }
-    }
-
-    private fun matchFilter(
-        credentialList: List<WalletCredential>,
-        filters: List<List<TypeFilter>>
-    ) = credentialList.filter { credential ->
-        filters.any { fields ->
-            fields.all { typeFilter ->
-                val credField = credential.parsedDocument!![typeFilter.path] ?: return@all false
-
-                when (credField) {
-                    is JsonPrimitive -> credField.jsonPrimitive.content == typeFilter.pattern
-                    is JsonArray ->
-                        credField.jsonArray.last().jsonPrimitive.content == typeFilter.pattern
-
-                    else -> false
-                }
-            }
-        }
-    }
-
-    private fun getFilters(presentationDefinition: PresentationDefinition): List<List<TypeFilter>> {
-        val filters =
-            presentationDefinition.inputDescriptors.mapNotNull { inputDescriptor ->
-                inputDescriptor.constraints
-                    ?.fields
-                    ?.filter { field -> field.path.any { path -> path.contains("type") } }
-                    ?.map {
-                        val path = it.path.first().removePrefix("$.")
-                        val filterType = it.filter?.get("type")?.jsonPrimitive?.content
-                        val filterPattern =
-                            it.filter?.get("pattern")?.jsonPrimitive?.content
-                                ?: throw IllegalArgumentException(
-                                    "No filter pattern in presentation definition constraint"
-                                )
-
-                        TypeFilter(path, filterType, filterPattern)
-                    }
-                    ?.plus(
-                        inputDescriptor.schema?.map { schema -> TypeFilter("type", "string", schema.uri) }
-                            ?: listOf())
-            }
-        return filters
-    }
-
-    data class TypeFilter(val path: String, val type: String? = null, val pattern: String)
 }
