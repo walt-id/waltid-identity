b6bf3ea2b081a6d096edb0bf9823cc7f7f02cc77
waltkb <68587968+waltkb@users.noreply.github.com>
2024-04-04 10:08:55 +0200

    Replace entrypoint with direct command for issuer & verifier

--- waltid-verifier-api/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# Custom entrypoint that prepares permissions for non-root users
-
-# Give non-root user permission to mounted data directory, config directory, ...
-chown -R waltid:waltid /waltid-verifier-api
-
-# Execute as non-root user
-exec runuser -u waltid /waltid-verifier-api/bin/waltid-verifier-api -- "$@"
