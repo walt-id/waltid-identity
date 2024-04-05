ca6f6905512dec7206a5baad11d49e34e6a3ad51
chsavvaidis <ch.savvaidis@gmail.com>
2024-04-05 14:14:37 +0300

    fix: update version dependency

--- waltid-mdoc-credentials/build.gradle.kts
+++ waltid-mdoc-credentials/build.gradle.kts
@@ -44,7 +44,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
                 //implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.5.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
