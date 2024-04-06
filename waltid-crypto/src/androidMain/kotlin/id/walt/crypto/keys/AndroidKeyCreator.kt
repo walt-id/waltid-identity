b63ef590bb8d82d66fababe17b61fdc45905e199
sunrisekcdeveloper <nadinebalanco@gmail.com>
2024-04-06 17:47:11 +0100

    update: function signature to replace removed LocalKeyMetadata

--- waltid-crypto/src/androidMain/kotlin/id/walt/crypto/keys/AndroidKeyCreator.kt
+++ waltid-crypto/src/androidMain/kotlin/id/walt/crypto/keys/AndroidKeyCreator.kt
@@ -1,5 +1,5 @@
 package id.walt.crypto.keys
 
 interface AndroidKeyCreator {
-    suspend fun generate(type: KeyType, metadata: LocalKeyMetadata = LocalKeyMetadata()): AndroidKey
+    suspend fun generate(type: KeyType, metadata: JwkKeyMeta? = null): AndroidKey
 }
