410139e4cdbcbcfbdeb338c9e6fdb9a78965e6c7
mikeplotean <mike@walt.id>
2024-04-15 17:07:47 +0300

    fix: jwt payload reference, bitstring value url-decode, missing credential type in credential validator, endpoint provider request content negotiation, status endpoint return type

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/JwsDecoder.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/JwsDecoder.kt
@@ -1,7 +1,11 @@
 package id.walt.webwallet.service
 
 import id.walt.crypto.utils.JwsUtils.decodeJws
+import id.walt.webwallet.utils.JsonUtils
+import kotlinx.serialization.json.jsonObject
 
 class JwsDecoder {
-    fun payload(data: String) = data.decodeJws(allowMissingSignature = true).payload
+    fun payload(data: String) = data.decodeJws(allowMissingSignature = true).payload.let {
+        JsonUtils.tryGetData(it, "vc")?.jsonObject ?: it
+    }
 }
