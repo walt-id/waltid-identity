7c60dac6bb795fa72c5250cd2a6996c5c739b8c9
mikeplotean <mike@walt.id>
2024-04-17 19:32:51 +0300

    match presentation definition filter pattern

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/usecase/exchange/strategies/BaseFilterPresentationDefinitionMatchStrategy.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/usecase/exchange/strategies/BaseFilterPresentationDefinitionMatchStrategy.kt
@@ -2,20 +2,21 @@ package id.walt.webwallet.usecase.exchange.strategies
 
 import id.walt.webwallet.db.models.WalletCredential
 import id.walt.webwallet.usecase.exchange.TypeFilter
+import id.walt.webwallet.utils.JsonUtils
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
 
 abstract class BaseFilterPresentationDefinitionMatchStrategy<T> : PresentationDefinitionMatchStrategy<T> {
-
     protected fun isMatching(credential: WalletCredential, fields: List<TypeFilter>) = fields.all { typeFilter ->
-        val credField = credential.parsedDocument!![typeFilter.path] ?: return@all false
+        val credField = JsonUtils.tryGetData(credential.parsedDocument!!, typeFilter.path) ?: return@all false
         when (credField) {
-            is JsonPrimitive -> credField.jsonPrimitive.content == typeFilter.pattern
-            is JsonArray -> credField.jsonArray.last().jsonPrimitive.content == typeFilter.pattern
-
-            else -> false
+            is JsonPrimitive -> credField.jsonPrimitive.content
+            is JsonArray -> credField.jsonArray.last().jsonPrimitive.content
+            else -> ""
+        }.let {
+            typeFilter.pattern.toRegex().matches(it)
         }
     }
 }
