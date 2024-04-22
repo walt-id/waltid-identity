baba8b57272496314885c5ea38f5f3718af4bd96
Severin Stampler <severin@walt.id>
2024-04-22 19:43:26 +0200

    implementation of accreditation client, to authorise and get credentials from trusted issuer (like VerifiableAuthorisationToOnboard)

--- waltid-did/src/jvmTest/kotlin/DidCreationTest.kt
+++ waltid-did/src/jvmTest/kotlin/DidCreationTest.kt
@@ -12,6 +12,7 @@ import id.walt.did.dids.registrar.local.web.DidWebRegistrar
 import id.walt.did.dids.resolver.DidResolver
 import id.walt.did.dids.resolver.local.DidEbsiResolver
 import id.walt.did.utils.randomUUID
+import id.walt.ebsi.accreditation.AccreditationClient
 import id.walt.ebsi.did.DidEbsiService
 import id.walt.ebsi.eth.TransactionService
 import id.walt.ebsi.rpc.EbsiRpcRequests
@@ -118,7 +119,7 @@ class DidCreationTest {
     }
 
     val CLIENT_MOCK_PORT = 5000
-    val CLIENT_MOCK_URL = "https://97e2-62-178-27-231.ngrok-free.app/client-mock"//"http://192.168.0.122:5000/client-mock"
+    val CLIENT_MOCK_URL = "https://21b4-62-178-27-231.ngrok-free.app/client-mock"//"http://192.168.0.122:5000/client-mock"
     val CLIENT_MAIN_KEY = runBlocking { JWKKey.generate(KeyType.secp256k1) }
     val CLIENT_VCSIGN_KEY = runBlocking { JWKKey.generate(KeyType.secp256r1) }
     fun startClientMockServer() {
@@ -149,115 +150,12 @@ class DidCreationTest {
         startClientMockServer()
         val did = DidEbsiService.generateRandomDid()
         val taoIssuer = "https://api-conformance.ebsi.eu/conformance/v3/issuer-mock"
-        //val taoIssuer = "http://localhost:3000/conformance/v3/issuer-mock"
-        val issuerMetadata = OpenID4VCI.resolveCIProviderMetadata(taoIssuer)
-        assertEquals(taoIssuer, issuerMetadata.credentialIssuer)
-        val authMetadata = OpenID4VCI.resolveAuthProviderMetadata(issuerMetadata.authorizationServer!!)
-        assertEquals(issuerMetadata.authorizationServer, authMetadata.issuer)
 
-        val clientId = CLIENT_MOCK_URL
-        val codeChallenge = randomUUID()
-        val authReq = AuthorizationRequest(
-            responseType = setOf(ResponseType.Code), clientId,
-            scope = setOf("openid"),
-            redirectUri = CLIENT_MOCK_URL,
-            authorizationDetails = listOf(
-                AuthorizationDetails.fromLegacyCredentialParameters(
-                    CredentialFormat.jwt_vc,
-                    issuerMetadata.credentialsSupported!!.first { it.types?.contains("VerifiableAuthorisationToOnboard") == true }.types!!,
-                    locations = listOf(issuerMetadata.credentialIssuer!!)
-                    )
-            ),
-            clientMetadata = OpenIDClientMetadata(customParameters = mapOf(
-                "jwks_uri" to JsonPrimitive("$CLIENT_MOCK_URL/jwks"),
-                "authorization_endpoint" to JsonPrimitive("openid:")
-            )),
-            codeChallenge = codeChallenge, codeChallengeMethod = "plain"
-        )
+        // TODO: reorganize this code into DidEbsiRegistrar, accreditation client parameters needed in did registration options
+        val accreditationClient = AccreditationClient(CLIENT_MOCK_URL, did, CLIENT_VCSIGN_KEY, taoIssuer)
+        val authToOnboardRes = accreditationClient.getAuthorisationToOnboard()
 
-        val signedRequestObject = CLIENT_VCSIGN_KEY.signJws(
-            authReq.toRequestObjectPayload(clientId, authMetadata.issuer!!).toString().toByteArray(),
-            mapOf("kid" to CLIENT_VCSIGN_KEY.getKeyId())
-        )
-
-        val httpResp = http.get(authMetadata.authorizationEndpoint!!) {
-            url { parameters.appendAll(parametersOf(authReq.toHttpParametersWithRequestObject(signedRequestObject)))
-            println(buildString())
-            }
-        }
-        val idTokenReqUrl = httpResp.headers["location"]!!
-        val idTokenReq = AuthorizationRequest.fromHttpParametersAuto(Url(idTokenReqUrl).parameters.toMap())
-        assertEquals(ResponseMode.direct_post, idTokenReq.responseMode)
-        assertContains(idTokenReq.scope, "openid")
-        assertContains(idTokenReq.responseType, ResponseType.IdToken)
-        assertNotNull(idTokenReq.redirectUri)
-
-        val idToken = CLIENT_VCSIGN_KEY.signJws(
-            buildJsonObject {
-                put("iss", did)
-                put("sub", did)
-                put("aud", authMetadata.issuer)
-                put("exp", Instant.DISTANT_FUTURE.epochSeconds)
-                put("iat", Clock.System.now().epochSeconds)
-                put("state", idTokenReq.state)
-                put("nonce", idTokenReq.nonce)
-            }.toString().toByteArray(),
-            mapOf("kid" to "$did#${CLIENT_VCSIGN_KEY.getKeyId()}", "typ" to "JWT")
-        )
-
-        val idTokenResp = TokenResponse.success(idToken = idToken, state = idTokenReq.state)
-        val authResp = http.submitForm(idTokenReq.redirectUri!!, parametersOf(idTokenResp.toHttpParameters()))
-        assertEquals(302, authResp.status.value)
-        val codeResp = AuthorizationCodeResponse.fromHttpQueryString(Url(authResp.headers["Location"]!!).encodedQuery)
-        assertNotNull(codeResp.code)
-
-        val tokenReq = TokenRequest(
-            GrantType.authorization_code,
-            CLIENT_MOCK_URL, CLIENT_MOCK_URL,
-            codeResp.code,
-            codeVerifier = codeChallenge,
-            customParameters = mapOf(
-                "client_assertion" to listOf(CLIENT_VCSIGN_KEY.signJws(
-                    buildJsonObject {
-                        put("iss", CLIENT_MOCK_URL)
-                        put("sub", CLIENT_MOCK_URL)
-                        put("aud", authMetadata.issuer)
-                        put("jti", randomUUID())
-                        put("exp", Instant.DISTANT_FUTURE.epochSeconds)
-                        put("iat", Clock.System.now().epochSeconds)
-                    }.toString().toByteArray(),
-                    mapOf("kid" to CLIENT_VCSIGN_KEY.getKeyId(), "typ" to "JWT")),
-                ),
-                "client_assertion_type" to listOf("urn:ietf:params:oauth:client-assertion-type:jwt-bearer")
-            )
-        )
-        val tokenRespRaw = http.submitForm(authMetadata.tokenEndpoint!!, parametersOf(tokenReq.toHttpParameters()))
-        assertEquals(HttpStatusCode.OK, tokenRespRaw.status)
-        val tokenResp = TokenResponse.fromJSONString(tokenRespRaw.bodyAsText())
-        assertNotNull(tokenResp.accessToken)
-        println(tokenResp.accessToken)
-
-        val jwtProof = ProofOfPossession.JWTProofBuilder(
-            issuerMetadata.credentialIssuer!!,
-            clientId, tokenResp.cNonce,
-            "$did#${CLIENT_VCSIGN_KEY.getKeyId()}"
-        ).build(CLIENT_VCSIGN_KEY)
-        println(jwtProof.jwt)
-
-        val credReq = CredentialRequest(
-            CredentialFormat.jwt_vc,
-            proof = jwtProof,
-            types = issuerMetadata.credentialsSupported!!.first { it.types?.contains("VerifiableAuthorisationToOnboard") == true }.types!!
-        )
-        val credRespRaw = http.post(issuerMetadata.credentialEndpoint!!) {
-            bearerAuth(tokenResp.accessToken!!)
-            contentType(ContentType.Application.Json)
-            setBody(credReq.toJSONString())
-        }
-        println(credRespRaw.bodyAsText())
-        assertEquals(HttpStatusCode.OK, credRespRaw.status)
-        val credResp = CredentialResponse.fromJSONString(credRespRaw.bodyAsText())
-        println(credResp.credential)
+        println(authToOnboardRes.credential)
 
         val presDefResp = http.get("https://api-conformance.ebsi.eu/authorisation/v4/presentation-definitions") {
             parameter("scope", "openid didr_invite")
@@ -269,7 +167,7 @@ class DidCreationTest {
         //    clientId = CLIENT_MOCK_URL, clientIdScheme = ClientIdScheme.RedirectUri, )
 
         val didResult = DidEbsiRegistrar().registerByKey(CLIENT_MAIN_KEY, DidEbsiCreateOptions(
-            5, credResp.credential!!, tokenResp.cNonce
+            5, authToOnboardRes.credential!!, authToOnboardRes.cNonce
         ))
         val didEbsiDoc = didResult.didDocument.let { Json.decodeFromJsonElement<DidEbsiDocument>(it.toJsonObject()) }
         assertEquals("$did#${CLIENT_MAIN_KEY.getKeyId()}", didEbsiDoc.verificationMethod!!.first().id)
---
+++ waltid-ebsi/src/commonMain/kotlin/id/walt/ebsi/accreditation/AccreditationClient.kt
@@ -0,0 +1,152 @@
+package id.walt.ebsi.accreditation
+
+import id.walt.crypto.keys.Key
+import id.walt.oid4vc.OpenID4VCI
+import id.walt.oid4vc.data.*
+import id.walt.oid4vc.requests.AuthorizationRequest
+import id.walt.oid4vc.requests.CredentialRequest
+import id.walt.oid4vc.requests.TokenRequest
+import id.walt.oid4vc.responses.AuthorizationCodeResponse
+import id.walt.oid4vc.responses.CredentialResponse
+import id.walt.oid4vc.responses.TokenResponse
+import id.walt.oid4vc.util.http
+import id.walt.oid4vc.util.randomUUID
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.util.*
+import io.ktor.utils.io.core.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+
+class AccreditationClient(
+  val clientUri: String,
+  val did: String,
+  val authenticationKey: Key,
+  val trustedIssuer: String,
+  val clientJwksUri: String = "$clientUri/jwks",
+  val clientRedirectUri: String = "$clientUri/code-cb",
+  val clientId: String = clientUri
+) {
+
+  protected suspend fun getIdTokenFor(idTokenRequest: AuthorizationRequest, authorisationServer: String): String {
+    return authenticationKey.signJws(
+      buildJsonObject {
+        put("iss", did)
+        put("sub", did)
+        put("aud", authorisationServer)
+        put("exp", Instant.DISTANT_FUTURE.epochSeconds)
+        put("iat", Clock.System.now().epochSeconds)
+        put("state", idTokenRequest.state)
+        put("nonce", idTokenRequest.nonce)
+      }.toString().toByteArray(),
+      mapOf("kid" to "$did#${authenticationKey.getKeyId()}", "typ" to "JWT")
+    )
+  }
+
+  suspend fun getTrustedCredential(credentialType: String): CredentialResponse {
+    // https://hub.ebsi.eu/conformance/build-solutions/accredit-and-authorise-functional-flows
+
+    // #### Discovery ####
+    val ciMetadata = OpenID4VCI.resolveCIProviderMetadata(trustedIssuer)
+    val authorisationServer = ciMetadata.authorizationServer ?: throw Exception("No authorization server found for given trusted issuer")
+    val credentialMetadata = ciMetadata.credentialsSupported?.find { it.types?.last()?.equals(credentialType) ?: false } ?: throw Exception("Credential type not supported by given trusted issuer")
+    val authMetadata = OpenID4VCI.resolveAuthProviderMetadata(authorisationServer)
+
+    // #### Authorise and Authenticate ####
+    val codeChallenge = randomUUID()
+    val authReq = AuthorizationRequest(
+      responseType = setOf(ResponseType.Code), clientId,
+      scope = setOf("openid"),
+      redirectUri = clientRedirectUri,
+      authorizationDetails = listOf(
+        AuthorizationDetails.fromLegacyCredentialParameters(
+          CredentialFormat.jwt_vc,
+          credentialMetadata.types!!,
+          locations = listOf(ciMetadata.credentialIssuer!!)
+        )
+      ),
+      clientMetadata = OpenIDClientMetadata(customParameters = mapOf(
+        "jwks_uri" to JsonPrimitive(clientJwksUri),
+        "authorization_endpoint" to JsonPrimitive("openid:")
+      )),
+      codeChallenge = codeChallenge, codeChallengeMethod = "plain"
+    )
+
+    val signedRequestObject = authenticationKey.signJws(
+      authReq.toRequestObjectPayload(clientId, authMetadata.issuer!!).toString().toByteArray(),
+      mapOf("kid" to authenticationKey.getKeyId())
+    )
+
+    val httpResp = http.get(authMetadata.authorizationEndpoint!!) {
+      url { parameters.appendAll(parametersOf(authReq.toHttpParametersWithRequestObject(signedRequestObject)))
+        println(buildString())
+      }
+    }
+    val idTokenReqUrl = httpResp.headers["location"]!!
+    val idTokenReq = AuthorizationRequest.fromHttpParametersAuto(Url(idTokenReqUrl).parameters.toMap())
+
+    if(ResponseMode.direct_post != idTokenReq.responseMode || !idTokenReq.scope.contains("openid") ||
+        !idTokenReq.responseType.contains(ResponseType.IdToken) || idTokenReq.redirectUri.isNullOrEmpty()) {
+      throw Exception("Invalid id_token request received")
+    }
+
+    val idToken = getIdTokenFor(idTokenReq, authorisationServer)
+    val authResp = http.submitForm(idTokenReq.redirectUri!!, parametersOf(
+      TokenResponse.success(idToken = idToken, state = idTokenReq.state).toHttpParameters()))
+    if(authResp.status.value != 302) throw Exception("Invalid auth response status")
+    val codeResp = AuthorizationCodeResponse.fromHttpQueryString(Url(authResp.headers["Location"]!!).encodedQuery)
+
+    val tokenReq = TokenRequest(
+      GrantType.authorization_code,
+      clientId, clientRedirectUri,
+      codeResp.code,
+      codeVerifier = codeChallenge,
+      customParameters = mapOf(
+        "client_assertion" to listOf(authenticationKey.signJws(
+          buildJsonObject {
+            put("iss", clientUri)
+            put("sub", clientUri)
+            put("aud", authMetadata.issuer)
+            put("jti", randomUUID())
+            put("exp", Instant.DISTANT_FUTURE.epochSeconds)
+            put("iat", Clock.System.now().epochSeconds)
+          }.toString().toByteArray(),
+          mapOf("kid" to authenticationKey.getKeyId(), "typ" to "JWT")),
+        ),
+        "client_assertion_type" to listOf("urn:ietf:params:oauth:client-assertion-type:jwt-bearer")
+      )
+    )
+    val tokenRespRaw = http.submitForm(authMetadata.tokenEndpoint!!, parametersOf(tokenReq.toHttpParameters()))
+    if(tokenRespRaw.status != HttpStatusCode.OK) throw Exception("Invalid token response status")
+    val tokenResp = TokenResponse.fromJSONString(tokenRespRaw.bodyAsText())
+    val accessToken = tokenResp.accessToken ?: throw Exception("No access_token received")
+
+    val jwtProof = ProofOfPossession.JWTProofBuilder(
+      ciMetadata.credentialIssuer!!,
+      clientId, tokenResp.cNonce,
+      "$did#${authenticationKey.getKeyId()}"
+    ).build(authenticationKey)
+
+    // #### Credential Issuance ####
+    val credReq = CredentialRequest(
+      CredentialFormat.jwt_vc,
+      proof = jwtProof,
+      types = credentialMetadata.types!!
+    )
+    val credRespRaw = http.post(ciMetadata.credentialEndpoint!!) {
+      bearerAuth(accessToken!!)
+      contentType(ContentType.Application.Json)
+      setBody(credReq.toJSONString())
+    }
+    println(credRespRaw.bodyAsText())
+    if(credRespRaw.status != HttpStatusCode.OK) throw Exception("Invalid credential response status")
+    return CredentialResponse.fromJSONString(credRespRaw.bodyAsText())
+  }
+
+  suspend fun getAuthorisationToOnboard() = getTrustedCredential("VerifiableAuthorisationToOnboard")
+}
