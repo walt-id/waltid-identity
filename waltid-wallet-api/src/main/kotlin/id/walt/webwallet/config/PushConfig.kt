39e3eb0f2e52e82867ca08f7a7c349cbf22f0435
mikeplotean <mike@walt.id>
2024-04-26 22:36:30 +0300

    add push-config serializer

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/PushConfig.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/PushConfig.kt
@@ -1,8 +1,25 @@
 package id.walt.webwallet.config
 
 import com.sksamuel.hoplite.Masked
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
-//@Serializable
-data class PushConfig(val pushPublicKey: String, val pushPrivateKey: Masked, val pushSubject: String) : WalletConfig()
+@Serializable
+data class PushConfig(
+    val pushPublicKey: String,
+    @Serializable(with = MaskedSerializer::class) val pushPrivateKey: Masked,
+    val pushSubject: String
+) : WalletConfig()
 
+object MaskedSerializer : KSerializer<Masked> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("pushPrivateKey", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: Masked) = encoder.encodeString(value.value)
+
+    override fun deserialize(decoder: Decoder): Masked = Masked(decoder.decodeString())
+}
