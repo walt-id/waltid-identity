885c4026a378c62fa5dece7974403eb5bcbcce02
sunrisekcdeveloper <nadinebalanco@gmail.com>
2024-05-06 16:30:30 +0100

    add: missing actual implementations in android for OCIKey

---
+++ waltid-crypto/src/androidMain/kotlin/id/walt/crypto/keys/oci/OCIsdkMetadata.android.kt
@@ -0,0 +1,8 @@
+package id.walt.crypto.keys.oci
+
+actual class OCIsdkMetadata actual constructor(vaultId: String, compartmentId: String) {
+    actual val vaultId: String
+        get() = TODO("Not yet implemented")
+    actual val compartmentId: String
+        get() = TODO("Not yet implemented")
+}
