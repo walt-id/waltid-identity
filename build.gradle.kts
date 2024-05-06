dac5d292ab6b9a0a04e0f1897b26a61c74d6d06b
sunrisekcdeveloper <nadinebalanco@gmail.com>
2024-05-06 16:27:59 +0100

    Revert "Revert "Android Sample App""
    This reverts commit 95c148ea

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,16 +5,21 @@ allprojects {
     version = "1.0.0-SNAPSHOT"
 
     repositories {
+        google()
         mavenCentral()
+        maven(url = "https://maven.walt.id/repository/waltid/")
+        maven(url = "https://jitpack.io")
     }
 }
 
 plugins {
     val kotlinVersion = "1.9.23"
+    id("com.android.application") version "8.1.2" apply false
     kotlin("multiplatform") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
     id("com.github.ben-manes.versions") version "0.49.0" apply false
     kotlin("jvm") version kotlinVersion
+    id("com.android.library") version "8.1.2" apply false
 }
 dependencies {
     implementation(kotlin("stdlib"))
