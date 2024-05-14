1b4a28d43c8a5b31bb79105e4648f928104b06e1
waltkb <68587968+waltkb@users.noreply.github.com>
2024-05-15 01:18:52 +0200

    Update waltid-android to use waltid-crypto-android and organize dependencies

--- waltid-android/build.gradle.kts
+++ waltid-android/build.gradle.kts
@@ -43,38 +43,50 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
         }
     }
 }
 
 dependencies {
-    api(project(":waltid-crypto"))
+    // walt.id
+    api(project(":waltid-crypto-android"))
+
     api(project(":waltid-did"))
     api(project(":waltid-verifiable-credentials"))
     api(project(":waltid-sdjwt"))
 
-    implementation("com.nimbusds:nimbus-jose-jwt:9.37.3")
-
-    implementation("androidx.biometric:biometric:1.2.0-alpha05")
-    implementation("androidx.biometric:biometric-ktx:1.2.0-alpha05")
-
+    // JSON
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
+    // -- Android --
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
+    implementation("androidx.biometric:biometric-ktx:1.2.0-alpha05")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose")
+
+    // Testing
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Debug
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
