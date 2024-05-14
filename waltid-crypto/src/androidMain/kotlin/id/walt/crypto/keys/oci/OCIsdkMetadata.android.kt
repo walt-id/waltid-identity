af174d8193a3acfdb69019d7e2239c77964d9027
waltkb <68587968+waltkb@users.noreply.github.com>
2024-05-15 01:07:29 +0200

    Remove Android based keys from waltid-crypto

--- waltid-crypto/src/androidMain/kotlin/id/walt/crypto/keys/oci/OCIsdkMetadata.android.kt
+++
@@ -1,8 +0,0 @@
-package id.walt.crypto.keys.oci
-
-actual class OCIsdkMetadata actual constructor(vaultId: String, compartmentId: String) {
-    actual val vaultId: String
-        get() = TODO("Not yet implemented")
-    actual val compartmentId: String
-        get() = TODO("Not yet implemented")
-}
--- waltid-crypto/src/androidMain/kotlin/id/walt/crypto/utils/CryptoUtil.android.kt
+++
@@ -1,5 +0,0 @@
-package id.walt.crypto.utils
-
-actual fun sha256WithRsa(privateKeyAsPem: String, data: ByteArray): ByteArray {
-    TODO("Not yet implemented")
-}
