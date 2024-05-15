5bb979d42e65ad6e7e247f21d03538fcfa31aea3
Severin Stampler <severin@walt.id>
2024-05-15 16:34:30 +0200

    fix random session ID always returning the same value (because the generated random number always returned 'Infinity')

--- waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/util/ShortIdUtils.kt
+++ waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/util/ShortIdUtils.kt
@@ -9,7 +9,7 @@ object ShortIdUtils {
     fun randomSessionId(): String {
         return squids.encode(
             listOf(
-                (SecureRandom.nextDouble(0.0, Double.MAX_VALUE) * SecureRandom.nextLong(0, Long.MAX_VALUE)).toLong()
+                SecureRandom.nextLong(0, Long.MAX_VALUE)
             )
         )
     }
