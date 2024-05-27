304d03a232564d30f01391c84c4d2970b580dddc
Severin Stampler <severin@walt.id>
2024-05-27 17:34:07 +0200

    set random default request signing key

--- waltid-verifier-api/src/main/kotlin/id/walt/verifier/oidc/RequestSigningCryptoProvider.kt
+++ waltid-verifier-api/src/main/kotlin/id/walt/verifier/oidc/RequestSigningCryptoProvider.kt
@@ -3,42 +3,42 @@ package id.walt.verifier.oidc
 import com.nimbusds.jose.JOSEObjectType
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.JWSHeader
-import com.nimbusds.jose.JWSSigner
 import com.nimbusds.jose.crypto.ECDSASigner
-import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jose.jwk.Curve
 import com.nimbusds.jose.jwk.ECKey
-import com.nimbusds.jose.jwk.RSAKey
-import com.nimbusds.jose.produce.JWSSignerFactory
+import com.nimbusds.jose.jwk.KeyUse
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator
 import com.nimbusds.jose.util.Base64
 import com.nimbusds.jose.util.X509CertChainUtils
 import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.SignedJWT
-import id.walt.crypto.keys.jwk.JWKKey
 import id.walt.sdjwt.JWTCryptoProvider
 import id.walt.sdjwt.JwtVerificationResult
-import id.walt.sdjwt.SimpleJWTCryptoProvider
 import id.walt.verifier.base.config.ConfigManager
 import id.walt.verifier.base.config.OIDCVerifierServiceConfig
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.JsonObject
-import java.io.File
+import kotlinx.uuid.UUID
+import kotlinx.uuid.generateUUID
 import java.io.FileReader
 
 object RequestSigningCryptoProvider: JWTCryptoProvider {
   val signingKey: ECKey? = ConfigManager.getConfig<OIDCVerifierServiceConfig>().requestSigningKeyFile?.let { runBlocking { ECKey.parseFromPEMEncodedObjects(FileReader(it).readText()).toECKey() } }
   val certificateChain: String? = ConfigManager.getConfig<OIDCVerifierServiceConfig>().requestSigningCertFile?.let { FileReader(it).readText() }
+  val defaultSigningKey = ECKeyGenerator(Curve.P_256).keyUse(KeyUse.SIGNATURE).keyID(UUID.generateUUID().toString()).generate()
 
   override fun sign(payload: JsonObject, keyID: String?, typ: String): String {
-    if(signingKey == null) throw UnsupportedOperationException("No request signing key was configured, or key could not be loaded")
-    if(!signingKey.isPrivate) throw UnsupportedOperationException("Request signing key is no private key")
-    if(certificateChain.isNullOrEmpty()) throw UnsupportedOperationException("No certificate chain for request signing key could be loaded")
-
+    val key = signingKey ?: defaultSigningKey
     return SignedJWT(
-      JWSHeader.Builder(JWSAlgorithm.ES256).keyID(signingKey.keyID).type(JOSEObjectType.JWT).x509CertChain(
-        X509CertChainUtils.parse(certificateChain).map { Base64.encode(it.encoded) }
-      ).build(),
+      JWSHeader.Builder(JWSAlgorithm.ES256).keyID(key.keyID).type(JOSEObjectType.JWT).also {
+        if(certificateChain != null) {
+          it.x509CertChain(
+            X509CertChainUtils.parse(certificateChain).map { Base64.encode(it.encoded) }
+          )
+        }
+      }.build(),
       JWTClaimsSet.parse(payload.toString())
-    ).also { it.sign(ECDSASigner(signingKey)) }.serialize()
+    ).also { it.sign(ECDSASigner(key)) }.serialize()
   }
 
   override fun verify(jwt: String): JwtVerificationResult {
