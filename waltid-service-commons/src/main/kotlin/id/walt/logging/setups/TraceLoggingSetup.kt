d59e2d417825846198d7a91225f79515f4b29525
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-03 04:06:42 +0200

    feat: Add trace logging setup

---
+++ waltid-service-commons/src/main/kotlin/id/walt/logging/setups/TraceLoggingSetup.kt
@@ -0,0 +1,33 @@
+package id.walt.logging.setups
+
+import id.walt.logging.LogStringManager
+import io.klogging.Level
+import io.klogging.sending.STDERR
+import io.klogging.sending.STDOUT
+
+class TraceLoggingSetup : LoggingSetup("trace", {
+    sink("stdout", LogStringManager.selectedRenderString.renderString, STDOUT)
+    sink("stderr", LogStringManager.selectedRenderString.renderString, STDERR)
+
+    logging {
+        fromLoggerBase("com.zaxxer.hikari", stopOnMatch = true)
+        fromMinLevel(Level.INFO) {
+            toSink("stdout")
+        }
+    }
+    logging {
+        fromLoggerBase("com.github.victools.jsonschema.generator", stopOnMatch = true)
+        fromMinLevel(Level.INFO) {
+            toSink("stdout")
+        }
+    }
+    logging {
+        fromMinLevel(Level.ERROR) {
+            toSink("stderr")
+        }
+        fromMinLevel(Level.TRACE) {
+            toSink("stdout")
+        }
+    }
+    minDirectLogLevel(Level.TRACE)
+})
