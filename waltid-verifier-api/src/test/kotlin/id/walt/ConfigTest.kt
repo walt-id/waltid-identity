51ece672a143e039be8d757fba033d317d2b3a15
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-03 15:04:08 +0200

    test: Add logging and correct configs in ConfigTest

--- waltid-verifier-api/src/test/kotlin/id/walt/ConfigTest.kt
+++ waltid-verifier-api/src/test/kotlin/id/walt/ConfigTest.kt
@@ -1,6 +1,7 @@
 package id.walt
 
 import id.walt.config.ConfigManager
+import id.walt.logging.TestLoggingUtils
 import id.walt.oid4vc.data.ClientIdScheme
 import id.walt.verifier.base.config.OIDCVerifierServiceConfig
 import id.walt.verifier.oidc.OIDCVerifierService
@@ -11,7 +12,8 @@ class ConfigTest {
 
     @Test
     fun testConfig() {
-        ConfigManager.loadConfigs(arrayOf())
+        TestLoggingUtils.setupTestLogging()
+        ConfigManager.testWithConfigs(testConfigs)
         ConfigManager.getConfig<OIDCVerifierServiceConfig>()
         assertEquals("verifier.potential.walt-test.cloud", OIDCVerifierService.config.clientIdMap[ClientIdScheme.X509SanDns])
     }
