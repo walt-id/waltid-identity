9c71358c58bb6f157a1ae059a34196264710ca77
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-04 11:09:33 +0200

    feat: Add feature catalog to issuer-api

---
+++ waltid-issuer-api/src/main/kotlin/id/walt/issuer/FeatureCatalog.kt
@@ -0,0 +1,9 @@
+package id.walt.issuer
+
+import id.walt.featureflag.OptionalFeature
+
+object FeatureCatalog {
+
+    val entra = OptionalFeature("entra", "Enable support for Microsoft Entra", emptyList())
+
+}
