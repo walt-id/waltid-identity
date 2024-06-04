7a9e5a100135f985fa819b7703f9461fa236f588
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-04 11:08:53 +0200

    feat: Add FeatureFlagInformationModule for the optional web server

---
+++ waltid-service-commons/src/main/kotlin/id/walt/web/modules/FeatureFlagInformationModule.kt
@@ -0,0 +1,32 @@
+package id.walt.web.modules
+
+import id.walt.featureflag.FeatureManager
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+object FeatureFlagInformationModule {
+
+    @Serializable
+    data class FeatureFlagInformation(
+        val enabled: Map<String, String>,
+        val total: Int
+    ) {
+        companion object {
+            fun createCurrent(): FeatureFlagInformation {
+                val enabled = FeatureManager.enabledFeatures.values.associate { it.name to it.description }
+                return FeatureFlagInformation(enabled, enabled.size)
+            }
+        }
+    }
+
+    fun Application.enable() {
+        routing {
+            get("/features/enabled") {
+                context.respond(FeatureFlagInformation.createCurrent())
+            }
+        }
+    }
+
+}
