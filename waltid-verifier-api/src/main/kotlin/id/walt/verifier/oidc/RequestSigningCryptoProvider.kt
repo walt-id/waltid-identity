0233185962adf9a0e5b174056a35089c7fa96995
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-05 09:54:11 +0200

    style: Format RequestSigningCryptoProvider

--- waltid-verifier-api/src/main/kotlin/id/walt/verifier/oidc/RequestSigningCryptoProvider.kt
+++ waltid-verifier-api/src/main/kotlin/id/walt/verifier/oidc/RequestSigningCryptoProvider.kt
@@ -15,44 +15,48 @@ import com.nimbusds.jwt.SignedJWT
 import id.walt.config.ConfigManager
 import id.walt.sdjwt.JWTCryptoProvider
 import id.walt.sdjwt.JwtVerificationResult
-import id.walt.verifier.base.config.OIDCVerifierServiceConfig
+import id.walt.verifier.config.OIDCVerifierServiceConfig
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.JsonObject
 import kotlinx.uuid.UUID
 import kotlinx.uuid.generateUUID
-import java.io.FileReader
 import java.io.File
+import java.io.FileReader
 
-object RequestSigningCryptoProvider: JWTCryptoProvider {
+object RequestSigningCryptoProvider : JWTCryptoProvider {
     val signingKey: ECKey = ConfigManager.getConfig<OIDCVerifierServiceConfig>().requestSigningKeyFile?.let {
-      runBlocking {
-        if (File(it).exists())
-          ECKey.Builder(Curve.P_256, (ECKey.parseFromPEMEncodedObjects(FileReader(it).readText()).toECKey().toECPublicKey()))
-          .privateKey(ECKey.parseFromPEMEncodedObjects(FileReader(it).readText()).toECKey().toECPrivateKey())
-          .keyID(UUID.generateUUID().toString())
-          .build()
-        else
-          null
-      }
-    }
-    ?: ECKeyGenerator(Curve.P_256).keyUse(KeyUse.SIGNATURE).keyID(UUID.generateUUID().toString()).generate()
-
-  val certificateChain: String? = ConfigManager.getConfig<OIDCVerifierServiceConfig>().requestSigningCertFile?.let{ runBlocking { if (File(it).exists()) FileReader(it).readText() else null } }
-
-  override fun sign(payload: JsonObject, keyID: String?, typ: String): String {
-    return SignedJWT(
-      JWSHeader.Builder(JWSAlgorithm.ES256).keyID(signingKey.keyID).type(JOSEObjectType.JWT).also {
-        if(certificateChain != null) {
-          it.x509CertChain(
-            X509CertChainUtils.parse(certificateChain).map { Base64.encode(it.encoded) }
-          )
+        runBlocking {
+            if (File(it).exists())
+                ECKey.Builder(Curve.P_256, (ECKey.parseFromPEMEncodedObjects(FileReader(it).readText()).toECKey().toECPublicKey()))
+                    .privateKey(ECKey.parseFromPEMEncodedObjects(FileReader(it).readText()).toECKey().toECPrivateKey())
+                    .keyID(UUID.generateUUID().toString())
+                    .build()
+            else
+                null
         }
-      }.build(),
-      JWTClaimsSet.parse(payload.toString())
-    ).also { it.sign(ECDSASigner(signingKey)) }.serialize()
-  }
+    }
+        ?: ECKeyGenerator(Curve.P_256).keyUse(KeyUse.SIGNATURE).keyID(UUID.generateUUID().toString()).generate()
 
-  override fun verify(jwt: String): JwtVerificationResult {
-    TODO("Not yet implemented")
-  }
+    val certificateChain: String? = ConfigManager.getConfig<OIDCVerifierServiceConfig>().requestSigningCertFile?.let {
+        runBlocking {
+            if (File(it).exists()) FileReader(it).readText() else null
+        }
+    }
+
+    override fun sign(payload: JsonObject, keyID: String?, typ: String): String {
+        return SignedJWT(
+            JWSHeader.Builder(JWSAlgorithm.ES256).keyID(signingKey.keyID).type(JOSEObjectType.JWT).also {
+                if (certificateChain != null) {
+                    it.x509CertChain(
+                        X509CertChainUtils.parse(certificateChain).map { Base64.encode(it.encoded) }
+                    )
+                }
+            }.build(),
+            JWTClaimsSet.parse(payload.toString())
+        ).also { it.sign(ECDSASigner(signingKey)) }.serialize()
+    }
+
+    override fun verify(jwt: String): JwtVerificationResult {
+        TODO("Not yet implemented")
+    }
 }
