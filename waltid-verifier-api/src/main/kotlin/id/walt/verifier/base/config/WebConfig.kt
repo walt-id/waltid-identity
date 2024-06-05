979619cdab8c87b89284352cbc429728cd4c2ca2
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-05 02:49:09 +0200

    fix: Remove old config from verifier-api

--- waltid-verifier-api/src/main/kotlin/id/walt/verifier/base/config/WebConfig.kt
+++
@@ -1,7 +0,0 @@
-package id.walt.verifier.base.config
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class WebConfig(val webHost: String = "0.0.0.0", val webPort: Int = 3000) : BaseConfig
-
