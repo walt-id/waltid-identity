2ce8fc37748c42bcdc9a984968c5e63bfce33fd6
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-05 04:25:58 +0200

    refactor: Extend issuer-api feature catalog

--- waltid-issuer-api/src/main/kotlin/id/walt/issuer/FeatureCatalog.kt
+++ waltid-issuer-api/src/main/kotlin/id/walt/issuer/FeatureCatalog.kt
@@ -1,9 +1,28 @@
 package id.walt.issuer
 
+import id.walt.featureflag.BaseFeature
 import id.walt.featureflag.OptionalFeature
+import id.walt.featureflag.ServiceFeatureCatalog
+import id.walt.issuer.config.CredentialTypeConfig
+import id.walt.issuer.config.OIDCIssuerServiceConfig
 
-object FeatureCatalog {
+object FeatureCatalog : ServiceFeatureCatalog {
 
-    val entra = OptionalFeature("entra", "Enable support for Microsoft Entra", emptyList())
+    val credentialTypes = BaseFeature(
+        "credential-types",
+        "Configure the credential types available in this issuer instance",
+        mapOf("credential-issuer-metadata" to CredentialTypeConfig::class)
+    )
+
+    val issuerService = BaseFeature(
+        "issuer-service",
+        "Issuer Service Implementation",
+        OIDCIssuerServiceConfig::class
+    )
+
+    val entra = OptionalFeature("entra", "Enable support for Microsoft Entra", default = false)
+
+    override val baseFeatures = listOf(credentialTypes, issuerService)
+    override val optionalFeatures = listOf(entra)
 
 }
