408249fd2fe4a0a6fd8261460ab43249599fdc36
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-05 04:24:24 +0200

    refactor: Simplify ConfigManager (as lots of logic/features are now part of the feature system)

--- waltid-service-commons/src/main/kotlin/id/walt/config/ConfigManager.kt
+++ waltid-service-commons/src/main/kotlin/id/walt/config/ConfigManager.kt
@@ -6,7 +6,6 @@ import java.util.concurrent.ConcurrentLinkedQueue
 import kotlin.reflect.KClass
 import kotlin.reflect.jvm.jvmName
 
-//interface WaltConfig
 typealias WaltConfig = Any
 
 object ConfigManager {
@@ -29,9 +28,9 @@ object ConfigManager {
     }
 
     @OptIn(ExperimentalHoplite::class)
-    private fun loadConfig(config: ConfigData, args: Array<String>) {
+    fun loadConfig(config: ConfigData, args: Array<String>): Result<WaltConfig> {
         val id = config.id
-        log.debug { "Loading ${if (config.required) "required" else "optional"} configuration: \"$id\" (${config.type.simpleName ?: config.type.jvmName})..." }
+        log.debug { "Loading configuration: \"$id\" (${config.type.simpleName ?: config.type.jvmName})..." }
 
         val type = config.type
         val configKey = Pair(id, type)
@@ -39,10 +38,10 @@ object ConfigManager {
         preloadedConfigurations[configKey]?.let {
             loadedConfigurations[configKey] = it
             log.info { "Overwrote wallet configuration with preload: $id" }
-            return
+            return Result.success(it)
         }
 
-        runCatching {
+        return runCatching {
             ConfigLoaderBuilder.default()
                 .addDecoder(JsonElementDecoder())
                 .addCommandLineSource(args)
@@ -55,9 +54,7 @@ object ConfigManager {
         }.onSuccess {
             log.trace { "Loaded config \"$id\": $it" }
             loadedConfigurations[configKey] = it
-            config.onLoad?.invoke(it)
         }.onFailure {
-            if (config.required) {
                 log.error {
                     """
                     |---- vvv Configuration error vvv ----
@@ -65,15 +62,6 @@ object ConfigManager {
                     |---- ^^^ Configuration error ^^^ ---
                     |""".trimMargin()
                 }
-            } else {
-                if (it.message != null) {
-                    val shorterMessage = it.message!!.removePrefix("Error loading config because:")
-                        .lines().filterNot { it.isBlank() }.joinToString() { it.trim().removePrefix("- ") }
-                    log.info { "OPTIONAL configuration \"$id\" not loaded: $shorterMessage [This is not a mistake if you are not using named feature. The feature will not be available when not configured.]" }
-                } else {
-                    log.info { "OPTIONAL configuration \"$id\" not loaded: ${it.stackTraceToString()}" }
-                }
-            }
         }
     }
 
@@ -103,53 +91,20 @@ object ConfigManager {
     fun registerConfig(
         id: String,
         type: KClass<out WaltConfig>,
-        multiple: Boolean = false,
-        onLoad: ((WaltConfig) -> Unit)? = null,
-    ) = registerConfig(ConfigData(id, type, false, multiple, onLoad))
+    ) = registerConfig(ConfigData(id, type/*, false, multiple, onLoad*/))
 
     fun registerRequiredConfig(
         id: String,
         type: KClass<out WaltConfig>,
-        multiple: Boolean = false,
-        onLoad: ((WaltConfig) -> Unit)? = null,
-    ) = registerConfig(ConfigData(id, type, true, multiple, onLoad))
+    ) = registerConfig(ConfigData(id, type/*, true, multiple, onLoad*/))
 
     private fun registerConfig(data: ConfigData) {
-        if (registeredConfigurations.any { it.id == data.id } && !data.multiple)
+        if (registeredConfigurations.any { it.id == data.id } /*&& !data.multiple*/)
             throw IllegalArgumentException(
                 "A configuration with the name \"${data.id}\" already exists!"
             )
 
         registeredConfigurations.add(data)
-    }
-
-    /** All configurations registered in this function will be loaded on startup */
-    private fun registerConfigurations() {
-        /*registerRequiredConfig("db", DatabaseConfiguration::class) {
-            val dbConfigFile = (it as DatabaseConfiguration).database
-
-            registerRequiredConfig(dbConfigFile, multiple = true, type = DatasourceJsonConfiguration::class)
-        }
-        registerRequiredConfig("web", WebConfig::class)
-        registerRequiredConfig("logins", LoginMethodsConfig::class)
-        registerRequiredConfig("auth", AuthConfig::class)
-
-        registerConfig("tenant", TenantConfig::class)
-        registerConfig("push", PushConfig::class)
-
-        registerConfig("wallet", RemoteWalletConfig::class)
-        registerConfig("marketplace", MarketPlaceConfiguration::class)
-        registerConfig("chainexplorer", ChainExplorerConfiguration::class)
-        registerConfig("runtime", RuntimeConfig::class)
-
-        registerConfig("oidc", OidcConfiguration::class)
-        registerConfig("trust", TrustConfig::class)
-        registerConfig("rejectionreason", RejectionReasonConfig::class)
-        registerConfig("registration-defaults", RegistrationDefaultsConfig::class)
-
-        registerConfig("oci-rest-api", OciRestApiKeyConfig::class)
-        registerConfig("notification", NotificationConfig::class)
-        registerConfig("oci", OciKeyConfig::class)*/
     }
 
     fun testWithConfigs(vararg configs: Pair<String, KClass<out Any>>) = testWithConfigs(configs.toList())
@@ -168,18 +123,11 @@ object ConfigManager {
     fun loadConfigs(args: Array<String> = emptyArray()) {
         log.debug { "Loading configurations..." }
 
-        if (registeredConfigurations.isEmpty()) registerConfigurations()
-
         registeredConfigurations.forEach { loadConfig(it, args) }
     }
 
     data class ConfigData(
         val id: String,
         val type: KClass<out WaltConfig>,
-        /** is this configuration mandatory or optional? */
-        val required: Boolean = false,
-        /** are multiple configurations of the same name allowed? */
-        val multiple: Boolean = false,
-        val onLoad: ((WaltConfig) -> Unit)? = null,
     )
 }
