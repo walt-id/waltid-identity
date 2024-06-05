4c65f64c49581f4ed524963d21db340e3c95bd5c
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-05 10:14:28 +0200

    refactor: Added service-commons status pages

---
+++ waltid-service-commons/src/main/kotlin/id/walt/web/plugins/StatusPages.kt
@@ -0,0 +1,47 @@
+package id.walt.web.plugins
+
+import id.walt.web.WebException
+import io.klogging.logger
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import kotlinx.serialization.json.JsonPrimitive
+
+
+private fun statusCodeForException(cause: Throwable) = when (cause) {
+    is IllegalArgumentException -> HttpStatusCode.BadRequest
+    is NotFoundException -> HttpStatusCode.NotFound
+    is WebException -> cause.status
+
+    else -> HttpStatusCode.InternalServerError
+}
+
+private val logger = logger("Web exception")
+
+fun Application.configureStatusPages() {
+    install(StatusPages) {
+
+        fun exceptionMap(cause: Throwable) =
+            mutableMapOf(
+                "exception" to JsonPrimitive(true),
+                "status" to JsonPrimitive(statusCodeForException(cause).description),
+                "code" to JsonPrimitive(statusCodeForException(cause).value.toString()),
+                "message" to JsonPrimitive(cause.message)
+            ).apply {
+                if (cause.cause != null && logger.isTraceEnabled()) put("cause", JsonPrimitive(cause.cause!!.message))
+            }
+
+
+        exception<WebException> { call, cause ->
+            logger.error(cause)
+            call.respond(cause.status, exceptionMap(cause))
+        }
+        exception<Throwable> { call, cause ->
+            logger.error(cause)
+
+            call.respond(statusCodeForException(cause), exceptionMap(cause))
+        }
+    }
+}
