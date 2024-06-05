2962dbce199ade74fe28d702ec68b179acb03daa
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-05 04:20:06 +0200

    feat: Add base and optional feature definition

--- waltid-service-commons/src/main/kotlin/id/walt/featureflag/OptionalFeature.kt
+++ waltid-service-commons/src/main/kotlin/id/walt/featureflag/OptionalFeature.kt
@@ -1,6 +1,17 @@
 package id.walt.featureflag
 
-class OptionalFeature(val name: String, val description: String, val requiredConfigs: List<Any>) {
+import id.walt.config.WaltConfig
+import kotlin.reflect.KClass
+
+class OptionalFeature(
+    override val name: String,
+    override val description: String,
+    override val configs: Map<String, KClass<out WaltConfig>> = emptyMap(),
+    override val dependsOn: List<AbstractFeature> = emptyList(),
+    val default: Boolean
+) : AbstractFeature(name, description, configs, dependsOn) {
+
+    constructor(name: String, description: String, config: KClass<out WaltConfig>, default: Boolean) : this(name, description, mapOf(name to config), emptyList(), default)
 
 }
 
