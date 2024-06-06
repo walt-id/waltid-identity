102e36f9739d60d20ae6ee828a0e4ea4b70715c0
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-06 15:01:50 +0200

    fix: Add default for KeyGenerationDefaultsConfig

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/KeyGenerationDefaultsConfig.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/KeyGenerationDefaultsConfig.kt
@@ -3,7 +3,7 @@ package id.walt.webwallet.config
 import kotlinx.serialization.json.JsonObject
 
 data class KeyGenerationDefaultsConfig(
-    val keyGenerationDefaults: Map<String, JsonObject>
+    val keyGenerationDefaults: Map<String, JsonObject> = emptyMap()
 ) {
     fun getConfigForBackend(name: String) = keyGenerationDefaults[name]
 }
