26850906353ea742536fd925d5fd47536e2a5d0d
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-06 02:26:53 +0200

    test: Start adding new e2e test

--- waltid-services/waltid-e2e-tests/build.gradle.kts
+++ waltid-services/waltid-e2e-tests/build.gradle.kts
@@ -11,6 +11,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test-jvm:1.8.1")
+    testImplementation("io.ktor:ktor-server-test-host:2.3.11")
+    implementation(project(":waltid-services:waltid-service-commons"))
+    implementation(project(":waltid-services:waltid-issuer-api"))
+    implementation(project(":waltid-services:waltid-verifier-api"))
+    implementation(project(":waltid-services:waltid-wallet-api"))
 }
 
 tasks.test {
