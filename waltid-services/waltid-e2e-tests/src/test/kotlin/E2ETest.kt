04692e2002cd9eee9544b5dc5cfeba0be4538c1a
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-06 15:07:32 +0200

    test: Stub new E2E tests

--- waltid-services/waltid-e2e-tests/src/test/kotlin/E2ETest.kt
+++ waltid-services/waltid-e2e-tests/src/test/kotlin/E2ETest.kt
@@ -1,16 +1,22 @@
 import id.walt.commons.ServiceConfiguration
 import id.walt.commons.ServiceInitialization
 import id.walt.commons.ServiceMain
+import id.walt.commons.web.plugins.configureSerialization
+import id.walt.commons.web.plugins.configureStatusPages
 import id.walt.credentials.verification.PolicyManager
+import id.walt.crypto.utils.JsonUtils.toJsonObject
 import id.walt.did.helpers.WaltidServices
 import id.walt.issuer.issuerModule
 import id.walt.verifier.policies.PresentationDefinitionPolicy
 import id.walt.verifier.verifierModule
-import id.walt.commons.web.plugins.configureSerialization
-import id.walt.commons.web.plugins.configureStatusPages
 import id.walt.webwallet.db.Db
 import id.walt.webwallet.webWalletModule
 import id.walt.webwallet.webWalletSetup
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.testing.*
 import kotlinx.coroutines.test.runTest
@@ -32,20 +38,19 @@ class E2ETest {
             module.invoke(this)
         }
 
-        fun run(block: ApplicationTestBuilder.() -> Unit): suspend () -> Unit = {
+        fun run(block: suspend ApplicationTestBuilder.() -> Unit): suspend () -> Unit = {
             testApplication {
                 application {
                     webServiceModule()
-
-                    block.invoke(this@testApplication)
                 }
+                block.invoke(this@testApplication)
             }
         }
     }
 
-    suspend fun tests(block: ApplicationTestBuilder.() -> Unit) {
+    suspend fun tests(block: suspend ApplicationTestBuilder.() -> Unit) {
         ServiceMain(
-            ServiceConfiguration("e2e"), ServiceInitialization(
+            ServiceConfiguration("e2e-test"), ServiceInitialization(
                 features = listOf(IssuerFeatureCatalog, VerifierFeatureCatalog, WalletFeatureCatalog),
                 init = {
                     webWalletSetup()
@@ -55,19 +60,31 @@ class E2ETest {
                 },
                 run = TestWebService(Application::e2eTestModule).run(block)
             )
-        ).main(emptyArray())
+        ).main(arrayOf("-l", "trace"))
     }
 
     @Test
     fun e2e() = runTest(timeout = 5.minutes) {
         tests {
             val client = createClient {
-                /*install(ContentNegotiation) {
+                install(ContentNegotiation) {
                     json()
-                }*/
+                }
+                install(DefaultRequest) {
+                    contentType(ContentType.Application.Json)
+                }
             }
 
             // the e2e http request tests here
+            client.post("/wallet-api/auth/login") {
+                setBody(
+                    mapOf(
+                        "type" to "email",
+                        "email" to "user@email.com",
+                        "password" to "password"
+                    ).toJsonObject()
+                )
+            }.let { println(it) }
         }
     }
 }
