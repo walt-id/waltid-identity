40d55b450fc34957fa78ae3034c3ffdd6a205537
Niclas Wimmerstedt <niclas@nomadio.se>
2024-06-06 14:10:56 +0200

    updated RegistrationDefaults to fix #475

--- waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/RegistrationDefaultsConfig.kt
+++ waltid-wallet-api/src/main/kotlin/id/walt/webwallet/config/RegistrationDefaultsConfig.kt
@@ -1,22 +1,36 @@
 package id.walt.webwallet.config
 
 import id.walt.crypto.keys.KeyGenerationRequest
+import id.walt.crypto.keys.KeyType
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.*
 
 @Serializable
 data class RegistrationDefaultsConfig(
-    private val defaultKeyConfig: JsonObject = buildJsonObject {
-        put("backend", JsonPrimitive("jwk"))
-        put("keyType", JsonPrimitive("Ed25519"))
-    },
-
-    private val defaultDidConfig: JsonObject = buildJsonObject {
-        put("method", JsonPrimitive("jwk"))
-    }
+    val defaultKeyConfig: DefaultKeyConfig = DefaultKeyConfig(
+         backend = "jwk",
+         keyType= KeyType.Ed25519,
+         config= null,
+    ),
+    val defaultDidConfig: DefaultDidConfig = DefaultDidConfig(
+        method = "key",
+         config = null,
+    ),
 ) : WalletConfig {
-    val keyGenerationRequest = Json.decodeFromJsonElement<KeyGenerationRequest>(defaultKeyConfig)
+    @Serializable
+    data class DefaultKeyConfig(
+        val backend: String = "jwk",
+        val keyType: KeyType = KeyType.Ed25519,
+        val config: Map<String, String>?
+    )
+    @Serializable
+    data class DefaultDidConfig(
+        val method: String,
+        val config: Map<String, String>?
+    )
 
-    val didMethod = defaultDidConfig["method"]!!.jsonPrimitive.content
-    val didConfig: Map<String, JsonPrimitive> = defaultDidConfig["config"]?.jsonObject?.mapValues { it.value.jsonPrimitive } ?: emptyMap()
+    val keyGenerationRequest = Json.decodeFromJsonElement<KeyGenerationRequest>(Json.encodeToJsonElement(defaultKeyConfig))
+
+    val didMethod = defaultDidConfig.method
+    val didConfig: Map<String, JsonPrimitive> = defaultDidConfig.config?.mapValues { JsonPrimitive(it.value) } ?: emptyMap()
 }
