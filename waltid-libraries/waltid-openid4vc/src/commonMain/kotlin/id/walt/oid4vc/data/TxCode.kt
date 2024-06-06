e64303a592acce8aaf84187ca8eb6d36642c08c7
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-06 11:08:45 +0200

    fix: fixed various qodana-reported issues

--- waltid-libraries/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/data/TxCode.kt
+++ waltid-libraries/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/data/TxCode.kt
@@ -8,29 +8,29 @@ import kotlinx.serialization.encoding.Encoder
 data class TxCode(
   @SerialName("input_mode") val inputMode: TxInputMode? = TxInputMode.numeric,
   @SerialName("length") val length: Int? = null,
-  @SerialName("description") val description: String? = null
+  @SerialName("description") val description: String? = null,
 )
 
 @Serializable(TxInputModeSerializer::class)
 enum class TxInputMode(val value: String) {
-  numeric("numeric"),
-  text("text");
+    numeric("numeric"),
+    text("text");
 
-  companion object {
-    fun fromValue(value: String): TxInputMode? {
-      return TxInputMode.values().find { it.value == value }
+    companion object {
+        fun fromValue(value: String): TxInputMode? {
+            return entries.find { it.value == value }
+        }
     }
-  }
 }
 
 @OptIn(ExperimentalSerializationApi::class)
 @Serializer(TxInputMode::class)
 object TxInputModeSerializer : KSerializer<TxInputMode> {
-  override fun serialize(encoder: Encoder, value: TxInputMode) {
-    encoder.encodeString(value.value)
-  }
+    override fun serialize(encoder: Encoder, value: TxInputMode) {
+        encoder.encodeString(value.value)
+    }
 
-  override fun deserialize(decoder: Decoder): TxInputMode {
-    return TxInputMode.fromValue(decoder.decodeString())!!
-  }
+    override fun deserialize(decoder: Decoder): TxInputMode {
+        return TxInputMode.fromValue(decoder.decodeString())!!
+    }
 }
