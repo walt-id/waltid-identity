7023b8013d2544ec69ed5f493be8391900e8a123
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-10 16:22:14 +0200

    style: Fix review regarding formatting

--- waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/featureflag/AbstractFeature.kt
+++ waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/featureflag/AbstractFeature.kt
@@ -3,4 +3,9 @@ package id.walt.commons.featureflag
 import id.walt.commons.config.WaltConfig
 import kotlin.reflect.KClass
 
-sealed class AbstractFeature(open val name: String, open val description: String, open val configs: Map<String, KClass<out WaltConfig>>, open val dependsOn: List<AbstractFeature>)
+sealed class AbstractFeature(
+    open val name: String,
+    open val description: String,
+    open val configs: Map<String, KClass<out WaltConfig>>,
+    open val dependsOn: List<AbstractFeature>,
+)
