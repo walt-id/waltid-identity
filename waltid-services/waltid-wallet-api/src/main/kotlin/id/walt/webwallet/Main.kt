b2c740b61d848f594d7f1d0987a47eeee4eb25ab
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-12 12:15:05 +0200

    refactor: Remove duplicated OpenAPI module from wallet-api

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/Main.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/Main.kt
@@ -3,9 +3,10 @@ package id.walt.webwallet
 import id.walt.commons.ServiceConfiguration
 import id.walt.commons.ServiceInitialization
 import id.walt.commons.ServiceMain
+import id.walt.commons.featureflag.CommonsFeatureCatalog
+import id.walt.commons.web.WebService
 import id.walt.crypto.keys.oci.WaltCryptoOci
 import id.walt.did.helpers.WaltidServices
-import id.walt.commons.web.WebService
 import id.walt.webwallet.db.Db
 import id.walt.webwallet.web.Administration.configureAdministration
 import id.walt.webwallet.web.controllers.*
@@ -13,8 +14,8 @@ import id.walt.webwallet.web.controllers.NotificationController.notifications
 import id.walt.webwallet.web.controllers.PushController.push
 import id.walt.webwallet.web.plugins.configureHTTP
 import id.walt.webwallet.web.plugins.configureMonitoring
-import id.walt.webwallet.web.plugins.configureOpenApi
 import id.walt.webwallet.web.plugins.configureRouting
+import id.walt.webwallet.web.plugins.walletOpenApiPluginAmendment
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.*
 import org.bouncycastle.jce.provider.BouncyCastleProvider
@@ -23,16 +24,21 @@ import java.security.Security
 private val log = KotlinLogging.logger { }
 
 suspend fun main(args: Array<String>) {
-    ServiceMain(ServiceConfiguration("wallet"), ServiceInitialization(
-        features = FeatureCatalog,
-        init = {
-            webWalletSetup()
-            WaltidServices.minimalInit()
-            WaltCryptoOci.init()
-            Db.start()
-        },
-        run =  WebService(Application::webWalletModule).run()
-    )).main(args)
+    ServiceMain(
+        ServiceConfiguration("wallet"), ServiceInitialization(
+            features = FeatureCatalog,
+            featureAmendments = mapOf(
+                CommonsFeatureCatalog.openApiFeature to walletOpenApiPluginAmendment
+            ),
+            init = {
+                webWalletSetup()
+                WaltidServices.minimalInit()
+                WaltCryptoOci.init()
+                Db.start()
+            },
+            run = WebService(Application::webWalletModule).run()
+        )
+    ).main(args)
 }
 
 fun webWalletSetup() {
@@ -46,9 +52,7 @@ private fun Application.configurePlugins() {
     configureMonitoring()
     configureAdministration()
     configureRouting()
-    configureOpenApi()
 }
-
 
 fun Application.webWalletModule(withPlugins: Boolean = true) {
     if (withPlugins) {
--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/plugins/OpenApi.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/plugins/OpenApi.kt
@@ -1,74 +1,48 @@
 package id.walt.webwallet.web.plugins
 
-import id.walt.commons.config.buildconfig.BuildConfig
-import io.github.smiley4.ktorswaggerui.SwaggerUI
+import id.walt.commons.web.modules.OpenApiModule
 import io.github.smiley4.ktorswaggerui.data.AuthKeyLocation
 import io.github.smiley4.ktorswaggerui.data.AuthScheme
 import io.github.smiley4.ktorswaggerui.data.AuthType
-import io.github.smiley4.ktorswaggerui.routing.openApiSpec
-import io.github.smiley4.ktorswaggerui.routing.swaggerUI
-import io.ktor.server.application.*
 import io.ktor.server.plugins.ratelimit.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 
-fun Application.configureOpenApi() {
-    routing {
-        route("swagger") {
-            swaggerUI("/api.json")
+internal val walletOpenApiPluginAmendment: suspend () -> Unit = suspend {
+    OpenApiModule.OpenApiConfig.apply {
+        customInfo = {
+            description +=
+                "\nAny HTTP status code of 200 - 299 indicates request success, 400 - 499 client error / invalid request, 500+ internal server processing exception."
+                    .replace("\n", "<br/>")
         }
 
-        route("api.json") {
-            openApiSpec()
-        }
+        custom = {
+            ignoredRouteSelectors += RateLimitRouteSelector::class // Do not break schemas due to rate limiting overlay
 
-        get("/") {
-            context.respondRedirect("swagger")
-        }
-    }
+            security {
+                securityScheme("auth-session") {
+                    name = "Session-Cookie Authentication"
+                    type = AuthType.API_KEY
+                    location = AuthKeyLocation.COOKIE
+                }
 
-    install(SwaggerUI) {
-        ignoredRouteSelectors += RateLimitRouteSelector::class
+                securityScheme("auth-bearer") {
+                    name = "Bearer token authentication"
+                    description = "Set as \"Authorization: Bearer %token-here%\" to authenticate."
+                    scheme = AuthScheme.BEARER
+                    type = AuthType.HTTP
+                }
 
-        info {
-            title = "walt.id wallet API"
-            version = BuildConfig.version
-            description = """
-                Interact with the wallet backend.
-                Any HTTP status code of 200 - 299 indicates request success, 400 - 499 client error / invalid request, 500+ internal server processing exception.
-            """.trimIndent().replace("\n", "<br/>")
-        }
+                securityScheme("auth-bearer-alternative") {
+                    name = "Bearer token authentication (alternative header)"
+                    description = "Set alternative header \"waltid-authorization: Bearer %token-here%\" to authenticate."
+                    scheme = AuthScheme.BEARER
+                    type = AuthType.HTTP
+                    location = AuthKeyLocation.HEADER
+                }
 
-        security {
-            securityScheme("auth-session") {
-                name = "Session-Cookie Authentication"
-                type = AuthType.API_KEY
-                location = AuthKeyLocation.COOKIE
+                defaultUnauthorizedResponse {
+                    description = "Invalid authentication"
+                }
             }
-
-            securityScheme("auth-bearer") {
-                name = "Bearer token authentication"
-                description = "Set as \"Authorization: Bearer %token-here%\" to authenticate."
-                scheme = AuthScheme.BEARER
-                type = AuthType.HTTP
-            }
-
-            securityScheme("auth-bearer-alternative") {
-                name = "Bearer token authentication (alternative header)"
-                description = "Set alternative header \"waltid-authorization: Bearer %token-here%\" to authenticate."
-                scheme = AuthScheme.BEARER
-                type = AuthType.HTTP
-                location = AuthKeyLocation.HEADER
-            }
-
-            defaultUnauthorizedResponse {
-                description = "Invalid authentication"
-            }
-        }
-
-        externalDocs {
-            url = "https://docs.walt.id"
-            description = "docs.walt.id"
         }
     }
 }
