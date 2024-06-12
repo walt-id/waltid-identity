71ddf9d8588ff364486bcf1ef2e25df786d806aa
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-12 12:13:49 +0200

    refactor: Update service initializer to support feature amendments

--- waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/ServiceCommons.kt
+++ waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/ServiceCommons.kt
@@ -1,6 +1,9 @@
 package id.walt.commons
 
-import id.walt.commons.config.buildconfig.BuildConfig
+import id.walt.commons.config.statics.BuildConfig
+import id.walt.commons.config.statics.ServiceConfig
+import id.walt.commons.config.statics.ServiceConfig.serviceString
+import id.walt.commons.featureflag.AbstractFeature
 import id.walt.commons.featureflag.CommonsFeatureCatalog
 import id.walt.commons.featureflag.FeatureManager
 import id.walt.commons.featureflag.ServiceFeatureCatalog
@@ -19,14 +22,16 @@ data class ServiceConfiguration(
 
 data class ServiceInitialization(
     val features: List<ServiceFeatureCatalog>,
+    val featureAmendments: Map<AbstractFeature, suspend () -> Unit> = emptyMap(),
     val init: suspend () -> Unit,
     val run: suspend () -> Unit,
 ) {
     constructor(
         features: ServiceFeatureCatalog,
+        featureAmendments: Map<AbstractFeature, suspend () -> Unit> = emptyMap(),
         init: suspend () -> Unit,
         run: suspend () -> Unit,
-    ) : this(listOf(features), init, run)
+    ) : this(listOf(features), featureAmendments, init, run)
 }
 
 object ServiceCommons {
@@ -34,41 +39,67 @@ object ServiceCommons {
     private fun debugLineString(): String =
         "Running on ${p("os.arch")} ${p("os.name")} ${p("os.version")} with ${p("java.vm.name")} ${p("java.vm.version")} in path ${Path(".").absolutePathString()}"
 
+    private suspend fun initFeatures(init: ServiceInitialization) {
+        val log = logger("Feature-Init")
+
+        log.info { "Initializing features..." }
+
+        measureTime {
+            log.info { "Registering common feature catalog..." }
+            FeatureManager.registerCatalog(CommonsFeatureCatalog)
+            log.info { "Registering service feature catalog..." }
+            FeatureManager.registerCatalogs(init.features)
+
+            log.info { "Loading features..." }
+            FeatureManager.load(init.featureAmendments)
+        }.also {
+            log.info { "Feature initialization completed ($it)." }
+        }
+    }
+
+    private suspend fun initService(init: ServiceInitialization) {
+        val log = logger("Service-Init")
+
+        log.info { "Initializing $serviceString..." }
+        measureTime {
+            init.init.invoke()
+        }.also {
+            log.info { "Service initialization completed ($it)." }
+        }
+    }
+
+    private suspend fun runService(init: ServiceInitialization) {
+        val log = logger("Service-Runner")
+
+        log.info { "Running $serviceString..." }
+        measureTime {
+            init.run.invoke()
+        }.also {
+            log.info { "Service run completed ($it)." }
+        }
+    }
+
     suspend fun runService(
         config: ServiceConfiguration,
         init: ServiceInitialization,
     ) {
-        val service = "${config.vendor} ${config.name} ${config.version}"
+        ServiceConfig.config = config
 
         val log = logger(config.vendor + " " + config.name)
-        log.info { "Starting $service..." }
+        log.info { "Starting $serviceString..." }
 
         log.debug { debugLineString() }
 
-        log.info { "Registering common feature catalog..." }
-        FeatureManager.registerCatalog(CommonsFeatureCatalog)
-        log.info { "Registering service feature catalog..." }
-        FeatureManager.registerCatalogs(init.features)
-
-        log.info { "Loading features..." }
-        FeatureManager.load()
-
-        log.info { "Initializing $service..." }
         measureTime {
-            init.init.invoke()
+            initFeatures(init)
+            initService(init)
+            runService(init)
         }.also {
-            log.info { "Initialization completed ($it)." }
-        }
-
-        log.info { "Running $service..." }
-        measureTime {
-            init.run.invoke()
-        }.also {
-            log.info { "Run completed ($it)." }
+            log.info { "Service phases ended after $it." }
         }
 
         delay(50)
-        log.info { "Service $service done." }
+        log.info { "Service $serviceString done." }
     }
 
 }
