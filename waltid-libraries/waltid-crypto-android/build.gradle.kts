00a681636e1035e37050129e6f2a1d7031315694
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-12 17:55:24 +0200

    deps: update suspend transform to 0.9.0

--- waltid-libraries/waltid-crypto-android/build.gradle.kts
+++ waltid-libraries/waltid-crypto-android/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("com.android.library")
     id("maven-publish")
     id("com.github.ben-manes.versions")
-    id("love.forte.plugin.suspend-transform") version "0.6.0"
+    id("love.forte.plugin.suspend-transform") version "0.9.0"
 }
 
 group = "id.walt.crypto"
