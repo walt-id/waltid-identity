031e729c04e92d9c6a387fdf3a9c0e33fb5a9216
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-12 12:14:03 +0200

    fix: Import for RunConfiguration

--- waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/ServiceMain.kt
+++ waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/ServiceMain.kt
@@ -1,7 +1,7 @@
 package id.walt.commons
 
 import id.walt.commons.commands.ServiceRunnableCommand
-import id.walt.commons.config.runconfig.RunConfiguration
+import id.walt.commons.config.statics.RunConfiguration
 
 class ServiceMain(
     val config: ServiceConfiguration,
