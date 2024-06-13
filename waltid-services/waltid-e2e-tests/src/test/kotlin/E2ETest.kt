d89327574903c064a71b9bab3764d028ddff0b2a
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-13 15:03:53 +0200

    test: additional e2e tests

--- waltid-services/waltid-e2e-tests/src/test/kotlin/E2ETest.kt
+++ waltid-services/waltid-e2e-tests/src/test/kotlin/E2ETest.kt
@@ -1,16 +1,21 @@
 import E2ETestWebService.test
 import E2ETestWebService.testBlock
-import id.walt.crypto.utils.JsonUtils.toJsonObject
+import id.walt.webwallet.web.model.EmailAccountRequest
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import kotlin.test.Test
+import kotlin.test.assertNotNull
 import kotlin.time.Duration.Companion.minutes
 
 class E2ETest {
@@ -18,35 +23,60 @@ class E2ETest {
     @Test
     fun e2e() = runTest(timeout = 5.minutes) {
         testBlock {
-            val client = HttpClient(CIO) {
-                install(ContentNegotiation) {
-                    json()
-                }
-                install(DefaultRequest) {
-                    contentType(ContentType.Application.Json)
-                    host = "127.0.0.1"
-                    port = 22222
-                }
-                install(Logging) {
-                    this.level = LogLevel.ALL
-                }
-            }
+            var client = testHttpClient()
 
             // the e2e http request tests here
-            test("wallet-api login") {
-                client.post("/wallet-api/auth/login") {
-                    setBody(
-                        mapOf(
-                            "type" to "email",
-                            "email" to "user@email.com",
-                            "password" to "password"
-                        ).toJsonObject()
-                    )
-                }.let { println(it) }
+            test("/wallet-api/auth/user-info - not logged in without token") {
+                client.get("/wallet-api/auth/user-info").apply {
+                    assert(status == HttpStatusCode.Unauthorized)
+                }
             }
 
-            test("other test") {
+            test("/wallet-api/auth/login - wallet-api login") {
+                client.post("/wallet-api/auth/login") {
+                    setBody(EmailAccountRequest(email = "user@email.com", password = "password").encodeWithType("email"))
+                }.expectSuccess().apply {
+                    body<JsonObject>().let { result ->
+                        assertNotNull(result["token"])
+                        val token = result["token"]!!.jsonPrimitive.content.expectLooksLikeJwt()
+
+                        client = testHttpClient(token = token)
+                    }
+                }
+            }
+
+            test("/wallet-api/auth/user-info - logged in after login") {
+                client.get("/wallet-api/auth/user-info").expectSuccess()
+            }
+
+            test("/wallet-api/wallet/accounts/wallets - get wallets") {
+                client.get("/wallet-api/wallet/accounts/wallets").expectSuccess()
+
             }
         }
     }
+
+    fun testHttpClient(token: String? = null) = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(DefaultRequest) {
+            contentType(ContentType.Application.Json)
+            host = "127.0.0.1"
+            port = 22222
+
+            if (token != null) bearerAuth(token)
+        }
+        install(Logging) {
+            level = LogLevel.ALL
+        }
+    }
+
+    private fun String.expectLooksLikeJwt(): String =
+        also { assert(startsWith("ey") && count { it == '.' } == 2) { "Does not look like JWT" } }
+
+    private fun HttpResponse.expectSuccess(): HttpResponse = also {
+        assert(status.isSuccess()) { "HTTP status is non-successful" }
+    }
+
 }
