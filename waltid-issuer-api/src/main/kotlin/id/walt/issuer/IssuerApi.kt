793d4fb9091538221a614a71fb7731934ca18f19
Severin Stampler <severin@walt.id>
2024-06-17 18:13:42 +0200

    start track 2 interop test, well-known/jwt-issuer endpoint, jwt proof with jwk header

--- waltid-issuer-api/src/main/kotlin/id/walt/issuer/IssuerApi.kt
+++ waltid-issuer-api/src/main/kotlin/id/walt/issuer/IssuerApi.kt
@@ -4,7 +4,6 @@ import id.walt.credentials.vc.vcs.W3CVC
 import id.walt.crypto.keys.KeyManager
 import id.walt.crypto.keys.KeySerialization
 import id.walt.crypto.keys.jwk.JWKKey
-import id.walt.crypto.utils.JsonUtils.toJsonElement
 import id.walt.did.dids.DidService
 import id.walt.issuer.IssuanceExamples.batchExample
 import id.walt.issuer.IssuanceExamples.issuerOnboardingRequestDefaultExample
@@ -363,6 +362,18 @@ fun Application.issuerApi() {
                         Json.parseToJsonElement(KeySerialization.serializeKey(jwkKey)).jsonObject,
                         "",
                         "org.iso.18013.5.1.mDL",
+                        W3CVC()
+                    ).let { createCredentialOfferUri(listOf(it)) }
+                    context.respond(
+                        HttpStatusCode.OK, offerUri
+                    )
+                }
+                get("lspPotentialCredentialOfferT2") {
+                    val jwkKey = JWKKey.importJWK(LspPotentialInteropEvent.POTENTIAL_ISSUER_KEY_JWK).getOrThrow()
+                    val offerUri = IssuanceRequest(
+                        Json.parseToJsonElement(KeySerialization.serializeKey(jwkKey)).jsonObject,
+                        "",
+                        "urn:eu.europa.ec.eudi:pid:1",
                         W3CVC()
                     ).let { createCredentialOfferUri(listOf(it)) }
                     context.respond(
