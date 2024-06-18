559557f650839472e9edb89d8a04a2e49ec38b62
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-18 11:44:08 +0200

    chore: cleanup old configs

--- waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/config/list/WebConfig.kt
+++ waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/config/list/WebConfig.kt
@@ -1,3 +1,6 @@
 package id.walt.commons.config.list
 
-data class WebConfig(val webHost: String = "0.0.0.0", val webPort: Int = 3000)
+data class WebConfig(
+    val webHost: String = "0.0.0.0",
+    val webPort: Int = 3000
+)
