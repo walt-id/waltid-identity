9aa789d9da1e117d54a507a2eb9c7853bb5fd1ad
Ivan Pagac <ivan.pagac@accenture.com>
2024-06-18 13:27:05 +0200

    [iOS, crypto] added p256 operations over ios keychain, plus test app to test those operations on simulator

---
+++ waltid-libraries/waltid-crypto-ios/crypto-ios-utils/waltid.crypto.ios.utils/CryptoUtils.swift
@@ -0,0 +1,245 @@
+//
+//  CryptoUtils.swift
+//  waltid.crypto.ios.utils
+//
+//  Created by Ivan Pagac on 17/06/2024.
+//
+
+import CryptoKit
+import Foundation
+import JOSESwift
+
+@objc
+public class HMAC_Operations: NSObject {
+    @objc
+    public static func sign(body: String, alg: String, key: Data, typ: String, keyId: String? = nil) -> SignResult {
+        return OperationsBase<Data>.sign(body: body, alg: alg, key: key, typ: typ)
+    }
+
+    @objc
+    public static func verify(jws: String, key: Data) -> VerifyResult {
+        return OperationsBase<Data>.verify(jws: jws, key: key)
+    }
+}
+
+@objc
+public class DS_Operations: NSObject {
+    @objc
+    static func sign(body: Data, alg: String, key: SecKey, headers: [String: String]) -> SignResult {
+        return OperationsBase<SecKey>.sign(body: body, alg: alg, key: key, headers: headers)
+    }
+
+    @objc
+    static func sign(body: Data, alg: String, key: SecKey, headersData: Data) -> SignResult {
+        return OperationsBase<SecKey>.sign(body: body, alg: alg, key: key, headersData: headersData)
+    }
+
+    @objc
+    public static func sign(body: String, alg: String, key: SecKey, typ: String, keyId: String? = nil) -> SignResult {
+        return OperationsBase<SecKey>.sign(body: body, alg: alg, key: key, typ: typ)
+    }
+
+    @objc
+    public static func verify(jws: String, key: SecKey) -> VerifyResult {
+        return OperationsBase<SecKey>.verify(jws: jws, key: key)
+    }
+}
+
+@objc
+public class SignResult: NSObject {
+    @objc
+    public var success: Bool {
+        errorMessage == nil
+    }
+
+    @objc
+    public let data: String?
+
+    @objc
+    public let errorMessage: String?
+
+    private init(data: String? = nil, errorMessage: String? = nil) {
+        self.data = data
+        self.errorMessage = errorMessage
+    }
+
+    static func with(success data: String) -> SignResult {
+        SignResult(data: data)
+    }
+
+    static func with(failure message: String) -> SignResult {
+        SignResult(errorMessage: message)
+    }
+}
+
+@objc
+public class VerifyResult: NSObject {
+    @objc
+    public var success: Bool {
+        errorMessage == nil
+    }
+
+    @objc
+    public let isValid: Bool
+
+    @objc
+    public let isValidData: Data?
+
+    @objc
+    public let errorMessage: String?
+
+    private init(isValid: Bool = false, errorMessage: String? = nil, isValidData: Data? = nil) {
+        self.isValid = isValid
+        self.errorMessage = errorMessage
+        self.isValidData = isValidData
+    }
+
+    static func with(isValid: Bool, data: Data?) -> VerifyResult {
+        guard isValid else {
+            return VerifyResult(isValid: isValid, errorMessage: "Verification failed.")
+        }
+
+        return VerifyResult(isValid: isValid, isValidData: data)
+    }
+
+    static func with(failure message: String) -> VerifyResult {
+        VerifyResult(errorMessage: message)
+    }
+}
+
+class OperationsBase<TKeyType>: NSObject {
+    public static func sign(body: Data, alg: String, key: TKeyType, headersData: Data) -> SignResult {
+        do {
+            guard let signingAlgorithm = JOSESwift.SignatureAlgorithm(rawValue: alg) else {
+                return SignResult.with(failure: "Unknown signature algorithm")
+            }
+
+            guard let signer = Signer(signingAlgorithm: signingAlgorithm, key: key) else {
+                return SignResult.with(failure: "Could not construct signer.")
+            }
+            
+            var header = try {
+                            var h = try JSONSerialization.jsonObject(with: headersData) as! [String:Any]
+                            h["alg"] = signingAlgorithm.rawValue
+                            return try JWSHeader(parameters: h)
+                        }()
+
+            let jws = try JWS(header: header, payload: Payload(body), signer: signer)
+            return SignResult.with(success: jws.compactSerializedString)
+        } catch {
+            return SignResult.with(failure: "Could not perform Sign, reason: \(error.localizedDescription).")
+        }
+    }
+
+    public static func sign(body: Data, alg: String, key: TKeyType, headers: [String: String]) -> SignResult {
+        do {
+            guard let signingAlgorithm = JOSESwift.SignatureAlgorithm(rawValue: alg) else {
+                return SignResult.with(failure: "Unknown signature algorithm")
+            }
+
+            guard let signer = Signer(signingAlgorithm: signingAlgorithm, key: key) else {
+                return SignResult.with(failure: "Could not construct signer.")
+            }
+
+            guard let header = try? JWSHeader(parameters: headers) else {
+                return SignResult.with(failure: "Could not parse header")
+            }
+
+            let jws = try JWS(header: header, payload: Payload(body), signer: signer)
+            return SignResult.with(success: jws.compactSerializedString)
+        } catch {
+            return SignResult.with(failure: "Could not perform Sign, reason: \(error.localizedDescription).")
+        }
+    }
+
+    public static func sign(body payload: String, alg: String, key: TKeyType, typ: String, keyId: String? = nil) -> SignResult {
+        do {
+            guard let signingAlgorithm = JOSESwift.SignatureAlgorithm(rawValue: alg) else {
+                return SignResult.with(failure: "Unknown signature algorithm")
+            }
+
+            guard let signer = Signer(signingAlgorithm: signingAlgorithm, key: key) else {
+                return SignResult.with(failure: "Could not construct signer.")
+            }
+
+            guard let payloadData = payload.data(using: .utf8) else {
+                return SignResult.with(failure: "Body not in UTF-8")
+            }
+
+            var header = JWSHeader(algorithm: signingAlgorithm)
+            header.typ = typ
+            if let keyId {
+                header.kid = keyId
+            }
+
+            let jws = try JWS(header: header, payload: Payload(payloadData), signer: signer)
+            return SignResult.with(success: jws.compactSerializedString)
+        } catch {
+            return SignResult.with(failure: "Could not perform Sign, reason: \(error.localizedDescription).")
+        }
+    }
+
+    public static func verify(jws: String, key: TKeyType) -> VerifyResult {
+        do {
+            let jws = try JWS(compactSerialization: jws)
+
+            guard let algorithm = jws.header.algorithm else {
+                return VerifyResult.with(failure: "JWS does not contain alg header or was not recognized. This header must be present and be valid.")
+            }
+
+            guard let verifier = Verifier(verifyingAlgorithm: algorithm, key: key) else {
+                return VerifyResult.with(failure: "Could not construct verifier with the provided key.")
+            }
+
+            return VerifyResult.with(isValid: jws.isValid(for: verifier), data: jws.payload.data())
+        } catch {
+            return VerifyResult.with(failure: "Could not perform Verify, reason: \(error.localizedDescription)")
+        }
+    }
+}
+
+extension Data {
+    /// Hexadecimal string representation of `Data` object.
+
+    var hexadecimal: String {
+        return map { String(format: "%02x", $0) }
+            .joined()
+    }
+}
+
+@objc
+public class SHA256Utils: NSObject {
+    @objc
+    public static func hash(data: Data) -> Data {
+        SHA256.hash(data: data).data
+    }
+}
+
+extension Digest {
+    var bytes: [UInt8] { Array(makeIterator()) }
+    var data: Data { Data(bytes) }
+}
+
+@objc
+public class ECKeyUtils: NSObject {
+    @objc static func exportJwt(publicKey: SecKey) throws -> String {
+        let publicKey = try! ECPublicKey(publicKey: publicKey)
+        return publicKey.jsonString()!
+    }
+
+    @objc static func thumbprint(publicKey: SecKey) throws -> String {
+        let publicKey = try! ECPublicKey(publicKey: publicKey)
+        return try! publicKey.thumbprint()
+    }
+
+    @objc static func pem(publicKeyRepresentation: Data) -> String {
+        print(publicKeyRepresentation.hexadecimal)
+
+        return try! P256.Signing.PublicKey(x963Representation: publicKeyRepresentation).pemRepresentation
+    }
+}
+
+extension OSStatus: Error {}
+extension String: Error {}
+
+
---
+++ waltid-libraries/waltid-crypto-ios/src/iosMain/kotlin/id/walt/crypto/P256.ios.kt
@@ -0,0 +1,217 @@
+package id.walt.crypto
+
+import id.walt.crypto.keys.JwkKeyMeta
+import id.walt.crypto.keys.Key
+import id.walt.crypto.keys.KeyMeta
+import id.walt.crypto.keys.KeyType
+import id.walt.platform.utils.ios.DS_Operations
+import id.walt.platform.utils.ios.ECKeyUtils
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.value
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonObject
+import platform.CoreFoundation.*
+import platform.Foundation.*
+import platform.Security.*
+
+private val ksecKeyType: CFStringRef? = kSecAttrKeyTypeECSECPrimeRandom
+
+class P256Key private constructor(
+    val keyId: String, override val keyType: KeyType = KeyType.secp256r1
+) : IosKey(), CoreFoundationSecOperations {
+
+
+    override val hasPrivateKey: Boolean
+        get() = true
+
+
+    override suspend fun getKeyId(): String = keyId
+
+    override suspend fun getThumbprint(): String = withSecKey(
+        this.keyId, ksecKeyType, null
+    ) { secKey ->
+        ECKeyUtils.thumbprintWithPublicKey(secKey, null)!!
+    }
+
+    override suspend fun exportJWKObject(): JsonObject =
+        exportJWK().let { Json.parseToJsonElement(it).jsonObject }
+
+    override suspend fun getPublicKey(): Key = this
+
+    override suspend fun exportJWK(): String = withSecKey(
+        this.keyId, ksecKeyType, null
+    ) { secKey ->
+        usePublicKey(secKey) { publicKey ->
+            ECKeyUtils.exportJwtWithPublicKey(publicKey, null)!!
+        }
+    }
+
+    override suspend fun exportPEM(): String = getPublicKeyRepresentation().let {
+        ECKeyUtils.pemWithPublicKeyRepresentation(it.toNSData())
+    }
+
+    override suspend fun signRaw(plaintext: ByteArray): ByteArray = signRaw(
+        keyId, ksecKeyType, kSecKeyAlgorithmECDSASignatureMessageX962SHA256, plaintext
+    )
+
+
+    override suspend fun signJws(bodyJson: ByteArray, headersJson: ByteArray): String = withSecKey(
+        keyId, ksecKeyType, null
+    ) { privateKey ->
+
+        val result = DS_Operations.signWithBody(
+            bodyJson.toNSData(), "ES256", privateKey, headersJson.toNSData()
+        )
+
+        check(result.success()) {
+            result.errorMessage()!!
+        }
+
+        result.data()!!
+    }
+
+    override suspend fun signJws(plaintext: ByteArray, headers: Map<String, String>): String =
+        withSecKey(
+            keyId, ksecKeyType, null
+        ) { privateKey ->
+            val result = DS_Operations.signWithBody(
+                plaintext.toNSData(), "ES256", privateKey, headers as Map<Any?, *>
+            )
+
+            check(result.success()) {
+                result.errorMessage()!!
+            }
+
+            result.data()!!
+        }
+
+    override suspend fun verifyRaw(
+        signature: ByteArray, signedData: ByteArray?
+    ): Result<ByteArray> = withSecKey(
+        keyId, ksecKeyType, null
+    ) { secKey ->
+        cfRetain(
+            signature.toNSData(), signedData!!.toNSData()
+        ) { signatureCF, signedDataCF ->
+            usePublicKey(secKey) { publicKey ->
+                verify(
+                    signatureCF,
+                    signedDataCF,
+                    publicKey,
+                    kSecKeyAlgorithmECDSASignatureMessageX962SHA256
+                )
+                Result.success(signedData)
+            }
+        }
+    }
+
+    override suspend fun verifyJws(signedJws: String): Result<JsonObject> = withSecKey(
+        keyId, ksecKeyType, null
+    ) { privateKey ->
+        usePublicKey(privateKey) { publicKey ->
+            val result = DS_Operations.verifyWithJws(signedJws, publicKey)
+
+            check(result.success()) {
+                result.errorMessage()!!
+            }
+
+            result.isValidData()!!.toByteArray().let {
+                Json.parseToJsonElement(it.decodeToString())
+            }.let {
+                Result.success(it.jsonObject)
+            }
+        }
+    }
+
+    override suspend fun getPublicKeyRepresentation(): ByteArray =
+        publicKeyExternalRepresentation(this.keyId, ksecKeyType)
+
+    override suspend fun getMeta(): KeyMeta = JwkKeyMeta(keyId, 256)
+
+
+    companion object : CoreFoundationSecOperations {
+        public fun create(kid: String, appId: String): P256Key {
+            delete(kid)
+
+            return cfRetain(kid.toNSData(), appId.toNSData()) { kidCf, appLabelCf ->
+
+                var privateKeyParams: CFMutableDictionaryRef? = null
+                var generateKeyPairQuery: CFMutableDictionaryRef? = null
+                val size: CFTypeRef? = CFBridgingRetain(NSNumber(int = 256))
+
+                try {
+                    privateKeyParams = CFDictionaryCreateMutable(
+                        kCFAllocatorDefault,
+                        4,
+                        kCFTypeDictionaryKeyCallBacks.ptr,
+                        kCFTypeDictionaryValueCallBacks.ptr
+                    ).apply {
+                        CFDictionaryAddValue(this, kSecAttrIsPermanent, kCFBooleanTrue)
+                        CFDictionaryAddValue(this, kSecAttrApplicationTag, kidCf)
+                        CFDictionaryAddValue(this, kSecAttrApplicationLabel, appLabelCf)
+                    }
+
+                    generateKeyPairQuery = CFDictionaryCreateMutable(
+                        kCFAllocatorDefault,
+                        3,
+                        kCFTypeDictionaryKeyCallBacks.ptr,
+                        kCFTypeDictionaryValueCallBacks.ptr
+                    ).apply {
+                        CFDictionaryAddValue(
+                            this, kSecAttrKeyType, kSecAttrKeyTypeECSECPrimeRandom
+                        )
+                        CFDictionaryAddValue(this, kSecAttrKeySizeInBits, size)
+                        CFDictionaryAddValue(this, kSecPrivateKeyAttrs, privateKeyParams)
+                    }
+
+                    val createRandomKeyError = alloc<CFErrorRefVar>()
+                    val privateKey = SecKeyCreateRandomKey(
+                        generateKeyPairQuery, createRandomKeyError.ptr
+                    )
+
+                    check(createRandomKeyError.value == null) {
+                        val nsError = CFBridgingRelease(createRandomKeyError.value) as NSError
+                        println(nsError.toString())
+                        nsError.toString()
+                    }
+
+                    P256Key(kid)
+
+                } finally {
+                    privateKeyParams?.let { CFBridgingRelease(it) }
+                    generateKeyPairQuery?.let { CFBridgingRelease(it) }
+                    CFBridgingRelease(size)
+                }
+            }
+        }
+
+        internal fun delete(kid: String) {
+            cfRetain(kid.toNSData()) { kidCF ->
+                operation(
+                    query(
+                        kidCF, ksecKeyType, null
+                    )
+                ) { query ->
+                    SecItemDelete(query)
+                }
+            }
+        }
+
+        internal fun load(kid: String): P256Key {
+            return withSecKey(
+                kid, ksecKeyType, null
+            ) {
+                P256Key(kid)
+            }
+        }
+
+        internal fun exist(kid: String) = try {
+            load(kid)
+            true
+        } catch (e: Throwable) {
+            false
+        }.also { println("P256:$kid:exist=$it") }
+    }
+}
