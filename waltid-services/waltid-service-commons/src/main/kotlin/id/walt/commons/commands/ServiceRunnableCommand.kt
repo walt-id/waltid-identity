f7be8885c93b5e3cf21c06385b528696cc55f57f
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-18 11:02:51 +0200

    fix: make it possible to pass configuration as command line arguments again

--- waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/commands/ServiceRunnableCommand.kt
+++ waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/commands/ServiceRunnableCommand.kt
@@ -1,12 +1,15 @@
 package id.walt.commons.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.choice
 import com.github.ajalt.clikt.parameters.types.enum
 import id.walt.commons.ServiceCommons
 import id.walt.commons.ServiceConfiguration
 import id.walt.commons.ServiceInitialization
+import id.walt.commons.config.statics.RunConfiguration
 import id.walt.commons.logging.LoggingManager
 import id.walt.commons.logging.RenderStrings
 import kotlinx.coroutines.runBlocking
@@ -18,11 +21,15 @@ class ServiceRunnableCommand(
     private val logLevel: String? by option("--log-level", "--logLevel", "-l").choice(*LoggingManager.loggingSetups.keys.toTypedArray())
     private val logType: RenderStrings? by option("--log-type", "--logType").enum<RenderStrings>()
 
+    private val configArgs by argument("config args", "pass configuration as command line arguments").multiple()
+
     init {
         println("-- ${config.vendor} ${config.name} ${config.version} --")
     }
 
     override fun run() {
+        RunConfiguration.configArgs = configArgs.toTypedArray()
+
         if (logLevel != null) {
             LoggingManager.useLoggingSetup(logLevel!!, logType)
         }
--- waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/config/statics/RunConfiguration.kt
+++ waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/config/statics/RunConfiguration.kt
@@ -3,5 +3,6 @@ package id.walt.commons.config.statics
 object RunConfiguration {
 
     lateinit var args: Array<String>
+    lateinit var configArgs: Array<String>
 
 }
