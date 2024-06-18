b8849c70b539114bee2e5c045d26a292771cb4bd
waltkb <68587968+waltkb@users.noreply.github.com>
2024-06-18 19:02:10 +0200

    deps: update json schema validator

--- waltid-libraries/waltid-verifiable-credentials/build.gradle.kts
+++ waltid-libraries/waltid-verifiable-credentials/build.gradle.kts
@@ -73,7 +73,7 @@ kotlin {
             dependencies {
                 // JSON
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
-                implementation("io.github.optimumcode:json-schema-validator:0.0.15")
+                implementation("io.github.optimumcode:json-schema-validator:0.2.0")
 
                 // Ktor client
                 implementation("io.ktor:ktor-client-core:$ktor_version")
