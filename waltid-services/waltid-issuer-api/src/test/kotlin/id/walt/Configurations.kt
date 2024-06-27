b91c5fa72ba4ffd1a178e8f67553e73cfbfd7aa8
Severin Stampler <severin@walt.id>
2024-06-27 17:39:40 +0200

    fix issuer tests and merge bugs

--- waltid-services/waltid-issuer-api/src/test/kotlin/id/walt/Configurations.kt
+++ waltid-services/waltid-issuer-api/src/test/kotlin/id/walt/Configurations.kt
@@ -1,9 +1,11 @@
 package id.walt
 
+import id.walt.commons.config.list.WebConfig
 import id.walt.issuer.config.CredentialTypeConfig
 import id.walt.issuer.config.OIDCIssuerServiceConfig
 
 val testConfigs = listOf(
+    "web" to WebConfig::class,
     "issuer-service" to OIDCIssuerServiceConfig::class,
     "credential-issuer-metadata" to CredentialTypeConfig::class
 )
