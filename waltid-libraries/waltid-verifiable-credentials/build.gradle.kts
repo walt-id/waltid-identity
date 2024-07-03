de3700e0ab3df53be670cf3f5d96d387b02fd47b
waltkb <68587968+waltkb@users.noreply.github.com>
2024-07-03 02:48:22 +0200

    deps: reverted versions to stable (after testing with beta versions in this PR)

--- waltid-libraries/waltid-verifiable-credentials/build.gradle.kts
+++ waltid-libraries/waltid-verifiable-credentials/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     kotlin("plugin.serialization")
     id("maven-publish")
     id("dev.petuska.npm.publish") version "3.4.3"
-    id("love.forte.plugin.suspend-transform") version "2.0.20-Beta1-0.9.1"
+    id("love.forte.plugin.suspend-transform") version "0.9.0"
     id("com.github.ben-manes.versions")
 }
 
