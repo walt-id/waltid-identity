64170047374253950c601bdfd0c3681178ce692e
mikeplotean <mike@walt.id>
2024-07-08 20:34:28 +0300

    refactor: exchange, issuer and verifier apis

--- waltid-services/waltid-e2e-tests/src/test/kotlin/CredentialsApi.kt
+++ waltid-services/waltid-e2e-tests/src/test/kotlin/CredentialsApi.kt
@@ -17,20 +17,19 @@ class CredentialsApi(private val client: HttpClient) {
         filter: CredentialFilterObject = CredentialFilterObject.default,
         expectedSize: Int = 0,
         vararg expectedCredential: String
-    ) =
-        test("/wallet-api/wallet/{wallet}/credentials - list credentials") {
-            client.get("/wallet-api/wallet/$wallet/credentials") {
-                url {
-                    filter.toMap().onEach {
-                        parameters.append(it.key, it.value.toString())
-                    }
+    ) = test("/wallet-api/wallet/{wallet}/credentials - list credentials") {
+        client.get("/wallet-api/wallet/$wallet/credentials") {
+            url {
+                filter.toMap().onEach {
+                    parameters.append(it.key, it.value.toString())
                 }
-            }.expectSuccess().apply {
-                val credentials = body<List<WalletCredential>>()
-                assert(credentials.size == expectedSize) { "should have $expectedSize credentials, but has ${credentials.size}" }
-                expectedCredential.onEach { cid -> assertNotNull(credentials.single { it.id == cid }) { "credential not found for id: $cid" } }
             }
+        }.expectSuccess().apply {
+            val credentials = body<List<WalletCredential>>()
+            assert(credentials.size == expectedSize) { "should have $expectedSize credentials, but has ${credentials.size}" }
+            expectedCredential.onEach { cid -> assertNotNull(credentials.single { it.id == cid }) { "credential not found for id: $cid" } }
         }
+    }
 
     suspend fun get(wallet: UUID, credential: String, output: ((WalletCredential) -> Unit)? = null) =
         test("/wallet-api/wallet/{wallet}/credentials/{credentialId} - get credential") {
@@ -49,10 +48,10 @@ class CredentialsApi(private val client: HttpClient) {
             }.expectSuccess()
         }
 
-    suspend fun restore(wallet: UUID, credential: String) =
+    suspend fun restore(wallet: UUID, credential: String, output: ((WalletCredential) -> Unit)? = null) =
         test("/wallet-api/wallet/{wallet}/credentials/{credentialId}/restore - restore credential") {
             client.post("/wallet-api/wallet/$wallet/credentials/$credential/restore").expectSuccess().apply {
-                val credential = body<WalletCredential>()
+                output?.invoke(body<WalletCredential>())
             }
         }
 
@@ -61,32 +60,38 @@ class CredentialsApi(private val client: HttpClient) {
             client.post("/wallet-api/wallet/$wallet/credentials/$credential/accept").expectSuccess()
         }
 
-    suspend fun reject(wallet: UUID, credential: String) =
+    suspend fun reject(wallet: UUID, credential: String, note: String? = null) =
         test("/wallet-api/wallet/{wallet}/credentials/{credentialId}/reject - reject credential") {
             client.post("/wallet-api/wallet/$wallet/credentials/$credential/reject") {
-                setBody(mapOf("note" to ""))
+                setBody(mapOf("note" to note))
             }.expectSuccess()
         }
 
-    suspend fun status(wallet: UUID, credential: String) =
+    suspend fun status(wallet: UUID, credential: String, output: ((List<CredentialStatusResult>) -> Unit)? = null) =
         test("/wallet-api/wallet/{wallet}/credentials/{credentialId}/status - get credential status") {
             client.get("/wallet-api/wallet/$wallet/credentials/$credential/status").expectSuccess().apply {
                 val result = body<List<CredentialStatusResult>>()
+                output?.invoke(result)
             }
         }
 
-    suspend fun attachCategory(wallet: UUID, credential: String, categories: List<String>) =
+    suspend fun attachCategory(wallet: UUID, credential: String, vararg categories: String) =
         test("/wallet-api/wallet/{wallet}/credentials/{credentialId}/category - attach category") {
             client.put("/wallet-api/wallet/$wallet/credentials/$credential/category") {
-                setBody(categories)
+                setBody(categories.toList())
             }.expectSuccess()
         }
 
-    suspend fun detachCategory(wallet: UUID, credential: String, categories: List<String>) =
+    suspend fun detachCategory(wallet: UUID, credential: String, vararg categories: String) =
         test("/wallet-api/wallet/{wallet}/credentials/{credentialId}/category - detach category") {
             client.delete("/wallet-api/wallet/$wallet/credentials/$credential/category") {
-                setBody(categories)
+                setBody(categories.toList())
             }.expectSuccess()
+        }
+
+    suspend fun store(wallet: UUID, credential: String) =
+        test("/wallet-api/wallet/{wallet}/credentials - store credential") {
+            TODO("Not implemented")
         }
 
     private fun CredentialFilterObject.toMap() = Json.encodeToJsonElement(this).jsonObject.toMap()
---
+++ waltid-services/waltid-e2e-tests/src/test/kotlin/VerifierApi.kt
@@ -0,0 +1,29 @@
+import E2ETestWebService.test
+import id.walt.verifier.oidc.PresentationSessionInfo
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+
+object Verifier {
+    class SessionApi(private val client: HttpClient) {
+        suspend fun get(verificationId: String, output: ((PresentationSessionInfo) -> Unit)? = null) =
+            test("/openid4vc/session/{id} - check if presentation definitions match") {
+                client.get("/openid4vc/session/$verificationId").expectSuccess().apply {
+                    output?.invoke(body<PresentationSessionInfo>())
+                }
+            }
+    }
+
+    class VerificationApi(private val client: HttpClient) {
+        suspend fun verify(payload: String, output: ((String) -> Unit)? = null) = test("/openid4vc/verify") {
+            client.post("/openid4vc/verify") {
+                setBody(Json.decodeFromString<JsonObject>(payload))
+            }.expectSuccess().apply {
+                output?.invoke(bodyAsText())
+            }
+        }
+    }
+}
