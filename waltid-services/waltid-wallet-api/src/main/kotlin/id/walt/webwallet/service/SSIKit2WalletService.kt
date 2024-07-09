7be8d3d218145863d9c507e6de9b33eb8de686ba
SuperBatata <khaledbattiche@gmail.com>
2024-07-09 16:41:45 +0100

    fix: fix verification for existing key

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/SSIKit2WalletService.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/SSIKit2WalletService.kt
@@ -454,10 +454,7 @@ class SSIKit2WalletService(
 
         val key = keyResult.getOrThrow()
         val keyId = key.getKeyId()
-        if (KeysService.get(walletId, keyId) !== null) {
-            throw IllegalArgumentException("Key with ID $keyId already exists in the database")
-        }
-
+        require(KeysService.get(walletId, keyId) === null) { "Key with ID $keyId already exists in the database" }
         eventUseCase.log(
             action = EventType.Key.Import,
             originator = "wallet",
