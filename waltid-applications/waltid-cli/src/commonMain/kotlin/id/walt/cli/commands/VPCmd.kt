c65b68cdd518b88c26617e70b4ecb97c50afdc7c
cpatsonakis <christos@walt.id>
2024-07-15 13:49:41 +0300

    docs: preliminary update on help messages of vp cmds

--- waltid-applications/waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/VPCmd.kt
+++ waltid-applications/waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/VPCmd.kt
@@ -1,16 +1,22 @@
 package id.walt.cli.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.core.subcommands
+import id.walt.cli.util.WaltIdCmdHelpOptionMessage
 
 class VPCmd : CliktCommand(
     name = "vp",
-    help = "Creating and verifying Verifiable Presentations",
+    help = "Create and apply a wide range of verification policies on W3C Verifiable Presentations (VPs).",
     printHelpOnEmptyArgs = true
 ) {
 
     init {
         subcommands(VPCreateCmd(), VPVerifyCmd())
+
+        context {
+            localization = WaltIdCmdHelpOptionMessage
+        }
     }
 
     override fun run(): Unit {}
