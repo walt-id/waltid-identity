0b4fa5e59125b9b2dd9de623906f423f24c2143c
cpatsonakis <christos@walt.id>
2024-07-15 14:47:55 +0300

    fix: add webdomain and webpath options in did create cmd to support creation of did:web

--- waltid-applications/waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/DidCreateCmd.kt
+++ waltid-applications/waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/DidCreateCmd.kt
@@ -2,14 +2,12 @@ package id.walt.cli.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.context
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.flag
-import com.github.ajalt.clikt.parameters.options.help
-import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.*
 import com.github.ajalt.clikt.parameters.types.enum
 import com.github.ajalt.clikt.parameters.types.file
 import id.walt.cli.util.*
 import id.walt.did.dids.registrar.dids.DidKeyCreateOptions
+import id.walt.did.dids.registrar.dids.DidWebCreateOptions
 import kotlinx.coroutines.runBlocking
 
 class DidCreateCmd : CliktCommand(
@@ -44,6 +42,13 @@ class DidCreateCmd : CliktCommand(
         .help("Flag to enable JWK_JCS-Pub encoding (default=off). Applies only to the did:key method and is relevant in the context of EBSI.")
         .flag(default = false)
 
+    private val webDomain by option("-wd", "--web-domain")
+        .help("The domain name to use when creating a did:web (required in this case).")
+
+    private val webPath by option("-wp", "--web-path")
+        .help("The URL path to append when creating a did:web (optional).")
+        .default("")
+
     override fun run() {
         runBlocking {
             val key = KeyUtil(this@DidCreateCmd).getKey(keyFile)
@@ -53,9 +58,22 @@ class DidCreateCmd : CliktCommand(
             print.green("DID Subject key to be used:")
             print.box(jwk)
 
+            if (method == DidMethod.WEB) {
+                if (webDomain == null || webDomain == "")
+                    throw IllegalArgumentException("The web domain cannot be null or any empty string when creating a did:web.")
+            }
+
             val result = if (useJwkJcsPub && method == DidMethod.KEY) {
-                DidUtil.createDid(method, key, DidKeyCreateOptions(key.keyType, useJwkJcsPub)) }
-            else
+                DidUtil.createDid(method, key, DidKeyCreateOptions(key.keyType, useJwkJcsPub))
+            } else if (method == DidMethod.WEB) {
+                if (webDomain == null || webDomain == "")
+                    throw IllegalArgumentException("The web domain cannot be null or any empty string when creating a did:web.")
+                DidUtil.createDid(
+                    method,
+                    key,
+                    DidWebCreateOptions(domain = webDomain!!, path = webPath, keyType = key.keyType)
+                )
+            } else
                 DidUtil.createDid(method, key)
 
             print.green("DID created:")
