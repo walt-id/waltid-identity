2f37af4c62c42b5a027d1d655f3114d6c36a14af
waltkb <68587968+waltkb@users.noreply.github.com>
2024-07-17 22:47:54 +0200

    build: fix Gradle warnings related to bundled annotations of plugin in waltid-crypto

--- waltid-libraries/waltid-crypto/build.gradle.kts
+++ waltid-libraries/waltid-crypto/build.gradle.kts
@@ -1,6 +1,7 @@
 import love.forte.plugin.suspendtrans.ClassInfo
 import love.forte.plugin.suspendtrans.SuspendTransformConfiguration
 import love.forte.plugin.suspendtrans.TargetPlatform
+import love.forte.plugin.suspendtrans.gradle.SuspendTransPluginConstants
 import love.forte.plugin.suspendtrans.gradle.SuspendTransformGradleExtension
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -24,6 +25,8 @@ suspendTransform {
     enabled = true
     includeRuntime = true
     useDefault()
+
+    includeAnnotation = false // Required in the current version to avoid "compileOnly" warning
 }
 
 tasks.withType<ProcessResources> {
@@ -130,6 +133,9 @@ kotlin {
 
                 // Logging
                 implementation("io.github.oshai:kotlin-logging:7.0.0")
+
+                // suspend-transform plugin annotations (required in the current version to avoid "compileOnly" warning)
+                implementation("${SuspendTransPluginConstants.ANNOTATION_GROUP}:${SuspendTransPluginConstants.ANNOTATION_NAME}:${SuspendTransPluginConstants.ANNOTATION_VERSION}")
             }
         }
         val commonTest by getting {
