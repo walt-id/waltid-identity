a7378e6aea1d6e81e6082ddce980fce1ca12977d
SuperBatata <khaledbattiche@gmail.com>
2024-07-18 10:39:06 +0100

    feat : added common exception class

--- waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/web/WebExceptions.kt
+++ waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/web/WebExceptions.kt
@@ -6,3 +6,6 @@ open class WebException(val status: HttpStatusCode, message: String) : Exception
 
 class UnauthorizedException(message: String) : WebException(HttpStatusCode.Unauthorized, message)
 class ConflictException(message: String) : WebException(HttpStatusCode.Conflict, message)
+class ForbiddenException(message: String) : WebException(HttpStatusCode.Forbidden, message)
+class BadRequestException(message: String) : WebException(HttpStatusCode.BadRequest, message)
+
