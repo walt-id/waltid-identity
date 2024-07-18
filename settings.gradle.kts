ed7991be19a8ac7c52d78fbe4b85a24657995ec2
Severin Stampler <severin@walt.id>
2024-07-18 18:18:51 +0200

    resolve rebase conflicts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -76,3 +76,28 @@ plugins {
 }
 
 rootProject.name = "waltid-identity"
+include(
+    // Base SSI libs
+    "waltid-crypto",
+    "waltid-did",
+    "waltid-verifiable-credentials",
+    "waltid-mdoc-credentials",
+    "waltid-sdjwt",
+
+    // Protocols
+    "waltid-openid4vc",
+
+    // Services based on libs
+    "waltid-issuer-api",
+    "waltid-verifier-api",
+    "waltid-wallet-api",
+
+    // CLI
+    "waltid-cli",
+
+    // Reporting
+    "waltid-reporting",
+
+  "waltid-credentials-base"
+)
+
