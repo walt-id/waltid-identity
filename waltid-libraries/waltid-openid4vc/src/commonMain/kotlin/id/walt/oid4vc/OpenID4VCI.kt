c1c9ac4ea107c98e38f887b3f1741f743efd7fff
chsavvaidis <ch.savvaidis@gmail.com>
2024-07-18 15:40:09 +0300

    feat: add authentication methods as isolated functions to support stateless implementation

--- waltid-libraries/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/OpenID4VCI.kt
+++ waltid-libraries/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/OpenID4VCI.kt
@@ -1,15 +1,29 @@
 package id.walt.oid4vc
 
-import id.walt.oid4vc.data.CredentialOffer
-import id.walt.oid4vc.data.OfferedCredential
-import id.walt.oid4vc.data.OpenIDProviderMetadata
+import id.walt.credentials.verification.Verifier
+import id.walt.credentials.verification.models.PolicyRequest.Companion.parsePolicyRequests
+import id.walt.crypto.keys.Key
+import id.walt.oid4vc.data.*
+import id.walt.oid4vc.data.ResponseType.Companion.getResponseTypeString
+import id.walt.oid4vc.data.dif.PresentationDefinition
+import id.walt.oid4vc.data.dif.PresentationSubmission
 import id.walt.oid4vc.definitions.CROSS_DEVICE_CREDENTIAL_OFFER_URL
+import id.walt.oid4vc.definitions.JWTClaims
+import id.walt.oid4vc.requests.AuthorizationRequest
 import id.walt.oid4vc.requests.CredentialOfferRequest
+import id.walt.oid4vc.requests.TokenRequest
+import id.walt.oid4vc.responses.TokenErrorCode
+import id.walt.oid4vc.errors.TokenError
+import id.walt.oid4vc.responses.AuthorizationCodeWithAuthorizationRequestResponse
+import id.walt.oid4vc.util.JwtUtils
 import id.walt.oid4vc.util.http
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.util.*
+import io.ktor.utils.io.core.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.json.Json
 
 object OpenID4VCI {
     fun getCredentialOfferRequestUrl(
@@ -86,64 +100,164 @@ object OpenID4VCI {
         }
     }
 
-//    fun processTokenRequest(tokenRequest: TokenRequest): TokenResponse {
-//        val code = when (tokenRequest.grantType) {
-//            GrantType.authorization_code -> tokenRequest.code ?: throw TokenError(
-//                tokenRequest = tokenRequest,
-//                errorCode = TokenErrorCode.invalid_grant,
-//                message = "No code parameter found on token request"
-//            )
-//
-//            GrantType.pre_authorized_code -> tokenRequest.preAuthorizedCode ?: throw TokenError(
-//                tokenRequest = tokenRequest,
-//                errorCode = TokenErrorCode.invalid_grant,
-//                message = "No pre-authorized_code parameter found on token request"
-//            )
-//
-//            else -> throw TokenError(tokenRequest, TokenErrorCode.unsupported_grant_type, "Grant type not supported")
-//        }
-//        val payload = validateAuthorizationCode(code) ?: throw TokenError(
-//            tokenRequest = tokenRequest,
-//            errorCode = TokenErrorCode.invalid_grant,
-//            message = "Authorization code could not be verified"
-//        )
-//
-//        val sessionId = payload["sub"]!!.jsonPrimitive.content
-//        val session = getVerifiedSession(sessionId) ?: throw TokenError(
-//            tokenRequest = tokenRequest,
-//            errorCode = TokenErrorCode.invalid_request,
-//            message = "No authorization session found for given authorization code, or session expired."
-//        )
-//
-//        return generateTokenResponse(session, tokenRequest)
-//    }
-//
-//    private fun generateTokenResponse(session: IssuanceSession, tokenRequest: TokenRequest, tokenProvider: ITokenProvider): TokenResponse {
-//        if (tokenRequest.grantType == GrantType.pre_authorized_code && !session.preAuthUserPin.isNullOrEmpty() &&
-//            session.preAuthUserPin != tokenRequest.userPin
-//        ) {
-//            throw TokenError(
-//                tokenRequest,
-//                TokenErrorCode.invalid_grant,
-//                message = "User PIN required for this issuance session has not been provided or PIN is wrong."
-//            )
-//        }
-//        return TokenResponse.success(
-//            generateToken(tokenProvider, session.id, TokenTarget.ACCESS, session.issuer),
-//            "bearer", state = session.authorizationRequest?.state,
-//            cNonce = session.cNonce,
-//            cNonceExpiresIn = session.expirationTimestamp - Clock.System.now()
-//            // TODO: authorization_pending, interval
-//        )
-//    }
-//
-//    private fun generateToken(tokenProvider: ITokenProvider, sub: String, audience: TokenTarget, issuer: String, tokenId: String? = null): String {
-//        return tokenProvider.signToken(audience, buildJsonObject {
-//            put(JWTClaims.Payload.subject, sub)
-//            put(JWTClaims.Payload.issuer, issuer)
-//            put(JWTClaims.Payload.audience, audience.name)
-//            tokenId?.let { put(JWTClaims.Payload.jwtID, it) }
-//        })
-//    }
+    fun validateAuthorizationRequestQueryString(authorizationRequestQueryString: String): AuthorizationRequest {
+        // Todo validate authorization request params e.g. client_id, etc
+        return parseAuthorizationRequestQueryString(authorizationRequestQueryString)
+    }
 
+    fun parseAuthorizationRequestQueryString(authorizationRequestQueryString: String): AuthorizationRequest {
+        return AuthorizationRequest.fromHttpQueryString(authorizationRequestQueryString)
+    }
+
+    fun validateTokenRequestRaw(tokenRequestRaw: Map<String, List<String>>, authorizationCode: String): TokenRequest {
+        val tokenRequest = parseTokenRequest(tokenRequestRaw)
+        validateAuthorizationCode(tokenRequest, authorizationCode).let {
+            if (!it) throw TokenError(tokenRequest, TokenErrorCode.invalid_request, "Code is not valid")
+        }
+        return tokenRequest
+    }
+
+    fun parseTokenRequest(tokenRequestRaw: Map<String, List<String>>): TokenRequest {
+        return TokenRequest.fromHttpParameters(tokenRequestRaw)
+    }
+
+    fun validateAuthorizationCode(tokenRequest: TokenRequest, authorizationCode: String): Boolean {
+        val code = when (tokenRequest.grantType) {
+            GrantType.authorization_code -> tokenRequest.code ?: throw TokenError(
+                tokenRequest = tokenRequest,
+                errorCode = TokenErrorCode.invalid_grant,
+                message = "No code parameter found on token request"
+            )
+
+            GrantType.pre_authorized_code -> tokenRequest.preAuthorizedCode ?: throw TokenError(
+                tokenRequest = tokenRequest,
+                errorCode = TokenErrorCode.invalid_grant,
+                message = "No pre-authorized_code parameter found on token request"
+            )
+
+            else -> throw TokenError(tokenRequest, TokenErrorCode.unsupported_grant_type, "Grant type not supported")
+        }
+        return code == authorizationCode
+    }
+
+    suspend fun signToken(privateKey: Key, payload: JsonObject, headers: Map<String, String>? = null): String {
+        return privateKey.signJws(payload.toString().toByteArray(), headers ?: emptyMap())
+    }
+
+    suspend fun verifyToken(token: String, key: Key): Result<JsonElement> {
+        return key.verifyJws(token)
+    }
+
+    suspend fun generateAuthorizationRequest(
+        authorizationRequest: AuthorizationRequest,
+        clientId: String,
+        privKey: Key,
+        responseType: ResponseType,
+        state: String,
+        nonce: String,
+        isJar: Boolean? = true,
+        presentationDefinition: PresentationDefinition? = null
+    ): AuthorizationCodeWithAuthorizationRequestResponse {
+        val authorizationResponseServerMode = ResponseMode.direct_post
+        val redirectUri = clientId + "/direct_post"
+        val scope = setOf("openid")
+
+        return AuthorizationCodeWithAuthorizationRequestResponse.success(
+            state = state,
+            clientId = clientId,
+            redirectUri = redirectUri,
+            responseType = getResponseTypeString(responseType),
+            responseMode = authorizationResponseServerMode,
+            scope = scope,
+            nonce = nonce,
+            requestUri = null,
+            request = when (isJar!!) {
+                // Create a jwt as request object as defined in JAR OAuth2.0 specification
+                true -> signToken(
+                    privKey,
+                    buildJsonObject {
+                        put(JWTClaims.Payload.issuer, clientId)
+                        put(JWTClaims.Payload.audience, authorizationRequest.clientId)
+                        put(JWTClaims.Payload.nonce, nonce)
+                        put("state", state)
+                        put("client_id", clientId)
+                        put("redirect_uri", redirectUri)
+                        put("response_type", getResponseTypeString(responseType))
+                        put("response_mode", authorizationResponseServerMode.name)
+                        put("scope", "openid")
+                        when (responseType) {
+                            ResponseType.VpToken -> put("presentation_definition", presentationDefinition!!.toJSON())
+                            else -> null
+                        }
+                    }
+                )
+
+                false -> null
+            },
+            presentationDefinition = when (responseType) {
+                ResponseType.VpToken -> presentationDefinition!!.toJSONString()
+                else -> null
+            }
+        )
+    }
+
+    suspend fun validateAuthorizationRequestToken(
+        token: String,
+        presentationSubmission: PresentationSubmission? = null
+    ): JsonObject {
+        // 1. Validate Header
+        val header = JwtUtils.parseJWTHeader(token)
+
+        if (!header.keys.containsAll(
+                setOf(
+                    JWTClaims.Header.type,
+                    JWTClaims.Header.keyID,
+                    JWTClaims.Header.algorithm,
+                )
+            )
+        ) {
+            throw IllegalStateException("Invalid header in token")
+        }
+
+        // 2. Validate Payload
+        val payload = JwtUtils.parseJWTPayload(token)
+        if (!payload.keys.containsAll(
+                setOf(
+                    JWTClaims.Payload.issuer,
+                    JWTClaims.Payload.audience,
+                    JWTClaims.Payload.issuedAtTime,
+                    JWTClaims.Payload.nonce,
+                )
+            )
+        ) {
+            throw IllegalArgumentException("Invalid payload in token")
+        }
+
+        // 3. Verify iss = sub = did
+        val sub = payload[JWTClaims.Payload.subject]!!.jsonPrimitive.content
+        val iss = payload[JWTClaims.Payload.issuer]!!.jsonPrimitive.content
+        val kid = header[JWTClaims.Header.keyID]!!.jsonPrimitive.content
+        val did = kid.substringBefore("#")
+
+        if (iss != sub || iss != did || sub != did) {
+            println("$sub $iss $did")
+            throw IllegalArgumentException("Invalid payload in token. sub != iss != did")
+        }
+
+        // 4. Verify VP or ID Token
+        val policies =
+            Json.parseToJsonElement("""["signature", "expired", "not-before"]""").jsonArray.parsePolicyRequests()
+        Verifier.verifyPresentation(
+            vpTokenJwt = token,
+            vpPolicies = policies,
+            globalVcPolicies = policies,
+            specificCredentialPolicies = emptyMap(),
+            when (presentationSubmission != null) {
+                true -> mapOf("presentationSubmission" to presentationSubmission)
+                else -> emptyMap()
+            }
+        )
+
+        return payload
+    }
 }
