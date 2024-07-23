17c1ff62c3431dcb5ed1216b92d25116b2b46119
Ivan Pagac <ivan.pagac@accenture.com>
2024-07-23 14:05:26 +0200

    update version kotlinx-serialization-json on mobile projects to 1.7.1, separate sdjwt-ios to module

--- waltid-libraries/waltid-target-ios/build.gradle.kts
+++ waltid-libraries/waltid-target-ios/build.gradle.kts
@@ -55,7 +55,7 @@ kotlin {
 
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
             }
         }
