d18a8db47fcc2df093457941c3871216946e560b
waltkb <68587968+waltkb@users.noreply.github.com>
2024-07-23 23:00:49 +0200

    feat: init auth kit

---
+++ waltid-libraries/waltid-authkit/src/main/kotlin/id/walt/plugins/Monitoring.kt
@@ -0,0 +1,15 @@
+package id.walt.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.slf4j.event.*
+
+fun Application.configureMonitoring() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+}
