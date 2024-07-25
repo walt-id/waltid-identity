3ce31d77e821d16c72285c55ac98b0aeac59aabe
mikeplotean <mike@walt.id>
2024-07-25 22:01:54 +0300

    refactor: expiration-date-policy

--- waltid-libraries/waltid-verifiable-credentials/src/commonMain/kotlin/id/walt/credentials/verification/policies/ExpirationDatePolicy.kt
+++ waltid-libraries/waltid-verifiable-credentials/src/commonMain/kotlin/id/walt/credentials/verification/policies/ExpirationDatePolicy.kt
@@ -1,12 +1,17 @@
 package id.walt.credentials.verification.policies
 
+import id.walt.credentials.Claims
 import id.walt.credentials.JwtClaims
 import id.walt.credentials.VcClaims
 import id.walt.credentials.verification.CredentialWrapperValidatorPolicy
+import id.walt.credentials.verification.DatePolicyUtils.checkJwt
+import id.walt.credentials.verification.DatePolicyUtils.checkVc
 import id.walt.credentials.verification.ExpirationDatePolicyException
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
-import kotlinx.serialization.json.*
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.jsonObject
 import love.forte.plugin.suspendtrans.annotation.JsPromise
 import love.forte.plugin.suspendtrans.annotation.JvmAsync
 import love.forte.plugin.suspendtrans.annotation.JvmBlocking
@@ -18,6 +23,9 @@ import kotlin.js.JsExport
 class ExpirationDatePolicy : CredentialWrapperValidatorPolicy(
     "expired", "Verifies that the credentials expiration date (`exp` for JWTs) has not been exceeded."
 ) {
+    private val vcClaims = listOf<Claims>(VcClaims.V2.NotAfter, VcClaims.V1.NotAfter)
+    private val jwtClaims = listOf(JwtClaims.NotAfter)
+
     @JvmBlocking
     @JvmAsync
     @JsPromise
@@ -34,39 +42,27 @@ class ExpirationDatePolicy : CredentialWrapperValidatorPolicy(
         }
     }
 
-    private fun getExpirationKeyValuePair(data: JsonObject): Pair<String, Instant>? =
-        checkVc(data["vc"]?.jsonObject) ?: checkVc(data) ?: checkJwt(data)
-
-    private fun checkJwt(data: JsonObject?) =
-        data?.jsonObject?.get(JwtClaims.NotAfter.getValue())?.jsonPrimitive?.longOrNull?.let {
-            Pair("jwt:${JwtClaims.NotAfter.getValue()}", Instant.fromEpochSeconds(it))
-        }
-
-    private fun checkVc(data: JsonObject?) =
-        data?.get(VcClaims.V2.NotAfter.getValue())?.jsonPrimitive?.let {
-            Pair(VcClaims.V2.NotAfter.getValue(), Instant.parse(it.content))
-        } ?: data?.get(VcClaims.V1.NotAfter.getValue())?.jsonPrimitive?.let {
-            Pair(VcClaims.V1.NotAfter.getValue(), Instant.parse(it.content))
-        }
+    private fun getExpirationKeyValuePair(data: JsonObject): Pair<Claims, Instant>? =
+        checkVc(data["vc"]?.jsonObject, vcClaims) ?: checkVc(data, vcClaims) ?: checkJwt(data, jwtClaims)
 
     private fun buildPolicyUnavailableResult() = Result.success(
         JsonObject(mapOf("policy_available" to JsonPrimitive(false)))
     )
 
-    private fun buildFailureResult(now: Instant, exp: Instant, key: String) = (now - exp).let {
+    private fun buildFailureResult(now: Instant, exp: Instant, key: Claims) = (now - exp).let {
         Result.failure<ExpirationDatePolicyException>(
             ExpirationDatePolicyException(
                 date = exp,
                 dateSeconds = exp.epochSeconds,
                 expiredSince = it,
                 expiredSinceSeconds = it.inWholeSeconds,
-                key = key,
+                key = key.getValue(),
                 policyAvailable = true
             )
         )
     }
 
-    private fun buildSuccessResult(now: Instant, exp: Instant, key: String) = (exp - now).let {
+    private fun buildSuccessResult(now: Instant, exp: Instant, key: Claims) = (exp - now).let {
         Result.success(
             JsonObject(
                 mapOf(
@@ -74,7 +70,7 @@ class ExpirationDatePolicy : CredentialWrapperValidatorPolicy(
                     "date_seconds" to JsonPrimitive(exp.epochSeconds),
                     "expires_in" to JsonPrimitive(it.toString()),
                     "expires_in_seconds" to JsonPrimitive(it.inWholeSeconds),
-                    "used_key" to JsonPrimitive(key),
+                    "used_key" to JsonPrimitive(key.getValue()),
                     "policy_available" to JsonPrimitive(true)
                 )
             )
