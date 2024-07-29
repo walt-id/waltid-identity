0ee1696c134a9d29a22dd01cf16925e48ff81b4e
waltkb <68587968+waltkb@users.noreply.github.com>
2024-07-29 23:12:03 +0200

    feat: add log format compatible with e.g. Datadog, New Relic

--- waltid-services/waltid-service-commons/build.gradle.kts
+++ waltid-services/waltid-service-commons/build.gradle.kts
@@ -23,8 +23,8 @@ dependencies {
     api("io.ktor:ktor-serialization-kotlinx-json-jvm:${Versions.KTOR_VERSION}")
 
     // Logging
-    api("io.klogging:klogging-jvm:0.6.1") // JVM + ~JS
-    implementation("io.klogging:slf4j-klogging:0.6.0")
+    api("io.klogging:klogging-jvm:0.7.0") // JVM + ~JS
+    implementation("io.klogging:slf4j-klogging:0.7.0")
     implementation("org.slf4j:jul-to-slf4j:2.0.13")
 
     // CLI
