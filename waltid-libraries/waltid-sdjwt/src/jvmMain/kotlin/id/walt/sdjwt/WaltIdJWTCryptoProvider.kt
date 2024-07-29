9ac20b13313596e525333117f92c05effd135b07
Severin Stampler <severin@walt.id>
2024-07-29 15:59:11 +0200

    some formatting

--- waltid-libraries/waltid-sdjwt/src/jvmMain/kotlin/id/walt/sdjwt/WaltIdJWTCryptoProvider.kt
+++ waltid-libraries/waltid-sdjwt/src/jvmMain/kotlin/id/walt/sdjwt/WaltIdJWTCryptoProvider.kt
@@ -9,13 +9,20 @@ class WaltIdJWTCryptoProvider(val keys: Map<String, Key>): JWTCryptoProvider {
   override fun sign(payload: JsonObject, keyID: String?, typ: String, headers: Map<String, Any>): String = runBlocking {
     val key = keyID?.let { keys[it] } ?: throw Exception("No key found")
     if(!key.hasPrivateKey) throw Exception("Key has no private key")
-    return@runBlocking key.signJws(payload.toString().encodeToByteArray(), headers.mapValues { it.toJsonElement() })
+    return@runBlocking key.signJws(
+      payload.toString().encodeToByteArray(),
+      headers.mapValues {
+        it.toJsonElement()
+      })
   }
 
   override fun verify(jwt: String, keyID: String?): JwtVerificationResult = runBlocking {
     val key = keyID?.let { keys[it] } ?: throw Exception("No key found")
     return@runBlocking key.verifyJws(jwt).let {
-      JwtVerificationResult(it.isSuccess, it.toString())
+      JwtVerificationResult(
+        it.isSuccess,
+        it.toString()
+      )
     }
   }
 
