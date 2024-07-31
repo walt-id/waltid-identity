eaf1ff02285c60a614480ce6ea72323c62af1e9f
waltkb <68587968+waltkb@users.noreply.github.com>
2024-08-01 01:02:47 +0200

    test: fix ImportJwkKeyTest

---
+++ waltid-libraries/credentials/waltid-dif-presentation-exchange/src/commonMain/kotlin/id/walt/presentationexchange/PresentationDefinitionParser.kt
@@ -0,0 +1,11 @@
+package id.walt.presentationexchange
+
+import kotlinx.serialization.json.JsonObject
+
+object PresentationDefinitionParser {
+
+    fun matchCredentialsForDefinition(credentials: List<JsonObject>, definition: PresentationDefinition) {
+
+    }
+
+}
--- waltid-libraries/crypto/waltid-crypto/src/jvmTest/kotlin/ImportJWKKeyTestsAndDidManagement.kt
+++ waltid-libraries/crypto/waltid-crypto/src/jvmTest/kotlin/ImportJWKKeyTestsAndDidManagement.kt
@@ -5,20 +5,14 @@ import TestUtils.loadResourceBytes
 import id.walt.crypto.keys.KeyType
 import id.walt.crypto.keys.jwk.JWKKey
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.Arguments.arguments
-import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
+import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ImportJWKKeyTestsAndDidManagement {
 
-    @ParameterizedTest
-    @MethodSource
-    fun `given jwk string, when imported then the import succeeds having the correct key type, key id and hasPrivate values`(
-        keyString: String, keyType: KeyType, isPrivate: Boolean
+    private fun testJwkImport(
+        keyString: String, keyType: KeyType, isPrivate: Boolean,
     ) = runTest {
         // Importing
         val imported = JWKKey.importJWK(keyString)
@@ -34,10 +28,8 @@ class ImportJWKKeyTestsAndDidManagement {
         assertEquals(key.getThumbprint(), key.getKeyId())
     }
 
-    @ParameterizedTest
-    @MethodSource
-    fun `given pem string, when imported then the import succeeds having the correct key type, key id and hasPrivate values`(
-        keyString: String, keyType: KeyType, isPrivate: Boolean
+    private fun testPemImport(
+        keyString: String, keyType: KeyType, isPrivate: Boolean,
     ) = runTest {
         println("> Importing supposed $keyType (${if (isPrivate) "private" else "public"}) as PEM:")
 
@@ -57,10 +49,8 @@ class ImportJWKKeyTestsAndDidManagement {
         println("All checks succeeded for: $keyType (${if (isPrivate) "private" else "public"})")
     }
 
-    @ParameterizedTest
-    @MethodSource
-    fun `given raw string, when imported then the import succeeds having the correct key type, key id and hasPrivate values`(
-        bytes: ByteArray, keyType: KeyType, isPrivate: Boolean
+    private fun testRawImport(
+        bytes: ByteArray, keyType: KeyType, isPrivate: Boolean,
     ) = runTest {
         // Importing
         val key = JWKKey.importRawPublicKey(keyType, bytes, null)
@@ -74,69 +64,51 @@ class ImportJWKKeyTestsAndDidManagement {
         println(key.exportJWK())
     }
 
+    @Test
+    fun testJwkEd25519PrivateImport() = testJwkImport(loadJwkLocal("ed25519.private.json"), KeyType.Ed25519, true)
+    @Test
+    fun testJwkSecp256k1PrivateImport() = testJwkImport(loadJwkLocal("secp256k1.private.json"), KeyType.secp256k1, true)
+    @Test
+    fun testJwkSecp256r1PrivateImport() = testJwkImport(loadJwkLocal("secp256r1.private.json"), KeyType.secp256r1, true)
+    @Test
+    fun testJwkRsaPrivateImport() = testJwkImport(loadJwkLocal("rsa.private.json"), KeyType.RSA, true)
 
-    companion object {
-        @JvmStatic
-        fun `given jwk string, when imported then the import succeeds having the correct key type, key id and hasPrivate values`(): Stream<Arguments> =
-            Stream.of(
-                // ed25519
-                arguments(loadJwkLocal("ed25519.private.json"), KeyType.Ed25519, true),
-                // secp256k1
-                arguments(loadJwkLocal("secp256k1.private.json"), KeyType.secp256k1, true),
-                // secp256r1
-                arguments(loadJwkLocal("secp256r1.private.json"), KeyType.secp256r1, true),
-                // rsa
-                arguments(loadJwkLocal("rsa.private.json"), KeyType.RSA, true),
-                // public
-                // ed25519
-                arguments(loadJwkLocal("ed25519.public.json"), KeyType.Ed25519, false),
-                // secp256k1
-                arguments(loadJwkLocal("secp256k1.public.json"), KeyType.secp256k1, false),
-                // secp256r1
-                arguments(loadJwkLocal("secp256r1.public.json"), KeyType.secp256r1, false),
-                // rsa
-                arguments(loadJwkLocal("rsa.public.json"), KeyType.RSA, false),
-            )
+    @Test
+    fun testJwkEd25519PublicImport() = testJwkImport(loadJwkLocal("ed25519.public.json"), KeyType.Ed25519, false)
+    @Test
+    fun testJwkSecp256k1PublicImport() = testJwkImport(loadJwkLocal("secp256k1.public.json"), KeyType.secp256k1, false)
+    @Test
+    fun testJwkSecp256r1PublicImport() = testJwkImport(loadJwkLocal("secp256r1.public.json"), KeyType.secp256r1, false)
+    @Test
+    fun testJwkRsaPublicImport() = testJwkImport(loadJwkLocal("rsa.public.json"), KeyType.RSA, false)
 
-        @JvmStatic
-        fun `given pem string, when imported then the import succeeds having the correct key type, key id and hasPrivate values`(): Stream<Arguments> =
-            Stream.of(
-                // ed25519 (not implemented)
-                //arguments(loadPemLocal("ed25519.private.pem"), KeyType.Ed25519, true),
-                // secp256k1
-                arguments(loadPemLocal("secp256k1.private.pem"), KeyType.secp256k1, true),
-                // secp256r1
-                arguments(loadPemLocal("secp256r1.private.pem"), KeyType.secp256r1, true),
-                // rsa
-                arguments(loadPemLocal("rsa.private.pem"), KeyType.RSA, true),
-                // public
-                // ed25519 (not implemented)
-                //arguments(loadPemLocal("ed25519.public.pem"), KeyType.Ed25519, false),
-                // secp256k1
-                arguments(loadPemLocal("secp256k1.public.pem"), KeyType.secp256k1, false),
-                // secp256r1
-                arguments(loadPemLocal("secp256r1.public.pem"), KeyType.secp256r1, false),
-                // rsa
-                arguments(loadPemLocal("rsa.public.pem"), KeyType.RSA, false),
-                // rsa (exported from Hashicorp Vault)
-                arguments(loadPemLocal("rsa-vault.public.pem"), KeyType.RSA, false),
-                // ECDSA P-256 / Secp256r1 (exported from Hashicorp Vault)
-                arguments(loadPemLocal("ecdsa-vault.public.pem"), KeyType.secp256r1, false),
-            )
+    @Test
+    fun testPemSecp256k1PrivateImport() = testPemImport(loadPemLocal("secp256k1.private.pem"), KeyType.secp256k1, true)
+    @Test
+    fun testPemSecp256r1PrivateImport() = testPemImport(loadPemLocal("secp256r1.private.pem"), KeyType.secp256r1, true)
+    @Test
+    fun testPemRsaPrivateImport() = testPemImport(loadPemLocal("rsa.private.pem"), KeyType.RSA, true)
 
-        @JvmStatic
-        fun `given raw string, when imported then the import succeeds having the correct key type, key id and hasPrivate values`(): Stream<Arguments> =
-            Stream.of(
-                arguments(loadResourceBytes("public-bytes/ed25519.bin"), KeyType.Ed25519, false),
+    @Test
+    fun testPemSecp256k1PublicImport() = testPemImport(loadPemLocal("secp256k1.public.pem"), KeyType.secp256k1, false)
+    @Test
+    fun testPemSecp256r1PublicImport() = testPemImport(loadPemLocal("secp256r1.public.pem"), KeyType.secp256r1, false)
+    @Test
+    fun testPemRsaPublicImport() = testPemImport(loadPemLocal("rsa.public.pem"), KeyType.RSA, false)
+    @Test
+    fun testPemRsaVaultPublicImport() = testPemImport(loadPemLocal("rsa-vault.public.pem"), KeyType.RSA, false)
+    @Test
+    fun testPemEcdsaVaultPublicImport() = testPemImport(loadPemLocal("ecdsa-vault.public.pem"), KeyType.secp256r1, false)
 
-                arguments(loadResourceBase64("public-bytes/ed25519-vault.base64"), KeyType.Ed25519, false),
+    @Test
+    fun testRawEd25519PublicImport() = testRawImport(loadResourceBytes("public-bytes/ed25519.bin"), KeyType.Ed25519, false)
+    @Test
+    fun testRawEd25519VaultPublicImport() = testRawImport(loadResourceBase64("public-bytes/ed25519-vault.base64"), KeyType.Ed25519, false)
+    @Test
+    fun testRawSecp256k1PublicImport() = testRawImport(loadResourceBytes("public-bytes/secp256k1.bin"), KeyType.secp256k1, false)
+    @Test
+    fun testRawSecp256r1PublicImport() = testRawImport(loadResourceBytes("public-bytes/secp256r1.bin"), KeyType.secp256r1, false)
+    @Test
+    fun testRawRsaPublicImport() = testRawImport(loadResourceBytes("public-bytes/rsa.bin"), KeyType.RSA, false)
 
-                // secp256k1
-                arguments(loadResourceBytes("public-bytes/secp256k1.bin"), KeyType.secp256k1, false),
-                // secp256r1
-                arguments(loadResourceBytes("public-bytes/secp256r1.bin"), KeyType.secp256r1, false),
-                arguments(loadResourceBytes("public-bytes/rsa.bin"), KeyType.RSA, false),
-            )
-
-    }
 }
