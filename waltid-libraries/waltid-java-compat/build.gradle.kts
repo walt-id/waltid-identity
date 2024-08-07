f9555813cc9e0168fe14498f98989fd80f7aeaab
waltkb <68587968+waltkb@users.noreply.github.com>
2024-08-07 03:15:56 +0200

    feat: make java compat publishable

--- waltid-libraries/waltid-java-compat/build.gradle.kts
+++ waltid-libraries/waltid-java-compat/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 group = "id.walt"
@@ -11,6 +12,36 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+
+publishing {
+    repositories {
+        val envUsername = System.getenv("MAVEN_USERNAME")
+        val envPassword = System.getenv("MAVEN_PASSWORD")
+        val usernameFile = File("secret_maven_username.txt")
+        val passwordFile = File("secret_maven_password.txt")
+        val secretMavenUsername =
+            envUsername ?: usernameFile.let { if (it.isFile) it.readLines().first() else "" }
+        val secretMavenPassword =
+            envPassword ?: passwordFile.let { if (it.isFile) it.readLines().first() else "" }
+        val hasMavenAuth = secretMavenUsername.isNotEmpty() && secretMavenPassword.isNotEmpty()
+        if (hasMavenAuth) {
+            maven {
+                val releasesRepoUrl = uri("https://maven.waltid.dev/releases")
+                val snapshotsRepoUrl = uri("https://maven.waltid.dev/snapshots")
+                url = uri(
+                    if (version.toString()
+                            .endsWith("SNAPSHOT")
+                    ) snapshotsRepoUrl else releasesRepoUrl
+                )
+                credentials {
+                    username = secretMavenUsername
+                    password = secretMavenPassword
+                }
+            }
+        }
+    }
 }
 
 tasks.test {
