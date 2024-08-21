38e132069acb30a1ee803b77e3585f5245053e92
kodisj <kodisj@gmail.com>
2024-08-21 20:38:18 +0530

    Added cognito urls and baseurl for wallet alb

--- waltid-services/waltid-issuer-api/src/main/kotlin/id/walt/issuer/config/AuthenticationServiceConfig.kt
+++ waltid-services/waltid-issuer-api/src/main/kotlin/id/walt/issuer/config/AuthenticationServiceConfig.kt
@@ -3,9 +3,9 @@ package id.walt.issuer.config
 import id.walt.commons.config.WaltConfig
 
 data class AuthenticationServiceConfig(
-    val name: String = "Keycloak",
-    val authorizeUrl: String = "https://keycloak.walt-test.cloud/realms/waltid-keycloak-ktor/protocol/openid-connect/auth",
-    val accessTokenUrl: String = "https://keycloak.walt-test.cloud/realms/waltid-keycloak-ktor/protocol/openid-connect/token",
-    val clientId: String = "issuer_api",
-    val clientSecret: String = "G9istkjAEOqk9WpZGLyEXuTf34eIRPob",
+    val name: String = "AWS-Documint-Cognito-UserPool",
+    val authorizeUrl: String = "https://documint.auth.ap-south-1.amazoncognito.com/oauth2/authorize",
+    val accessTokenUrl: String = "https://documint.auth.ap-south-1.amazoncognito.com/oauth2/token",
+    val clientId: String = "30375ch7m71i1mbarifp1fc27f",
+    val clientSecret: String = "r5qudm9lk44tj9ugbnd12e5bmo369vqp25fr7evjrathon5aj9b",
 )  : WaltConfig()
