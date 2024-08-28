85c2b732370273e3fd90929d6773c3de4bb2e05e
waltkb <68587968+waltkb@users.noreply.github.com>
2024-08-28 03:19:18 +0200

    feat: update LDAP method

--- waltid-libraries/auth/waltid-authkit/src/main/kotlin/id/walt/authkit/methods/LDAP.kt
+++ waltid-libraries/auth/waltid-authkit/src/main/kotlin/id/walt/authkit/methods/LDAP.kt
@@ -1,124 +1,62 @@
 package id.walt.authkit.methods
 
+import id.walt.authkit.AuthContext
+import id.walt.authkit.exceptions.authFailure
+import id.walt.authkit.methods.data.AuthMethodStoredData
+import id.walt.authkit.sessions.AuthSession
+import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import java.util.*
-import javax.naming.*
-import javax.naming.directory.*
+import io.ktor.server.routing.*
+import io.ktor.util.pipeline.*
+import kotlinx.serialization.Serializable
+import org.apache.directory.api.ldap.model.exception.LdapException
+import org.apache.directory.ldap.client.api.LdapConnection
+import org.apache.directory.ldap.client.api.LdapNetworkConnection
 
-/**
- * Provides the ability to authenticate an LDAP user.
- * This function accepts a credential and validates it against a specified LDAP server.
- *
- * To learn more about LDAP authentication in Ktor, see [LDAP](https://ktor.io/docs/ldap.html).
- */
-public fun <K : Credential, P : Any> ldapAuthenticate(
-    credential: K,
-    ldapServerURL: String,
-    ldapEnvironmentBuilder: (MutableMap<String, Any?>) -> Unit = {},
-    doVerify: InitialDirContext.(K) -> P?
-): P? {
-    return try {
-        val root = ldapLogin(ldapServerURL, ldapEnvironmentBuilder)
+object LDAP : UserPassBasedAuthMethod("ldap") {
+
+    @Serializable
+    data class LDAPConfiguration(
+        val ldapServerUrl: String,
+        val userDNFormat: String
+    ): AuthMethodStoredData
+
+    // Todo: Move to configuration (is not stored data)
+
+    override suspend fun auth(session: AuthSession, credential: UserPasswordCredential) {
+        //val identifier = UsernameIdentifier(credential.name)
+        //val storedData: UserPassStoredData = lookupStoredData(identifier /*context()*/)
+
+        val ldapServerUrl = "ldap://localhost:3893"
+        val userDNFormat = "cn=%s,ou=superheros,dc=glauth,dc=com"
+        val username = "hackers"
+        val password = "dogood"
+
+        val (hostname, port) = ldapServerUrl.removePrefix("ldap://").split(":")
+        val userDN = userDNFormat.format(username)
+
+        var connection: LdapConnection? = null
+
         try {
-            root.doVerify(credential)
+            connection = LdapNetworkConnection(hostname, port.toInt())
+            connection.bind(userDN, password)
+        } catch (e: LdapException) {
+            e.printStackTrace()
+            authFailure(e.message ?: "LDAP auth failed")
         } finally {
-            root.close()
-        }
-    } catch (ne: NamingException) {
-        null
-    }
-}
-
-/**
- * Provides the ability to authenticates an LDAP user.
- * This function accepts [UserPasswordCredential] and validates it against a specified LDAP server.
- *
- * To learn more about LDAP authentication in Ktor, see [LDAP](https://ktor.io/docs/ldap.html).
- */
-public fun <P : Principal> ldapAuthenticate(
-    credential: UserPasswordCredential,
-    ldapServerURL: String,
-    userDNFormat: String,
-    validate: InitialDirContext.(UserPasswordCredential) -> P?
-): P? {
-    val configurator: (MutableMap<String, Any?>) -> Unit = { env ->
-        env[Context.SECURITY_AUTHENTICATION] = "simple"
-        env[Context.SECURITY_PRINCIPAL] = userDNFormat.format(ldapEscape(credential.name))
-        env[Context.SECURITY_CREDENTIALS] = credential.password
-    }
-
-    return ldapAuthenticate(credential, ldapServerURL, configurator, validate)
-}
-
-/**
- * Provides the ability to authenticates an LDAP user.
- * This function accepts [UserPasswordCredential] and validates it against a specified LDAP server.
- *
- * To learn more about LDAP authentication in Ktor, see [LDAP](https://ktor.io/docs/ldap.html).
- */
-public fun ldapAuthenticate(
-    credential: UserPasswordCredential,
-    ldapServerURL: String,
-    userDNFormat: String
-): UserIdPrincipal? {
-    return ldapAuthenticate(credential, ldapServerURL, userDNFormat) { UserIdPrincipal(it.name) }
-}
-
-private fun ldapLogin(ldapURL: String, ldapEnvironmentBuilder: (MutableMap<String, Any?>) -> Unit): InitialDirContext {
-    val env = Hashtable<String, Any?>()
-    env[Context.INITIAL_CONTEXT_FACTORY] = "com.sun.jndi.ldap.LdapCtxFactory"
-    env[Context.PROVIDER_URL] = ldapURL
-
-    ldapEnvironmentBuilder(env)
-
-    return InitialDirContext(env)
-}
-
-internal fun ldapEscape(string: String): String {
-    for (index in 0..string.lastIndex) {
-        val character = string[index]
-        if (character.shouldEscape()) {
-            return ldapEscapeImpl(string, index)
+            connection?.unBind()
+            connection?.close()
         }
     }
 
-    return string
-}
+    override fun Route.register(authContext: PipelineContext<Unit, ApplicationCall>.() -> AuthContext) {
+        post("ldap") {
+            val session = getSession(authContext)
 
-private fun ldapEscapeImpl(string: String, firstIndex: Int): String = buildString {
-    var lastIndex = 0
-    for (index in firstIndex..string.lastIndex) {
-        val character = string[index]
-        if (character.shouldEscape()) {
-            append(string, lastIndex, index)
-            if (character in ESCAPE_CHARACTERS) {
-                append('\\')
-                append(character)
-            } else {
-                character.toString().toByteArray().let { encoded ->
-                    for (byteIndex in 0 until encoded.size) {
-                        val unsignedValue = encoded[byteIndex].toInt() and 0xff
-                        append('\\')
-                        append(unsignedValue.toString(16).padStart(2, '0'))
-                    }
-                }
-            }
+            val credential = call.getUsernamePasswordFromRequest()
 
-            lastIndex = index + 1
+            auth(session, credential)
         }
     }
 
-    append(string, lastIndex, string.length)
-}
-
-private val ESCAPE_CHARACTERS = charArrayOf(' ', '"', '#', '+', ',', ';', '<', '=', '>', '\\')
-
-private fun Char.shouldEscape(): Boolean = this.code.let { codepoint ->
-    when (codepoint) {
-        in 0x3f..0x7e -> codepoint == 0x5c // the only forbidden character is backslash
-        in 0x2d..0x3a -> false // minus, point, slash (allowed), digits + colon :
-        in 0x24..0x2a -> false // $%&'()*
-        0x21 -> false // exclamation
-        else -> true
-    }
 }
