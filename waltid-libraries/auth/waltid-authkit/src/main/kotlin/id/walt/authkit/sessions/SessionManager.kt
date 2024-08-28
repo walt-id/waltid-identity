e515b871001c7a5b9e14924be7cf81c14c866383
waltkb <68587968+waltkb@users.noreply.github.com>
2024-08-28 03:22:07 +0200

    feat: add more session handling to session manager

--- waltid-libraries/auth/waltid-authkit/src/main/kotlin/id/walt/authkit/sessions/SessionManager.kt
+++ waltid-libraries/auth/waltid-authkit/src/main/kotlin/id/walt/authkit/sessions/SessionManager.kt
@@ -1,16 +1,40 @@
 package id.walt.authkit.sessions
 
+import id.walt.authkit.flows.AuthFlow
+import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
-@OptIn(ExperimentalStdlibApi::class)
+@OptIn(ExperimentalUuidApi::class)
 object SessionManager {
 
-    fun openSession() {
-        val sessionId = Uuid.random()
+    // flow is explicitly provided when starting session
+    suspend fun openExplicitSession(authFlow: AuthFlow): AuthSession {
+        val sessionId = Uuid.random().toString()
+
+        val newSession = AuthSession(
+            id = sessionId,
+            status = AuthSessionStatus.CONTINUE_NEXT_STEP,
+            flow = authFlow
+        )
+
+        SessionStore.store(newSession)
+
+        return newSession
+    }
+
+    // figure out flow based on first login method
+    suspend fun openImplicitSession(): AuthSession {
+        val sessionId = Uuid.random().toString()
 
         val newSession = AuthSession(sessionId)
 
         SessionStore.store(newSession)
+
+        return newSession
+    }
+
+    suspend fun updateSession(updatedAuthSession: AuthSession) {
+        SessionStore.store(updatedAuthSession)
     }
 
 }
