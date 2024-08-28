981c69171cd3759955f18554c461aa7fa9184c66
waltkb <68587968+waltkb@users.noreply.github.com>
2024-08-28 03:18:57 +0200

    feat: add id and load for AuthenticationMethod

--- waltid-libraries/auth/waltid-authkit/src/main/kotlin/id/walt/authkit/methods/AuthenticationMethod.kt
+++ waltid-libraries/auth/waltid-authkit/src/main/kotlin/id/walt/authkit/methods/AuthenticationMethod.kt
@@ -4,20 +4,32 @@ import id.walt.authkit.AuthContext
 import id.walt.authkit.accounts.AccountStore
 import id.walt.authkit.accounts.identifiers.AccountIdentifier
 import id.walt.authkit.methods.data.AuthMethodStoredData
+import id.walt.authkit.sessions.AuthSession
+import id.walt.authkit.sessions.SessionStore
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.util.pipeline.*
 
-abstract class AuthenticationMethod {
+abstract class AuthenticationMethod(open val id: String) {
     abstract fun Route.register(authContext: PipelineContext<Unit, ApplicationCall>.() -> AuthContext)
 
 //    abstract val identifier: AccountIdentifier
 
-    inline fun <reified V: AuthMethodStoredData> lookupStoredData(identifier: AccountIdentifier): V {
+    inline fun <reified V : AuthMethodStoredData> lookupStoredData(identifier: AccountIdentifier): V {
         val storedData = AccountStore.lookupStoredDataFor(identifier, this)
         return (storedData as? V) ?: error("${storedData::class.simpleName} is not requested ${V::class.simpleName}")
     }
 
+    inline fun <reified V : AuthMethodStoredData> lookupStoredMultiData(session: AuthSession): V {
+        val storedData = AccountStore.lookupStoredMultiDataForAccount(session, this)
+        return (storedData as? V) ?: error("${storedData::class.simpleName} is not requested ${V::class.simpleName}")
+    }
+
+    fun PipelineContext<Unit, ApplicationCall>.getSession(authContext: PipelineContext<Unit, ApplicationCall>.() -> AuthContext): AuthSession {
+        val currentContext = authContext.invoke(this)
+        val session = SessionStore.resolveSessionId(currentContext.sessionId)
+        return session
+    }
 
 
 }
