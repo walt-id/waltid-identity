f4d1541afc95963a5b11edda88cb6ad13b597cbd
waltkb <68587968+waltkb@users.noreply.github.com>
2024-08-28 03:22:24 +0200

    feat: update TestApplication

--- waltid-libraries/auth/waltid-authkit/src/test/kotlin/id/walt/TestApplication.kt
+++ waltid-libraries/auth/waltid-authkit/src/test/kotlin/id/walt/TestApplication.kt
@@ -1,13 +1,17 @@
 package id.walt
 
 import id.walt.authkit.AuthContext
+import id.walt.authkit.flows.AuthFlow
 import id.walt.authkit.methods.AuthenticationMethod
-import id.walt.authkit.methods.OIDC
+import id.walt.authkit.methods.TOTP
 import id.walt.authkit.methods.UserPass
+import id.walt.authkit.sessions.SessionManager
 import io.ktor.server.application.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.pipeline.*
+import org.intellij.lang.annotations.Language
 
 
 fun Application.testApp() {
@@ -16,12 +20,37 @@ fun Application.testApp() {
 
             val contextFunction: PipelineContext<Unit, ApplicationCall>.() -> AuthContext = {
                 AuthContext(
-                    tenant = call.request.host()
+                    tenant = call.request.host(),
+                    sessionId = call.parameters["sessionId"] ?: error("Missing sessionId")
                 )
             }
 
-            registerAuthenticationMethod(UserPass, contextFunction)
-            registerAuthenticationMethod(OIDC, contextFunction)
+            route("flows") {
+
+                route("flow1") {
+                    @Language("JSON")
+                    val flowConfig = """
+                        {
+                            "method": "userpass",
+                            "continue": {
+                              "method": "totp",
+                              "ok": true
+                            }
+                        }
+                    """.trimIndent()
+                    val authFlow = AuthFlow.fromConfig(flowConfig)
+
+                    route("{sessionId}") {
+                        registerAuthenticationMethod(UserPass, contextFunction)
+                        registerAuthenticationMethod(TOTP, contextFunction)
+                    }
+
+                    post("start") {
+                        val session = SessionManager.openExplicitSession(authFlow)
+                        context.respond(session.toInformation())
+                    }
+                }
+            }
         }
     }
 }
