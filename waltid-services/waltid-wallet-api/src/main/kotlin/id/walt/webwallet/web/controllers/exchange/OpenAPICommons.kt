0641e55662c661f6d6cdff1e3ec0f820a57c17aa
Christos Patsonakis <christos@walt.id>
2024-09-02 15:15:25 +0300

    feat: setup handlers for oid4vci with external signatures and refactor exchange request and response docs in openapi commons

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/controllers/exchange/OpenAPICommons.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/controllers/exchange/OpenAPICommons.kt
@@ -1,5 +1,7 @@
 package id.walt.webwallet.web.controllers.exchange
 
+import id.walt.webwallet.db.models.WalletCredential
+import io.github.smiley4.ktorswaggerui.dsl.routes.OpenApiRequest
 import io.github.smiley4.ktorswaggerui.dsl.routes.OpenApiResponses
 import io.github.smiley4.ktorswaggerui.dsl.routes.OpenApiRoute
 import io.ktor.http.*
@@ -23,6 +25,22 @@ object OpenAPICommons {
             description = "Presentation was not accepted"
             body<JsonObject> {
                 description = """{"redirectUri": String?, "errorMessage": String}"""
+            }
+        }
+    }
+
+    fun useOfferRequestEndpointRequestParams(): OpenApiRequest.() -> Unit = {
+        queryParameter<String>("did") { description = "The DID to issue the credential(s) to" }
+        queryParameter<Boolean>("requireUserInput") { description = "Whether to claim as pending acceptance" }
+        body<String> {
+            description = "The offer request to use"
+        }
+    }
+
+    fun useOfferRequestEndpointResponseParams(): OpenApiResponses.() -> Unit = {
+        HttpStatusCode.OK to {
+            body<List<WalletCredential>> {
+                description = "List of credentials"
             }
         }
     }
