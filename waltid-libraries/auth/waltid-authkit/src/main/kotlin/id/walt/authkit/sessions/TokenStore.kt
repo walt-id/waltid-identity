8ced426a7b829fb9047feca71a8a96b69f4d1758
waltkb <68587968+waltkb@users.noreply.github.com>
2024-09-03 11:59:23 +0200

    feat: add more functionality to token store interface

--- waltid-libraries/auth/waltid-authkit/src/main/kotlin/id/walt/authkit/sessions/TokenStore.kt
+++ waltid-libraries/auth/waltid-authkit/src/main/kotlin/id/walt/authkit/sessions/TokenStore.kt
@@ -1,5 +1,7 @@
 package id.walt.authkit.sessions
 
+import id.walt.authkit.AuthKitManager
+
 interface TokenStore {
 
     /**
@@ -7,6 +9,16 @@ interface TokenStore {
      */
     fun mapToken(token: String, sessionId: String)
 
+    fun getTokenSessionId(token: String): String
+
+    fun validateToken(token: String): Boolean
+
     fun dropToken(token: String)
+
+
+
+    fun resolveTokenToSession(token: String) = getTokenSessionId(token)
+        .let { sessionId -> AuthKitManager.sessionStore.resolveSessionId(sessionId) }
+        .also { session -> check(token == session.token) { "Token was not mapped to correct session" } }
 
 }
