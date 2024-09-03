11a8bc9aa189e8ee4044f96f39785bc57f56e62c
Christos Patsonakis <christos@walt.id>
2024-09-03 09:01:12 +0300

    refactor: move explicit claim strategy claim code to commons for future reuse

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/usecase/claim/ExplicitClaimStrategy.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/usecase/claim/ExplicitClaimStrategy.kt
@@ -3,10 +3,8 @@ package id.walt.webwallet.usecase.claim
 import id.walt.webwallet.db.models.WalletCredential
 import id.walt.webwallet.service.SSIKit2WalletService
 import id.walt.webwallet.service.credentials.CredentialsService
-import id.walt.webwallet.service.events.EventType
 import id.walt.webwallet.service.exchange.IssuanceService
 import id.walt.webwallet.usecase.event.EventLogUseCase
-import kotlinx.datetime.Clock
 import kotlinx.uuid.UUID
 
 class ExplicitClaimStrategy(
@@ -16,40 +14,20 @@ class ExplicitClaimStrategy(
 ) {
     suspend fun claim(
         tenant: String, account: UUID, wallet: UUID, did: String, offer: String, pending: Boolean = true,
-    ): List<WalletCredential> = issuanceService.useOfferRequest(
-        offer = offer,
-        credentialWallet = SSIKit2WalletService.getCredentialWallet(did),
-        clientId = SSIKit2WalletService.testCIClientConfig.clientID
-    ).map {
-        WalletCredential(
-            wallet = wallet,
-            id = it.id,
-            document = it.document,
-            disclosures = it.disclosures,
-            addedOn = Clock.System.now(),
-            manifest = it.manifest,
-            deletedOn = null,
-            pending = pending,
-            format = it.format,
-        ).also { credential ->
-            eventUseCase.log(
-                action = EventType.Credential.Receive,
-                originator = "", //parsedOfferReq.credentialOffer!!.credentialIssuer,
-                tenant = tenant,
-                accountId = account,
-                walletId = wallet,
-                data = eventUseCase.credentialEventData(
-                    credential = credential,
-                    subject = eventUseCase.subjectData(credential),
-                    organization = eventUseCase.issuerData(credential),
-                    type = it.type
-                ),
-                credentialId = credential.id,
-            )
-        }
-    }.also {
-        credentialService.add(
-            wallet = wallet, credentials = it.toTypedArray()
+    ): List<WalletCredential> {
+        val offerCredentialDataResults = issuanceService.useOfferRequest(
+            offer = offer,
+            credentialWallet = SSIKit2WalletService.getCredentialWallet(did),
+            clientId = SSIKit2WalletService.testCIClientConfig.clientID
+        )
+        return ClaimCommons.mapCredentialDataResultsToWalletCredentials(
+            offerCredentialDataResults,
+            wallet,
+            tenant,
+            account,
+            pending,
+            eventUseCase,
+            credentialService,
         )
     }
 }
