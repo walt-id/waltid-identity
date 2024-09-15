0cece55cfd65300a65e4d84fcc15f36742165354
Christos Patsonakis <christos@walt.id>
2024-09-15 19:58:17 +0300

    feat: preliminary verification method and type implementations

--- waltid-libraries/waltid-did/src/commonMain/kotlin/id/walt/did/dids/document/models/verification/method/VerificationMaterialType.kt
+++ waltid-libraries/waltid-did/src/commonMain/kotlin/id/walt/did/dids/document/models/verification/method/VerificationMaterialType.kt
@@ -2,23 +2,23 @@ package id.walt.did.dids.document.models.verification.method
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 
+@OptIn(ExperimentalJsExport::class)
+@JsExport
 @Serializable
 enum class VerificationMaterialType {
     @SerialName("publicKeyJwk")
     PublicKeyJwk,
 
     @SerialName("publicKeyMultibase")
-    PublicKeyMultibase,
-
-    @SerialName("blockchainAccountId")
-    BlockchainAccountId;
+    PublicKeyMultibase;
 
     override fun toString(): String {
         return when (this) {
             PublicKeyJwk -> "publicKeyJwk"
             PublicKeyMultibase -> "publicKeyMultibase"
-            BlockchainAccountId -> "blockchainAccountId"
         }
     }
 }
--- waltid-libraries/waltid-did/src/commonMain/kotlin/id/walt/did/dids/document/models/verification/method/VerificationMethodType.kt
+++ waltid-libraries/waltid-did/src/commonMain/kotlin/id/walt/did/dids/document/models/verification/method/VerificationMethodType.kt
@@ -1,13 +1,17 @@
 package id.walt.did.dids.document.models.verification.method
 
 import kotlinx.serialization.Serializable
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 
 //as defined in https://www.w3.org/TR/2024/NOTE-did-spec-registries-20240830/#verification-method-types
+@OptIn(ExperimentalJsExport::class)
+@JsExport
 @Serializable
 enum class VerificationMethodType {
     JsonWebKey2020,
     EcdsaSecp256k1VerificationKey2019,
-    Ed25519VerificationKey2018,
+    Ed25519VerificationKey2020, //it used to be 2018 for base58 encoding, but since that is deprecated in favor of multibase it is updated
     Bls12381G1Key2020,
     Bls12381G2Key2020,
     PgpVerificationKey2021,
