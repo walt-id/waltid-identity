b9c21ca64c140c4bf76c3ee794a7cb5dd54aa45d
Christos Patsonakis <christos@walt.id>
2024-09-17 17:35:33 +0300

    docs: did web create options

--- waltid-libraries/waltid-did/src/commonMain/kotlin/id/walt/did/dids/registrar/dids/DidWebCreateOptions.kt
+++ waltid-libraries/waltid-did/src/commonMain/kotlin/id/walt/did/dids/registrar/dids/DidWebCreateOptions.kt
@@ -4,6 +4,17 @@ import id.walt.crypto.keys.KeyType
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 
+/**
+ * Class encompassing the set of parameters that are involved in the creation of [`did:web`](https://w3c-ccg.github.io/did-method-web/) DIDs via the [id.walt.did.dids.registrar.local.web.DidWebRegistrar] class.
+ * @property domain The web domain name (cannot be empty).
+ * @property path Optional URL path parameter.
+ * @property keyType The type of [id.walt.crypto.keys.Key] that will be generated. This parameter is used in the context of the [id.walt.did.dids.registrar.local.web.DidWebRegistrar.register] function and is ignored when the [didDocConfig] property is specified.
+ * @property didDocConfig Optional parameter that, in short, provides users with more control over the did document that will be produced. Refer to [DidDocConfig] for more information. This parameter, when specified, takes precedence in the [id.walt.did.dids.registrar.local.web.DidWebRegistrar.register] function over the `keyType` property.
+ * @see [id.walt.did.dids.registrar.local.web.DidWebRegistrar]
+ * @see [DidDocConfig]
+ * @see [KeyType]
+ * @see [id.walt.crypto.keys.Key]
+ */
 @OptIn(ExperimentalJsExport::class)
 @JsExport
 class DidWebCreateOptions(
