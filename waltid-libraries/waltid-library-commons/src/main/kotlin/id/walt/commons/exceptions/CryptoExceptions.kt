63482916a6507ea717ac00e80b8708a7b24156cf
SuperBatata <khaledbattiche@gmail.com>
2024-09-18 23:30:18 +0100

    feat : create custom exception for crypto

--- waltid-libraries/waltid-library-commons/src/main/kotlin/id/walt/commons/exceptions/CryptoExceptions.kt
+++ waltid-libraries/waltid-library-commons/src/main/kotlin/id/walt/commons/exceptions/CryptoExceptions.kt
@@ -2,4 +2,8 @@ package id.walt.commons.exceptions
 
 import io.ktor.http.HttpStatusCode
 
-open class CryptoException(val status: HttpStatusCode, message: String) : Exception(message)
+open class CryptoException( val status: HttpStatusCode,message: String) : Throwable(message)
+
+
+class KeyTypeNotSupportedException(type: String) : CryptoException(HttpStatusCode.BadRequest, "Key type $type not supported")
+class KeyBackendNotSupportedException(backend: String) : CryptoException(HttpStatusCode.BadRequest, "Key backend $backend not registered")
