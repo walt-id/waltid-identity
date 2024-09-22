077998988d614d937cd041873437d9ed36c3c656
SuperBatata <khaledbattiche@gmail.com>
2024-09-22 15:03:42 +0100

    feat: extend CryptoExceptions.kt with more custom exceptions

--- waltid-libraries/waltid-library-commons/src/commonMain/kotlin/id/walt/commons/exceptions/CryptoExceptions.kt
+++ waltid-libraries/waltid-library-commons/src/commonMain/kotlin/id/walt/commons/exceptions/CryptoExceptions.kt
@@ -5,5 +5,17 @@ import io.ktor.http.HttpStatusCode
 open class CryptoException( val status: HttpStatusCode,message: String) : Exception(message)
 
 
-class KeyTypeNotSupportedException(type: String) : CryptoException(HttpStatusCode.BadRequest, "Key type $type not Registered")
-class KeyBackendNotSupportedException(backend: String) : CryptoException(HttpStatusCode.BadRequest, "Key backend $backend not registered")
+class KeyTypeNotSupportedException(type: String) :
+    CryptoException(HttpStatusCode.BadRequest, "Key type $type not Supported")
+
+class KeyBackendNotSupportedException(backend: String) :
+    CryptoException(HttpStatusCode.BadRequest, "Key backend $backend not Supported")
+
+class MissingKeyTypeException() :
+    CryptoException(HttpStatusCode.BadRequest, "Key type is missing in the serialized key")
+
+class KeyNotFoundException(id: String) : CryptoException(HttpStatusCode.NotFound, "Key with id $id not found")
+
+class SigningException(message: String) : CryptoException(HttpStatusCode.InternalServerError, message)
+class VerificationException(message: String) : CryptoException(HttpStatusCode.InternalServerError, message)
+class PublicKeyNotFoundException(message: String) : CryptoException(HttpStatusCode.NotFound, message)
