3e136d4af0c7f3e13d14e58028926580a02710d1
SuperBatata <khaledbattiche@gmail.com>
2024-09-24 16:30:55 +0100

    feat: add TSEObject and enhance custom exceptions

--- waltid-libraries/waltid-library-commons/src/commonMain/kotlin/id/walt/commons/exceptions/CryptoExceptions.kt
+++ waltid-libraries/waltid-library-commons/src/commonMain/kotlin/id/walt/commons/exceptions/CryptoExceptions.kt
@@ -6,22 +6,51 @@ open class CryptoException(val status: HttpStatusCode, message: String) : Except
 
 
 class KeyTypeNotSupportedException(type: String) :
-    CryptoException(HttpStatusCode.BadRequest, "Key type $type not Supported")
+    CryptoException(HttpStatusCode.BadRequest, "Key type $type not supported")
 
 class KeyBackendNotSupportedException(backend: String) :
-    CryptoException(HttpStatusCode.BadRequest, "Key backend $backend not Supported")
+    CryptoException(HttpStatusCode.BadRequest, "Key backend $backend not supported")
 
-class MissingKeyTypeException() :
+class KeyTypeMissingException() :
     CryptoException(HttpStatusCode.BadRequest, "Key type is missing in the serialized key")
 
-class KeyNotFoundException(id: String) : CryptoException(HttpStatusCode.NotFound, "Key with id $id not found")
+class KeyNotFoundException(
+    id: String,
+    message: String = "Key with id $id not found"
+) : CryptoException(HttpStatusCode.NotFound, message)
 
 class SigningException(message: String) : CryptoException(HttpStatusCode.InternalServerError, message)
 class VerificationException(message: String) : CryptoException(HttpStatusCode.InternalServerError, message)
-class PublicKeyNotFoundException(message: String) : CryptoException(HttpStatusCode.NotFound, message)
-class InvalidAuthenticationMethodException() : CryptoException(
-    HttpStatusCode.BadRequest,
-    "No valid authentication method passed! Provide either accessKey, roleId and secretId, or username and password."
-)
 
-class TSELoginException(message: String) : CryptoException(HttpStatusCode.InternalServerError, message)
+class MissingSignatureException(message: String) : CryptoException(HttpStatusCode.BadRequest, message)
+object TSEError {
+    class LoginException(errors: List<String>) : CryptoException(
+        HttpStatusCode.InternalServerError,
+        "Errors occurred at TSE login: ${errors.joinToString { it }}"
+    )
+
+    class MissingAuthTokenException :
+        CryptoException(HttpStatusCode.InternalServerError, "Did not receive token after login!")
+
+    class MissingKeyNameException : CryptoException(HttpStatusCode.BadRequest, "Key name is missing in the request!")
+    class MissingKeyDataException : CryptoException(HttpStatusCode.BadRequest, "Key data is missing in the request!")
+    object InvalidAuthenticationMethod {
+        abstract class BaseAuthenticationMethodException(reason: String) : CryptoException(
+            HttpStatusCode.BadRequest,
+            "No valid authentication method passed!: $reason"
+        )
+
+        class MissingAuthenticationMethodException : BaseAuthenticationMethodException(
+            "Expecting any of: accessKey, roleId and secretId or username and password."
+        )
+
+        class IncompleteRoleAuthenticationMethodException : BaseAuthenticationMethodException(
+            "Both roleId and secretId are required."
+        )
+
+        class IncompleteUserAuthenticationMethodException : BaseAuthenticationMethodException(
+            "Both username and password are required."
+        )
+
+    }
+}
