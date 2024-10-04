b4a2991e5489d40584e1ec4d6144dd5cdbe5ef8e
SuperBatata <khaledbattiche@gmail.com>
2024-10-04 16:19:58 +0100

    feat: added AWSKeyMetadata.kt class

---
+++ waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/aws/AWSKeyMetadata.kt
@@ -0,0 +1,14 @@
+package id.walt.crypto.keys.aws
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class AWSKeyMetadata(
+
+    val accessKeyId: String,
+    val secretAccessKey: String,
+    val region: String,
+
+    )
+
