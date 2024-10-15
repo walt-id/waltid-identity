915d77c141b78c5a5c475abd59daaca097abd08e
Dinkar <jaindinkar@ymail.com>
2024-10-15 13:13:27 +0530

    chore: Added README for waltid-permissions

--- waltid-libraries/auth/waltid-permissions/build.gradle.kts
+++ waltid-libraries/auth/waltid-permissions/build.gradle.kts
@@ -130,3 +130,21 @@ extensions.getByType<SuspendTransformGradleExtension>().apply {
         )
     )
 }
+
+npmPublish {
+    registries {
+        val envToken = System.getenv("NPM_TOKEN")
+        val npmTokenFile = File("secret_npm_token.txt")
+        val secretNpmToken =
+            envToken ?: npmTokenFile.let { if (it.isFile) it.readLines().first() else "" }
+        val hasNPMToken = secretNpmToken.isNotEmpty()
+        val isReleaseBuild = Regex("\\d+.\\d+.\\d+").matches(version.get())
+        if (isReleaseBuild && hasNPMToken) {
+            readme.set(File("README-JS.md"))
+            register("npmjs") {
+                uri.set(uri("https://registry.npmjs.org"))
+                authToken.set(secretNpmToken)
+            }
+        }
+    }
+}
