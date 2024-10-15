e9f8789d766211f33c3b31ddaafdcac7e1014351
waltkb <68587968+waltkb@users.noreply.github.com>
2024-10-15 02:53:07 +0200

    feat: update presentation definition parser to allow for streaming filtering

--- waltid-libraries/credentials/waltid-dif-definitions-parser/src/commonMain/kotlin/id/walt/definitionparser/PresentationDefinitionParser.kt
+++ waltid-libraries/credentials/waltid-dif-definitions-parser/src/commonMain/kotlin/id/walt/definitionparser/PresentationDefinitionParser.kt
@@ -2,12 +2,12 @@ package id.walt.definitionparser
 
 import com.nfeld.jsonpathkt.JsonPath
 import com.nfeld.jsonpathkt.kotlinx.resolveOrNull
-import id.walt.credentials.vc.vcs.W3CVC
 import id.walt.definitionparser.PresentationDefinition.InputDescriptor.Constraints.Field
 import io.github.optimumcode.json.schema.JsonSchema
 import io.github.optimumcode.json.schema.OutputCollector
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.serialization.json.Json
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
 
@@ -21,7 +21,7 @@ class JsonObjectEnquirer {
         compiledJsonPaths.getOrPut(path) { JsonPath.compile(path) }
 
 
-    fun filterDocumentsByConstraints(documents: List<JsonObject>, constraints: List<Field>): List<JsonObject> =
+    fun filterDocumentsByConstraints(documents: Flow<JsonObject>, constraints: List<Field>): Flow<JsonObject> =
         documents.filter { document ->
             constraints.all { field ->
                 val resolvedPath = field.path.firstNotNullOfOrNull { document.resolveOrNull(getJsonPath(it)) }
@@ -31,7 +31,7 @@ class JsonObjectEnquirer {
                 } else {
                     if (field.filter != null) {
                         val schema = JsonSchema.fromJsonElement(field.filter)
-                        when(resolvedPath) {
+                        when (resolvedPath) {
                             is JsonArray -> resolvedPath.any { schema.validate(it, OutputCollector.flag()).valid }
                             else -> schema.validate(resolvedPath, OutputCollector.flag()).valid
                         }
@@ -43,7 +43,10 @@ class JsonObjectEnquirer {
 
 object PresentationDefinitionParser {
 
-    fun matchCredentialsForInputDescriptor(credentials: List<JsonObject>, inputDescriptor: PresentationDefinition.InputDescriptor): List<JsonObject> {
+    fun matchCredentialsForInputDescriptor(
+        credentials: Flow<JsonObject>,
+        inputDescriptor: PresentationDefinition.InputDescriptor,
+    ): Flow<JsonObject> {
 
         log.debug { "--- Checking descriptor ${inputDescriptor.name} --" }
 
