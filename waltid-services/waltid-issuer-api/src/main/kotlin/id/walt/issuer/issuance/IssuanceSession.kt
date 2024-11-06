781282336fdc85aa3a3c7c8b18dc3a2fdeaf5f1f
Severin Stampler <severin@walt.id>
2024-11-06 18:56:23 +0100

    move issuance session data class to issuer-api service and extend parameters

--- waltid-services/waltid-issuer-api/src/main/kotlin/id/walt/issuer/issuance/IssuanceSession.kt
+++ waltid-services/waltid-issuer-api/src/main/kotlin/id/walt/issuer/issuance/IssuanceSession.kt
@@ -1,7 +1,8 @@
-package id.walt.oid4vc.providers
+package id.walt.issuer.issuance
 
 import id.walt.oid4vc.data.CredentialOffer
 import id.walt.oid4vc.data.TxCode
+import id.walt.oid4vc.providers.AuthorizationSession
 import id.walt.oid4vc.requests.AuthorizationRequest
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
@@ -12,10 +13,12 @@ data class IssuanceSession(
     override val id: String,
     override val authorizationRequest: AuthorizationRequest?,
     override val expirationTimestamp: Instant,
+    val issuanceRequests: List<IssuanceRequest>,
     val txCode: TxCode? = null,
     val txCodeValue: String? = null,
     override val authServerState: String?  = null, //the state used for additional authentication with pwd, id_token or vp_token.
     val credentialOffer: CredentialOffer? = null,
     val cNonce: String? = null,
+    val callbackUrl: String? = null,
     val customParameters: Map<String, JsonElement>? = null,
 ) : AuthorizationSession()
