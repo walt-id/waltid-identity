4198df050774ed1dc4fe1886422ff188f9c4916e
SuperBatata <khaledbattiche@gmail.com>
2024-11-19 14:37:53 +0100

    fix: constructor in AWSKeyMetadata.kt

--- waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/aws/AWSKeyMetadata.kt
+++ waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/aws/AWSKeyMetadata.kt
@@ -6,5 +6,12 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class AWSKeyMetadata(
     val auth: AWSAuth,
-)
+) {
+    constructor(
+        accessKeyId: String? = null,
+        secretAccessKey: String? = null,
+        region: String? = null,
+        roleName: String? = null,
+    ) : this(AWSAuth(accessKeyId, secretAccessKey, region, roleName))
+}
 
