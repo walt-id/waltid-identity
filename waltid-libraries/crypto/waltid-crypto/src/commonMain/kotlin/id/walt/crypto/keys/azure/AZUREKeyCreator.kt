e67ea9668e4c80bce08e34557af2e0dea21fda2f
SuperBatata <khaledbattiche@gmail.com>
2024-11-26 17:20:37 +0100

    fix: generate key function

--- waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/azure/AZUREKeyCreator.kt
+++ waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/azure/AZUREKeyCreator.kt
@@ -1,10 +1,9 @@
 package id.walt.crypto.keys.azure
 
-import id.walt.crypto.keys.Key
 import id.walt.crypto.keys.KeyType
 
 
 interface AZUREKeyCreator {
 
-    suspend fun generate(type: KeyType, keyName: String, metadata: AZUREKeyMetadata): Key
+    suspend fun generate(type: KeyType, keyName: String? = "waltid", metadata: AZUREKeyMetadata): AZUREKEY
 }
