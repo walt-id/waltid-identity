1353935a72c9b23dcd0313ddc632ed468f7144d6
SuperBatata <khaledbattiche@gmail.com>
2024-12-12 15:56:20 +0100

    Add AWS key management support in WaltCryptoAws

---
+++ waltid-libraries/crypto/waltid-crypto-aws/src/main/kotlin/id.walt.crypto.keys.aws/WaltCryptoAws.kt
@@ -0,0 +1,20 @@
+package id.walt.crypto.keys.aws
+
+import id.walt.crypto.keys.KeyGenerationRequest
+import id.walt.crypto.keys.KeyManager
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromJsonElement
+
+object WaltCryptoAws {
+    fun init() {
+        KeyManager.register<AWSKey>("aws") { generateRequest: KeyGenerationRequest ->
+            AWSKey.generateKey(
+                generateRequest.keyType,
+                Json.decodeFromJsonElement(generateRequest.config!!)
+            )
+        }
+
+
+    }
+
+}
