2b867b91f28d4391c4efafc0a359b5b773842fdb
mikeplotean <mike@walt.id>
2024-12-19 16:28:12 +0200

    Revert "Update StatusElement.kt"
    This reverts commit efae145751870e42e7a19724c15b424d89a80dda.

--- waltid-libraries/credentials/waltid-mdoc-credentials/src/commonMain/kotlin/id/walt/mdoc/mso/StatusElement.kt
+++ waltid-libraries/credentials/waltid-mdoc-credentials/src/commonMain/kotlin/id/walt/mdoc/mso/StatusElement.kt
@@ -3,6 +3,15 @@ package id.walt.mdoc.mso
 import id.walt.mdoc.dataelement.*
 import kotlinx.serialization.Serializable
 
+/**
+ * Mobile security object, representing the payload of the issuer signature, for the issuer signed part of the mdoc.
+ * @param version Version of the MSO
+ * @param digestAlgorithm digest algorithm used
+ * @param valueDigests  Digests of the signed items
+ * @param deviceKeyInfo Device key info, containing the mdoc authentication public key and related information
+ * @param docType Document type
+ * @param validityInfo Time-wise validity information about this MSO
+ */
 @Serializable
 class StatusElement (
   val statusListInfo: StatusListInfo
