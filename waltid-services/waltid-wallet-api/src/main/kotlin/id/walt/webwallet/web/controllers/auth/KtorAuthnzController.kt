9f97c997fc6e59011246a57227d3a475db4f8f64
waltkb <68587968+waltkb@users.noreply.github.com>
2025-01-15 04:06:55 +0100

    Add account registration for web3Registration

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/controllers/auth/KtorAuthnzController.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/controllers/auth/KtorAuthnzController.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
 package id.walt.webwallet.web.controllers.auth
 
 import id.walt.commons.config.ConfigManager
@@ -5,30 +7,51 @@ import id.walt.ktorauthnz.AuthContext
 import id.walt.ktorauthnz.KtorAuthnzManager
 import id.walt.ktorauthnz.accounts.identifiers.methods.Web3Identifier
 import id.walt.ktorauthnz.amendmends.AuthMethodFunctionAmendments
+import id.walt.ktorauthnz.amendmends.AuthMethodFunctionAmendments.Registration
 import id.walt.ktorauthnz.methods.AuthMethodManager
 import id.walt.ktorauthnz.methods.AuthenticationMethod
 import id.walt.ktorauthnz.methods.Web3
 import id.walt.ktorauthnz.methods.registerAuthenticationMethod
 import id.walt.webwallet.config.KtorAuthnzConfig
-import id.walt.ktorauthnz.amendmends.AuthMethodFunctionAmendments.*
+import id.walt.webwallet.db.models.Accounts
 import id.walt.webwallet.db.models.authnz.AuthnzUsers
+import id.walt.webwallet.service.account.AccountsService.initializeUserAccount
 import io.github.smiley4.ktorswaggerui.dsl.routing.route
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import io.ktor.util.pipeline.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.toJavaInstant
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 private val authConfig = ConfigManager.getConfig<KtorAuthnzConfig>()
 private val flowConfig = authConfig.authFlow
 
 
-
 private val web3Registration: suspend (any: Any) -> Unit = { any ->
     val identifier = any as? Web3Identifier ?: error("Provided argument is not web3 identifier")
-    val newAuthnzUserId =  transaction { AuthnzUsers.insert{}[AuthnzUsers.id] }.toString()
+    val web3Address = identifier.address
+
+    val newAuthnzUserId = transaction { AuthnzUsers.insert {}[AuthnzUsers.id] }.toString()
+    val newAuthnzUserUuid = Uuid.parse(newAuthnzUserId)
+
+
     KtorAuthnzManager.accountStore.addAccountIdentifierToAccount(newAuthnzUserId, identifier)
+
+    transaction {
+        val accountId = Accounts.insert {
+            it[tenant] = ""
+            it[id] = newAuthnzUserUuid
+            it[name] = web3Address
+            it[createdOn] = Clock.System.now().toJavaInstant()
+        }[Accounts.id]
+    }
+
+    initializeUserAccount(name = web3Address, registeredUserId = Uuid.parse(newAuthnzUserId))
 }
 
 private val authMethodFunctionAmendments: Map<AuthenticationMethod, Map<AuthMethodFunctionAmendments, suspend (Any) -> Unit>> =
