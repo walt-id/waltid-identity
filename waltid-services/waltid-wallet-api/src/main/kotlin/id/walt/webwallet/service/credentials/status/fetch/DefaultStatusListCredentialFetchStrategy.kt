14044dc653302d67bea69d93e9e23684893305a4
mikeplotean <mike@walt.id>
2025-01-16 17:43:18 +0200

    refactor: default-status-credential-fetch-strategy to use jws-decoder test: default-status-credential-fetch-strategy

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/credentials/status/fetch/DefaultStatusListCredentialFetchStrategy.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/credentials/status/fetch/DefaultStatusListCredentialFetchStrategy.kt
@@ -1,20 +1,17 @@
 package id.walt.webwallet.service.credentials.status.fetch
 
-import id.walt.crypto.utils.JwsUtils.decodeJws
+import id.walt.webwallet.service.JwsDecoder
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 
 class DefaultStatusListCredentialFetchStrategy(
     private val http: HttpClient,
+    private val jwsDecoder: JwsDecoder,
 ) : StatusListCredentialFetchStrategy {
 
-    private val json = Json { ignoreUnknownKeys = true }
-    override suspend fun fetch(url: String): JsonObject = http.get(url).bodyAsText().let { content ->
-        runCatching { content.decodeJws().payload }.fold(onSuccess = { it }, onFailure = {
-            json.decodeFromString(content)
-        })
+    override suspend fun fetch(url: String): JsonObject = http.get(url).bodyAsText().let {
+        jwsDecoder.payload(it)
     }
 }
