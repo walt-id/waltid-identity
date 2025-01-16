9d77eed5ba7cdf6151374be49d7d05ad9a9e11a8
mikeplotean <mike@walt.id>
2025-01-16 17:42:11 +0200

    test: jws-decoder, added test resources for status credential refactor: rename status credential resources chore: make decode-jws-parts private

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/JwsDecoder.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/JwsDecoder.kt
@@ -2,10 +2,15 @@ package id.walt.webwallet.service
 
 import id.walt.crypto.utils.JwsUtils.decodeJws
 import id.walt.webwallet.utils.JsonUtils
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 
 class JwsDecoder {
-    fun payload(data: String) = data.decodeJws().payload.let {
+    private val json = Json { ignoreUnknownKeys = true }
+
+    fun payload(data: String) = runCatching {
+        data.decodeJws().payload
+    }.fold(onSuccess = { it }, onFailure = { json.decodeFromString(data) }).let {
         JsonUtils.tryGetData(it, "vc")?.jsonObject ?: it
     }
 }
