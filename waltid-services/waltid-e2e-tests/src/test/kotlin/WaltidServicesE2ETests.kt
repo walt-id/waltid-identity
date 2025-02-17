1d2965c07cee56560d57f6fa8d4c128c2a69c79f
waltkb <68587968+waltkb@users.noreply.github.com>
2025-02-18 00:07:58 +0100

    Update dependencies, fix build issues

--- waltid-services/waltid-e2e-tests/src/test/kotlin/WaltidServicesE2ETests.kt
+++ waltid-services/waltid-e2e-tests/src/test/kotlin/WaltidServicesE2ETests.kt
@@ -43,6 +43,7 @@ import io.ktor.server.application.*
 import io.ktor.server.util.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.*
+import kotlin.system.exitProcess
 import kotlin.test.*
 import kotlin.time.Duration.Companion.minutes
 import kotlin.uuid.ExperimentalUuidApi
