570ccbed7d3dd621f8464c9d53320e0181baa31e
Sergio Shevchenko <sergio.shevchenko@etuitus.it>
2025-03-12 18:15:43 +0100

    Added ttl support in persistence

--- waltid-libraries/protocols/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/interfaces/ISessionCache.kt
+++ waltid-libraries/protocols/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/interfaces/ISessionCache.kt
@@ -8,9 +8,13 @@ interface ISessionCache<T> {
 
     /**
      * Puts the specified [session] with the specified [id] in the session cache.
+     * @param id The ID to store the session under
+     * @param session The session to store
+     * @param ttl Optional custom time-to-live duration for this session.
+     *            If null, the default expiration is used.
      * @return the previous value associated with the [id], or `null` if the key was not present in the cache.
      */
-    fun putSession(id: String, session: T)
+    fun putSession(id: String, session: T, ttl: kotlin.time.Duration? = null)
 
     fun getSessionByAuthServerState(authServerState: String): T?
     /**
