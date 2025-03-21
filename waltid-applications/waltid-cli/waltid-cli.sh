0c6ec359af083dcef7871099bd7690ac82328da2
waltkb <68587968+waltkb@users.noreply.github.com>
2025-03-21 01:40:14 +0100

    Fixed issue in waltid-cli, where both JVM and Multiplatform compilation are applied (causing warnings whenever Gradle module configuration is evaluated, which became an error now). Dropped no longer necessary plugins (where the used distribution functionality is now natively implemented in the multiplatform plugin. Updated CLI run scripts to be compatible with new build plugin, fix paths, gradle task names

--- waltid-applications/waltid-cli/waltid-cli.sh
+++ waltid-applications/waltid-cli/waltid-cli.sh
@@ -3,14 +3,14 @@
 # Launcher script which builds the waltid-cli from source **once** and executes the command supplied
 # If changes are made to the source then waltid-cli has to manually be rebuild (e.g. through waltid-cli-development.sh)
 
-if [ -f 'build/install/waltid/bin/waltid' ]; then
-  build/install/waltid/bin/waltid "$@"
+if [ -f 'build/install/waltid-jvm/bin/waltid' ]; then
+  build/install/waltid-jvm/bin/waltid "$@"
 else
   echo "waltid-cli not yet build."
 
   echo "Running build..."
-  ../../gradlew installDist
+  ../../gradlew installJvmDist
 
   echo "Trying to run CLI command after build..."
-  build/install/waltid/bin/waltid "$@"
+  build/install/waltid-jvm/bin/waltid "$@"
 fi
