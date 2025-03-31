f643b8c3c6288a0e9ec74f3e2b20a240674c36a7
Philipp Potisk <phil@walt.id>
2025-03-31 10:57:39 +0200

    Made DID web test server ports more generic

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ plugins {
     kotlin("plugin.serialization") version kotlinVersion apply false
 
     id("love.forte.plugin.suspend-transform") version "2.1.20-0.11.1" apply false
-    id("com.android.library") version "8.7.3" apply false
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.9.0" apply false
+    id("com.android.application") version "8.9.0" apply false
 
     id("com.github.ben-manes.versions") version "0.52.0" apply false
 }
--- waltid-libraries/waltid-did/src/jvmTest/kotlin/TestServer.kt
+++ waltid-libraries/waltid-did/src/jvmTest/kotlin/TestServer.kt
@@ -13,21 +13,19 @@ import java.net.URI
 import javax.security.auth.x500.X500Principal
 
 object TestServer {
+
+    private const val DID_WEB_PORT = 8888
+    const val DID_WEB_SSL_PORT = 8443
+    
+    private const val DID_WEB_PORT_PLACEHOLDER = "<DID_WEB_PORT>"
+
     private val keyStoreFile = File(this.javaClass.classLoader.getResource("")!!.path.plus("keystore.jks"))
-    private val ed25519DocumentResponse =
-        URI(
-            this.javaClass.classLoader.getResource("did-web/ed25519.json")!!.toString()
-        ).path.let { File(it).readText() }
-    private val secp256k1DocumentResponse =
-        URI(
-            this.javaClass.classLoader.getResource("did-web/secp256k1.json")!!.toString()
-        ).path.let { File(it).readText() }
-    private val secp256r1DocumentResponse =
-        URI(
-            this.javaClass.classLoader.getResource("did-web/secp256r1.json")!!.toString()
-        ).path.let { File(it).readText() }
-    private val rsaDocumentResponse =
-        URI(this.javaClass.classLoader.getResource("did-web/rsa.json")!!.toString()).path.let { File(it).readText() }
+
+    private fun loadDidWebReferenceFile(fileName: String): String =
+        URI(this.javaClass.classLoader.getResource(fileName)!!.toString()).path.let {
+            File(it).readText().replace(DID_WEB_PORT_PLACEHOLDER, DID_WEB_SSL_PORT.toString())
+        }
+
     private val keyStore = buildKeyStore {
         certificate("test") {
             password = "test123"
@@ -47,32 +45,31 @@ object TestServer {
         }
         routing {
             get("/secp256k1/did.json") {
-                call.respond<JsonObject>(Json.decodeFromString<JsonObject>(secp256k1DocumentResponse))
+                call.respond<JsonObject>(Json.decodeFromString<JsonObject>(loadDidWebReferenceFile("did-web/secp256k1.json")))
             }
             get("/secp256r1/did.json") {
-                call.respond<JsonObject>(Json.decodeFromString<JsonObject>(secp256r1DocumentResponse))
+                call.respond<JsonObject>(Json.decodeFromString<JsonObject>(loadDidWebReferenceFile("did-web/secp256r1.json")))
             }
             get("/ed25519/did.json") {
-                call.respond<JsonObject>(Json.decodeFromString<JsonObject>(ed25519DocumentResponse))
+                call.respond<JsonObject>(Json.decodeFromString<JsonObject>(loadDidWebReferenceFile("did-web/ed25519.json")))
             }
             get("/rsa/did.json") {
-                call.respond<JsonObject>(Json.decodeFromString<JsonObject>(rsaDocumentResponse))
+                call.respond<JsonObject>(Json.decodeFromString<JsonObject>(loadDidWebReferenceFile("did-web/rsa.json")))
             }
         }
     }
 
     private fun ApplicationEngine.Configuration.envConfig() {
         connector {
-            port = 8000
+            port = DID_WEB_PORT
         }
         sslConnector(
             keyStore = keyStore,
             keyAlias = "test",
             keyStorePassword = { "test123".toCharArray() },
             privateKeyPassword = { "test123".toCharArray() }) {
-            port = 8080
+            port = DID_WEB_SSL_PORT
             keyStorePath = keyStoreFile
         }
     }
-
 }
