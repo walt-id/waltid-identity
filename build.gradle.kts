23cf86e3eccbd4eec2b61fb9d373a82b3fb42f03
waltkb <68587968+waltkb@users.noreply.github.com>
2025-04-09 01:21:25 +0200

    Always encode SdJwtSelectiveDisclosures automatically

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 allprojects {
-    version = "credentiallayer-2025-04-08_12"
+    version = "credentiallayer-2025-04-08_13"
 
     repositories {
         google()
