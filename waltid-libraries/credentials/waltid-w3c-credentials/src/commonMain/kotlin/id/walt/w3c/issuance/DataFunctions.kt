33b6e372f1c56256dded32575ac8ea830b7ef745
SuperBatata <khaledbattiche@gmail.com>
2025-04-10 08:22:25 +0100

    feat : mapping for credential display

--- waltid-libraries/credentials/waltid-w3c-credentials/src/commonMain/kotlin/id/walt/w3c/issuance/DataFunctions.kt
+++ waltid-libraries/credentials/waltid-w3c-credentials/src/commonMain/kotlin/id/walt/w3c/issuance/DataFunctions.kt
@@ -1,6 +1,8 @@
 package id.walt.w3c.issuance
 
 import id.walt.w3c.utils.CredentialDataMergeUtils
+import id.walt.w3c.utils.CredentialSupported
+import id.walt.w3c.utils.CredentialTypeConfig
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
@@ -8,6 +10,7 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
@@ -15,13 +18,50 @@ import kotlin.time.Duration
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
+
 @OptIn(ExperimentalJsExport::class, ExperimentalUuidApi::class)
 @JsExport
 val dataFunctions = mapOf<String, suspend (call: CredentialDataMergeUtils.FunctionCall) -> JsonElement>(
     "subjectDid" to { it.fromContext() },
     "issuerDid" to { it.fromContext() },
     "context" to { it.context[it.args!!]!! },
-    "display" to { it.fromContext() },
+    "display" to {
+        val credentialType = "testCredential+jwt-vc-json"
+        val credentialConfig = CredentialTypeConfig().supportedCredentialTypes[credentialType]
+        println("credentialConfig: $credentialConfig")
+        if (credentialConfig is CredentialSupported && credentialConfig.display.isNotEmpty()) {
+            val display = credentialConfig.display[0]
+            JsonObject(
+                mapOf(
+                    "name" to JsonPrimitive(display.name),
+                    "description" to JsonPrimitive(display.description),
+                    "logo" to JsonObject(
+                        mapOf(
+                            "url" to JsonPrimitive(display.logo.url),
+                            "altText" to JsonPrimitive(display.logo.altText)
+                        )
+                    ),
+                    "backgroundColor" to JsonPrimitive(display.backgroundColor),
+                    "textColor" to JsonPrimitive(display.textColor)
+                )
+            )
+        } else {
+            JsonObject(
+                mapOf(
+                    "name" to JsonPrimitive("Default Display Name"),
+                    "description" to JsonPrimitive("No description available"),
+                    "logo" to JsonObject(
+                        mapOf(
+                            "url" to JsonPrimitive("default_logo_url"),
+                            "altText" to JsonPrimitive("Default Logo AltText")
+                        )
+                    ),
+                    "backgroundColor" to JsonPrimitive("#FFFFFF"),
+                    "textColor" to JsonPrimitive("#000000")
+                )
+            )
+        }
+    },
     "timestamp-ebsi" to { JsonPrimitive(Clock.System.now().toIso8681WithoutSubSecondPrecision())},
     "timestamp-ebsi-in" to { JsonPrimitive((Clock.System.now() + Duration.parse(it.args!!)).toIso8681WithoutSubSecondPrecision()) },
 
