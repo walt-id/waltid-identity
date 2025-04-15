a591b60e3e35e81fc1afe4bd9681912cac3c178b
waltkb <68587968+waltkb@users.noreply.github.com>
2025-04-15 17:55:10 +0200

    Allow nullable disclosures to be missing

--- waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/formats/w3c/W3C2.kt
+++ waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/formats/w3c/W3C2.kt
@@ -9,13 +9,18 @@ import kotlinx.serialization.json.JsonObject
 @Serializable
 @SerialName("vc-w3c_2")
 data class W3C2(
+    // Selective disclosure
     override val disclosables: Map<String, Set<String>>? = null,
     override val disclosures: List<SdJwtSelectiveDisclosure>? = null,
-    override val signature: CredentialSignature?,
-    override val signed: String?,
-    override val signedWithDisclosures: String?,
+    override val signedWithDisclosures: String? = null,
+
+    // Data
     override val credentialData: JsonObject,
-    override val originalCredentialData: JsonObject? = null
+    override val originalCredentialData: JsonObject? = null,
+
+    // Signature
+    override val signature: CredentialSignature?,
+    override val signed: String?
 ) : AbstractW3C() {
 
 }
