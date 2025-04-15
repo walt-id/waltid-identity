199894ebeac0f0b3c18674f39b41c5e354dec17c
waltid-kb <68587968+waltkb@users.noreply.github.com>
2025-04-15 11:21:27 +0000

    Update aws crypto: Use LTS Java version

--- waltid-libraries/crypto/waltid-crypto-aws/build.gradle.kts
+++ waltid-libraries/crypto/waltid-crypto-aws/build.gradle.kts
@@ -33,8 +33,8 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_15
-    targetCompatibility = JavaVersion.VERSION_15
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     withJavadocJar()
     withSourcesJar()
 }
