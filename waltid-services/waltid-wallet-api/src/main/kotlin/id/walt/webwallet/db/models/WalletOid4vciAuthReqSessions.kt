6140a1eca968b7fc0dd52a675bde8af3d5d107ec
chsavvaidis <ch.savvaidis@gmail.com>
2025-04-23 13:01:07 +0200

    fix: json instance in credential offer

--- waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/db/models/WalletOid4vciAuthReqSessions.kt
+++ waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/db/models/WalletOid4vciAuthReqSessions.kt
@@ -2,6 +2,7 @@ package id.walt.webwallet.db.models
 
 import id.walt.oid4vc.data.OpenIDProviderMetadata
 import id.walt.commons.temp.UuidSerializer
+import id.walt.oid4vc.data.CredentialOffer
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.toJavaInstant
@@ -25,6 +26,7 @@ object WalletOid4vciAuthReqSessions : Table("auth_req_sessions") {
     val accountId = uuid("account_id")
     val authorizationRequest = text("authorization_request")
     val issuerMetadata = text("issuer_metadata")
+    val credentialOffer = text("credential_offer")
     val successRedirectUri = text("success_redirect_uri")
     val createdOn = timestamp("created_on")
 
@@ -39,6 +41,7 @@ data class WalletOid4vciAuthReqSession @OptIn(ExperimentalUuidApi::class) constr
     val accountId: Uuid,
     val authorizationRequest: JsonObject,
     val issuerMetadata: OpenIDProviderMetadata,
+    val credentialOffer: CredentialOffer,
     val successRedirectUri: String,
     val createdOn: Instant
 ) {
@@ -48,6 +51,7 @@ data class WalletOid4vciAuthReqSession @OptIn(ExperimentalUuidApi::class) constr
         id = row[WalletOid4vciAuthReqSessions.id],
         authorizationRequest = Json.parseToJsonElement(row[WalletOid4vciAuthReqSessions.authorizationRequest]).jsonObject,
         issuerMetadata = OpenIDProviderMetadata.fromJSON(Json.parseToJsonElement(row[WalletOid4vciAuthReqSessions.issuerMetadata]).jsonObject),
+        credentialOffer = CredentialOffer.fromJSON(Json.parseToJsonElement(row[WalletOid4vciAuthReqSessions.credentialOffer]).jsonObject),
         successRedirectUri = row[WalletOid4vciAuthReqSessions.successRedirectUri],
         createdOn = row[WalletOid4vciAuthReqSessions.createdOn].toKotlinInstant(),
     )
@@ -62,6 +66,7 @@ fun insertAuthReqSession(wallet: Uuid, session: WalletOid4vciAuthReqSession) {
             it[WalletOid4vciAuthReqSessions.wallet] = wallet.toJavaUuid()
             it[authorizationRequest] = session.authorizationRequest.toString()
             it[issuerMetadata] = Json.encodeToString(OpenIDProviderMetadata.serializer(), session.issuerMetadata)
+            it[credentialOffer] = Json.encodeToString(CredentialOffer.serializer(), session.credentialOffer)
             it[successRedirectUri] = session.successRedirectUri
             it[createdOn] = Clock.System.now().toJavaInstant()
         }
