7a9b6adc08157744e2497921ed9cc3a9cefbb235
waltkb <68587968+waltkb@users.noreply.github.com>
2025-04-30 03:24:23 +0200

    Tests for generic issuer & verifier for credentials

--- waltid-libraries/credentials/waltid-digital-credentials/src/commonTest/kotlin/id/walt/credentials/W3CExamples.kt
+++ waltid-libraries/credentials/waltid-digital-credentials/src/commonTest/kotlin/id/walt/credentials/W3CExamples.kt
@@ -25,6 +25,10 @@ object W3CExamples {
           }
         }
     """.trimIndent()
+    val w3cCredentialValues = mapOf(
+        "issuer" to "https://university.example/issuers/565049",
+        "subject" to "did:example:ebfeb1f712ebc6f1c276e12ec21"
+    )
 
     // application/vc
     //language=JSON
@@ -58,6 +62,10 @@ object W3CExamples {
           }
         }
     """.trimIndent()
+    val dipEcdsaSignedW3CCredentialValues = mapOf(
+        "issuer" to "https://university.example/issuers/565049",
+        "subject" to "did:example:ebfeb1f712ebc6f1c276e12ec21"
+    )
 
     // application/vc
     //language=JSON
@@ -91,6 +99,10 @@ object W3CExamples {
           }
         }
     """.trimIndent()
+    val dipEddsaSignedW3CCredentialValues = mapOf(
+        "issuer" to "https://university.example/issuers/565049",
+        "subject" to "did:example:ebfeb1f712ebc6f1c276e12ec21"
+    )
 
     // application/vc
     //language=JSON
@@ -124,6 +136,10 @@ object W3CExamples {
           }
         }
     """.trimIndent()
+    val dipEcdsaSdSignedW3CCredentialValues = mapOf(
+        "issuer" to "https://university.example/issuers/565049",
+        "subject" to "did:example:ebfeb1f712ebc6f1c276e12ec21"
+    )
 
     // application/vc
     //language=JSON
@@ -156,11 +172,19 @@ object W3CExamples {
           }
         }
     """.trimIndent()
+    val dipBbsSignedW3CCredentialValues = mapOf(
+        "issuer" to "https://university.example/issuers/565049",
+        "subject" to "did:example:ebfeb1f712ebc6f1c276e12ec21"
+    )
 
     // application/vc-ld+jwt
     val joseSignedW3CCredential = """
         eyJraWQiOiJFeEhrQk1XOWZtYmt2VjI2Nm1ScHVQMnNVWV9OX0VXSU4xbGFwVXpPOHJvIiwiYWxnIjoiRVMyNTYifQ.eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvbnMvY3JlZGVudGlhbHMvdjIiLCJodHRwczovL3d3dy53My5vcmcvbnMvY3JlZGVudGlhbHMvZXhhbXBsZXMvdjIiXSwiaWQiOiJodHRwOi8vdW5pdmVyc2l0eS5leGFtcGxlL2NyZWRlbnRpYWxzLzM3MzIiLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIiwiRXhhbXBsZURlZ3JlZUNyZWRlbnRpYWwiXSwiaXNzdWVyIjoiaHR0cHM6Ly91bml2ZXJzaXR5LmV4YW1wbGUvaXNzdWVycy81NjUwNDkiLCJ2YWxpZEZyb20iOiIyMDEwLTAxLTAxVDAwOjAwOjAwWiIsImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImlkIjoiZGlkOmV4YW1wbGU6ZWJmZWIxZjcxMmViYzZmMWMyNzZlMTJlYzIxIiwiZGVncmVlIjp7InR5cGUiOiJFeGFtcGxlQmFjaGVsb3JEZWdyZWUiLCJuYW1lIjoiQmFjaGVsb3Igb2YgU2NpZW5jZSBhbmQgQXJ0cyJ9fX0.MA98XcfFDHT1NbcrNqxKRokCHvyqh3sUutKf8Eo69wRK0Bagn_kMfda62RUpTCIWymNo4IJCLlDQyBnLLJ4GUQ
     """.trimIndent()
+    val joseSignedW3CCredentialValues = mapOf(
+        "issuer" to "https://university.example/issuers/565049",
+        "subject" to "did:example:ebfeb1f712ebc6f1c276e12ec21"
+    )
 
     //  application/vc-ld+cose
     val coseSignedW3CCredential = """
@@ -174,47 +198,59 @@ object W3CExamples {
     val waltidIssuedJoseSignedW3CCredential = """
         eyJraWQiOiJkaWQ6a2V5Ono2TWtqb1JocTFqU05KZExpcnVTWHJGRnhhZ3FyenRaYVhIcUhHVVRLSmJjTnl3cCN6Nk1ram9SaHExalNOSmRMaXJ1U1hyRkZ4YWdxcnp0WmFYSHFIR1VUS0piY055d3AiLCJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.eyJpc3MiOiJkaWQ6a2V5Ono2TWtqb1JocTFqU05KZExpcnVTWHJGRnhhZ3FyenRaYVhIcUhHVVRLSmJjTnl3cCIsInN1YiI6ImRpZDprZXk6ejZNa2t2WFNUWWExZnRpU2E5Wll2aWFmM1RFUEhWeVZQMVZoQjdNc2p1OUVyN0xHIiwidmMiOnsiQGNvbnRleHQiOlsiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvdjEiLCJodHRwczovL3B1cmwuaW1zZ2xvYmFsLm9yZy9zcGVjL29iL3YzcDAvY29udGV4dC5qc29uIl0sImlkIjoidXJuOnV1aWQ6ZjdmNDMzMTItMWRmMi00YzNiLWI5NWUtNzIxNTBiMzlhZjQyIiwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIk9wZW5CYWRnZUNyZWRlbnRpYWwiXSwibmFtZSI6IkpGRiB4IHZjLWVkdSBQbHVnRmVzdCAzIEludGVyb3BlcmFiaWxpdHkiLCJpc3N1ZXIiOnsidHlwZSI6WyJQcm9maWxlIl0sIm5hbWUiOiJKb2JzIGZvciB0aGUgRnV0dXJlIChKRkYpIiwidXJsIjoiaHR0cHM6Ly93d3cuamZmLm9yZy8iLCJpbWFnZSI6Imh0dHBzOi8vdzNjLWNjZy5naXRodWIuaW8vdmMtZWQvcGx1Z2Zlc3QtMS0yMDIyL2ltYWdlcy9KRkZfTG9nb0xvY2t1cC5wbmciLCJpZCI6ImRpZDprZXk6ejZNa2pvUmhxMWpTTkpkTGlydVNYckZGeGFncXJ6dFphWEhxSEdVVEtKYmNOeXdwIn0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7InR5cGUiOlsiQWNoaWV2ZW1lbnRTdWJqZWN0Il0sImFjaGlldmVtZW50Ijp7ImlkIjoidXJuOnV1aWQ6YWMyNTRiZDUtOGZhZC00YmIxLTlkMjktZWZkOTM4NTM2OTI2IiwidHlwZSI6WyJBY2hpZXZlbWVudCJdLCJuYW1lIjoiSkZGIHggdmMtZWR1IFBsdWdGZXN0IDMgSW50ZXJvcGVyYWJpbGl0eSIsImRlc2NyaXB0aW9uIjoiVGhpcyB3YWxsZXQgc3VwcG9ydHMgdGhlIHVzZSBvZiBXM0MgVmVyaWZpYWJsZSBDcmVkZW50aWFscyBhbmQgaGFzIGRlbW9uc3RyYXRlZCBpbnRlcm9wZXJhYmlsaXR5IGR1cmluZyB0aGUgcHJlc2VudGF0aW9uIHJlcXVlc3Qgd29ya2Zsb3cgZHVyaW5nIEpGRiB4IFZDLUVEVSBQbHVnRmVzdCAzLiIsImNyaXRlcmlhIjp7InR5cGUiOiJDcml0ZXJpYSIsIm5hcnJhdGl2ZSI6IldhbGxldCBzb2x1dGlvbnMgcHJvdmlkZXJzIGVhcm5lZCB0aGlzIGJhZGdlIGJ5IGRlbW9uc3RyYXRpbmcgaW50ZXJvcGVyYWJpbGl0eSBkdXJpbmcgdGhlIHByZXNlbnRhdGlvbiByZXF1ZXN0IHdvcmtmbG93LiBUaGlzIGluY2x1ZGVzIHN1Y2Nlc3NmdWxseSByZWNlaXZpbmcgYSBwcmVzZW50YXRpb24gcmVxdWVzdCwgYWxsb3dpbmcgdGhlIGhvbGRlciB0byBzZWxlY3QgYXQgbGVhc3QgdHdvIHR5cGVzIG9mIHZlcmlmaWFibGUgY3JlZGVudGlhbHMgdG8gY3JlYXRlIGEgdmVyaWZpYWJsZSBwcmVzZW50YXRpb24sIHJldHVybmluZyB0aGUgcHJlc2VudGF0aW9uIHRvIHRoZSByZXF1ZXN0b3IsIGFuZCBwYXNzaW5nIHZlcmlmaWNhdGlvbiBvZiB0aGUgcHJlc2VudGF0aW9uIGFuZCB0aGUgaW5jbHVkZWQgY3JlZGVudGlhbHMuIn0sImltYWdlIjp7ImlkIjoiaHR0cHM6Ly93M2MtY2NnLmdpdGh1Yi5pby92Yy1lZC9wbHVnZmVzdC0zLTIwMjMvaW1hZ2VzL0pGRi1WQy1FRFUtUExVR0ZFU1QzLWJhZGdlLWltYWdlLnBuZyIsInR5cGUiOiJJbWFnZSJ9fSwiaWQiOiJkaWQ6a2V5Ono2TWtrdlhTVFlhMWZ0aVNhOVpZdmlhZjNURVBIVnlWUDFWaEI3TXNqdTlFcjdMRyJ9LCJpc3N1YW5jZURhdGUiOiIyMDI1LTAzLTE5VDEzOjU0OjA1LjkwMTk2ODAzN1oiLCJleHBpcmF0aW9uRGF0ZSI6IjIwMjYtMDMtMTlUMTM6NTQ6MDUuOTAyMDIwMDM4WiJ9LCJqdGkiOiJ1cm46dXVpZDpmN2Y0MzMxMi0xZGYyLTRjM2ItYjk1ZS03MjE1MGIzOWFmNDIiLCJleHAiOjE3NzM5Mjg0NDUsImlhdCI6MTc0MjM5MjQ0NSwibmJmIjoxNzQyMzkyNDQ1fQ.eppq-IFQ5LW7UZ514jvn9rgrHQ23iDTsCGDq3EH6FbYf-RkN763eAEmuYIzXykgc5bRqdHOrmV_IJ4VUJedkBg
     """.trimIndent()
+    val waltidIssuedJoseSignedW3CCredentialValues = mapOf(
+        "issuer" to "did:key:z6MkjoRhq1jSNJdLiruSXrFFxagqrztZaXHqHGUTKJbcNywp",
+        "subject" to "did:key:z6MkkvXSTYa1ftiSa9ZYviaf3TEPHVyVP1VhB7Msju9Er7LG"
+    )
 
     val ossIssuedW3CSdJwt = """
         eyJraWQiOiJkaWQ6a2V5Ono2TWtqb1JocTFqU05KZExpcnVTWHJGRnhhZ3FyenRaYVhIcUhHVVRLSmJjTnl3cCN6Nk1ram9SaHExalNOSmRMaXJ1U1hyRkZ4YWdxcnp0WmFYSHFIR1VUS0piY055d3AiLCJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.eyJpc3MiOiJkaWQ6a2V5Ono2TWtqb1JocTFqU05KZExpcnVTWHJGRnhhZ3FyenRaYVhIcUhHVVRLSmJjTnl3cCIsInN1YiI6ImRpZDprZXk6ejZNa2t2WFNUWWExZnRpU2E5Wll2aWFmM1RFUEhWeVZQMVZoQjdNc2p1OUVyN0xHIiwidmMiOnsiQGNvbnRleHQiOlsiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvdjEiLCJodHRwczovL3B1cmwuaW1zZ2xvYmFsLm9yZy9zcGVjL29iL3YzcDAvY29udGV4dC5qc29uIl0sImlkIjoidXJuOnV1aWQ6YmRmMjE2ZGItOTJmOC00MTYwLThiYTYtNmRlNDliODRhNGQ5IiwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIk9wZW5CYWRnZUNyZWRlbnRpYWwiXSwiaXNzdWVyIjp7InR5cGUiOlsiUHJvZmlsZSJdLCJuYW1lIjoiSm9icyBmb3IgdGhlIEZ1dHVyZSAoSkZGKSIsInVybCI6Imh0dHBzOi8vd3d3LmpmZi5vcmcvIiwiaW1hZ2UiOiJodHRwczovL3czYy1jY2cuZ2l0aHViLmlvL3ZjLWVkL3BsdWdmZXN0LTEtMjAyMi9pbWFnZXMvSkZGX0xvZ29Mb2NrdXAucG5nIiwiaWQiOiJkaWQ6a2V5Ono2TWtqb1JocTFqU05KZExpcnVTWHJGRnhhZ3FyenRaYVhIcUhHVVRLSmJjTnl3cCJ9LCJjcmVkZW50aWFsU3ViamVjdCI6eyJ0eXBlIjpbIkFjaGlldmVtZW50U3ViamVjdCJdLCJhY2hpZXZlbWVudCI6eyJpZCI6InVybjp1dWlkOmFjMjU0YmQ1LThmYWQtNGJiMS05ZDI5LWVmZDkzODUzNjkyNiIsInR5cGUiOlsiQWNoaWV2ZW1lbnQiXSwibmFtZSI6IkpGRiB4IHZjLWVkdSBQbHVnRmVzdCAzIEludGVyb3BlcmFiaWxpdHkiLCJkZXNjcmlwdGlvbiI6IlRoaXMgd2FsbGV0IHN1cHBvcnRzIHRoZSB1c2Ugb2YgVzNDIFZlcmlmaWFibGUgQ3JlZGVudGlhbHMgYW5kIGhhcyBkZW1vbnN0cmF0ZWQgaW50ZXJvcGVyYWJpbGl0eSBkdXJpbmcgdGhlIHByZXNlbnRhdGlvbiByZXF1ZXN0IHdvcmtmbG93IGR1cmluZyBKRkYgeCBWQy1FRFUgUGx1Z0Zlc3QgMy4iLCJjcml0ZXJpYSI6eyJ0eXBlIjoiQ3JpdGVyaWEiLCJuYXJyYXRpdmUiOiJXYWxsZXQgc29sdXRpb25zIHByb3ZpZGVycyBlYXJuZWQgdGhpcyBiYWRnZSBieSBkZW1vbnN0cmF0aW5nIGludGVyb3BlcmFiaWxpdHkgZHVyaW5nIHRoZSBwcmVzZW50YXRpb24gcmVxdWVzdCB3b3JrZmxvdy4gVGhpcyBpbmNsdWRlcyBzdWNjZXNzZnVsbHkgcmVjZWl2aW5nIGEgcHJlc2VudGF0aW9uIHJlcXVlc3QsIGFsbG93aW5nIHRoZSBob2xkZXIgdG8gc2VsZWN0IGF0IGxlYXN0IHR3byB0eXBlcyBvZiB2ZXJpZmlhYmxlIGNyZWRlbnRpYWxzIHRvIGNyZWF0ZSBhIHZlcmlmaWFibGUgcHJlc2VudGF0aW9uLCByZXR1cm5pbmcgdGhlIHByZXNlbnRhdGlvbiB0byB0aGUgcmVxdWVzdG9yLCBhbmQgcGFzc2luZyB2ZXJpZmljYXRpb24gb2YgdGhlIHByZXNlbnRhdGlvbiBhbmQgdGhlIGluY2x1ZGVkIGNyZWRlbnRpYWxzLiJ9LCJpbWFnZSI6eyJpZCI6Imh0dHBzOi8vdzNjLWNjZy5naXRodWIuaW8vdmMtZWQvcGx1Z2Zlc3QtMy0yMDIzL2ltYWdlcy9KRkYtVkMtRURVLVBMVUdGRVNUMy1iYWRnZS1pbWFnZS5wbmciLCJ0eXBlIjoiSW1hZ2UifX0sImlkIjoiZGlkOmtleTp6Nk1ra3ZYU1RZYTFmdGlTYTlaWXZpYWYzVEVQSFZ5VlAxVmhCN01zanU5RXI3TEcifSwiaXNzdWFuY2VEYXRlIjoiMjAyNS0wNC0wOFQxOTo1MzozOS4zMjUzODIyMTdaIiwiZXhwaXJhdGlvbkRhdGUiOiIyMDI2LTA0LTA4VDE5OjUzOjM5LjMyNTQwMzkxN1oiLCJfc2QiOlsibVZkTkExaS1ZSjdQT0FWMmFNTjV2Xy1BU3pHOVhfSC12WDZ2NEpySWR3YyJdfSwianRpIjoidXJuOnV1aWQ6YmRmMjE2ZGItOTJmOC00MTYwLThiYTYtNmRlNDliODRhNGQ5IiwiZXhwIjoxNzc1Njc4MDE5LCJpYXQiOjE3NDQxNDIwMTksIm5iZiI6MTc0NDE0MjAxOX0.OZ3EywnJ_KDTPH5-S0OC3jN67pIdrJAwTugHh53jrI471TAbCCJ48-YVgX5l8ZLWLeKQMdFyvNpNQLqsbu9ECA~WyIzX0hndTl4UDh2T25qaW03UE4yc3FRPT0iLCJuYW1lIiwiSkZGIHggdmMtZWR1IFBsdWdGZXN0IDMgSW50ZXJvcGVyYWJpbGl0eSJd
     """.trimIndent()
+    val ossIssuedW3CSdJwtValues = mapOf(
+        "issuer" to "did:key:z6MkjoRhq1jSNJdLiruSXrFFxagqrztZaXHqHGUTKJbcNywp",
+        "subject" to "did:key:z6MkkvXSTYa1ftiSa9ZYviaf3TEPHVyVP1VhB7Msju9Er7LG"
+    )
 
     val ossIssuedW3CSdJwt2 = """
         eyJraWQiOiJkaWQ6a2V5Ono2TWtqb1JocTFqU05KZExpcnVTWHJGRnhhZ3FyenRaYVhIcUhHVVRLSmJjTnl3cCN6Nk1ram9SaHExalNOSmRMaXJ1U1hyRkZ4YWdxcnp0WmFYSHFIR1VUS0piY055d3AiLCJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.eyJpc3MiOiJkaWQ6a2V5Ono2TWtqb1JocTFqU05KZExpcnVTWHJGRnhhZ3FyenRaYVhIcUhHVVRLSmJjTnl3cCIsInN1YiI6ImRpZDprZXk6ejZNa2t2WFNUWWExZnRpU2E5Wll2aWFmM1RFUEhWeVZQMVZoQjdNc2p1OUVyN0xHIiwidmMiOnsiQGNvbnRleHQiOlsiaHR0cHM6Ly93d3cudzMub3JnL25zL2NyZWRlbnRpYWxzL3YyIiwiaHR0cHM6Ly9wdXJsLmltc2dsb2JhbC5vcmcvc3BlYy9vYi92M3AwL2NvbnRleHQuanNvbiJdLCJpZCI6InVybjp1dWlkOjI1MjkyOWRjLThmMTUtNGMyOS05MjQ5LTUxM2QxMGNjNjc4YiIsInR5cGUiOlsiVmVyaWZpYWJsZUNyZWRlbnRpYWwiLCJPcGVuQmFkZ2VDcmVkZW50aWFsIl0sImlzc3VlciI6eyJ0eXBlIjpbIlByb2ZpbGUiXSwibmFtZSI6IkpvYnMgZm9yIHRoZSBGdXR1cmUgKEpGRikiLCJ1cmwiOiJodHRwczovL3d3dy5qZmYub3JnLyIsImltYWdlIjoiaHR0cHM6Ly93M2MtY2NnLmdpdGh1Yi5pby92Yy1lZC9wbHVnZmVzdC0xLTIwMjIvaW1hZ2VzL0pGRl9Mb2dvTG9ja3VwLnBuZyIsImlkIjoiZGlkOmtleTp6Nk1ram9SaHExalNOSmRMaXJ1U1hyRkZ4YWdxcnp0WmFYSHFIR1VUS0piY055d3AifSwiY3JlZGVudGlhbFN1YmplY3QiOnsidHlwZSI6WyJBY2hpZXZlbWVudFN1YmplY3QiXSwiYWNoaWV2ZW1lbnQiOnsiaWQiOiJ1cm46dXVpZDphYzI1NGJkNS04ZmFkLTRiYjEtOWQyOS1lZmQ5Mzg1MzY5MjYiLCJ0eXBlIjpbIkFjaGlldmVtZW50Il0sIm5hbWUiOiJKRkYgeCB2Yy1lZHUgUGx1Z0Zlc3QgMyBJbnRlcm9wZXJhYmlsaXR5IiwiaW1hZ2UiOnsiaWQiOiJodHRwczovL3czYy1jY2cuZ2l0aHViLmlvL3ZjLWVkL3BsdWdmZXN0LTMtMjAyMy9pbWFnZXMvSkZGLVZDLUVEVS1QTFVHRkVTVDMtYmFkZ2UtaW1hZ2UucG5nIiwidHlwZSI6IkltYWdlIn0sIl9zZCI6WyJVTlprdVp6WTZrdW96RHFiQjlTZ08yZFE1aElKMDJ1R3ZfVTQyMDlGRF9jIiwiRUFqdUdjQjMwbnpPSVhVOHN0RE5KS0ltbjhXVjZCNFhsRkdyNlVDOTVtcyJdfSwiaWQiOiJkaWQ6a2V5Ono2TWtrdlhTVFlhMWZ0aVNhOVpZdmlhZjNURVBIVnlWUDFWaEI3TXNqdTlFcjdMRyJ9LCJpc3N1YW5jZURhdGUiOiIyMDI1LTA0LTA4VDIzOjI5OjUxLjQ1NjQwMDYwMVoiLCJleHBpcmF0aW9uRGF0ZSI6IjIwMjYtMDQtMDhUMjM6Mjk6NTEuNDU2NDIxNjAxWiIsIl9zZCI6WyItYnBYUUhXS0lseGtiaF95Y2VVRzMwYktwS3lyZjE0OUdoY0hGei1EUXpFIl19LCJqdGkiOiJ1cm46dXVpZDoyNTI5MjlkYy04ZjE1LTRjMjktOTI0OS01MTNkMTBjYzY3OGIiLCJleHAiOjE3NzU2OTA5OTEsImlhdCI6MTc0NDE1NDk5MSwibmJmIjoxNzQ0MTU0OTkxfQ.mI0HjLiauyZ0nN_KZMyWLuJqGuWXLyrNw9EWg_4mvLt1jJ_4Y8MFb7L1Zw04ColQ2_7pNRh45xPgacVNKOnSDg~WyJ0QlFGazR2WlF4d05zNmN1SzFqRUtRPT0iLCJuYW1lIiwiSkZGIHggdmMtZWR1IFBsdWdGZXN0IDMgSW50ZXJvcGVyYWJpbGl0eSJd~WyJqRDE3ZFRCOWFwSWRpblQ4b0hpVmNRPT0iLCJkZXNjcmlwdGlvbiIsIlRoaXMgd2FsbGV0IHN1cHBvcnRzIHRoZSB1c2Ugb2YgVzNDIFZlcmlmaWFibGUgQ3JlZGVudGlhbHMgYW5kIGhhcyBkZW1vbnN0cmF0ZWQgaW50ZXJvcGVyYWJpbGl0eSBkdXJpbmcgdGhlIHByZXNlbnRhdGlvbiByZXF1ZXN0IHdvcmtmbG93IGR1cmluZyBKRkYgeCBWQy1FRFUgUGx1Z0Zlc3QgMy4iXQ~WyJya01mMkthSkdDWkJLdERDRkpSeTNBPT0iLCJjcml0ZXJpYSIseyJ0eXBlIjoiQ3JpdGVyaWEiLCJuYXJyYXRpdmUiOiJXYWxsZXQgc29sdXRpb25zIHByb3ZpZGVycyBlYXJuZWQgdGhpcyBiYWRnZSBieSBkZW1vbnN0cmF0aW5nIGludGVyb3BlcmFiaWxpdHkgZHVyaW5nIHRoZSBwcmVzZW50YXRpb24gcmVxdWVzdCB3b3JrZmxvdy4gVGhpcyBpbmNsdWRlcyBzdWNjZXNzZnVsbHkgcmVjZWl2aW5nIGEgcHJlc2VudGF0aW9uIHJlcXVlc3QsIGFsbG93aW5nIHRoZSBob2xkZXIgdG8gc2VsZWN0IGF0IGxlYXN0IHR3byB0eXBlcyBvZiB2ZXJpZmlhYmxlIGNyZWRlbnRpYWxzIHRvIGNyZWF0ZSBhIHZlcmlmaWFibGUgcHJlc2VudGF0aW9uLCByZXR1cm5pbmcgdGhlIHByZXNlbnRhdGlvbiB0byB0aGUgcmVxdWVzdG9yLCBhbmQgcGFzc2luZyB2ZXJpZmljYXRpb24gb2YgdGhlIHByZXNlbnRhdGlvbiBhbmQgdGhlIGluY2x1ZGVkIGNyZWRlbnRpYWxzLiJ9XQ
     """.trimIndent()
+    val ossIssuedW3CSdJwt2Values = mapOf(
+        "issuer" to "did:key:z6MkjoRhq1jSNJdLiruSXrFFxagqrztZaXHqHGUTKJbcNywp",
+        "subject" to "did:key:z6MkkvXSTYa1ftiSa9ZYviaf3TEPHVyVP1VhB7Msju9Er7LG"
+    )
 
-    val allW3CCredentialExamples = listOf(
-        w3cCredential to CredentialDetectionResult(
+        val allW3CCredentialExamples = listOf(
+        w3cCredential to w3cCredentialValues to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_2,
             CredentialDetectorTypes.SignaturePrimaryType.UNSIGNED
         ), // unsigned
-        dipEcdsaSignedW3CCredential to CredentialDetectionResult(
+        dipEcdsaSignedW3CCredential to dipEcdsaSignedW3CCredentialValues to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_2,
             CredentialDetectorTypes.SignaturePrimaryType.DATA_INTEGRITY_PROOF
         ), // DataIntegrityProof: ECDSA
-        dipEddsaSignedW3CCredential to CredentialDetectionResult(
+        dipEddsaSignedW3CCredential to dipEddsaSignedW3CCredentialValues to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_2,
             CredentialDetectorTypes.SignaturePrimaryType.DATA_INTEGRITY_PROOF
         ), // DataIntegrityProof: EdDSA
-        dipEcdsaSdSignedW3CCredential to CredentialDetectionResult(
+        dipEcdsaSdSignedW3CCredential to dipEcdsaSdSignedW3CCredentialValues to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_2,
             CredentialDetectorTypes.SignaturePrimaryType.DATA_INTEGRITY_PROOF
         ), // DataIntegrityProof: ECDSA-SD
-        dipBbsSignedW3CCredential to CredentialDetectionResult(
+        dipBbsSignedW3CCredential to dipBbsSignedW3CCredentialValues to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_2,
             CredentialDetectorTypes.SignaturePrimaryType.DATA_INTEGRITY_PROOF
         ), // DataIntegrityProof: BBS
-        joseSignedW3CCredential to CredentialDetectionResult(
+        joseSignedW3CCredential to joseSignedW3CCredentialValues to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_2,
             CredentialDetectorTypes.SignaturePrimaryType.JWT
         ), // JOSE
-        waltidIssuedJoseSignedW3CCredential to CredentialDetectionResult(
+        waltidIssuedJoseSignedW3CCredential to waltidIssuedJoseSignedW3CCredentialValues to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_1_1,
             CredentialDetectorTypes.SignaturePrimaryType.JWT
@@ -227,14 +263,14 @@ object W3CExamples {
             containsDisclosables = true,
             providesDisclosures = true
         ), // SD-JWT*/
-        ossIssuedW3CSdJwt to CredentialDetectionResult(
+        ossIssuedW3CSdJwt to ossIssuedW3CSdJwtValues to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_1_1,
             CredentialDetectorTypes.SignaturePrimaryType.SDJWT,
             containsDisclosables = true,
             providesDisclosures = true
         ),
-        ossIssuedW3CSdJwt2 to CredentialDetectionResult(
+        ossIssuedW3CSdJwt2 to ossIssuedW3CSdJwt2Values to CredentialDetectionResult(
             CredentialPrimaryDataType.W3C,
             W3CSubType.W3C_2,
             CredentialDetectorTypes.SignaturePrimaryType.SDJWT,
