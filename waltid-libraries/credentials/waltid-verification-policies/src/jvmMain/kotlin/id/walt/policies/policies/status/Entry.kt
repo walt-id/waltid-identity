9f88fdd99d882ac8ee0eadb491d15a48a76886fb
mikeplotean <mike@walt.id>
2025-05-27 20:34:57 +0300

    feat: add credential status entry parser

---
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/Entry.kt
@@ -0,0 +1,40 @@
+package id.walt.policies.policies.status
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+//todo: maybe use waltid-credential-status domain?
+
+@Serializable
+data class W3CEntry(
+    val id: String,
+    val type: String,
+    val statusPurpose: String,
+    val statusListIndex: String,
+    val statusSize: Int,
+    val statusListCredential: String,
+    val statusMessage: List<Status>? = null,
+    val statusReference: String? = null,
+) {
+    @Serializable
+    data class Status(
+        @SerialName("status")
+        val type: String,
+        val message: String,
+    )
+}
+
+@Serializable
+data class IETFEntry(
+    @SerialName("status_list")
+    val statusList: StatusListField,
+) {
+
+    @Serializable
+    data class StatusListField(
+        @SerialName("idx")
+        val index: Int,
+        val uri: String,
+    )
+}
