b2422ded5518d6b46fbc203921e794ebccb7ac89
mikeplotean <mike@walt.id>
2025-05-28 21:21:12 +0300

    refactor: status value readers
    Signed-off-by: mikeplotean <mike@walt.id>

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/reader/IETFStatusValueReader.kt
+++
@@ -1,9 +0,0 @@
-package id.walt.policies.policies.status.reader
-
-class IETFStatusValueReader : StatusValueReader {
-    override fun read(
-        response: String, statusListIndex: ULong
-    ): Result<List<Char>> {
-        TODO("Not yet implemented")
-    }
-}
