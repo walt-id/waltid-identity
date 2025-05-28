51d47d73cd6ddfe5a0e1be30e5bc6d185e08f843
mikeplotean <mike@walt.id>
2025-05-28 18:33:50 +0300

    refactor: rename expansion package
    Signed-off-by: mikeplotean <mike@walt.id>

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/decoder/StatusListDecoder.kt
+++
@@ -1,7 +0,0 @@
-package id.walt.policies.policies.status.decoder
-
-import java.io.InputStream
-
-interface StatusListDecoder {
-    fun decode(bitstring: String): InputStream
-}
--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/expansion/TokenStatusListExpansionAlgorithm.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/expansion/TokenStatusListExpansionAlgorithm.kt
@@ -1,10 +1,10 @@
-package id.walt.policies.policies.status.decoder
+package id.walt.policies.policies.status.expansion
 
 import id.walt.policies.policies.Base64Utils
 import java.io.InputStream
 import java.util.zip.InflaterInputStream
 
-class TokenStatusListDecoder : StatusListDecoder {
-    override fun decode(bitstring: String): InputStream =
+class TokenStatusListExpansionAlgorithm : StatusListExpansionAlgorithm {
+    override operator fun invoke(bitstring: String): InputStream =
         InflaterInputStream(Base64Utils.urlDecode(bitstring).inputStream())
 }
