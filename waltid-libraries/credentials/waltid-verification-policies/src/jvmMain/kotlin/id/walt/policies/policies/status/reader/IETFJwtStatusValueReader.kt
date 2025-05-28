6b699d660c9fdae38d4354a653f8063ac96dfa9a
mikeplotean <mike@walt.id>
2025-05-28 21:21:12 +0300

    refactor: status value readers
    Signed-off-by: mikeplotean <mike@walt.id>

---
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/reader/IETFJwtStatusValueReader.kt
@@ -0,0 +1,18 @@
+package id.walt.policies.policies.status.reader
+
+import id.walt.policies.policies.status.BitValueReader
+import id.walt.policies.policies.status.parser.ContentParser
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.jsonObject
+
+class IETFJwtStatusValueReader(
+    parser: ContentParser<JsonObject>,
+    bitValueReader: BitValueReader,
+) : StatusValueReaderBase<JsonObject, IETFStatusContent>(parser, bitValueReader) {
+    override fun parseStatusList(payload: JsonObject): IETFStatusContent {
+        val statusList = payload["status_list"]?.jsonObject!!
+        logger.debug { "status_list: $statusList" }
+        return jsonModule.decodeFromJsonElement<IETFStatusContent>(statusList)
+    }
+}
--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/reader/W3CStatusValueReader.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/reader/W3CStatusValueReader.kt
@@ -1,36 +1,19 @@
 package id.walt.policies.policies.status.reader
 
-import id.walt.policies.policies.Base64Utils
 import id.walt.policies.policies.status.BitValueReader
-import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.serialization.json.Json
+import id.walt.policies.policies.status.parser.ContentParser
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
 
 class W3CStatusValueReader(
-    private val bitValueReader: BitValueReader
-) : StatusValueReader {
-    private val logger = KotlinLogging.logger {}
+    parser: ContentParser<JsonObject>,
+    bitValueReader: BitValueReader,
+) : StatusValueReaderBase<JsonObject, W3CStatusContent>(parser, bitValueReader) {
 
-    override fun read(response: String, statusListIndex: ULong) = runCatching {
-        val payload = response.substringAfter(".").substringBefore(".")
-            .let { Json.decodeFromString<JsonObject>(Base64Utils.decode(it).decodeToString()) }
-
-        logger.debug { "Payload: $payload" }
+    override fun parseStatusList(payload: JsonObject): W3CStatusContent {
         val credentialSubject = payload["vc"]!!.jsonObject["credentialSubject"]?.jsonObject!!
         logger.debug { "CredentialSubject: $credentialSubject" }
-        val encodedList = credentialSubject["encodedList"]?.jsonPrimitive?.content ?: ""
-        logger.debug { "EncodedList: $encodedList" }
-        val bitValue = bitValueReader.get(encodedList, statusListIndex)
-        logger.debug { "EncodedList[$statusListIndex] = $bitValue" }
-        // ensure bitValue always consists of valid binary characters (0,1)
-        require(!bitValue.isNullOrEmpty()) { "Null or empty bit value" }
-        require(isBinaryValue(bitValue)) { "Invalid bit value: $bitValue" }
-        bitValue
-    }
-
-    fun isBinaryValue(value: List<Char>) = setOf('0', '1').let { valid ->
-        value.all { it in valid }
+        return jsonModule.decodeFromJsonElement<W3CStatusContent>(credentialSubject)
     }
 }
