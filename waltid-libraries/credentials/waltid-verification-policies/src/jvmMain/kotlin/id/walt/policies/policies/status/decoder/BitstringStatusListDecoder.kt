46eaad11b94925b1891b6d8d772a83ff0cd041b3
mikeplotean <mike@walt.id>
2025-05-28 18:33:50 +0300

    refactor: rename expansion package
    Signed-off-by: mikeplotean <mike@walt.id>

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/decoder/BitstringStatusListDecoder.kt
+++
@@ -1,9 +0,0 @@
-package id.walt.policies.policies.status.decoder
-
-import id.walt.policies.policies.Base64Utils
-import java.io.InputStream
-import java.util.zip.GZIPInputStream
-
-class BitstringStatusListDecoder : StatusListDecoder {
-    override fun decode(bitstring: String): InputStream = GZIPInputStream(Base64Utils.urlDecode(bitstring).inputStream())
-}
