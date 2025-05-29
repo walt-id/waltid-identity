89f6a258b947c3e2411ae22b7a751845038a1710
mikeplotean <mike@walt.id>
2025-05-29 11:20:57 +0300

    refactor: status policy arguments

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/CredentialStatusPolicyArguments.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/CredentialStatusPolicyArguments.kt
@@ -1,22 +1,23 @@
 package id.walt.policies.policies.status
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class CredentialStatusPolicyArguments {
-    abstract val type: String
-    abstract val expectedValue: String
+    abstract val value: String
 }
 
 @Serializable
+@SerialName("w3c")
 data class W3CStatusPolicyArguments(
     val purpose: String,
-    override val type: String,
-    override val expectedValue: String,
+    val type: String,
+    override val value: String,
 ) : CredentialStatusPolicyArguments()
 
 @Serializable
+@SerialName("ietf")
 data class IETFStatusPolicyArguments(
-    override val type: String,
-    override val expectedValue: String,
+    override val value: String,
 ) : CredentialStatusPolicyArguments()
