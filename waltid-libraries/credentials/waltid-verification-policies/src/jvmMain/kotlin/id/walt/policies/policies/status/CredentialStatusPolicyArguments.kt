0b54501a8310b04d3d9bf83ae27d09722374e2c7
mikeplotean <mike@walt.id>
2025-05-29 19:19:02 +0300

    refactor: update status policy arguments fields types
    Signed-off-by: mikeplotean <mike@walt.id>

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/CredentialStatusPolicyArguments.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/CredentialStatusPolicyArguments.kt
@@ -1,11 +1,15 @@
 package id.walt.policies.policies.status
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonClassDiscriminator
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
+@JsonClassDiscriminator("discriminator")
 sealed class CredentialStatusPolicyArguments {
-    abstract val value: String
+    abstract val value: UInt
 }
 
 @Serializable
@@ -13,11 +17,11 @@ sealed class CredentialStatusPolicyArguments {
 data class W3CStatusPolicyArguments(
     val purpose: String,
     val type: String,
-    override val value: String,
+    override val value: UInt,
 ) : CredentialStatusPolicyArguments()
 
 @Serializable
 @SerialName("ietf")
 data class IETFStatusPolicyArguments(
-    override val value: String,
+    override val value: UInt,
 ) : CredentialStatusPolicyArguments()
