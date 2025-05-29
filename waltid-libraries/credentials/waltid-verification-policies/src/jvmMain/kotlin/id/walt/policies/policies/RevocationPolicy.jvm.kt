c6e9c8c55a03ffbddf27f7aa9f38095fb788a97d
mikeplotean <mike@walt.id>
2025-05-29 19:21:22 +0300

    refactor: revocation policy to use status validator
    Signed-off-by: mikeplotean <mike@walt.id>

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/RevocationPolicy.jvm.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/RevocationPolicy.jvm.kt
@@ -1,17 +1,22 @@
 package id.walt.policies.policies
 
-import id.walt.policies.policies.status.BitValueReader
 import id.walt.policies.policies.status.CredentialFetcher
-import id.walt.policies.policies.status.expansion.StatusList2021ExpansionAlgorithm
+import id.walt.policies.policies.status.W3CStatusPolicyArguments
+import id.walt.policies.policies.status.entry.EntryParser
+import id.walt.policies.policies.status.entry.W3CEntry
 import id.walt.policies.policies.status.parser.JwtParser
 import id.walt.policies.policies.status.reader.W3CStatusValueReader
-import io.github.oshai.kotlinlogging.KotlinLogging
+import id.walt.policies.policies.status.validator.W3CStatusValidator
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
-import kotlinx.serialization.json.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.serializer
 import love.forte.plugin.suspendtrans.annotation.JvmAsync
 import love.forte.plugin.suspendtrans.annotation.JvmBlocking
 import java.util.*
@@ -19,8 +24,6 @@ import java.util.*
 
 @Serializable
 actual class RevocationPolicy : RevocationPolicyMp() {
-    @Transient
-    private val logger = KotlinLogging.logger {}
     //todo: inject through constructor (not allowed by interface atm - needs serializable)
     @Transient
     private val httpClient = HttpClient {
@@ -29,43 +32,31 @@ actual class RevocationPolicy : RevocationPolicyMp() {
         }
     }
     @Transient
+    private val jsonModule = Json { ignoreUnknownKeys = true }
+    @Transient
     private val credentialFetcher = CredentialFetcher(httpClient)
     @Transient
-    private val bitValueReader = BitValueReader(StatusList2021ExpansionAlgorithm())
+    private val statusReader = W3CStatusValueReader(JwtParser())
     @Transient
-    private val statusReader = W3CStatusValueReader(JwtParser(), bitValueReader)
+    private val validator = W3CStatusValidator(credentialFetcher, statusReader)
     //endtodo
 
     @JvmBlocking
     @JvmAsync
     actual override suspend fun verify(data: JsonObject, args: Any?, context: Map<String, Any>): Result<Any> {
-        val credentialStatus = data["vc"]?.jsonObject?.get("credentialStatus")
-            ?: return Result.success(
-                JsonObject(mapOf("policy_available" to JsonPrimitive(false)))
-            )
-
-        logger.debug { "Credential status: $credentialStatus" }
-        val statusListIndex = credentialStatus.jsonObject["statusListIndex"]?.jsonPrimitive?.content?.toULong()!!
-        val statusListCredentialUrl = credentialStatus.jsonObject["statusListCredential"]?.jsonPrimitive?.content
-        logger.debug { "Status list index: $statusListIndex" }
-        logger.debug { "Credential URL: $statusListCredentialUrl" }
-        val response = credentialFetcher.fetch(statusListCredentialUrl!!).getOrThrow()
-        // response is a jwt
-        val bitValue = statusReader.read(response, statusListIndex).getOrElse {
-            return Result.failure(Throwable(it.cause))
-        }
-
-        checkStatus(bitValue).getOrElse {
-            return Result.failure(Throwable("Credential has been revoked"))
-        }
-        return Result.success(statusListCredentialUrl)
+        // extract status entry from holder credential
+        val credentialStatus = data["vc"]?.jsonObject?.get("credentialStatus") ?: return Result.success(
+            JsonObject(mapOf("policy_available" to JsonPrimitive(false)))
+        )
+        // parse status entry
+        val entry = EntryParser.objectParser(jsonModule, serializer<W3CEntry>()).parse(credentialStatus)
+        val arguments = (args as? W3CStatusPolicyArguments) ?: W3CStatusPolicyArguments(
+            purpose = "revocation",
+            type = "StatusList2021",
+            value = 0u
+        )
+        return validator.validate(entry, arguments)
     }
-
-    private fun checkStatus(it: List<Char>) = runCatching {
-        require(binToInt(it.joinToString("")) == 0)
-    }
-
-    fun binToInt(bin: String) = bin.toInt(2)
 }
 
 object Base64Utils {
