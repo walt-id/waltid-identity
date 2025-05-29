7ba4bd57d5ba60a9a8cd5fb3658f310d7a67d0a7
mikeplotean <mike@walt.id>
2025-05-29 19:18:11 +0300

    refactor: bit value reader to update package and inject bit representation
    Signed-off-by: mikeplotean <mike@walt.id>

---
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/bit/BitRepresentationStrategy.kt
@@ -0,0 +1,13 @@
+package id.walt.policies.policies.status.bit
+
+interface BitRepresentationStrategy {
+    operator fun invoke(): IntProgression
+}
+
+class BigEndianRepresentation : BitRepresentationStrategy {
+    override fun invoke(): IntProgression = 7 downTo 0
+}
+
+class LittleEndianRepresentation : BitRepresentationStrategy {
+    override fun invoke(): IntProgression = 0..7
+}
