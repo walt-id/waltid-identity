fb568337ff1fa26ceab90bafa65a8505c94cebe0
mikeplotean <mike@walt.id>
2025-05-30 23:58:43 +0300

    refactor: status validator
    Signed-off-by: mikeplotean <mike@walt.id>

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/StatusValidatorBase.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/StatusValidatorBase.kt
@@ -1,20 +1,52 @@
 package id.walt.policies.policies.status.validator
 
-import id.walt.policies.policies.status.CredentialStatusPolicyArguments
+import id.walt.policies.policies.status.CredentialFetcher
+import id.walt.policies.policies.status.CredentialStatusPolicyAttribute
+import id.walt.policies.policies.status.StatusContent
+import id.walt.policies.policies.status.bit.BigEndianRepresentation
+import id.walt.policies.policies.status.bit.BitValueReader
+import id.walt.policies.policies.status.entry.StatusEntry
+import id.walt.policies.policies.status.expansion.StatusListExpansionAlgorithm
+import id.walt.policies.policies.status.reader.StatusValueReader
 import io.github.oshai.kotlinlogging.KotlinLogging
 
-abstract class StatusValidatorBase<K, T : CredentialStatusPolicyArguments> : StatusValidator<K, T> {
+abstract class StatusValidatorBase<K : StatusContent, M : StatusEntry, T : CredentialStatusPolicyAttribute>(
+    private val fetcher: CredentialFetcher,
+    private val reader: StatusValueReader<K>,
+) : StatusValidator<M, T> {
     protected val logger = KotlinLogging.logger {}
 
-    //    protected suspend fun processStatus(url: String, index: ULong): Result<K> {
-//        // process status
-//        logger.debug { "Status list index: $index" }
-//        logger.debug { "Credential URL: $url" }
-//        // download status credential
-//        val response = fetcher.fetch(url).getOrThrow()
-//        // parse status list, response is a jwt
-//        return reader.read(response)
-//    }
+
+    override suspend fun validate(entry: M, attribute: T): Result<String> = runCatching {
+        logger.debug { "Credential URL: ${entry.uri}" }
+        // download status credential
+        val statusListContent = fetcher.fetch(entry.uri).getOrThrow()
+        // parse status list, response is a jwt
+        val statusList = reader.read(statusListContent).getOrThrow()
+        val bitValueReader = BitValueReader(
+            expansionAlgorithm = getStatusListExpansionAlgorithm(statusList = statusList),
+            bitRepresentationStrategy = BigEndianRepresentation()
+        )
+        val bitValue = bitValueReader.get(
+            bitstring = statusList.list,
+            idx = entry.index,
+            bitSize = statusList.size,
+        )
+        customValidations(statusList, attribute)
+        logger.debug { "Status list index: ${entry.index}" }
+        logger.debug { "EncodedList[${entry.index}] = $bitValue" }
+        require(bitValue.isNotEmpty()) { "Null or empty bit value" }
+        // ensure bitValue always consists of valid binary characters (0,1)
+        require(isBinaryValue(bitValue)) { "Invalid bit value: $bitValue" }
+        val binaryString = bitValue.joinToString("")
+        require(binToInt(binaryString).toUInt() == attribute.value)
+        binaryString
+    }
+
+    protected abstract fun getStatusListExpansionAlgorithm(statusList: K): StatusListExpansionAlgorithm
+
+    protected abstract fun customValidations(statusList: K, attribute: T)
+
     protected fun isBinaryValue(value: List<Char>) = setOf('0', '1').let { valid ->
         value.all { it in valid }
     }
