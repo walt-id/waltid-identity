d7b74c4961d951d2a2469cb7015a33c1123e0d89
mikeplotean <mike@walt.id>
2025-05-30 23:58:43 +0300

    refactor: status validator
    Signed-off-by: mikeplotean <mike@walt.id>

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/IETFStatusValidator.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/IETFStatusValidator.kt
@@ -1,16 +1,20 @@
 package id.walt.policies.policies.status.validator
 
 import id.walt.policies.policies.status.CredentialFetcher
-import id.walt.policies.policies.status.IETFStatusContent
 import id.walt.policies.policies.status.IETFCredentialStatusPolicyAttribute
+import id.walt.policies.policies.status.IETFStatusContent
 import id.walt.policies.policies.status.entry.IETFEntry
+import id.walt.policies.policies.status.expansion.StatusListExpansionAlgorithm
+import id.walt.policies.policies.status.expansion.TokenStatusListExpansionAlgorithm
 import id.walt.policies.policies.status.reader.StatusValueReader
 
 class IETFStatusValidator(
     fetcher: CredentialFetcher,
     reader: StatusValueReader<IETFStatusContent>,
 ) : StatusValidatorBase<IETFStatusContent, IETFEntry, IETFCredentialStatusPolicyAttribute>(fetcher, reader) {
-    override suspend fun validate(entry: IETFEntry, arguments: IETFCredentialStatusPolicyAttribute): Result<String> {
-        TODO("Not yet implemented")
-    }
+
+    override fun getStatusListExpansionAlgorithm(statusList: IETFStatusContent): StatusListExpansionAlgorithm =
+        TokenStatusListExpansionAlgorithm()
+
+    override fun customValidations(statusList: IETFStatusContent, attribute: IETFCredentialStatusPolicyAttribute) = Unit
 }
