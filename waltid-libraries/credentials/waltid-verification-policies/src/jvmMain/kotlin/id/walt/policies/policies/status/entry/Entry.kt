243b56fdc47c3082ddadab90eb44fd479e1e90ff
mikeplotean <mike@walt.id>
2025-05-30 23:34:48 +0300

    refactor: status entry
    Signed-off-by: mikeplotean <mike@walt.id>

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/entry/Entry.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/entry/Entry.kt
@@ -5,6 +5,10 @@ import kotlinx.serialization.Serializable
 
 
 //todo: maybe use waltid-credential-status domain?
+sealed class StatusEntry {
+    abstract val index: ULong
+    abstract val uri: String
+}
 
 @Serializable
 data class W3CEntry(
@@ -13,16 +17,16 @@ data class W3CEntry(
     @SerialName("statusPurpose")
     val purpose: String,
     @SerialName("statusListIndex")
-    val index: ULong,
+    override val index: ULong,
     @SerialName("statusSize")
     val size: Int,
     @SerialName("statusListCredential")
-    val uri: String,
+    override val uri: String,
     @SerialName("statusMessage")
     val statuses: List<Status>? = null,
     @SerialName("statusReference")
     val reference: String? = null,
-) {
+) : StatusEntry() {
     @Serializable
     data class Status(
         @SerialName("status")
@@ -35,7 +39,9 @@ data class W3CEntry(
 data class IETFEntry(
     @SerialName("status_list")
     val statusList: StatusListField,
-) {
+) : StatusEntry() {
+    override val index: ULong = statusList.index
+    override val uri: String = statusList.uri
 
     @Serializable
     data class StatusListField(
