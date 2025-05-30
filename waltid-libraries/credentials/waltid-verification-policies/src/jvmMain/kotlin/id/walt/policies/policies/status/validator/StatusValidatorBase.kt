63636f00aec55d2de956ffae396fab5ba27dd974
mikeplotean <mike@walt.id>
2025-05-30 20:54:16 +0300

    refactor: status validator base

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/StatusValidatorBase.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/StatusValidatorBase.kt
@@ -1,17 +1,12 @@
 package id.walt.policies.policies.status.validator
 
-import id.walt.policies.policies.status.CredentialFetcher
 import id.walt.policies.policies.status.CredentialStatusPolicyArguments
-import id.walt.policies.policies.status.reader.StatusValueReader
 import io.github.oshai.kotlinlogging.KotlinLogging
 
-abstract class StatusValidatorBase<K, T : CredentialStatusPolicyArguments>(
-    private val fetcher: CredentialFetcher,
-    private val reader: StatusValueReader<K>,
-) : StatusValidator<K, T> {
+abstract class StatusValidatorBase<K, T : CredentialStatusPolicyArguments> : StatusValidator<K, T> {
     protected val logger = KotlinLogging.logger {}
 
-//    protected suspend fun processStatus(url: String, index: ULong): Result<K> {
+    //    protected suspend fun processStatus(url: String, index: ULong): Result<K> {
 //        // process status
 //        logger.debug { "Status list index: $index" }
 //        logger.debug { "Credential URL: $url" }
@@ -20,4 +15,9 @@ abstract class StatusValidatorBase<K, T : CredentialStatusPolicyArguments>(
 //        // parse status list, response is a jwt
 //        return reader.read(response)
 //    }
+    protected fun isBinaryValue(value: List<Char>) = setOf('0', '1').let { valid ->
+        value.all { it in valid }
+    }
+
+    protected fun binToInt(bin: String) = bin.toInt(2)
 }
