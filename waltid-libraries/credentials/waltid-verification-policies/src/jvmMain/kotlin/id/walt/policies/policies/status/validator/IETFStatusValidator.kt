3e396559c635cef3736119fb8eb652b27a0e4eed
mikeplotean <mike@walt.id>
2025-06-02 18:13:21 +0300

    refactor: update status validator return type, error handling, inject bit value reader

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/IETFStatusValidator.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/IETFStatusValidator.kt
@@ -3,18 +3,25 @@ package id.walt.policies.policies.status.validator
 import id.walt.policies.policies.status.CredentialFetcher
 import id.walt.policies.policies.status.IETFCredentialStatusPolicyAttribute
 import id.walt.policies.policies.status.IETFStatusContent
+import id.walt.policies.policies.status.bit.BitValueReaderFactory
+import id.walt.policies.policies.status.bit.LittleEndianRepresentation
 import id.walt.policies.policies.status.entry.IETFEntry
-import id.walt.policies.policies.status.expansion.StatusListExpansionAlgorithm
 import id.walt.policies.policies.status.expansion.TokenStatusListExpansionAlgorithm
 import id.walt.policies.policies.status.reader.StatusValueReader
 
 class IETFStatusValidator(
     fetcher: CredentialFetcher,
     reader: StatusValueReader<IETFStatusContent>,
+    bitValueReaderFactory: BitValueReaderFactory,
 ) : StatusValidatorBase<IETFStatusContent, IETFEntry, IETFCredentialStatusPolicyAttribute>(fetcher, reader) {
+    private val bitValueReader = bitValueReaderFactory.new(strategy = LittleEndianRepresentation())
 
-    override fun getStatusListExpansionAlgorithm(statusList: IETFStatusContent): StatusListExpansionAlgorithm =
-        TokenStatusListExpansionAlgorithm()
+    override fun getBitValue(statusList: IETFStatusContent, index: ULong): List<Char> = bitValueReader.get(
+        bitstring = statusList.list,
+        idx = index,
+        bitSize = statusList.size,
+        expansionAlgorithm = TokenStatusListExpansionAlgorithm(),
+    )
 
     override fun customValidations(statusList: IETFStatusContent, attribute: IETFCredentialStatusPolicyAttribute) = Unit
 }
