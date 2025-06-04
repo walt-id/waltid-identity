8438a9a94a6f3080c2612779a30ca3d1aadd0f31
mikeplotean <mike@walt.id>
2025-06-04 18:35:35 +0300

    refactor: update status policy argument hierarchy
    Signed-off-by: mikeplotean <mike@walt.id>
    
    # Conflicts:
    #	waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/RevocationPolicy.jvm.kt

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/StatusValidator.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/validator/StatusValidator.kt
@@ -1,7 +1,8 @@
 package id.walt.policies.policies.status.validator
 
-import id.walt.policies.policies.status.CredentialStatusPolicyArguments
+import id.walt.policies.policies.status.CredentialStatusPolicyAttribute
+import id.walt.policies.policies.status.entry.StatusEntry
 
-interface StatusValidator<in K, T : CredentialStatusPolicyArguments> {
+interface StatusValidator<in K : StatusEntry, in T : CredentialStatusPolicyAttribute> {
     suspend fun validate(entry: K, arguments: T): Result<String>
 }
