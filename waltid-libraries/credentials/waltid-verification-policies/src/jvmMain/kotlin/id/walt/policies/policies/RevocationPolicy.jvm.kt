27afd69a4f09c0f9f23fbb7bf1749d16e9d8182c
mikeplotean <mike@walt.id>
2025-06-12 20:40:14 +0300

    refactor: remove duplication for status and revocation policies, make stauts policy pass as well when no status field is found in credential

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/RevocationPolicy.jvm.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/RevocationPolicy.jvm.kt
@@ -1,12 +1,10 @@
 package id.walt.policies.policies
 
-import id.walt.policies.policies.status.StatusPolicyImplementation.getStatusEntryElementExtractor
-import id.walt.policies.policies.status.StatusPolicyImplementation.processStatusEntry
+import id.walt.policies.policies.status.StatusPolicyImplementation.verifyWithAttributes
 import id.walt.policies.policies.status.Values
 import id.walt.policies.policies.status.model.W3CStatusPolicyAttribute
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
 import love.forte.plugin.suspendtrans.annotation.JvmAsync
 import love.forte.plugin.suspendtrans.annotation.JvmBlocking
 
@@ -14,17 +12,11 @@ import love.forte.plugin.suspendtrans.annotation.JvmBlocking
 @Serializable
 actual class RevocationPolicy : RevocationPolicyMp() {
 
+    @Transient
+    private val attribute = W3CStatusPolicyAttribute(value = 0u, purpose = "revocation", type = Values.STATUS_LIST_2021)
+
     @JvmBlocking
     @JvmAsync
-    actual override suspend fun verify(data: JsonObject, args: Any?, context: Map<String, Any>): Result<Any> {
-        val attribute = W3CStatusPolicyAttribute(
-            value = 0u,
-            purpose = "revocation",
-            type = Values.STATUS_LIST_2021
-        )
-        val statusElement = getStatusEntryElementExtractor(attribute).extract(data)
-            ?: return Result.success(JsonObject(mapOf("policy_available" to JsonPrimitive(false))))
-        val result = processStatusEntry(statusElement, attribute)
-        return result
-    }
+    actual override suspend fun verify(data: JsonObject, args: Any?, context: Map<String, Any>): Result<Any> =
+        verifyWithAttributes(data, attribute)
 }
