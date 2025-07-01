e9f16910e11c73927513a59a018e88b3389c5ccf
mikeplotean <mike@walt.id>
2025-07-01 11:53:01 +0300

    refactor: use base64-url handler for expansion algorithms

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/expansion/TokenStatusListExpansionAlgorithm.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/expansion/TokenStatusListExpansionAlgorithm.kt
@@ -1,10 +1,16 @@
 package id.walt.policies.policies.status.expansion
 
-import id.walt.policies.policies.Base64Utils
+import id.walt.policies.policies.Base64UrlHandler
+import id.walt.policies.policies.Base64UrlType
 import java.io.InputStream
 import java.util.zip.InflaterInputStream
 
-class TokenStatusListExpansionAlgorithm : StatusListExpansionAlgorithm {
-    override operator fun invoke(bitstring: String): InputStream =
-        InflaterInputStream(Base64Utils.urlDecode(bitstring).inputStream())
+class TokenStatusListExpansionAlgorithm(
+    private val base64UrlHandler: Base64UrlHandler,
+) : StatusListExpansionAlgorithm {
+    override operator fun invoke(bitstring: String): InputStream {
+        val base64UrlResult = base64UrlHandler.decodeBase64Url(bitstring)
+        require(base64UrlResult.type == Base64UrlType.Regular) { "Expecting regular base64-url, got: $bitstring" }
+        return InflaterInputStream(base64UrlResult.decodedData.inputStream())
+    }
 }
