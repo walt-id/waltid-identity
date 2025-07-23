a7b9dc1c1f22d46c740ed612375e219eb0203cc6
waltkb <68587968+waltkb@users.noreply.github.com>
2025-07-23 02:35:56 +0200

    Update credential signature to implement from JwtBasedSignature for JWT and SD-JWT signatures

--- waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/signatures/SdJwtCredentialSignature.kt
+++ waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/signatures/SdJwtCredentialSignature.kt
@@ -3,11 +3,12 @@ package id.walt.credentials.signatures
 import id.walt.credentials.signatures.sdjwt.SdJwtSelectiveDisclosure
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonObject
 
 @Serializable
 @SerialName("signature-sd_jwt")
 data class SdJwtCredentialSignature(
     val signature: String,
+    override val jwtHeader: JsonObject?,
     val providedDisclosures: List<SdJwtSelectiveDisclosure>? = null
-) : CredentialSignature() {
-}
+) : CredentialSignature(), JwtBasedSignature
