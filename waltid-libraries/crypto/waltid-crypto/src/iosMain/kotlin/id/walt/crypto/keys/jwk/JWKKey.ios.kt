927e85452808e327ef977acef5e9ae2dfba27582
mikeplotean <mike@walt.id>
2025-08-27 14:33:23 +0300

    fix: missing parameter in override functions signRaw and verifyRaw for ios jwk-key

--- waltid-libraries/crypto/waltid-crypto/src/iosMain/kotlin/id/walt/crypto/keys/jwk/JWKKey.ios.kt
+++ waltid-libraries/crypto/waltid-crypto/src/iosMain/kotlin/id/walt/crypto/keys/jwk/JWKKey.ios.kt
@@ -62,7 +62,7 @@ actual class JWKKey actual constructor(private val jwk: String?, private val _ke
      * @param plaintext data to be signed
      * @return signed (JWS)
      */
-    actual override suspend fun signRaw(plaintext: ByteArray): ByteArray {
+    actual override suspend fun signRaw(plaintext: ByteArray, customSignatureAlgorithm: String?): ByteArray {
         error("Not implemented")
     }
 
@@ -78,7 +78,7 @@ actual class JWKKey actual constructor(private val jwk: String?, private val _ke
      * @return Result wrapping the plaintext; Result failure when the signature fails
      */
     actual override suspend fun verifyRaw(
-        signed: ByteArray, detachedPlaintext: ByteArray?
+        signed: ByteArray, detachedPlaintext: ByteArray?, customSignatureAlgorithm: String?
     ): Result<ByteArray> = when (keyType) {
         KeyType.secp256r1 -> P256.PublicKey.fromJwk(jwk!!)
         KeyType.Ed25519 -> Ed25519.PublicKey.fromJwk(jwk!!)
