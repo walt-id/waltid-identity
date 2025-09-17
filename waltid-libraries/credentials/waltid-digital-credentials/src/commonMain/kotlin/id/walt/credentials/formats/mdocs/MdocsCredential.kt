90c7639e1777eb33a99c6de118eece81830ff8e6
waltkb <68587968+waltkb@users.noreply.github.com>
2025-09-17 03:21:45 +0200

    Update credential layer mdocs credential representation

--- waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/formats/mdocs/MdocsCredential.kt
+++ waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/formats/mdocs/MdocsCredential.kt
@@ -1,11 +1,15 @@
 package id.walt.credentials.formats
 
+import id.walt.cose.coseCompliantCbor
 import id.walt.credentials.signatures.CredentialSignature
+import id.walt.credentials.utils.Base64Utils.matchesBase64Url
+import id.walt.credentials.utils.HexUtils.matchesHex
 import id.walt.crypto.keys.Key
-import kotlinx.serialization.EncodeDefault
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import id.walt.crypto.utils.Base64Utils.decodeFromBase64Url
+import id.walt.isocred.DeviceResponse
+import id.walt.isocred.Document
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.serialization.*
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 
@@ -13,9 +17,17 @@ import kotlinx.serialization.json.JsonObject
 @Serializable
 @SerialName("vc-mdocs")
 data class MdocsCredential(
-    override val signature: CredentialSignature?,
-    override val signed: String?, override val credentialData: JsonObject,
+    override val credentialData: JsonObject,
+
+    /** raw, base64url-encoded DeviceResponse string */
+    override val signed: String?,
+
+    /** The document type, e.g., "org.iso.18013.5.1.mDL". */
     val docType: String,
+
+    // The signature is implicit within the COSE structures of the mdoc.
+    override val signature: CredentialSignature? = null,
+
 
     @EncodeDefault
     override var issuer: String? = null,
@@ -24,7 +36,39 @@ data class MdocsCredential(
 ) : DigitalCredential() {
     override val format: String = "mso_mdoc"
 
+    companion object {
+        private val log = KotlinLogging.logger { }
+    }
+
+    fun parseToDocument(): Document {
+        requireNotNull(signed) { "No signed in Mdocs credential" }
+        log.trace { "Signed is: $signed" }
+
+        val isHex = signed.matchesHex()
+        val isBase64 = signed.matchesBase64Url()
+
+        require(isHex || isBase64) { "Signed is neither hex nor base64" }
+
+        val signedBytes = if (isHex) signed.hexToByteArray() else signed.decodeFromBase64Url()
+
+        val document = runCatching {
+            log.trace { "Decoding DeviceResponse from: ${signedBytes.toHexString()}" }
+            val deviceResponse = coseCompliantCbor.decodeFromByteArray<DeviceResponse>(signedBytes)
+            val document =
+                deviceResponse.documents?.firstOrNull() ?: throw IllegalArgumentException("Mdoc document not found in DeviceResponse")
+            document
+        }.recoverCatching {
+            log.trace { "Failed deserializing as DeviceResponse, trying as Document: ${it.stackTraceToString()}" }
+            val document = coseCompliantCbor.decodeFromByteArray<Document>(signedBytes)
+            document
+        }.getOrThrow()
+
+        return document
+
+    }
+
     override suspend fun verify(publicKey: Key): Result<JsonElement> {
         TODO("Not yet implemented: verify mdocs")
+        // TODO: Move certain code parts from Mdocs presentation validator to here
     }
 }
