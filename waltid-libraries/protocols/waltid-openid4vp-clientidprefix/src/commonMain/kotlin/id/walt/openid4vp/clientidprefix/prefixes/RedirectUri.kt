d6c7ce410be4da5545e3810a215b4fc5de0fceca
waltkb <68587968+waltkb@users.noreply.github.com>
2025-10-14 07:46:24 +0200

    Implement RedirectUri client id prefix with simple validation

--- waltid-libraries/protocols/waltid-openid4vp-clientidprefix/src/commonMain/kotlin/id/walt/openid4vp/clientidprefix/prefixes/RedirectUri.kt
+++ waltid-libraries/protocols/waltid-openid4vp-clientidprefix/src/commonMain/kotlin/id/walt/openid4vp/clientidprefix/prefixes/RedirectUri.kt
@@ -1,22 +1,31 @@
 package id.walt.openid4vp.clientidprefix.prefixes
 
 import id.walt.openid4vp.clientidprefix.ClientIdError
-import id.walt.openid4vp.clientidprefix.ClientMetadata
 import id.walt.openid4vp.clientidprefix.ClientValidationResult
 import id.walt.openid4vp.clientidprefix.RequestContext
+import id.walt.verifier.openid.models.authorization.ClientMetadata
+import io.ktor.http.*
+import kotlinx.serialization.Serializable
 
 /**
  * Handles `redirect_uri` prefix per OpenID4VP 1.0, Section 5.9.3.
  */
-class RedirectUri(override val context: RequestContext) : ClientId {
-    override suspend fun validate(): ClientValidationResult {
-        // Requests using this prefix cannot be signed.
+@Serializable
+data class RedirectUri(val uri: Url, override val rawValue: String) : ClientId {
+    init {
+        require(uri.isAbsolutePath) { "Redirect URI must be an absolute URL." }
+    }
+
+    suspend fun authenticateRedirectUri(context: RequestContext): ClientValidationResult {
         if (context.requestObjectJws != null) {
-            return ClientValidationResult.Failure(ClientIdError.InvalidSignature)
+            return ClientValidationResult.Failure(ClientIdError.DoesNotSupportSignature)
         }
-        // All Verifier metadata parameters MUST be passed using the client_metadata parameter.
-        return context.clientMetadataJson?.let {
-            ClientValidationResult.Success(ClientMetadata(it))
-        } ?: ClientValidationResult.Failure(ClientIdError.MissingClientMetadata)
+        val metadataJson = context.clientMetadataJson
+            ?: return ClientValidationResult.Failure(ClientIdError.MissingClientMetadata)
+
+        return ClientMetadata.fromJson(metadataJson).fold(
+            onSuccess = { ClientValidationResult.Success(it) },
+            onFailure = { ClientValidationResult.Failure(ClientIdError.InvalidMetadata(it.message!!)) }
+        )
     }
 }
