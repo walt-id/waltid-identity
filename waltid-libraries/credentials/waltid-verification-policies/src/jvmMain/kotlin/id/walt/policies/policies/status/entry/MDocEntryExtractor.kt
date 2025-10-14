c063ef1f20e3ad59c387e29d522fe8f7b8680739
mikeplotean <mike@walt.id>
2025-10-14 17:54:02 +0300

    refactor: update ietf entry extractor to support mdoc

--- waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/entry/MDocEntryExtractor.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmMain/kotlin/id/walt/policies/policies/status/entry/MDocEntryExtractor.kt
@@ -1,20 +1,21 @@
 package id.walt.policies.policies.status.entry
 
 import id.walt.mdoc.doc.MDoc
+import id.walt.mdoc.mso.StatusListInfo
 import id.walt.policies.policies.status.model.IETFEntry
-import id.walt.policies.policies.status.model.StatusEntry
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.*
 
 class MDocEntryExtractor : EntryExtractor {
-    override fun extract(data: JsonElement): StatusEntry? = data.jsonObject["mdoc"]?.let {
+    private val json = Json
+    override fun extract(data: JsonElement): JsonElement? = data.jsonObject["mdoc"]?.let {
         MDoc.fromCBORHex(it.jsonPrimitive.content).MSO?.status?.statusList?.let {
-            IETFEntry(
-                statusList = IETFEntry.StatusListField(
-                    index = it.index.toULong(), uri = it.uri
-                )
-            )
+            json.encodeToJsonElement(createEntry(it))
         }
     }
+
+    private fun createEntry(info: StatusListInfo): IETFEntry = IETFEntry(
+        statusList = IETFEntry.StatusListField(
+            index = info.index.toULong(), uri = info.uri
+        )
+    )
 }
