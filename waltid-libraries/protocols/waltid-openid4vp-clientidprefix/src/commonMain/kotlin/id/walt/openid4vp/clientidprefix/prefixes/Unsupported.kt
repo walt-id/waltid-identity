1460cb904c9722884a7b08b024640a40351fbc52
waltkb <68587968+waltkb@users.noreply.github.com>
2025-10-14 07:46:35 +0200

    Implement Unsupported client id prefix

--- waltid-libraries/protocols/waltid-openid4vp-clientidprefix/src/commonMain/kotlin/id/walt/openid4vp/clientidprefix/prefixes/Unsupported.kt
+++ waltid-libraries/protocols/waltid-openid4vp-clientidprefix/src/commonMain/kotlin/id/walt/openid4vp/clientidprefix/prefixes/Unsupported.kt
@@ -1,14 +1,9 @@
 package id.walt.openid4vp.clientidprefix.prefixes
 
-import id.walt.openid4vp.clientidprefix.ClientIdError
-import id.walt.openid4vp.clientidprefix.ClientValidationResult
-import id.walt.openid4vp.clientidprefix.RequestContext
+import kotlinx.serialization.Serializable
 
 /**
  * Handles an unsupported prefix.
  */
-class Unsupported(override val context: RequestContext, private val prefix: String) : ClientId {
-    override suspend fun validate(): ClientValidationResult {
-        return ClientValidationResult.Failure(ClientIdError.UnsupportedPrefix(prefix))
-    }
-}
+@Serializable
+data class Unsupported(val prefix: String, override val rawValue: String) : ClientId
