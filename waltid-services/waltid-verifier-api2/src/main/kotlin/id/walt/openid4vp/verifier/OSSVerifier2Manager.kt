a95fecaa38f47c2c7fd54411cf2bc4dbce1fcdd7
waltkb <68587968+waltkb@users.noreply.github.com>
2025-10-21 08:43:14 +0200

    Remove no longer needed arguments

--- waltid-services/waltid-verifier-api2/src/main/kotlin/id/walt/openid4vp/verifier/OSSVerifier2Manager.kt
+++ waltid-services/waltid-verifier-api2/src/main/kotlin/id/walt/openid4vp/verifier/OSSVerifier2Manager.kt
@@ -3,7 +3,6 @@ package id.walt.openid4vp.verifier
 import id.walt.commons.config.ConfigManager
 import id.walt.crypto.keys.KeyManager
 import id.walt.openid4vp.verifier.VerificationSessionCreator.VerificationSessionSetup
-import id.walt.verifier.openid.models.authorization.ClientMetadata
 
 object OSSVerifier2Manager {
 
@@ -11,16 +10,12 @@ object OSSVerifier2Manager {
 
     suspend fun createVerificationSession(
         setup: VerificationSessionSetup,
-
-        clientId: String? = null,
-        clientMetadata: ClientMetadata? = null,
-        uriPrefix: String? = null,
     ): Verification2Session {
         val newSession = VerificationSessionCreator.createVerificationSession(
             setup = setup,
-            clientId = clientId ?: setup.clientId ?: config.clientId,
-            clientMetadata = clientMetadata ?: setup.clientMetadata ?: config.clientMetadata,
-            urlPrefix = uriPrefix ?: setup.urlPrefix ?: config.urlPrefix,
+            clientId = setup.clientId ?: config.clientId,
+            clientMetadata = setup.clientMetadata ?: config.clientMetadata,
+            urlPrefix = setup.urlPrefix ?: config.urlPrefix,
             urlHost = setup.urlHost ?: config.urlHost,
             key = setup.key?.key ?: config.key?.let { KeyManager.resolveSerializedKey(it) },
             x5c = setup.x5c ?: config.x5c
