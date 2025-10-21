a8f6e071ada671c20ac86c0e4685e7a184516bd5
waltkb <68587968+waltkb@users.noreply.github.com>
2025-10-21 06:33:01 +0200

    Refactor of key resolvers

--- waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/jwk/JWKKeyCreator.kt
+++ waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/keys/jwk/JWKKeyCreator.kt
@@ -3,6 +3,7 @@ package id.walt.crypto.keys.jwk
 import id.walt.crypto.keys.JwkKeyMeta
 import id.walt.crypto.keys.Key
 import id.walt.crypto.keys.KeyType
+import io.github.oshai.kotlinlogging.KotlinLogging
 import love.forte.plugin.suspendtrans.annotation.JsPromise
 import love.forte.plugin.suspendtrans.annotation.JvmAsync
 import love.forte.plugin.suspendtrans.annotation.JvmBlocking
@@ -13,6 +14,11 @@ import kotlin.js.JsExport
 @OptIn(ExperimentalJsExport::class)
 @JsExport
 abstract class JWKKeyCreator {
+
+    companion object {
+        private val log = KotlinLogging.logger {  }
+    }
+
     @JvmBlocking
     @JvmAsync
     @JsPromise
@@ -51,6 +57,19 @@ ${Base64.Pem.encode(data)}
 
     fun convertX5cToPemCertificate(x5cElement: String): String =
         wrapAsPem(Base64.decode(x5cElement))
+
+    @JvmBlocking
+    @JvmAsync
+    @JsPromise
+    @JsExport.Ignore
+    suspend fun importDERorPEM(derOrPem: String): Result<JWKKey> {
+        val pem = if (!derOrPem.startsWith("-----BEGIN")) {
+            val pem = convertX5cToPemCertificate(derOrPem)
+            log.trace { "Converted x5c to PEM: $pem" }
+            pem
+        } else derOrPem
+        return JWKKey.importPEM(pem)
+    }
 
     @JvmBlocking
     @JvmAsync
