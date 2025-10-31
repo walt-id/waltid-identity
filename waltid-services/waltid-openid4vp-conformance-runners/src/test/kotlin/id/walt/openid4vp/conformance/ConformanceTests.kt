771a8332f6e2d3e9c139103c5311708fb7d637cd
khaledbattiche <khaledbattiche@gmail.com>
2025-10-31 14:06:24 +0100

    disable test temporary

--- waltid-services/waltid-openid4vp-conformance-runners/src/test/kotlin/id/walt/openid4vp/conformance/ConformanceTests.kt
+++ waltid-services/waltid-openid4vp-conformance-runners/src/test/kotlin/id/walt/openid4vp/conformance/ConformanceTests.kt
@@ -1,25 +1,22 @@
 package id.walt.openid4vp.conformance
 
-import id.walt.openid4vp.conformance.testplans.http.ConformanceInterface
-import kotlinx.coroutines.runBlocking
-
 class ConformanceTests {
 
-    companion object {
-        val verifier2UrlPrefix: String = "https://verifier2.localhost/verification-session" // "https://xyz.ngrok-free.app/verification-session"
-        val conformanceHost: String = "conformance.waltid.cloud" // "localhost.emobix.co.uk"
-        val conformancePort: Int = 443 // 8443
-
-        val conformanceServerVersionResult = runBlocking {
-            runCatching {
-                ConformanceInterface(conformanceHost, conformancePort).getServerVersion()
-            }.onFailure {
-                println("Error getting server version: $it")
-            }
-        }
-        @JvmStatic
-        val isConformanceAvailable = conformanceServerVersionResult.isSuccess
-    }
+//    companion object {
+//        val verifier2UrlPrefix: String = "https://verifier2.localhost/verification-session" // "https://xyz.ngrok-free.app/verification-session"
+//        val conformanceHost: String = "conformance.waltid.cloud" // "localhost.emobix.co.uk"
+//        val conformancePort: Int = 443 // 8443
+//
+//        val conformanceServerVersionResult = runBlocking {
+//            runCatching {
+//                ConformanceInterface(conformanceHost, conformancePort).getServerVersion()
+//            }.onFailure {
+//                println("Error getting server version: $it")
+//            }
+//        }
+//        @JvmStatic
+//        val isConformanceAvailable = conformanceServerVersionResult.isSuccess
+//    }
 
 //    @Test
 //    @EnabledIf("isConformanceAvailable")
