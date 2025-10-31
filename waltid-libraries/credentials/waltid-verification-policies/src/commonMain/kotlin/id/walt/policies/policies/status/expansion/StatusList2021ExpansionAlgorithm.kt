eced31f8b40c7d20062e216435a72c967542b3a0
mikeplotean <mike@walt.id>
2025-10-31 15:03:44 +0200

    refactor: status list expansion algorithms to use korlibs-io

--- waltid-libraries/credentials/waltid-verification-policies/src/commonMain/kotlin/id/walt/policies/policies/status/expansion/StatusList2021ExpansionAlgorithm.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/commonMain/kotlin/id/walt/policies/policies/status/expansion/StatusList2021ExpansionAlgorithm.kt
@@ -1,10 +1,11 @@
 package id.walt.policies.policies.status.expansion
 
 import id.walt.policies.policies.Base64Utils
-import java.io.InputStream
-import java.util.zip.GZIPInputStream
+import korlibs.io.compression.deflate.GZIP
+import korlibs.io.compression.uncompress
+
 
 class StatusList2021ExpansionAlgorithm : StatusListExpansionAlgorithm {
-    override operator fun invoke(bitstring: String): InputStream =
-        GZIPInputStream(Base64Utils.decode(bitstring).inputStream())
+    override suspend operator fun invoke(bitstring: String): ByteArray =
+        GZIP.uncompress(Base64Utils.decode(bitstring))
 }
--- waltid-libraries/credentials/waltid-verification-policies/src/commonMain/kotlin/id/walt/policies/policies/status/expansion/TokenStatusListExpansionAlgorithm.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/commonMain/kotlin/id/walt/policies/policies/status/expansion/TokenStatusListExpansionAlgorithm.kt
@@ -2,15 +2,15 @@ package id.walt.policies.policies.status.expansion
 
 import id.walt.policies.policies.Base64UrlHandler
 import id.walt.policies.policies.Base64UrlType
-import java.io.InputStream
-import java.util.zip.InflaterInputStream
+import korlibs.io.compression.deflate.ZLib
+import korlibs.io.compression.uncompress
 
 class TokenStatusListExpansionAlgorithm(
     private val base64UrlHandler: Base64UrlHandler,
 ) : StatusListExpansionAlgorithm {
-    override operator fun invoke(bitstring: String): InputStream {
+    override suspend operator fun invoke(bitstring: String): ByteArray {
         val base64UrlResult = base64UrlHandler.decodeBase64Url(bitstring)
         require(base64UrlResult.type == Base64UrlType.Regular) { "Expecting regular base64-url, got: $bitstring" }
-        return InflaterInputStream(base64UrlResult.decodedData.inputStream())
+        return ZLib.uncompress(base64UrlResult.decodedData)
     }
 }
