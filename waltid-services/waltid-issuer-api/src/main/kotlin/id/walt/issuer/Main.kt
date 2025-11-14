5df2718cb5c3ac5157fb088edc14990b5ed6dc3c
Philipp Potisk <phil@walt.id>
2025-11-14 12:13:20 +0100

    Fixed imports

--- waltid-services/waltid-issuer-api/src/main/kotlin/id/walt/issuer/Main.kt
+++ waltid-services/waltid-issuer-api/src/main/kotlin/id/walt/issuer/Main.kt
@@ -1,18 +1,13 @@
 package id.walt.issuer
 
-import id.walt.commons.ServiceCommons
 import id.walt.commons.ServiceConfiguration
 import id.walt.commons.ServiceInitialization
 import id.walt.commons.ServiceMain
-import id.walt.commons.config.ConfigManager
-import id.walt.commons.config.list.DevModeConfig
 import id.walt.commons.featureflag.CommonsFeatureCatalog
-import id.walt.commons.featureflag.FeatureManager
 import id.walt.commons.featureflag.FeatureManager.whenFeature
 import id.walt.commons.web.WebService
 import id.walt.crypto.keys.aws.WaltCryptoAws
 import id.walt.did.dids.DidService
-import id.walt.did.dids.resolver.local.DidWebResolver
 import id.walt.issuer.entra.entraIssuance
 import id.walt.issuer.issuance.OidcApi.oidcApi
 import id.walt.issuer.issuance.issuerApi
