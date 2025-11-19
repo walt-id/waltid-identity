4c96768d7c567a0f48edb905b83c38a1b19aaa73
mikeplotean <mike@walt.id>
2025-11-19 18:14:13 +0200

    feat: add mdocs-credential custom serializer to include the mso

--- waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/formats/mdocs/MdocsCredential.kt
+++ waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/formats/mdocs/MdocsCredential.kt
@@ -6,19 +6,20 @@ import id.walt.credentials.signatures.CredentialSignature
 import id.walt.crypto.keys.Key
 import id.walt.did.dids.resolver.local.DidJwkResolver
 import id.walt.mdoc.objects.document.Document
+import id.walt.mdoc.objects.mso.MobileSecurityObject
 import id.walt.mdoc.parser.MdocParser
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.serialization.EncodeDefault
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 
 private const val VC_MDOCS_SERIAL_NAME = "vc-mdocs"
 
 @OptIn(ExperimentalSerializationApi::class)
-@Serializable
+@Serializable(with = MdocsCredential.MdocsCredentialSerializer::class)
 @SerialName(VC_MDOCS_SERIAL_NAME)
 data class MdocsCredential(
     override val credentialData: JsonObject,
@@ -89,5 +90,57 @@ data class MdocsCredential(
     suspend fun verify(): Result<JsonElement> {
         val signerKey = getSignerKey() ?: throw IllegalArgumentException("Missing signer key for mdocs credential")
         return verify(signerKey)
+    }
+
+    object MdocsCredentialSerializer : KSerializer<MdocsCredential> {
+
+        @Serializable
+        @SerialName(VC_MDOCS_SERIAL_NAME)
+        private data class MdocsCredentialDataTransferObject(
+            // kotlinx.serialization doesn't support unwrapping,
+            // so listing each property, instead of nesting the MdocsCredential object
+            val credentialData: JsonObject,
+            val signed: String?,
+            val docType: String,
+            val signature: CredentialSignature? = null,
+            val issuer: String? = null,
+            val subject: String? = null,
+            val format: String,
+            val mso: MobileSecurityObject?
+        )
+
+        override val descriptor: SerialDescriptor = MdocsCredentialDataTransferObject.serializer().descriptor
+
+        override fun serialize(encoder: Encoder, value: MdocsCredential) {
+            val mso = runCatching {
+                value.parseToDocument().issuerSigned.decodeMobileSecurityObject()
+            }.getOrNull()
+
+            val dataTransferObject = MdocsCredentialDataTransferObject(
+                credentialData = value.credentialData,
+                signed = value.signed,
+                docType = value.docType,
+                signature = value.signature,
+                issuer = value.issuer,
+                subject = value.subject,
+                format = value.format,
+                mso = mso
+            )
+
+            encoder.encodeSerializableValue(MdocsCredentialDataTransferObject.serializer(), dataTransferObject)
+        }
+
+        override fun deserialize(decoder: Decoder): MdocsCredential {
+            val dataTransferObject = decoder.decodeSerializableValue(MdocsCredentialDataTransferObject.serializer())
+
+            return MdocsCredential(
+                credentialData = dataTransferObject.credentialData,
+                signed = dataTransferObject.signed,
+                docType = dataTransferObject.docType,
+                signature = dataTransferObject.signature,
+                issuer = dataTransferObject.issuer,
+                subject = dataTransferObject.subject
+            )
+        }
     }
 }
