fd2e603a5b39aac9dd23439f785018f76196ab50
mikeplotean <mike@walt.id>
2025-11-21 19:53:37 +0200

    build: specify explicitly jvm toolchain for libraries
    openid-verifier, openid-verifier-api, waltid-mdoc-credentials2

--- waltid-libraries/protocols/waltid-openid4vp-verifier-openapi/build.gradle.kts
+++ waltid-libraries/protocols/waltid-openid4vp-verifier-openapi/build.gradle.kts
@@ -39,6 +39,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
 
+kotlin {
+    jvmToolchain(21)
+}
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
