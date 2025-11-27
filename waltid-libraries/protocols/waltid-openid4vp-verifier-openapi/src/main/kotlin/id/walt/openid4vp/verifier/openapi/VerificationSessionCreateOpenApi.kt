075e431852d00564de46fc1b95bb2bfecf2ac9be
waltkb <68587968+waltkb@users.noreply.github.com>
2025-11-27 09:38:11 +0100

    Added various new examples to Verifier2 session creation OpenAPI definition, fixed old examples

--- waltid-libraries/protocols/waltid-openid4vp-verifier-openapi/src/main/kotlin/id/walt/openid4vp/verifier/openapi/VerificationSessionCreateOpenApi.kt
+++ waltid-libraries/protocols/waltid-openid4vp-verifier-openapi/src/main/kotlin/id/walt/openid4vp/verifier/openapi/VerificationSessionCreateOpenApi.kt
@@ -1,8 +1,10 @@
 package id.walt.openid4vp.verifier.openapi
 
-import id.walt.openid4vp.verifier.VerificationSessionCreator.VerificationSessionCreationResponse
-import id.walt.openid4vp.verifier.VerificationSessionCreator.VerificationSessionSetup
-import id.walt.openid4vp.verifier.openapi.Verifier2OpenApiExamples.exampleOf
+import id.walt.openid4vp.verifier.data.CrossDeviceFlowSetup
+import id.walt.openid4vp.verifier.data.DcApiFlowSetup
+import id.walt.openid4vp.verifier.data.VerificationSessionSetup
+import id.walt.openid4vp.verifier.handlers.sessioncreation.VerificationSessionCreator.VerificationSessionCreationResponse
+import id.walt.openid4vp.verifier.openapi.Verifier2OpenApiExamples.nestedPresentationRequestWithMultipleClaims
 import io.github.smiley4.ktoropenapi.config.RouteConfig
 import io.ktor.http.*
 
@@ -14,18 +16,24 @@ object VerificationSessionCreateOpenApi {
             headerParameter<String>("X-Request-Id")
             body<VerificationSessionSetup> {
                 example("Basic example") { value = Verifier2OpenApiExamples.basicExample }
-                example("W3C + path") { value = exampleOf(Verifier2OpenApiExamples.w3cPlusPath) }
-                example("Empty meta") { value = exampleOf(Verifier2OpenApiExamples.emptyMeta) }
-                example("Nested presentation request") {
-                    value = exampleOf(Verifier2OpenApiExamples.nestedPresentationRequestW3C)
-                }
-                example("Nested presentation request + multiple claims") {
-                    value = exampleOf(Verifier2OpenApiExamples.nestedPresentationRequestWithMultipleClaims)
-                }
-                example("W3C type values") { value = exampleOf(Verifier2OpenApiExamples.w3cTypeValues) }
-                example("W3C without claims") { value = exampleOf(Verifier2OpenApiExamples.W3CWithoutClaims) }
-                example("W3C with claims and values") { value = exampleOf(Verifier2OpenApiExamples.W3CWithClaimsAndValues) }
-                example("VICAL policy (only for ISO mDL/mdoc") { value = exampleOf(Verifier2OpenApiExamples.VicalPolicyValues) }
+
+                example("Cross device flow: mDL or Photo ID") { value = CrossDeviceFlowSetup.EXAMPLE_MDL_OR_PHOTOID }
+                example("Cross device flow: dc+sd-jwt PID") { value = CrossDeviceFlowSetup.EXAMPLE_SDJWT_PID }
+                //example("Same device flow") { value = SameDeviceFlow.EXAMPLE }
+                example("DC API flow: Signed mDL") { value = DcApiFlowSetup.EXAMPLE_SIGNED_MDL }
+                example("DC API flow: Signed & encrypted mDL") { value = DcApiFlowSetup.EXAMPLE_SIGNED_ENCRYPTED_MDL }
+                example("DC API flow: Signed Photo ID") { value = DcApiFlowSetup.EXAMPLE_SIGNED_PHOTOID }
+
+                example("DCQL example: Basic example") { value = Verifier2OpenApiExamples.basicExample }
+                example("DCQL example: W3C + path") { value = Verifier2OpenApiExamples.w3cPlusPath }
+                example("DCQL example: Empty meta") { value = Verifier2OpenApiExamples.emptyMeta }
+                example("DCQL example: Nested presentation request") { value = Verifier2OpenApiExamples.nestedPresentationRequestW3C }
+                example("DCQL example: Nested presentation request + multiple claims") { value = nestedPresentationRequestWithMultipleClaims }
+                example("DCQL example: W3C type values") { value = Verifier2OpenApiExamples.w3cTypeValues }
+                example("DCQL example: W3C without claims") { value = Verifier2OpenApiExamples.W3CWithoutClaims }
+                example("DCQL example: W3C with claims and values") { value = Verifier2OpenApiExamples.W3CWithClaimsAndValues }
+
+                example("VICAL: VICAL policy (only for ISO mDL/mdoc") { value = Verifier2OpenApiExamples.VicalPolicyValues }
             }
         }
         response {
