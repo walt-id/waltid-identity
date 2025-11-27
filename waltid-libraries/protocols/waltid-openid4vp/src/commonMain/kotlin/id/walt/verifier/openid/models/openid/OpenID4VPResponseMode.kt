049e0848e75870bd640ac46d0595e798080583ae
waltkb <68587968+waltkb@users.noreply.github.com>
2025-11-27 09:02:18 +0100

    Converted OpenID4VPResponseMode modes to sets, added cleartext response listing

--- waltid-libraries/protocols/waltid-openid4vp/src/commonMain/kotlin/id/walt/verifier/openid/models/openid/OpenID4VPResponseMode.kt
+++ waltid-libraries/protocols/waltid-openid4vp/src/commonMain/kotlin/id/walt/verifier/openid/models/openid/OpenID4VPResponseMode.kt
@@ -45,8 +45,14 @@ enum class OpenID4VPResponseMode {
     FORM_POST;
 
     companion object {
-        val ENCRYPTED_RESPONSES = listOf(DIRECT_POST_JWT, DC_API_JWT)
-        val DC_API_RESPONSES = listOf(DC_API, DC_API_JWT)
-        val DIRECT_POST_RESPONSES = listOf(DIRECT_POST, DIRECT_POST_JWT)
+        val ENCRYPTED_RESPONSES = setOf(DIRECT_POST_JWT, DC_API_JWT)
+        val DC_API_RESPONSES = setOf(DC_API, DC_API_JWT)
+        val DIRECT_POST_RESPONSES = setOf(DIRECT_POST, DIRECT_POST_JWT)
+
+        val CLEARTEXT_NORMAL_RESPONSES = OpenID4VPResponseMode.entries - ENCRYPTED_RESPONSES - DC_API_RESPONSES
+
+        init {
+            check(DC_API !in CLEARTEXT_NORMAL_RESPONSES && DIRECT_POST_JWT !in CLEARTEXT_NORMAL_RESPONSES)
+        }
     }
 }
