c771b2fd5cc960807a7932f398998b20b7a21e7f
waltkb <68587968+waltkb@users.noreply.github.com>
2025-12-03 10:35:23 +0100

    Parsed presentation available event

--- waltid-libraries/protocols/waltid-openid4vp-verifier/src/commonMain/kotlin/id/walt/openid4vp/verifier/data/SessionEvent.kt
+++ waltid-libraries/protocols/waltid-openid4vp-verifier/src/commonMain/kotlin/id/walt/openid4vp/verifier/data/SessionEvent.kt
@@ -4,6 +4,7 @@ package id.walt.openid4vp.verifier.data
 enum class SessionEvent {
     authorization_request_requested,
     attempted_presentation,
+    parsed_presentation_available,
     validated_credentials_available,
     presentation_fulfils_dcql_query,
     policy_results_available,
