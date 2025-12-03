b662c2a693c10b718d6f68f170e7fbbc7253197b
waltkb <68587968+waltkb@users.noreply.github.com>
2025-12-03 05:40:01 +0100

    Make sure all master classes are serializable

--- waltid-libraries/credentials/waltid-verification-policies2-vp/src/commonMain/kotlin/id/walt/policies2/vp/policies/jwt_vc_json/JwtVcJsonVPPolicy.kt
+++ waltid-libraries/credentials/waltid-verification-policies2-vp/src/commonMain/kotlin/id/walt/policies2/vp/policies/jwt_vc_json/JwtVcJsonVPPolicy.kt
@@ -3,7 +3,9 @@
 package id.walt.policies2.vp.policies
 
 import id.walt.credentials.presentations.formats.JwtVcJsonPresentation
+import kotlinx.serialization.Serializable
 
+@Serializable
 abstract class JwtVcJsonVPPolicy() : VPPolicy2() {
     abstract suspend fun VPPolicyRunContext.verifyJwtVcJsonPolicy(
         presentation: JwtVcJsonPresentation,
--- waltid-libraries/credentials/waltid-verification-policies2-vp/src/commonMain/kotlin/id/walt/policies2/vp/policies/mso_mdoc/MdocVPPolicy.kt
+++ waltid-libraries/credentials/waltid-verification-policies2-vp/src/commonMain/kotlin/id/walt/policies2/vp/policies/mso_mdoc/MdocVPPolicy.kt
@@ -4,7 +4,9 @@ package id.walt.policies2.vp.policies
 
 import id.walt.mdoc.objects.document.Document
 import id.walt.mdoc.objects.mso.MobileSecurityObject
+import kotlinx.serialization.Serializable
 
+@Serializable
 sealed class MdocVPPolicy() : VPPolicy2() {
 
     abstract suspend fun VPPolicyRunContext.verifyMdocPolicy(
