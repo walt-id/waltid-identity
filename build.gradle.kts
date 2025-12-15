8557ea979824bd13f1fb71d8c909ec9d3acbc756
waltkb <68587968+waltkb@users.noreply.github.com>
2025-12-15 12:12:06 +0100

    Set repositories explicitly as CI fails to find anything?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
 allprojects {
     version = "1.0.0-SNAPSHOT"
+
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.waltid.dev/releases")
+        maven("https://maven.waltid.dev/snapshots")
+    }
 }
