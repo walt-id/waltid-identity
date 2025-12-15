034e50c2ed5228d6941c8b2d2a5efe379123ddef
Philipp Potisk <phil@walt.id>
2025-12-15 23:24:00 +0100

    removed kickoff pack

--- annex-c-codex-kickoff-pack-full/waltid-libraries/waltid-18013-7-verifier/src/main/kotlin/id/walt/iso18013/annexc/AnnexC.kt
+++
@@ -1,5 +0,0 @@
-package id.walt.iso18013.annexc
-
-object AnnexC {
-    const val PROTOCOL = "org.iso.mdoc"
-}
--- annex-c-codex-kickoff-pack-full/waltid-libraries/waltid-18013-7-verifier/src/main/kotlin/id/walt/iso18013/annexc/cbor/Base64UrlNoPad.kt
+++
@@ -1,8 +0,0 @@
-package id.walt.iso18013.annexc.cbor
-
-import java.util.Base64
-
-object Base64UrlNoPad {
-    fun encode(data: ByteArray): String = Base64.getUrlEncoder().withoutPadding().encodeToString(data)
-    fun decode(text: String): ByteArray = Base64.getUrlDecoder().decode(text)
-}
