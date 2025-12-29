af5c649ba6aac6379d5f8cd0b4a0a895b03aad30
cpatsonakis <christos@walt.id>
2025-12-29 23:49:12 +0200

    refactor: minor

--- waltid-libraries/crypto/waltid-x509/src/jvmMain/kotlin/id/walt/x509/iso/IssuerAlternativeNameExtensions.kt
+++ waltid-libraries/crypto/waltid-x509/src/jvmMain/kotlin/id/walt/x509/iso/IssuerAlternativeNameExtensions.kt
@@ -10,13 +10,14 @@ import java.security.cert.X509Certificate
 fun IssuerAlternativeName.Companion.parseFromX509Certificate(
     cert: X509Certificate,
 ): IssuerAlternativeName {
-    val issAltNamesBytes = requireNotNull(
+    val issAltNamesExtRawBytes = requireNotNull(
         cert.getExtensionValue(Extension.issuerAlternativeName.id)
     ) {
         "Issuer alternative name X509 certificate extension must exist, but was found missing from input certificate"
     }
-    val asn1OctetStr = ASN1OctetString.getInstance(issAltNamesBytes)
-    val names = GeneralNames.getInstance(asn1OctetStr.octets).names
+    val names = GeneralNames.getInstance(
+        ASN1OctetString.getInstance(issAltNamesExtRawBytes).octets
+    ).names
     require(names.isNotEmpty() && names.size <= 2)
     return IssuerAlternativeName(
         email = names.find { it.tagNo == GeneralName.rfc822Name }?.name?.toString(),
