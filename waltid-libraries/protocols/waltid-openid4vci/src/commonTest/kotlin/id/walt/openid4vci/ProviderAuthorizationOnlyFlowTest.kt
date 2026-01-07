4344d78122a7f134e2b7bd12ff2fb6b890cb0d6a
chsavvaidis <ch.savvaidis@gmail.com>
2026-01-07 19:04:32 +0100

    test: add tests for unsupported grant/response types

--- waltid-libraries/protocols/waltid-openid4vci/src/commonTest/kotlin/id/walt/openid4vci/ProviderAuthorizationOnlyFlowTest.kt
+++ waltid-libraries/protocols/waltid-openid4vci/src/commonTest/kotlin/id/walt/openid4vci/ProviderAuthorizationOnlyFlowTest.kt
@@ -5,6 +5,7 @@ import id.walt.openid4vci.core.AccessResponseResult
 import id.walt.openid4vci.core.AuthorizeRequestResult
 import id.walt.openid4vci.core.AuthorizeResponseResult
 import id.walt.openid4vci.core.buildOAuth2Provider
+import id.walt.openid4vci.request.AccessTokenRequest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
@@ -118,5 +119,61 @@ class ProviderAuthorizationOnlyFlowTest {
         )
 
         assertTrue(authorizeResponse is AuthorizeResponseResult.Failure)
+
+        val authorizeResponse2 = provider.createAuthorizeResponse(
+            authorizeRequest,
+            DefaultSession()
+        )
+
+        assertTrue(authorizeResponse2 is AuthorizeResponseResult.Failure)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `access request rejects unsupported grant_type`() = runTest {
+        val provider = buildOAuth2Provider(createTestConfig())
+        val accessRequestResult = provider.createAccessRequest(
+            mapOf("grant_type" to "client_credentials"),
+        )
+        assertTrue(accessRequestResult is AccessRequestResult.Failure)
+        assertEquals("unsupported_grant_type", accessRequestResult.error.error)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `access response rejects when no handler supports grant_type`() = runTest {
+        val provider = buildOAuth2Provider(
+            config = createTestConfig(),
+            includeAuthorizationCodeDefaultHandlers = false,
+            includePreAuthorizedCodeDefaultHandlers = false,
+        )
+        val request = AccessTokenRequest().apply { appendGrantType(GrantType.AuthorizationCode.value) }
+        val response = provider.createAccessResponse(request)
+        assertTrue(response is AccessResponseResult.Failure)
+        assertEquals("unsupported_grant_type", response.error.error)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `authorize response rejects when no handler supports response_type`() = runTest {
+        val provider = buildOAuth2Provider(
+            config = createTestConfig(),
+            includeAuthorizationCodeDefaultHandlers = false,
+            includePreAuthorizedCodeDefaultHandlers = false,
+        )
+        val authorizeRequestResult = provider.createAuthorizeRequest(
+            mapOf(
+                "response_type" to "code",
+                "client_id" to "demo-client",
+                "redirect_uri" to "https://openid4vci.walt.id/callback",
+            ),
+        )
+        assertTrue(authorizeRequestResult is AuthorizeRequestResult.Success)
+        val authorizeResponse = provider.createAuthorizeResponse(
+            authorizeRequestResult.request,
+            DefaultSession(subject = "sub"),
+        )
+        assertTrue(authorizeResponse is AuthorizeResponseResult.Failure)
+        assertEquals("unsupported_response_type", authorizeResponse.error.error)
     }
 }
