ecb397658513786c5139909c7d3736ea3fb54e2a
mikeplotean <mike@walt.id>
2026-01-22 21:11:55 +0200

    feat: add StatusValueReaderBase abstract class
    Introduces StatusValueReaderBase, an abstract class for reading status values using a FormatMatcher. This provides a base implementation for canHandle based on content format matching.

--- waltid-libraries/credentials/waltid-verification-policies2/src/commonMain/kotlin/id/walt/policies2/vc/policies/status/reader/JwtStatusValueReaderBase.kt
+++ waltid-libraries/credentials/waltid-verification-policies2/src/commonMain/kotlin/id/walt/policies2/vc/policies/status/reader/JwtStatusValueReaderBase.kt
@@ -2,6 +2,7 @@ package id.walt.policies2.vc.policies.status.reader
 
 import id.walt.policies2.vc.policies.status.content.ContentParser
 import id.walt.policies2.vc.policies.status.model.StatusContent
+import id.walt.policies2.vc.policies.status.reader.format.FormatMatcher
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
