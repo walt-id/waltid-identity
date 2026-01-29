d2c33f91cc511717ecf81bebbf2835c08ebc263a
mikeplotean <mike@walt.id>
2026-01-29 16:16:54 +0200

    fix: IETFStatusValidator parameter order
    Reordered the parameters in IETFStatusValidator to place bitValueReaderFactory and expansionAlgorithm before vararg reader, matching the constructor definition. Updated usages in StatusPolicyImplementation and related tests for consistency (code review).

--- waltid-libraries/credentials/waltid-verification-policies/src/commonMain/kotlin/id/walt/policies/policies/status/validator/IETFStatusValidator.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/commonMain/kotlin/id/walt/policies/policies/status/validator/IETFStatusValidator.kt
@@ -11,9 +11,9 @@ import id.walt.policies.policies.status.reader.StatusValueReader
 
 class IETFStatusValidator(
     fetcher: CredentialFetcher,
-    vararg reader: StatusValueReader<IETFStatusContent>,
     bitValueReaderFactory: BitValueReaderFactory,
     private val expansionAlgorithm: StatusListExpansionAlgorithm,
+    vararg reader: StatusValueReader<IETFStatusContent>,
 ) : StatusValidatorBase<IETFStatusContent, IETFEntry, IETFStatusPolicyAttribute>(fetcher, *reader) {
     private val bitValueReader = bitValueReaderFactory.new(strategy = LittleEndianRepresentation())
 
--- waltid-libraries/credentials/waltid-verification-policies/src/jvmTest/kotlin/id/walt/policies/policies/status/validator/IETFStatusValidatorTests.kt
+++ waltid-libraries/credentials/waltid-verification-policies/src/jvmTest/kotlin/id/walt/policies/policies/status/validator/IETFStatusValidatorTests.kt
@@ -32,9 +32,9 @@ class IETFStatusValidatorTests : StatusValidatorTestsBase<IETFEntry, IETFStatusP
 
     override fun createStatusValidator() = IETFStatusValidator(
         fetcher = mockFetcher,
-        mockStatusReader,
         bitValueReaderFactory = mockBitValueReaderFactory,
         expansionAlgorithm = mockExpansionAlgorithm,
+        mockStatusReader,
     )
 
     override fun createEntry(scenario: TestScenario, size: Int) = IETFEntry(
