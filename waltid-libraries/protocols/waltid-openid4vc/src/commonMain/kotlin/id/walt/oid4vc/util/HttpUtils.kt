41603cf7c5487d24ab6922e631aae81e8c892039
Harald Okorn <harald@walt.id>
2026-02-18 12:33:57 +0100

    reconfigure openid4vc http client to have a small connectTimeout in order to reduce maxEndpointIdleTime

--- waltid-libraries/protocols/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/util/HttpUtils.kt
+++ waltid-libraries/protocols/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/util/HttpUtils.kt
@@ -1,10 +1,19 @@
 package id.walt.oid4vc.util
 
 import io.ktor.client.*
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 
 val http = HttpClient {
+
+    install( HttpTimeout) {
+        // For CI/CD a low maxEndpointIdleTime seems to be needed. For the CIO client the time is calculated:
+        // private val maxEndpointIdleTime: Long = 2 * config.endpoint.connectTimeout
+
+        // default seems to be 5s
+        connectTimeoutMillis = 500
+    }
     install(ContentNegotiation) {
         json()
     }
