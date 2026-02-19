dba85b0a2e0e7a06cfd3403b7ba3dd6c050fc2bc
Harald Okorn <harald@walt.id>
2026-02-19 09:17:37 +0100

    clean up enterprise stack feature configuration

--- waltid-libraries/protocols/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/util/HttpUtils.kt
+++ waltid-libraries/protocols/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/util/HttpUtils.kt
@@ -1,6 +1,7 @@
 package id.walt.oid4vc.util
 
 import io.ktor.client.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 
@@ -11,6 +12,13 @@ val http = HttpClient {
     // which can lead to hanging tests. OkHttp is more robust in these environments.
     install(ContentNegotiation) {
         json()
+    }
+
+    install(HttpTimeout) {
+        // For CI/CD a low maxEndpointIdleTime seems to be needed.
+
+        // default seems to be 5s
+        connectTimeoutMillis = 500
     }
     /*install(ContentEncoding) {
         deflate(1.0F)
