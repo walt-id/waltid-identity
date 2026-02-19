d477c07331219620b02bcdb4c0f9738ddc9be07e
Harald Okorn <harald@walt.id>
2026-02-19 11:42:03 +0100

    fixed stopwatch configuration

--- waltid-libraries/protocols/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/util/HttpUtils.kt
+++ waltid-libraries/protocols/waltid-openid4vc/src/commonMain/kotlin/id/walt/oid4vc/util/HttpUtils.kt
@@ -1,10 +1,17 @@
 package id.walt.oid4vc.util
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 
 val http = HttpClient {
+
+    val log = KotlinLogging.logger("id.walt.oid4vc.http")
+
+    engine {
+        log.info {"Initializing oid4vc HTTP client with engine config: ${this::class.simpleName}" }
+    }
 
     // For CI/CD OkHttp client should be used. CIO client seems to have some issues with timeouts when connecting to
     // localhost in CI/CD pipelines,
