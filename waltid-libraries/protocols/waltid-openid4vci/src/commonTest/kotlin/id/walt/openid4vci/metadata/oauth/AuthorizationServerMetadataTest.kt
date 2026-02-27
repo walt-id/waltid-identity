6bf6c02164e3086d30b8420395557cc1941b8600
chsavvaidis <ch.savvaidis@gmail.com>
2026-02-27 15:10:48 +0100

    test: update authorization server metadata to include updated defaults

--- waltid-libraries/protocols/waltid-openid4vci/src/commonTest/kotlin/id/walt/openid4vci/metadata/oauth/AuthorizationServerMetadataTest.kt
+++ waltid-libraries/protocols/waltid-openid4vci/src/commonTest/kotlin/id/walt/openid4vci/metadata/oauth/AuthorizationServerMetadataTest.kt
@@ -75,11 +75,86 @@ class AuthorizationServerMetadataTest {
         assertEquals("https://issuer.example/jwks", metadata.jwksUri)
         assertEquals(setOf(ResponseType.CODE.value), metadata.responseTypesSupported)
         assertEquals(setOf(ResponseMode.QUERY.value, ResponseMode.FRAGMENT.value), metadata.responseModesSupported)
+        assertEquals(setOf("attest_jwt_client_auth"), metadata.tokenEndpointAuthMethodsSupported)
+        assertEquals(setOf("ES256"), metadata.clientAttestationSigningAlgValuesSupported)
+        assertEquals(setOf("ES256"), metadata.clientAttestationPopSigningAlgValuesSupported)
+        assertEquals(setOf("ES256"), metadata.dpopSigningAlgValuesSupported)
         assertEquals(null, metadata.pushedAuthorizationRequestEndpoint)
         assertEquals(
             setOf(GrantType.AuthorizationCode.value, GrantType.PreAuthorizedCode.value),
             metadata.grantTypesSupported,
         )
+    }
+
+    @Test
+    fun `fromBaseUrl applies provided values correctly`() {
+        val metadata = AuthorizationServerMetadata.fromBaseUrl(
+            baseUrl = "https://issuer.example/",
+            authorizationEndpointPath = "/oauth2/authorize",
+            tokenEndpointPath = "/oauth2/token",
+            jwksUriPath = "/oauth2/jwks",
+            responseTypesSupported = setOf(ResponseType.CODE.value),
+            responseModesSupported = setOf(ResponseMode.QUERY.value),
+            grantTypesSupported = setOf(GrantType.AuthorizationCode.value),
+            tokenEndpointAuthMethodsSupported = setOf("private_key_jwt"),
+            tokenEndpointAuthSigningAlgValuesSupported = setOf("RS256"),
+            clientAttestationSigningAlgValuesSupported = setOf("ES384"),
+            clientAttestationPopSigningAlgValuesSupported = setOf("ES512"),
+            dpopSigningAlgValuesSupported = setOf("ES384"),
+            pushedAuthorizationRequestEndpointPath = "/oauth2/par",
+            requirePushedAuthorizationRequests = true,
+        )
+
+        assertEquals("https://issuer.example", metadata.issuer)
+        assertEquals("https://issuer.example/oauth2/authorize", metadata.authorizationEndpoint)
+        assertEquals("https://issuer.example/oauth2/token", metadata.tokenEndpoint)
+        assertEquals("https://issuer.example/oauth2/jwks", metadata.jwksUri)
+        assertEquals(setOf(ResponseType.CODE.value), metadata.responseTypesSupported)
+        assertEquals(setOf(ResponseMode.QUERY.value), metadata.responseModesSupported)
+        assertEquals(setOf(GrantType.AuthorizationCode.value), metadata.grantTypesSupported)
+        assertEquals(setOf("private_key_jwt"), metadata.tokenEndpointAuthMethodsSupported)
+        assertEquals(setOf("RS256"), metadata.tokenEndpointAuthSigningAlgValuesSupported)
+        assertEquals(setOf("ES384"), metadata.clientAttestationSigningAlgValuesSupported)
+        assertEquals(setOf("ES512"), metadata.clientAttestationPopSigningAlgValuesSupported)
+        assertEquals(setOf("ES384"), metadata.dpopSigningAlgValuesSupported)
+        assertEquals("https://issuer.example/oauth2/par", metadata.pushedAuthorizationRequestEndpoint)
+        assertEquals(true, metadata.requirePushedAuthorizationRequests)
+    }
+
+    @Test
+    fun `token endpoint JWT auth methods require signing algs`() {
+        assertFailsWith<IllegalArgumentException> {
+            AuthorizationServerMetadata(
+                issuer = "https://issuer.example",
+                authorizationEndpoint = "https://issuer.example/authorize",
+                tokenEndpoint = "https://issuer.example/token",
+                responseTypesSupported = setOf(ResponseType.CODE.value),
+                tokenEndpointAuthMethodsSupported = setOf("private_key_jwt"),
+            )
+        }
+        assertFailsWith<IllegalArgumentException> {
+            AuthorizationServerMetadata(
+                issuer = "https://issuer.example",
+                authorizationEndpoint = "https://issuer.example/authorize",
+                tokenEndpoint = "https://issuer.example/token",
+                responseTypesSupported = setOf(ResponseType.CODE.value),
+                tokenEndpointAuthMethodsSupported = setOf("client_secret_jwt"),
+            )
+        }
+    }
+
+    @Test
+    fun `token endpoint signing algs must not include none`() {
+        assertFailsWith<IllegalArgumentException> {
+            AuthorizationServerMetadata(
+                issuer = "https://issuer.example",
+                authorizationEndpoint = "https://issuer.example/authorize",
+                tokenEndpoint = "https://issuer.example/token",
+                responseTypesSupported = setOf(ResponseType.CODE.value),
+                tokenEndpointAuthMethodsSupported = setOf("private_key_jwt"),
+                tokenEndpointAuthSigningAlgValuesSupported = setOf("none"),
+            )
+        }
     }
 
     @Test
@@ -96,6 +171,9 @@ class AuthorizationServerMetadataTest {
             grantTypesSupported = setOf(GrantType.AuthorizationCode.value),
             tokenEndpointAuthMethodsSupported = setOf("client_secret_basic"),
             tokenEndpointAuthSigningAlgValuesSupported = setOf("RS256"),
+            clientAttestationSigningAlgValuesSupported = setOf("ES256"),
+            clientAttestationPopSigningAlgValuesSupported = setOf("ES256"),
+            dpopSigningAlgValuesSupported = setOf("ES256"),
             pushedAuthorizationRequestEndpoint = "https://issuer.example/par",
             requirePushedAuthorizationRequests = true,
             serviceDocumentation = "https://issuer.example/docs",
@@ -124,6 +202,9 @@ class AuthorizationServerMetadataTest {
         assertEquals(setOf(GrantType.AuthorizationCode.value), metadata.grantTypesSupported)
         assertEquals(setOf("client_secret_basic"), metadata.tokenEndpointAuthMethodsSupported)
         assertEquals(setOf("RS256"), metadata.tokenEndpointAuthSigningAlgValuesSupported)
+        assertEquals(setOf("ES256"), metadata.clientAttestationSigningAlgValuesSupported)
+        assertEquals(setOf("ES256"), metadata.clientAttestationPopSigningAlgValuesSupported)
+        assertEquals(setOf("ES256"), metadata.dpopSigningAlgValuesSupported)
         assertEquals("https://issuer.example/par", metadata.pushedAuthorizationRequestEndpoint)
         assertEquals(true, metadata.requirePushedAuthorizationRequests)
         assertEquals("https://issuer.example/docs", metadata.serviceDocumentation)
