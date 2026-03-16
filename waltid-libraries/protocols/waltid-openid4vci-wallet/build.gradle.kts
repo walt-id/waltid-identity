0452665512cd250c9a0140e50e96a1c3744dabe4
khaledbattiche <khaledbattiche@gmail.com>
2026-03-16 19:30:43 +0100

    feat: implement AuthorizationRequestBuilder for OpenID4VCI authorization requests

--- waltid-libraries/protocols/waltid-openid4vci-wallet/build.gradle.kts
+++ waltid-libraries/protocols/waltid-openid4vci-wallet/build.gradle.kts
@@ -1,18 +1,49 @@
 plugins {
-    kotlin("jvm") version "2.3.10"
+    id("waltid.multiplatform.library")
+    id("waltid.publish.maven")
+    id("waltid.publish.npm")
 }
 
-group = "id.waltid"
-version = "1.0.0-SNAPSHOT"
+group = "id.walt.protocols"
 
-repositories {
-    mavenCentral()
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            // Coroutines
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
+            // HTTP
+            implementation(identityLibs.bundles.waltid.ktor.client)
+
+            // Logging
+            implementation(identityLibs.oshai.kotlinlogging)
+
+            // Kotlinx
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1")
+
+            // JSON
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+
+
+            implementation(identityLibs.kotlincrypto.hash.sha2)
+            /*
+             * walt.id dependencies:
+             */
+            // OpenID4VCI shared protocol models
+            implementation(project(":waltid-libraries:protocols:waltid-openid4vci"))
+
+            // Crypto for key operations and proof signing
+            implementation(project(":waltid-libraries:crypto:waltid-crypto"))
+        }
+        commonTest.dependencies {
+            implementation(identityLibs.bundles.waltid.ktortesting)
+        }
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
+mavenPublishing {
+    pom {
+        name.set("walt.id Wallet SDK - OpenID4VCI version")
+        description.set("walt.id Kotlin/Java Wallet for OpenID4VCI")
+    }
 }
