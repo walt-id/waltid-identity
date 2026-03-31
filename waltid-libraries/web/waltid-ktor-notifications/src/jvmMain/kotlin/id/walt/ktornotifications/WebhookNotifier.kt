6ecb935269942c59b0464a83978d055060f9830d
waltkb <68587968+waltkb@users.noreply.github.com>
2026-03-31 05:56:35 +0200

    Better error message when connection to webhook URL is not possible

--- waltid-libraries/web/waltid-ktor-notifications/src/jvmMain/kotlin/id/walt/ktornotifications/WebhookNotifier.kt
+++ waltid-libraries/web/waltid-ktor-notifications/src/jvmMain/kotlin/id/walt/ktornotifications/WebhookNotifier.kt
@@ -3,6 +3,7 @@ package id.walt.ktornotifications
 import id.walt.ktornotifications.core.KtorSessionNotifications.VerificationSessionWebhookNotification
 import id.walt.ktornotifications.core.KtorSessionUpdate
 import io.ktor.client.*
+import io.ktor.client.network.sockets.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
@@ -17,16 +18,24 @@ object WebhookNotifier {
     }
 
     suspend fun notify(update: KtorSessionUpdate, config: VerificationSessionWebhookNotification) {
-        webhookClient.post(config.url) {
-            contentType(ContentType.Application.Json)
-            setBody(update)
+        runCatching {
+            webhookClient.post(config.url) {
+                contentType(ContentType.Application.Json)
+                setBody(update)
 
-            if (config.basicAuthUser != null && config.basicAuthPass != null) {
-                basicAuth(config.basicAuthUser!!, config.basicAuthPass!!)
+                if (config.basicAuthUser != null && config.basicAuthPass != null) {
+                    basicAuth(config.basicAuthUser!!, config.basicAuthPass!!)
+                }
+
+                if (config.bearerToken != null) {
+                    bearerAuth(config.bearerToken!!)
+                }
             }
-
-            if (config.bearerToken != null) {
-                bearerAuth(config.bearerToken!!)
+        }.getOrElse { ex ->
+            if (ex is ConnectTimeoutException) {
+                throw IllegalArgumentException("Could not connect to configured webhook URL for session update: ${config.url.hostWithPortIfSpecified}", ex)
+            } else {
+                throw IllegalArgumentException("Could not notify configured webhook for session update: ${ex.message}", ex)
             }
         }
     }
