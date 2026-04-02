ff56b00a85e9912e85772e0e5ec471fdf0a3dcad
waltkb <68587968+waltkb@users.noreply.github.com>
2026-04-02 19:32:40 +0200

    Add unified engine interface

---
+++ waltid-libraries/web/waltid-web-data-fetching/src/commonMain/kotlin/id/walt/webdatafetching/config/HttpEngines.kt
@@ -0,0 +1,21 @@
+package id.walt.webdatafetching.config
+
+enum class HttpEngines {
+    /** Official Coroutine-IO engine */
+    CIO,
+
+    /** Java HTTP Client (HTTP2 capable), requires Java 11 */
+    Java,
+
+    /** Supports HTTP/1.1 and HTTP/2 */
+    Apache5,
+
+    /** OkHttp (optimized for Android, also works on JVM) */
+    OkHttp,
+
+    /** Android on Android, Darwin on macOS/iOS, WinHttp on Windows, Curl on Linux, Js/fetch on JavaScript */
+    Native
+
+    // Supports ONLY HTTP2, requires Java 11
+    // Jetty
+}
