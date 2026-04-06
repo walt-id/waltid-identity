96f7d5e9acf0a2c99cee85738097df76410da0fc
waltkb <68587968+waltkb@users.noreply.github.com>
2026-04-07 01:00:41 +0200

    Show config examples for URLs for digital credentials demo

--- waltid-applications/waltid-digital-credentials/src/main.ts
+++ waltid-applications/waltid-digital-credentials/src/main.ts
@@ -3,6 +3,7 @@ import './style.css';
 const VERIFIER_PRESETS = {
   'open-source': {
     verifierBase: 'https://verifier2.portal.test.waltid.cloud'
+    // verifierBase: '/'
   },
   enterprise: {
     verifierBase: '/verifier-api/v1/waltid.tenant1.verifier2/verifier2-service-api'
