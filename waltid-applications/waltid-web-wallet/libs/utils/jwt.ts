8bf796e787eec3d9d1bb0e8233c1a1ed9f8a3852
Peter Szij <szijpeter@gmail.com>
2026-04-09 17:56:45 +0200

    refactor: reuse jwt parsing for request objects

--- waltid-applications/waltid-web-wallet/libs/utils/jwt.ts
+++ waltid-applications/waltid-web-wallet/libs/utils/jwt.ts
@@ -1,15 +1,25 @@
 function parseJwt(token: string) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    const base64Url = token.split(".")[1];
+    if (!base64Url) {
+        throw new Error("JWT is missing a payload segment.");
+    }
+
+    const base64 = base64UrlToBase64(base64Url);
+    const jsonPayload = decodeURIComponent(window.atob(base64).split("").map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(""));
 
     try {
         return JSON.parse(jsonPayload);
     } catch (error) {
         return jsonPayload;
     }
+}
+
+function base64UrlToBase64(value: string) {
+    const base64 = value.replaceAll("-", "+").replaceAll("_", "/");
+    const padding = (4 - (base64.length % 4)) % 4;
+    return base64 + "=".repeat(padding);
 }
 
 export { parseJwt };
