efa82e13b7de1eca832a91bdb1a1bf305b3f4ea1
waltkb <68587968+waltkb@users.noreply.github.com>
2026-04-13 12:50:20 +0200

    Allow setting global default web data fetcher configuration

--- waltid-libraries/web/waltid-web-data-fetching/src/commonMain/kotlin/id/walt/webdatafetching/WebDataFetcherManager.kt
+++ waltid-libraries/web/waltid-web-data-fetching/src/commonMain/kotlin/id/walt/webdatafetching/WebDataFetcherManager.kt
@@ -1,8 +1,13 @@
 package id.walt.webdatafetching
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+
 object WebDataFetcherManager {
 
+    private val log = KotlinLogging.logger { }
+
     private val fetcherConfigurations = HashMap<String, WebDataFetchingConfiguration>()
+    var defaultConfiguration: WebDataFetchingConfiguration = WebDataFetchingConfiguration.Default
 
     fun getConfigurationForId(id: String, defaultConfiguration: WebDataFetchingConfiguration?): WebDataFetchingConfiguration {
         return fetcherConfigurations[id]
@@ -11,6 +16,7 @@ object WebDataFetcherManager {
     }
 
     fun applyConfigurations(configs: Map<String, WebDataFetchingConfiguration>) {
+        log.trace { "Applying configurations for ${configs.keys}" }
         fetcherConfigurations.putAll(configs)
     }
 
