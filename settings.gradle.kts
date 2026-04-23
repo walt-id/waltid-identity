8fcfe7a452b20b36a9ebf99d044953828b2e4da3
Philipp Potisk <phil@walt.id>
2026-04-23 17:56:24 +0200

    removed waltid-trust-registry-service

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -89,8 +89,7 @@ val modules = listOfNotNull(
     "$services:waltid-issuer-api",
     "$services:waltid-verifier-api",
     "$services:waltid-verifier-api2",
-    "$services:waltid-wallet-api",
-    "$services:waltid-trust-registry-service",
+    "$services:waltid-wallet-api"
 
     // Service tests
     "$services:waltid-e2e-tests",
--- waltid-services/waltid-trust-registry-service/src/main/kotlin/id/walt/trust/service/plugins/Routing.kt
+++
@@ -1,8 +0,0 @@
-package id.walt.trust.service.plugins
-
-import io.ktor.server.application.*
-import io.ktor.server.plugins.autohead.*
-
-fun Application.configureRouting() {
-    install(AutoHeadResponse)
-}
