ac3890f24180d54d3ec7dd9750f9f0ee9d6fda57
chsavvaidis <ch.savvaidis@gmail.com>
2026-05-21 15:13:14 +0200

    fix: update credential format

--- waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/events/filter/IssuanceEventFilter.kt
+++ waltid-services/waltid-service-commons/src/main/kotlin/id/walt/commons/events/filter/IssuanceEventFilter.kt
@@ -1,12 +1,11 @@
 package id.walt.commons.events.filter
 
-import id.walt.oid4vc.data.CredentialFormat
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class IssuanceEventFilter(
     val credentialConfigurationId: Set<String>? = null,
-    val format: Set<CredentialFormat>? = null,
+    val format: Set<String>? = null,
     val sessionId: String? = null,
     val proofType: Set<String>? = null,
     val holder: Set<String>? = null,
