c2f2b7c567e69772379b13fdd06ce6ddf4520561
waltkb <68587968+waltkb@users.noreply.github.com>
2026-05-26 00:50:04 +0200

    COSE Signatures for W3C

--- waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/formats/w3c/AbstractW3C.kt
+++ waltid-libraries/credentials/waltid-digital-credentials/src/commonMain/kotlin/id/walt/credentials/formats/w3c/AbstractW3C.kt
@@ -1,8 +1,11 @@
 package id.walt.credentials.formats
 
+import id.walt.cose.CoseSign1
+import id.walt.cose.toCoseVerifier
 import id.walt.credentials.signatures.*
 import id.walt.credentials.signatures.sdjwt.SelectivelyDisclosableVerifiableCredential
 import id.walt.crypto.keys.Key
+import id.walt.crypto.utils.Base64Utils.decodeFromBase64Url
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -14,6 +17,7 @@ sealed class AbstractW3C(
         when (signature) {
             null -> null
             is JwtBasedSignature -> (signature as JwtBasedSignature).getJwtBasedIssuer(credentialData)
+            is CoseCredentialSignature -> (signature as CoseCredentialSignature).signerKey.key
             else -> throw NotImplementedError("Not yet implemented: Retrieve issuer key from SdJwtCredential with ${signature!!::class.simpleName} signature")
         }
 
@@ -28,7 +32,17 @@ sealed class AbstractW3C(
                 publicKey.verifyJws(signed!!)
             }
 
-            is CoseCredentialSignature -> throw UnsupportedOperationException("COSE signature verification for W3C credentials is not yet implemented")
+            is CoseCredentialSignature -> {
+                // W3C VCDM secured with COSE_Sign1 (vc-jose-cose §securing-vcs-with-cose).
+                // The `signed` field holds the base64url-encoded COSE_Sign1 bytes.
+                require(signed != null) { "Cannot verify unsigned COSE credential" }
+                val coseBytes = signed!!.decodeFromBase64Url()
+                val coseSign1 = CoseSign1.fromTagged(coseBytes)
+                val valid = coseSign1.verify(publicKey.toCoseVerifier())
+                require(valid) { "COSE_Sign1 signature on W3C credential failed to verify." }
+                Result.success(credentialData)
+            }
+
             is DataIntegrityProofCredentialSignature -> throw UnsupportedOperationException("Data Integrity Proof (LDP) signature verification for W3C credentials is not yet implemented")
             null -> throw IllegalArgumentException("Credential contains no signature, cannot verify")
         }
