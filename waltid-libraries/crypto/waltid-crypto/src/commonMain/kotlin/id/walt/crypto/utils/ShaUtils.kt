8a51f57b723a67730ad50d5ba0a8bf496e40e471
waltkb <68587968+waltkb@users.noreply.github.com>
2026-05-27 15:14:15 +0200

    add helper to sha256 base64url an input bytearray

--- waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/utils/ShaUtils.kt
+++ waltid-libraries/crypto/waltid-crypto/src/commonMain/kotlin/id/walt/crypto/utils/ShaUtils.kt
@@ -2,14 +2,19 @@ package id.walt.crypto.utils
 
 import org.kotlincrypto.hash.sha2.SHA256
 import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
+@OptIn(ExperimentalEncodingApi::class)
+private val base64UrlNoPad = Base64.UrlSafe.withPadding(Base64.PaddingOption.ABSENT_OPTIONAL)
 
 object ShaUtils {
 
-    // Helper to calculate SHA-256 and then Base64URL encode
+    // Helper to calculate SHA-256 and then Base64URL encode from a String
     fun calculateSha256Base64Url(input: String): String =
-        Base64.UrlSafe.withPadding(Base64.PaddingOption.ABSENT_OPTIONAL)
-            .encode(
-                SHA256().digest(input.encodeToByteArray())
-            )
+        base64UrlNoPad.encode(SHA256().digest(input.encodeToByteArray()))
+
+    // Helper to calculate SHA-256 and then Base64URL encode from a ByteArray
+    fun sha256Base64Url(input: ByteArray): String =
+        base64UrlNoPad.encode(SHA256().digest(input))
 
 }
