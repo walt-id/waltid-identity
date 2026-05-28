ddf5ace68c14a99baf92b62107009300fd025b6a
Peter Szij <szijpeter@gmail.com>
2026-05-28 10:52:53 +0200

    fix: add X5CChainValidatorHelper actual for iOS native targets
    The iosMain intermediate source set isn't auto-wired by the convention
    plugin in this module, so target-specific actuals are needed for
    iosArm64 and iosSimulatorArm64 to compile the shared framework.

---
+++ waltid-libraries/credentials/waltid-credential-key-resolver/src/iosArm64Main/kotlin/id/walt/credentials/trustedauthorities/X5CChainValidatorHelper.kt
@@ -0,0 +1,7 @@
+package id.walt.credentials.trustedauthorities
+
+actual object X5CChainValidatorHelper {
+    actual fun verifyChain(certChainDer: List<ByteArray>) {
+        throw UnsupportedOperationException("X5CChainValidatorHelper is not supported on iOS")
+    }
+}
