6d6df55aa209607426adaf3010823212d402b104
chsavvaidis <ch.savvaidis@gmail.com>
2026-06-18 13:11:15 +0200

    fix: naming

--- waltid-libraries/protocols/waltid-openid4vci/src/commonTest/kotlin/id/walt/openid4vci/ProviderCombinedFlowTest.kt
+++ waltid-libraries/protocols/waltid-openid4vci/src/commonTest/kotlin/id/walt/openid4vci/ProviderCombinedFlowTest.kt
@@ -31,7 +31,7 @@ class ProviderCombinedFlowTest {
         val provider = buildOAuth2Provider(config)
 
         // Authorization code flow
-        val AuthorizationRequestResult = provider.createAuthorizationRequest(
+        val authorizationRequestResult = provider.createAuthorizationRequest(
             mapOf(
                 "response_type" to listOf(ResponseType.CODE.value),
                 "client_id" to listOf("demo-client"),
@@ -40,8 +40,8 @@ class ProviderCombinedFlowTest {
                 "state" to listOf("abc"),
             ),
         )
-        assertTrue(AuthorizationRequestResult.isSuccess())
-        val authorizeRequest = (AuthorizationRequestResult as AuthorizationRequestResult.Success).request.withIssuer(issClaim)
+        assertTrue(authorizationRequestResult.isSuccess())
+        val authorizeRequest = (authorizationRequestResult as AuthorizationRequestResult.Success).request.withIssuer(issClaim)
 
         val session = DefaultSession(subject = "demo-subject")
         val authorizeResponse = provider.createAuthorizationResponse(authorizeRequest, session)
