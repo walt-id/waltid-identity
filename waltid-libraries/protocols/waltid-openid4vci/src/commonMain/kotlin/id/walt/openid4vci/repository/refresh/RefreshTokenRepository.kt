4fc37b8bec6eca8bc6132722db6fc4c2072ff461
chsavvaidis <ch.savvaidis@gmail.com>
2026-06-19 18:44:50 +0200

    refactor(openid4vci): align refresh repository structure

--- waltid-libraries/protocols/waltid-openid4vci/src/commonMain/kotlin/id/walt/openid4vci/repository/refresh/RefreshTokenRepository.kt
+++ waltid-libraries/protocols/waltid-openid4vci/src/commonMain/kotlin/id/walt/openid4vci/repository/refresh/RefreshTokenRepository.kt
@@ -1,9 +1,6 @@
 package id.walt.openid4vci.repository.refresh
 
-import id.walt.openid4vci.Session
 import id.walt.openid4vci.repository.authorization.DuplicateCodeException
-import id.walt.openid4vci.requests.token.AccessTokenRequest
-import kotlin.time.Instant
 
 interface RefreshTokenRepository {
     @Throws(DuplicateCodeException::class)
@@ -19,27 +16,3 @@ interface RefreshTokenRepository {
     @Throws(DuplicateCodeException::class)
     suspend fun rotate(tokenSignature: String, replacement: RefreshTokenRecord): RefreshTokenRecord?
 }
-
-interface RefreshTokenRecord {
-    val tokenSignature: String
-    val active: Boolean
-    val accessTokenRequest: AccessTokenRequest
-    val accessTokenSignature: String
-    val clientId: String?
-    val grantedScopes: Set<String>
-    val grantedAudience: Set<String>
-    val session: Session
-    val expiresAt: Instant
-}
-
-data class DefaultRefreshTokenRecord(
-    override val tokenSignature: String,
-    override val active: Boolean = true,
-    override val accessTokenRequest: AccessTokenRequest,
-    override val accessTokenSignature: String,
-    override val clientId: String?,
-    override val grantedScopes: Set<String>,
-    override val grantedAudience: Set<String>,
-    override val session: Session,
-    override val expiresAt: Instant,
-) : RefreshTokenRecord
