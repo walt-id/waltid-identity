e9189930eeab219bdfbd124eb23277bb4a2682ed
Peter Szij <szijpeter@gmail.com>
2026-06-23 15:11:45 +0200

    chore: nest android configuration inside kotlin block in waltid.android.app
    - Move the `android` configuration block into the `kotlin` extension block.
    - Consolidate Kotlin and Android setup within the `waltid.android.app.gradle.kts` convention plugin.

--- build-logic/src/main/kotlin/waltid.android.app.gradle.kts
+++ build-logic/src/main/kotlin/waltid.android.app.gradle.kts
@@ -4,24 +4,23 @@ plugins {
     id("com.android.application")
     kotlin("plugin.compose")
 }
+kotlin {
+    android {
+        namespace = project.group.toString()
 
-android {
-    namespace = project.group.toString()
-
-    compileSdk = BuildConstants.COMPILE_SDK
-    defaultConfig { minSdk = BuildConstants.MIN_SDK }
-    compileOptions {
-        sourceCompatibility = JavaVersion.toVersion(project.javaLibraryVersion)
-        targetCompatibility = JavaVersion.toVersion(project.javaLibraryVersion)
-    }
-    packaging {
-        resources {
-            excludes += BuildConstants.META_INF_EXCLUDES
+        compileSdk = BuildConstants.COMPILE_SDK
+        defaultConfig { minSdk = BuildConstants.MIN_SDK }
+        compileOptions {
+            sourceCompatibility = JavaVersion.toVersion(project.javaLibraryVersion)
+            targetCompatibility = JavaVersion.toVersion(project.javaLibraryVersion)
+        }
+        packaging {
+            resources {
+                excludes += BuildConstants.META_INF_EXCLUDES
+            }
         }
     }
-}
 
-kotlin {
     compilerOptions {
         jvmTarget.set(JvmTarget.fromTarget(project.javaLibraryVersion.toString()))
     }
