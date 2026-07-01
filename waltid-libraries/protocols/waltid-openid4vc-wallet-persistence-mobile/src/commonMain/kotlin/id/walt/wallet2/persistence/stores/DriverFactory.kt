4858d1b3a31caff27ce403ddf2fb6052aa08179a
Peter Szij <szijpeter@gmail.com>
2026-07-01 12:55:22 +0200

    docs(mobile): add KDoc for wallet SDK

--- waltid-libraries/protocols/waltid-openid4vc-wallet-persistence-mobile/src/commonMain/kotlin/id/walt/wallet2/persistence/stores/DriverFactory.kt
+++ waltid-libraries/protocols/waltid-openid4vc-wallet-persistence-mobile/src/commonMain/kotlin/id/walt/wallet2/persistence/stores/DriverFactory.kt
@@ -2,6 +2,14 @@ package id.walt.wallet2.persistence.stores
 
 import app.cash.sqldelight.db.SqlDriver
 
+/**
+ * Platform factory for SQLDelight drivers used by the mobile wallet database.
+ */
 expect class DriverFactory {
+    /**
+     * Creates a SQLDelight driver for the named wallet database.
+     *
+     * @param databaseName Base database name. Platform implementations add the expected file extension.
+     */
     fun createDriver(databaseName: String): SqlDriver
 }
