af8e6c2b8596815098798fd9d627afaa36239aa2
chsavvaidis <ch.savvaidis@gmail.com>
2026-07-04 20:43:13 +0200

    feat(wallet-api2): add attestation service config

--- waltid-services/waltid-wallet-api2/src/main/kotlin/id/walt/wallet2/OSSWallet2ServiceConfig.kt
+++ waltid-services/waltid-wallet-api2/src/main/kotlin/id/walt/wallet2/OSSWallet2ServiceConfig.kt
@@ -11,11 +11,20 @@ import kotlinx.serialization.Serializable
  * ```hocon
  * wallet-service {
  *     publicBaseUrl = "http://localhost:4000"
+ *     attestationConfig {
+ *         attesterUrl = "https://wallet-provider.example.com/wallet-instance-attestation/jwk"
+ *     }
  * }
  * ```
  */
 @Serializable
 data class OSSWallet2ServiceConfig(
     /** Public-facing base URL for this wallet service. */
-    val publicBaseUrl: Url
+    val publicBaseUrl: Url,
+    val attestationConfig: WalletAttestationConfig? = null,
 ) : WaltConfig()
+
+@Serializable
+data class WalletAttestationConfig(
+    val attesterUrl: String,
+)
