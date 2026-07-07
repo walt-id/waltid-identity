0c6c6b092302ff6f39b57d5089f3ef042bbd33bb
chsavvaidis <ch.savvaidis@gmail.com>
2026-07-07 18:11:30 +0200

    test(issuer-api2): update client attestation test support

--- waltid-services/waltid-issuer-api2/src/test/kotlin/id/walt/issuer2/testsupport/Issuer2ClientAttestationTestSupport.kt
+++ waltid-services/waltid-issuer-api2/src/test/kotlin/id/walt/issuer2/testsupport/Issuer2ClientAttestationTestSupport.kt
@@ -3,8 +3,8 @@ package id.walt.issuer2.testsupport
 import id.walt.crypto.keys.Key
 import id.walt.crypto.keys.KeyType
 import id.walt.crypto.keys.jwk.JWKKey
-import id.walt.issuer2.config.ClientAuthenticationConfig
-import id.walt.issuer2.config.ClientAuthenticationMethod
+import id.walt.openid4vci.clientauth.ClientAuthenticationConfig
+import id.walt.openid4vci.clientauth.ClientAuthenticationMethodConfig
 import id.walt.openid4vci.clientauth.attestation.ClientAttestationJwtTypes
 import id.walt.openid4vci.clientauth.attestation.verifier.ClientAttestationVerificationMethod
 import id.walt.openid4vci.clientauth.attestation.verifier.ClientAttestationVerifierConfig
@@ -30,7 +30,7 @@ suspend fun createIssuer2ClientAttestationTestMaterial(): Issuer2ClientAttestati
     return Issuer2ClientAttestationTestMaterial(
         clientAuthenticationConfig = ClientAuthenticationConfig(
             supportedMethods = listOf(
-                ClientAuthenticationMethod.ClientAttestation(
+                ClientAuthenticationMethodConfig.ClientAttestation(
                     config = ClientAttestationVerifierConfig(
                         verificationMethod = ClientAttestationVerificationMethod.StaticJwk(
                             jwk = Json.parseToJsonElement(attesterKey.getPublicKey().exportJWK()).jsonObject,
