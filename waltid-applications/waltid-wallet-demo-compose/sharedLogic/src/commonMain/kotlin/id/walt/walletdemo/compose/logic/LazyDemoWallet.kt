bb21c45a3883ba83f213640ddcad0780654a312f
khaledbattiche <khaledbattiche@gmail.com>
2026-07-13 12:50:19 +0800

    Update LazyDemoWallet with credential details and offer resolution support

--- waltid-applications/waltid-wallet-demo-compose/sharedLogic/src/commonMain/kotlin/id/walt/walletdemo/compose/logic/LazyDemoWallet.kt
+++ waltid-applications/waltid-wallet-demo-compose/sharedLogic/src/commonMain/kotlin/id/walt/walletdemo/compose/logic/LazyDemoWallet.kt
@@ -20,8 +20,14 @@ internal class LazyDemoWallet(
     override suspend fun listCredentials(): List<WalletDemoCredential> =
         wallet().listCredentials()
 
-    override suspend fun receive(offerUrl: String): List<String> =
-        wallet().receive(offerUrl)
+    override suspend fun credentialDetails(id: String): WalletDemoCredentialDetails? =
+        wallet().credentialDetails(id)
+
+    override suspend fun resolveOffer(offerUrl: String): DemoOfferResolution =
+        wallet().resolveOffer(offerUrl)
+
+    override suspend fun receive(offerUrl: String, txCode: String?): List<String> =
+        wallet().receive(offerUrl, txCode)
 
     override suspend fun present(requestUrl: String, did: String?): WalletDemoOperationResult =
         wallet().present(requestUrl, did)
