fb1f9c4ab6013a2a193b6dcf4bccab21a28b7d8b
Harald Okorn <harald@walt.id>
2026-08-13 10:37:06 +0200

    fix: x509 lib ios build

--- waltid-libraries/crypto/waltid-x509/src/iosMain/kotlin/id/walt/certificate/x509/X509CertificateUtil.ios.kt
+++ waltid-libraries/crypto/waltid-x509/src/iosMain/kotlin/id/walt/certificate/x509/X509CertificateUtil.ios.kt
@@ -3,9 +3,9 @@ package id.walt.certificate.x509
 import id.walt.certificate.x509.signum.*
 import id.walt.certificate.x509.truststore.InMemoryTrustStore
 import id.walt.certificate.x509.validation.X509CertificateChainValidator
+import id.walt.certificate.x509.validation.validator.X509CertificateBasicConstraintsValidator
 import id.walt.certificate.x509.validation.validator.X509CertificateSignatureValidator
 import id.walt.certificate.x509.validation.validator.X509CertificateValidityValidator
-import id.walt.certificate.x509.signum.SignumSupremeSignatureValidationImplementation
 import id.walt.crypto2.CryptoRuntime
 import id.walt.crypto2.providers.cryptography.defaultSoftwareKeyProviders
 
