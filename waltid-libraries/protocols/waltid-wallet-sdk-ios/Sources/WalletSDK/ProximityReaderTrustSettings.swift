c91959fbf6cf815364a26a83b4927b21b9360e60
Peter Szij <szijpeter@gmail.com>
2026-09-01 12:34:02 +0200

    fix(wallet): preserve proximity transport with reader trust

--- waltid-libraries/protocols/waltid-wallet-sdk-ios/Sources/WalletSDK/ProximityReaderTrustSettings.swift
+++ waltid-libraries/protocols/waltid-wallet-sdk-ios/Sources/WalletSDK/ProximityReaderTrustSettings.swift
@@ -120,10 +120,8 @@ public struct ProximityReaderTrustSettings: Sendable, Equatable {
 
         return try ProximityConfiguration(
             profile: configuration.profile,
-            bleRoles: configuration.bleRoles,
-            bearerPolicy: configuration.bearerPolicy,
-            engagementMethods: configuration.engagementMethods,
-            retrievalMethods: configuration.retrievalMethods,
+            engagement: configuration.engagement,
+            retrieval: configuration.retrieval,
             readerPolicy: readerPolicy.presentationPolicy,
             deviceAuthenticationPolicy: configuration.deviceAuthenticationPolicy,
             readerTrustEvaluator: evaluator,
