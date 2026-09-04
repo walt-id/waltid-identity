c88105f0c1aefe1095e31c31c676753b4615503e
chsavvaidis <ch.savvaidis@gmail.com>
2026-09-04 11:40:16 +0300

    test(batch-issuance): refine IssuanceNotificationServiceTest.kt part 69

--- waltid-services/waltid-issuer-api2/src/test/kotlin/id/walt/issuer2/notifications/IssuanceNotificationServiceTest.kt
+++ waltid-services/waltid-issuer-api2/src/test/kotlin/id/walt/issuer2/notifications/IssuanceNotificationServiceTest.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.withTimeout
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import kotlinx.serialization.json.put
