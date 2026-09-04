ced97028881e5e08a720c621fa2d4c801d539b48
chsavvaidis <ch.savvaidis@gmail.com>
2026-09-04 11:40:17 +0300

    test(batch-issuance): refine Wallet2MoreUseCasesTest.kt part 80

--- waltid-services/waltid-wallet-api2/src/test/kotlin/id/walt/wallet2/Wallet2MoreUseCasesTest.kt
+++ waltid-services/waltid-wallet-api2/src/test/kotlin/id/walt/wallet2/Wallet2MoreUseCasesTest.kt
@@ -228,7 +228,7 @@ class Wallet2MoreUseCasesTest {
                     val credResp = provider.createCredentialResponse(
                         request = request,
                         configuration = configuration, issuerKey = issuerKey,
-                        issuerId = issuerBase, credentialData = credentialData,
+                        issuerId = issuerBase, issuanceInputData = testIssuanceInputData(credentialData),
                         selectiveDisclosure = selectiveDisclosure,
                         proofValidationContext = proofSupport.validationContext(request)
                     )
