ca2c99f41f6033c0298db7967cc6b9711918cc13
chsavvaidis <ch.savvaidis@gmail.com>
2026-09-08 21:13:54 +0300

    fix: issuer2 notifications tests

--- waltid-services/waltid-issuer-api2/src/test/kotlin/id/walt/issuer2/notifications/IssuanceNotificationRouteTest.kt
+++ waltid-services/waltid-issuer-api2/src/test/kotlin/id/walt/issuer2/notifications/IssuanceNotificationRouteTest.kt
@@ -129,7 +129,6 @@ class IssuanceNotificationRouteTest {
 
             val status = receivedUpdates.first { it.event == IssuanceSessionEvent.ISSUANCE_STATUS_CHANGED.value }
             assertEquals("SUCCESSFUL", status.session["status"]?.jsonPrimitive?.contentOrNull)
-            assertEquals("false", status.session["isClosed"]?.jsonPrimitive?.contentOrNull)
         } finally {
             notificationServer.stopServer()
         }
