9546f8da87452a20a0d3dcc6ce6d33cccbf4a07c
Peter Szij <szijpeter@gmail.com>
2026-09-15 14:29:30 +0200

    fix(build): validate crypto ABI with optional iOS targets

--- waltid-libraries/crypto/waltid-crypto2/build.gradle.kts
+++ waltid-libraries/crypto/waltid-crypto2/build.gradle.kts
@@ -42,6 +42,7 @@ val chromeExecutable = providers.environmentVariable("CHROME_BIN").orNull
 
 plugins {
     id("waltid.full.library")
+    id("waltid.optional-ios-abi")
     id("waltid.publish.maven")
 }
 
