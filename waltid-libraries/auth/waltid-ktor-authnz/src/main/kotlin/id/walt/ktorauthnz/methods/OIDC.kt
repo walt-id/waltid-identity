1f972b1e4b57836f51a82a4ddfa6bc46f7aa9e69
Philipp Potisk <phil@walt.id>
2026-09-17 10:58:52 +0200

    Sonar Cleanup

--- waltid-libraries/auth/waltid-ktor-authnz/src/main/kotlin/id/walt/ktorauthnz/methods/OIDC.kt
+++ waltid-libraries/auth/waltid-ktor-authnz/src/main/kotlin/id/walt/ktorauthnz/methods/OIDC.kt
@@ -617,6 +617,7 @@ object OIDC : AuthenticationMethod("oidc") {
         if (payload.containsKey("nonce")) {
             throw IllegalArgumentException("Logout Token must not contain a nonce.")
         }
+        // NOSONAR: this is the OIDC Back-Channel Logout spec-defined event identifier, not a network endpoint
         if (payload["events"]?.jsonObject?.containsKey("http://schemas.openid.net/event/backchannel-logout") != true) {
             throw IllegalArgumentException("Token is not a back-channel logout event token.")
         }
